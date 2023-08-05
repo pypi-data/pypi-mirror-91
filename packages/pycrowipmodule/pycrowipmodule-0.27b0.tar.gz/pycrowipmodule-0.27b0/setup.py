af480a0d054075b77cc871202bbcb911e9a04766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:41 +0000

    Add package pycrowipmodule/0.27b0/pycrowipmodule-0.27b0.tar.gz

---
+++ packages/pycrowipmodule/pycrowipmodule-0.27b0.tar.gz/pycrowipmodule-0.27b0/setup.py
@@ -0,0 +1,18 @@
+"""Crow/AAP Alarm Ip Module package."""
+from setuptools import setup, find_packages
+
+setup(
+    name='pycrowipmodule',
+    version='0.27b0',
+    description='Library for Crow/AAP Alarm Ip Module',
+    url='https://github.com/febalci/pycrowipmodule',
+    author='febalci',
+    author_email='febalci@yahoo.com',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['requests'],
+    keywords='Crow/AAP Alarm Ip Module for Home Assistant',
+    include_package_data=True,
+    zip_safe=False
+)
+
