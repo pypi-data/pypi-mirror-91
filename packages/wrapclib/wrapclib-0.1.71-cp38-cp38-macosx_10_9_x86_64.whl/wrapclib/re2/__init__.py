ea62cceae676c94f112d33cc34f3bc171d167307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:25 +0000

    Add package wrapclib/0.1.71/wrapclib-0.1.71-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/wrapclib/wrapclib-0.1.71-cp38-cp38-macosx_10_9_x86_64.whl/wrapclib/re2/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+@file
+@brief Shortcut to *re2*.
+"""
+
+from .re2 import compile  # pylint: disable=W0622
+from .re2 import findall, search, match, fullmatch, Set, UNANCHORED, ANCHOR_START, ANCHOR_BOTH
