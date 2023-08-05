4323ad8cbf67d61d6fa45343ec17b8edecb9369d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package skylearnn/0.0.1/skylearnn-0.0.1.tar.gz

---
+++ packages/skylearnn/skylearnn-0.0.1.tar.gz/skylearnn-0.0.1/skylearnn/__init__.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# Author: yunhao
+# Github: https://github.com/yunhao233
+# CreatDate: 2021/1/10 16:48
+# Description:
+from skylearnn.skylearn import *
+
+def sayhello():
+    return "hello world"
