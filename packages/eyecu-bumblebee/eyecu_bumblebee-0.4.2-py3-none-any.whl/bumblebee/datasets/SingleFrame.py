1ad201dcb34b67e70055006f9dc75026b1d8535f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:39 +0000

    Add package eyecu-bumblebee/0.4.2/eyecu_bumblebee-0.4.2-py3-none-any.whl

---
+++ packages/eyecu-bumblebee/eyecu_bumblebee-0.4.2-py3-none-any.whl/bumblebee/datasets/SingleFrame.py
@@ -0,0 +1,22 @@
+from typing import Union
+from ..bases.Source import Source
+from ..bases.IDataset import IDataset
+
+
+class SingleFrame(IDataset):
+
+    def __init__(self, data_source ):
+
+        self.src = data_source
+
+    def __iter__(self):
+        return self
+
+    def get_props(self):
+        return 1, *self.src.get_props()
+
+    def __len__(self):
+        return 1
+
+
+
