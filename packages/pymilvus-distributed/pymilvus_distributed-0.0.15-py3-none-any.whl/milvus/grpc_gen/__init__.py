b0384e8e546888befab69c032addd1fd32bf30d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:13 +0000

    Add package pymilvus-distributed/0.0.15/pymilvus_distributed-0.0.15-py3-none-any.whl

---
+++ packages/pymilvus-distributed/pymilvus_distributed-0.0.15-py3-none-any.whl/milvus/grpc_gen/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    'status_pb2', 'status_pb2_grpc',
+    'milvus_pb2', 'milvus_pb2_grpc',
+    'common_pb2', 'common_pb2_grpc',
+    'schema_pb2', 'schema_pb2_grpc',
+    'service_pb2', 'service_pb2_grpc',
+    'service_msg_pb2', 'service_msg_pb2_grpc',
+]
