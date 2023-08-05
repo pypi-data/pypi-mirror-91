94f556949aa486de1de9724e6c0a895e18ba0754
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:11:26 +0000

    Add package pushdit/0.0.7/pushdit-0.0.7.tar.gz

---
+++ packages/pushdit/pushdit-0.0.7.tar.gz/pushdit-0.0.7/pushdit/__init__.py
@@ -0,0 +1,1 @@
+from .pushit import *
---
+++ packages/pushdit/pushdit-0.0.7.tar.gz/pushdit-0.0.7/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r', encoding="utf=8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "pushdit",
+    version = "0.0.7",
+    author = "Antsthebul",
+    author_email = "anthony.allen.srt@gmail.com",
+    description = "pushd command like Linux",
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    url = "https://github.com/Antsthebul/pushdit",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent"
+    ],
+    python_require = '>=python3.6'
+)
