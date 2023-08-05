38f545e04a839e7832a3c0fb41f136e9a53f6c30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:15 +0000

    Add package w3helpers/0.0.6/w3helpers-0.0.6.tar.gz

---
+++ packages/w3helpers/w3helpers-0.0.6.tar.gz/w3helpers-0.0.6/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="w3helpers",  # Replace with your own username
+    version="0.0.6",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    python_requires='>=3.9',
+)
