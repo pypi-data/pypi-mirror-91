591b988192dd3ccee52aafde941f8e2e2650259c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:35 +0000

    Add package wiserHeatAPIv2/0.0.1/wiserHeatAPIv2-0.0.1.tar.gz

---
+++ packages/wiserHeatAPIv2/wiserHeatAPIv2-0.0.1.tar.gz/wiserHeatAPIv2-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'wiserHeatAPIv2',
+  packages = ['wiserHeatAPIv2'],
+  version = '0.0.1',
+  license='MIT',
+  description = 'A simple API for controlling the Drayton Wiser Heating system',
+  author = 'Mark Parker',
+  author_email = 'msparker@sky.com',
+  url = 'https://github.com/msp1974/wiserheatapiv2',
+  download_url = 'https://github.com/msp1974/wiserheatapiv2/archive/v0.0.1.tar.gz',    # I explain this later on
+  keywords = ['Wiser', 'Heating', 'V2', 'API'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
