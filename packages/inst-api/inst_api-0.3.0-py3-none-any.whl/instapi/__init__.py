8c8f373e3b950b97f0a4e76933c45b024df278c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:31 +0000

    Add package inst-api/0.3.0/inst_api-0.3.0-py3-none-any.whl

---
+++ packages/inst-api/inst_api-0.3.0-py3-none-any.whl/instapi/__init__.py
@@ -0,0 +1,28 @@
+from instapi.client import bind
+from instapi.exceptions import ClientNotInitedException
+from instapi.models import (
+    Candidate,
+    Comment,
+    Direct,
+    Feed,
+    Image,
+    Message,
+    Resource,
+    Resources,
+    User,
+    Video,
+)
+
+__all__ = [
+    "bind",
+    "ClientNotInitedException",
+    "Comment",
+    "Candidate",
+    "Direct",
+    "Feed",
+    "Image",
+    "Resource",
+    "Resources",
+    "User",
+    "Video",
+]
