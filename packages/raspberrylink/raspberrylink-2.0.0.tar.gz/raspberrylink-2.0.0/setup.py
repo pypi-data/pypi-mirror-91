f1dde5fedf48a28429809c96ce322c7ad4fe3ba4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:45 +0000

    Add package raspberrylink/2.0.0/raspberrylink-2.0.0.tar.gz

---
+++ packages/raspberrylink/raspberrylink-2.0.0.tar.gz/raspberrylink-2.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+__author__ = "jython234"
+
+setup(
+    name='raspberrylink',
+    version='2.0.0',
+    description='Raspberry Pi based handsfree audio server for automotive settings',
+    author=__author__,
+    author_email='jython234@gmail.com',
+    license='MPL-2.0',
+    url='https://github.com/jython234/raspberry-link',
+    packages=['raspberrylink', 'raspberrylink.audio'],
+    install_requires=['flask', 'waitress', 'dbus-python', 'PyGObject'],
+    entry_points={
+        'console_scripts': [
+            'raspilink-server=raspberrylink.server:startup',
+            'raspilink-agent=raspberrylink.agent:run_agent'
+        ]
+    },
+    data_files=[
+        ('/opt/raspberrylink/service-files', ['init-scripts/raspberrylink-agent.service',
+                                              'init-scripts/raspberrylink.service', 'init-scripts/raspberrylink'],
+         ),
+        ('/opt/raspberrylink', ['bluetooth-scripts/raspilink-bt-init', 'bluetooth-scripts/raspilink-bt-agent.py']),
+        ('/etc/', ['default-config/raspberrylink-server.conf'])
+    ]
+)
