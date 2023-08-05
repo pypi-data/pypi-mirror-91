133d958f32801ac4fe485789cce55a8c9b1c4283
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:24 +0000

    Add package pyp-boy/1.0a2/pyp_boy-1.0a2-py3-none-any.whl

---
+++ packages/pyp-boy/pyp_boy-1.0a2-py3-none-any.whl/pyp_boy/__init__.py
@@ -0,0 +1,60 @@
+class PypBoy(object):
+    # word_list = ["hello", "shell", "bells", "hells"]
+
+    def __init__(self, word_list):
+        self.word_list = word_list
+
+        self.possibles = []
+        # self.example_list = [
+        #     "LOYALIST",
+        #     "FEVERISH",
+        #     "CITIZENS",
+        #     "RESULTED",
+        #     "SUDDENLY",
+        #     "UNWANTED",
+        #     "HUMANITY",
+        #     "CONQUEST",
+        #
+        # ]
+
+    def compare(self, guess_str, word, num_correct):
+        correct = 0
+        for x, y in zip(guess_str, word):
+            print(f"Comparing {x} amd {y}")
+            if x == y:
+                print("Match!")
+                correct += 1
+
+        print(correct)
+
+        if int(correct) == int(num_correct):
+            correct = 0
+            self.possibles.append(word)
+            print(self.possibles)
+            return True
+
+    def guess(self, guess_str, num_correct):
+
+        new_list = []
+        print(guess_str)
+        print(num_correct)
+
+        for word in self.word_list:
+            if word == guess_str:
+                pass
+            else:
+                if self.compare(guess_str, word, num_correct):
+                    self.possibles.append(word)
+
+        self.word_list = self.possibles
+
+        return self.word_list
+
+    class Stats(object):
+        def __init__(self):
+            self.ledger = {}
+
+        def add(self, guessed_word, ):
+            entry = {}
+
+    class CLI()
