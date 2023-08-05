183243446f47cfe971b7260a6de73ba1bfb8cc1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:07 +0000

    Add package pomace/0.6.4/pomace-0.6.4.tar.gz

---
+++ packages/pomace/pomace-0.6.4.tar.gz/pomace-0.6.4/pomace/__init__.py
@@ -0,0 +1,11 @@
+from pkg_resources import DistributionNotFound, get_distribution
+
+from .api import *
+from .models import *
+from .types import *
+
+
+try:
+    __version__ = get_distribution("pomace").version
+except DistributionNotFound:
+    __version__ = "(local)"
