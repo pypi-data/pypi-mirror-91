8d435881a2089c501ce06869a4ea7f24cdf328d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:17 +0000

    Add package pwninit/0.1.4/pwninit-0.1.4.tar.gz

---
+++ packages/pwninit/pwninit-0.1.4.tar.gz/pwninit-0.1.4/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pwninit']
+entry_points = \
+{'console_scripts': ['pwninit = pwninit:main']}
+
+setup_kwargs = {
+    'name': 'pwninit',
+    'version': '0.1.4',
+    'description': 'pwnable challenge quick starter.',
+    'long_description': None,
+    'author': 'exd0tpy',
+    'author_email': 'jsw5258@ajou.ac.kr',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*',
+}
+
+
+setup(**setup_kwargs)
