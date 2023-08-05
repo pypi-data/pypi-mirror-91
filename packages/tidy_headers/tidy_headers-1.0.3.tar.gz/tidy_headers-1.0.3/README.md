8bc8cedf7dd5c1aedcfbd1633ba4136da67cfbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:46 +0000

    Add package tidy_headers/1.0.3/tidy_headers-1.0.3.tar.gz

---
+++ packages/tidy_headers/tidy_headers-1.0.3.tar.gz/tidy_headers-1.0.3/README.md
@@ -0,0 +1,56 @@
+# tidy_headers
+
+`tidy_headers` stores metadata in beautifully readable plain text headers.
+
+## example
+
+For some `filepath`, 
+
+```python
+import collections
+import tidy_headers
+meta = collections.OrderedDict()
+meta['date'] = '2017-11-10'
+meta['location'] = 'Madison, Wisconsin, USA'
+meta['name'] = ['batch', 'apples', 'pineapple', 'oregano', 'tomatoes']
+tidy_headers.write(filepath, meta)
+```
+
+To get the dictionary back, simply `read`:
+
+```python
+meta = tidy_headers.read(filepath)
+```
+
+:tada:
+
+See the examples directory for more:
+* [minimal.txt](../master/examples/minimal.txt)
+* [named.txt](../master/examples/named.txt)
+
+## installation
+
+### using pip
+
+```
+pip install tidy_headers
+```
+
+### using conda
+
+```
+conda config --add channels conda-forge
+conda install tidy_headers
+```
+
+## allowed values
+
+`tidy_headers` strives to write plain text headers in an unambiguous way such that an identical metadata dictionary can be generated using `read`. This process is only reliable for the following subset of python types:
+
+- `str`
+- `int`
+- `float`
+- `list` containing any combination of the above
+- `numpy.ndarray` (arbitrary size and dimensionality)
+
+Metadata dictionaries containing only these types are guaranteed to `read` and `write` succesfully.
---
+++ packages/tidy_headers/tidy_headers-1.0.3.tar.gz/tidy_headers-1.0.3/tidy_headers/_main.py
@@ -0,0 +1,89 @@
+"""Main."""
+
+
+# --- import -------------------------------------------------------------------------------------
+
+
+import re
+import os
+import copy
+import collections
+
+import numpy as np
+
+from ._parse_item import item2string, string2item
+
+
+# --- define -------------------------------------------------------------------------------------
+
+
+__all__ = ["read", "write"]
+
+
+# --- helpers ------------------------------------------------------------------------------------
+
+
+def read(filepath, *, encoding="utf-8"):
+    """Read headers from given filepath.
+
+    Parameters
+    ----------
+    filepath : path-like or iterable of strings
+        Path to file or iterable of stings
+    encoding : str
+        Encoding to use when opening the file.
+        No effect if iterable of strings given.
+
+    Returns
+    -------
+    collections.OrderedDict
+        Dictionary containing header information.
+    """
+    headers = collections.OrderedDict()
+    ds = np.DataSource(None)
+    # The following code is adapted from np.genfromtxt source
+    try:
+        if isinstance(filepath, os.PathLike):
+            filepath = os.fspath(filepath)
+        if isinstance(filepath, str):
+            fhd = iter(ds.open(filepath, "rt", encoding=encoding))
+        else:
+            fhd = iter(filepath)
+    except TypeError:
+        raise TypeError(
+            "filepath must be a path-like, list of strings, "
+            "or generator. Got %s instead." % type(filepath)
+        )
+    for line in fhd:
+        if line[0] == "#":
+            split = re.split(": |:\t", line, maxsplit=1)
+            key = split[0][2:]
+            headers[key] = string2item(split[1])
+        else:
+            break  # all header lines are at the beginning
+    return headers
+
+
+def write(filepath, dictionary):
+    """Write headers to given filepath.
+
+    Parameters
+    ----------
+    filepath : str
+        Path of file. File must not yet exist.
+    dictionary : dict or OrderedDict
+        Dictionary of header items.
+    """
+    dictionary = copy.deepcopy(dictionary)
+    # write header
+    for key, value in dictionary.items():
+        dictionary[key] = item2string(value)
+    lines = []
+    for key, value in dictionary.items():
+        if "\t" in value:
+            joiner = ""
+        else:
+            joiner = "\t"
+        lines.append(joiner.join([key + ":", value]))
+    header_str = "\n".join(lines)
+    np.savetxt(filepath, [], header=header_str)
