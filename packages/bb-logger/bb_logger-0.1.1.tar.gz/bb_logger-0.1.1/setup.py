c6cbbe587874ce71966f61f87a93912f82d3d4e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:33 +0000

    Add package bb-logger/0.1.1/bb_logger-0.1.1.tar.gz

---
+++ packages/bb-logger/bb_logger-0.1.1.tar.gz/bb_logger-0.1.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+    name='bb_logger',
+    packages=['bb_logger'],
+    version='0.1.1',
+    # Chose a license from here:
+    # https://help.github.com/articles/licensing-a-repository
+    license='MIT',
+    description='setup logger bb',
+    author='le du',
+    author_email='dulx96@gmail.com',
+    # Provide either the link to your github or to your website
+    url='https://github.com/dulx96/bb_logger.git',
+    download_url='https://github.com/dulx96/bb_logger/archive/0.1.tar.gz',
+    keywords=['BB', 'LOGGER', 'LOGGING'],
+    install_requires=[],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as
+        'Development Status :: 3 - Alpha',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
