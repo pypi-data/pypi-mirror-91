0ac808fbe6efddbb092e747842abe385feddf3ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:33 +0000

    Add package fugue/0.5.0.dev2/fugue-0.5.0.dev2.tar.gz

---
+++ packages/fugue/fugue-0.5.0.dev2.tar.gz/fugue-0.5.0.dev2/fugue/extensions/_builtins/__init__.py
@@ -0,0 +1,26 @@
+# flake8: noqa
+from fugue.extensions._builtins.creators import CreateData, Load, LoadYielded
+from fugue.extensions._builtins.outputters import (
+    AssertEqual,
+    AssertNotEqual,
+    RunOutputTransformer,
+    Save,
+    Show,
+)
+from fugue.extensions._builtins.processors import (
+    AlterColumns,
+    Distinct,
+    DropColumns,
+    Dropna,
+    Fillna,
+    Limit,
+    Rename,
+    RunJoin,
+    RunSetOperation,
+    RunSQLSelect,
+    RunTransformer,
+    Sample,
+    SaveAndUse,
+    SelectColumns,
+    Zip,
+)
