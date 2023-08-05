964e8b1c8be57f0eb726181906f8c0674fb596ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:32 +0000

    Add package sklearnn/0.0.1/sklearnn-0.0.1.tar.gz

---
+++ packages/sklearnn/sklearnn-0.0.1.tar.gz/sklearnn-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="sklearnn",
+    version="0.0.1",
+    author="ken",
+    author_email="kenbliky@gmail.com",
+    description="Image kernel.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenblikylee/imgkernel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
