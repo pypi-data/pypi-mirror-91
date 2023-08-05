0e1b8bb49adf9626f27ea7c04b0623a03002c14b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:37 +0000

    Add package sentinel-datetime/0.0.3/sentinel-datetime-0.0.3.tar.gz

---
+++ packages/sentinel-datetime/sentinel-datetime-0.0.3.tar.gz/sentinel-datetime-0.0.3/sentinel_datetime.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+
+
+class sentinel:
+    def __init__(
+        self,
+        year=0,
+        month=0,
+        day=0,
+        hour=0,
+        minute=0,
+        second=0,
+        microsecond=0,
+        default=None,
+    ):
+        self._year = year
+        self._month = month
+        self._day = day
+
+        self._hour = hour
+        self._minute = minute
+        self._second = second
+        self._microsecond = microsecond
+
+        if default is None:
+            default = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+
+        self.default = default
+
+    def __getattr__(self, attr):
+        return getattr(self.default, attr)
+
+    @property
+    def has_year(self):
+        return self._year != 0
+
+    @property
+    def has_month(self):
+        return self._month != 0
+
+    @property
+    def has_day(self):
+        return self._day != 0
+
+    def todatetime(self):
+        res = {
+            attr: value
+            for attr, value in [
+                ("year", self._year),
+                ("month", self._month),
+                ("day", self._day),
+                ("hour", self._hour),
+                ("minute", self._minute),
+                ("second", self._second),
+                ("microsecond", self._microsecond),
+            ]
+            if value
+        }
+        return self.default.replace(**res)
+
+    def replace(self, **res):
+        return sentinel(**res, default=self.default)
+
+    def __repr__(self):
+        return "%s(%d, %d, %d, %d, %d)" % (
+            self.__class__.__qualname__,
+            self.year,
+            self.month,
+            self.day,
+            self.hour,
+            self.minute,
+        )
