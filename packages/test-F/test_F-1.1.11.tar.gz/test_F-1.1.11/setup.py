1f627dc62bbfe3adab8ec3b131aeaf1f1dbe9c21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:08 +0000

    Add package test-F/1.1.11/test_F-1.1.11.tar.gz

---
+++ packages/test-F/test_F-1.1.11.tar.gz/test_F-1.1.11/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from io import open
+import os
+
+print(os.environ.get("CI_COMMIT_TAG", "0.0.0"))
+_version = os.environ.get("CI_COMMIT_TAG", "1.3.0")
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+
+requirements = [
+    "ta", "requests", "importlib_metadata", "matplotlib", "pandas", "pydantic"
+]
+
+setup(
+    name="test_F",  # Required
+    version=_version,  # Required
+    description="financial mining",  # Optional
+    long_description=long_description,  # Optional
+    long_description_content_type="text/markdown",  # Optional (see note above)
+    url="https://github.com/linsamtw",  # Optional
+    author="linsam",  # Optional
+    author_email="samlin266118@gmail.com",  # Optional
+    classifiers=[  # Optional
+        "Development Status :: 3 - Alpha",
+        # "Intended Audience :: Developers",
+        # "Topic :: Software Development :: Buil Tools",
+        # "License :: OSI Approved :: MIT License",
+        # "Programming Language :: Python :: 3.6",
+    ],
+    keywords="financial, python",  # Optional
+    packages=find_packages(exclude=["importlib", "ta"]),
+    install_requires=requirements,
+    project_urls={  # Optional
+        "documentation": "https://linsamtw.github.io/FinMindDoc/",
+        "Source": "https://github.com/linsamtw/FinMind",
+    },
+)
