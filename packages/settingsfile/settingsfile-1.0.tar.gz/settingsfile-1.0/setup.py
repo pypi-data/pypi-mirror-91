8d8e1c15ecbbb28b80da586b58df192ecdbdf413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:20 +0000

    Add package settingsfile/1.0/settingsfile-1.0.tar.gz

---
+++ packages/settingsfile/settingsfile-1.0.tar.gz/settingsfile-1.0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="settingsfile", # Replace with your own username
+    version="1.0",
+    author="Ömer Selçuk",
+    author_email="omrfyyz@gmail.com",
+    description="Memorize settings in Python easily. Autoload and autosave settings to a file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/divisia/settingsfile",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
