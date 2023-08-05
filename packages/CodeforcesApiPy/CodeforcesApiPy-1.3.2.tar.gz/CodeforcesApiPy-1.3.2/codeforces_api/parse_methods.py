61f16e67802053c6bb5f33a9c7890090b5389140
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package CodeforcesApiPy/1.3.2/CodeforcesApiPy-1.3.2.tar.gz

---
+++ packages/CodeforcesApiPy/CodeforcesApiPy-1.3.2.tar.gz/CodeforcesApiPy-1.3.2/codeforces_api/parse_methods.py
@@ -0,0 +1,23 @@
+"""
+Parse methods from codeforces.com
+"""
+import requests
+from lxml import html
+
+
+class CodeforcesParser:
+    def get_solution(self, contest_id, submit_id):
+        """
+        Returns source code for specified submit.
+        """
+        solutionPage = requests.get(
+            "https://codeforces.com/contest/"
+            + str(contest_id)
+            + "/submission/"
+            + str(submit_id)
+        )
+        tree2 = html.fromstring(solutionPage.text)
+        code = tree2.xpath('//*[@id="pageContent"]/div[3]/pre/text()')
+        if len(code) == 0:
+            raise ValueError("Incorrect contest_id or submit_id")
+        return code[0].replace("\r", "")
