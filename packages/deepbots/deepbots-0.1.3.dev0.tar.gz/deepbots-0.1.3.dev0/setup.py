3cbb3d8b0fa217f0b763069aae17a7a8ca523c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:06 +0000

    Add package deepbots/0.1.3.dev0/deepbots-0.1.3.dev0.tar.gz

---
+++ packages/deepbots/deepbots-0.1.3.dev0.tar.gz/deepbots-0.1.3.dev0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+DESCRIPTION = "A wrapper framework for Reinforcement Learning in Webots \
+    simulator"
+
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="deepbots",
+    author="aidudezzz",
+    author_email="deepbots@protonmail.com",
+    version="0.1.3-dev0",
+    description=DESCRIPTION,
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+)
