439ab9505380843fe843b95ee971fbb1b5875c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:25 +0000

    Add package alleycat-ui/0.0.5/alleycat-ui-0.0.5.tar.gz

---
+++ packages/alleycat-ui/alleycat-ui-0.0.5.tar.gz/alleycat-ui-0.0.5/setup.py
@@ -0,0 +1,22 @@
+from setuptools import find_namespace_packages, setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="alleycat-ui",
+    version="0.0.5",
+    author="Xavier Cho",
+    author_email="mysticfallband@gmail.com",
+    description="Lightweight GUI widget toolkit for UPBGE (Uchronia Project Blender Game Engine).",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mysticfall/alleycat-ui",
+    packages=find_namespace_packages(include=["alleycat.*"]),
+    install_requires=["alleycat-reactive==0.4.7", "returns==0.15.0", "rx==3.1.1", "pangocairocffi==0.5.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
