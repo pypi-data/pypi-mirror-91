413543e17e6159606427c81c87ba84d972a9c5ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:45 +0000

    Add package MCHammer/1.0.2/MCHammer-1.0.2.tar.gz

---
+++ packages/MCHammer/MCHammer-1.0.2.tar.gz/MCHammer-1.0.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="MCHammer",
+    version="1.0.2",
+    author="Andrew Tarzia",
+    author_email="andrew.tarzia@gmail.com",
+    description="Contains MC algorithm for optimising molecules.",
+    url="https://github.com/andrewtarzia/MCHammer",
+    packages=setuptools.find_packages(),
+    install_requires=(
+        'scipy',
+        'matplotlib',
+        'networkx',
+        'numpy',
+    ),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
