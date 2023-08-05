311c2c5362a502efb3564db4cc1a3d7279ca0118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:02 +0000

    Add package whitebeam/1.0.3/whitebeam-1.0.3-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/whitebeam/whitebeam-1.0.3-cp38-cp38-manylinux2014_x86_64.whl/whitebeam/base/gini.py
@@ -0,0 +1,25 @@
+# gini tree
+# Whitebeam | Kieran Molloy | Lancaster University 2020
+
+"""
+This class implements the classification tree in CART 
+by inheriting the alpha tree (alpha=2).
+"""
+
+from whitebeam.base.alpha import AlphaTree
+import numpy as np
+
+class GiniTree(AlphaTree):
+
+    def __init__(self, 
+                max_depth=5, 
+                min_samples_split=2,
+                min_samples_leaf=1):
+
+        AlphaTree.__init__(self, 
+                        alpha=2.0,
+                        max_depth=max_depth,
+                        min_samples_split=min_samples_split,
+                        min_samples_leaf=min_samples_leaf)
+
+
