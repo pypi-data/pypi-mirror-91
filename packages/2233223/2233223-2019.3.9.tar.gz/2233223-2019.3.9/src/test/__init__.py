f198923db0893985e263af310ff0f4fc9593d59f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:20 +0000

    Add package 2233223/2019.3.9/2233223-2019.3.9.tar.gz

---
+++ packages/2233223/2233223-2019.3.9.tar.gz/2233223-2019.3.9/src/test/__init__.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8-*-
+# Author: yunhao
+# Github: https://github.com/yunhao233
+# CreatDate: 2021/1/9 16:01
+# Description:
+from __future__ import absolute_import
+from .hello import *
+
+__version__ = '1.0.0'
+__license__ = ''
