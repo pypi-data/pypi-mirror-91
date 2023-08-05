6da5a9eaa2d1f1df3f45f4ddf91ff972d6b343ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package gamma-logger/0.1.0/gamma_logger-0.1.0.tar.gz

---
+++ packages/gamma-logger/gamma_logger-0.1.0.tar.gz/gamma_logger-0.1.0/gamma_logger/__init__.py
@@ -0,0 +1,23 @@
+import logging
+import logging.config
+import os
+
+class logger():
+	def __init__(self, name):
+		this_dir, this_filename = os.path.split(__file__)
+		CONF_PATH = os.path.join(this_dir, "data", "config", "logging.conf")
+		logging.config.fileConfig(CONF_PATH)
+		self.name = name
+		self.logger = logging.getLogger(self.name)
+        
+	def write_to_log(self, error_msg, error_type='C'):
+		if error_type == 'D':
+			self.logger.debug(error_msg)
+		elif error_type == 'I':
+			self.logger.info(error_msg)
+		elif error_type == 'W':
+			self.logger.warning(error_msg)
+		elif error_type == 'E':
+			self.logger.error(error_msg)
+		else:	
+			self.logger.critical(error_msg)
