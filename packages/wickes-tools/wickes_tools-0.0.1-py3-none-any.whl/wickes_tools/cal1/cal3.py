b9694a82de02c5d24884560524799c5fb230ac76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:33 +0000

    Add package wickes-tools/0.0.1/wickes_tools-0.0.1-py3-none-any.whl

---
+++ packages/wickes-tools/wickes_tools-0.0.1-py3-none-any.whl/wickes_tools/cal1/cal3.py
@@ -0,0 +1,22 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+def divide_numbers(num1, num2):
+    return num1 / num2
+
+
+def main():
+    print("calculator module 2")
+
+
+if __name__ == "__main__":
+    main()
