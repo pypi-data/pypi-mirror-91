c76e9518f722c7e455f56409dcd0fef8260f13ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:18 +0000

    Add package radiko/1.4.0/radiko-1.4.0-py3-none-any.whl

---
+++ packages/radiko/radiko-1.4.0-py3-none-any.whl/Radiko/__init__.py
@@ -0,0 +1,11 @@
+##############################################################
+### radiko                                                 ###
+### Copyright © 2020 kokarare1212 All rights reserved.     ###
+###                                                        ###
+### This software is released under the Apache License 2.0 ###
+### see http://www.apache.org/licenses/LICENSE-2.0         ###
+##############################################################
+
+from .radiko import RadikoClient, __version__
+
+__all__ = ["RadikoClient", "__version__"]
