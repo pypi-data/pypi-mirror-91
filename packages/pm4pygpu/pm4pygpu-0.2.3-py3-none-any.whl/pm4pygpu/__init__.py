b2f93c8371aec2fe2540351f227cd3445a39d45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:53 +0000

    Add package pm4pygpu/0.2.3/pm4pygpu-0.2.3-py3-none-any.whl

---
+++ packages/pm4pygpu/pm4pygpu-0.2.3-py3-none-any.whl/pm4pygpu/__init__.py
@@ -0,0 +1,6 @@
+__version__ = '0.2.3'
+__doc__ = "PM4Py GPU"
+__author__ = 'PADS'
+__author_email__ = "alessandro.berti89@gmail.com"
+__maintainer__ = 'PADS'
+__maintainer_email__ = "alessandro.berti89@gmail.com"
