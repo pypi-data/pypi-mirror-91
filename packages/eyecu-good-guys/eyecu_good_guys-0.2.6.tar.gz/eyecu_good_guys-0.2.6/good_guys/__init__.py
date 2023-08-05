b3423139d7d5a2d06e23411647bdccff92d72f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package eyecu-good-guys/0.2.6/eyecu_good_guys-0.2.6.tar.gz

---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.2.6.tar.gz/eyecu_good_guys-0.2.6/good_guys/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.2.6'
+
+from . import layers, models
+from . import activations
---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.2.6.tar.gz/eyecu_good_guys-0.2.6/good_guys/layers/__init__.py
@@ -0,0 +1,8 @@
+from . import basic
+from . import pooling
+
+from good_guys.layers.complex.Inception import Inception
+
+from good_guys.layers.basic.Reshape import Reshape
+
+from good_guys.layers.convs.DepthwiseSeparableConv2d import DepthwiseSeparableConv2d
