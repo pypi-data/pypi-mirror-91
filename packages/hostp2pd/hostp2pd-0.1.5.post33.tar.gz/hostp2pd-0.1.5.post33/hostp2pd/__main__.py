8cf4d783e211ec6ceacb86ce38a1346fa9b9a418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:38 +0000

    Add package hostp2pd/0.1.5.post33/hostp2pd-0.1.5.post33.tar.gz

---
+++ packages/hostp2pd/hostp2pd-0.1.5.post33.tar.gz/hostp2pd-0.1.5.post33/hostp2pd/__main__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+##########################################################################
+# hostp2pd - The Wi-Fi Direct Session Manager
+# wpa_cli controller of Wi-Fi Direct connections handled by wpa_supplicant
+# https://github.com/Ircama/hostp2pd
+# (C) Ircama 2021 - CC-BY-NC-SA-4.0
+#########################################################################
+
+from . import main
+
+if __name__ == "__main__":
+    main()
