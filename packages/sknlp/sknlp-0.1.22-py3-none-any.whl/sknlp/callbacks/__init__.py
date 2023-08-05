a785fc3a0a0f69927ca00039990b5740f3eb7b54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:41 +0000

    Add package sknlp/0.1.22/sknlp-0.1.22-py3-none-any.whl

---
+++ packages/sknlp/sknlp-0.1.22-py3-none-any.whl/sknlp/callbacks/__init__.py
@@ -0,0 +1,6 @@
+from .model_score import ModelScoreCallback
+from .save import ModelSave
+from .weight_decay import WeightDecayScheduler
+
+
+__all__ = ['ModelScoreCallback', 'WeightDecayScheduler', 'ModelSave']
