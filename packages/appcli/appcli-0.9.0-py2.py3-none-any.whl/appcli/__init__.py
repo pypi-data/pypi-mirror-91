e5da438a7582a44c160f1f1ba207a109b01168d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:29 +0000

    Add package appcli/0.9.0/appcli-0.9.0-py2.py3-none-any.whl

---
+++ packages/appcli/appcli-0.9.0-py2.py3-none-any.whl/appcli/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+An object-oriented framework for command-line apps.
+"""
+
+__version__ = '0.9.0'
+
+from .app import App, FromParams
+from .model import init, load, reload
+from .params import param, Key
+from .toggle import Toggle as toggle, pick_toggled, toggle_param
+from .attrs import config_attr
+from .utils import lookup
+from .layers import *
+from .config import *
+from .errors import *
