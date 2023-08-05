90e99536fa682e987d3856584a308a235c6cdee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:31 +0000

    Add package hidapi-usb/0.2.5/hidapi-usb-0.2.5.tar.gz

---
+++ packages/hidapi-usb/hidapi-usb-0.2.5.tar.gz/hidapi-usb-0.2.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hidapi-usb',
+    version="0.2.5",
+    description=("CFFI wrapper for hidapi with changes by flok"),
+    author="Florian K",
+    url="https://github.com/flok/hidapi-cffi.git",
+    author_email="37000563+flok@users.noreply.github.com",
+    license='BSD',
+    py_modules=['hidapi'],
+    install_requires=['cffi']
+)
