acc9bae18e4b243a8623b5538305da3ebe2903be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:40 +0000

    Add package casinogv/0.0.0.3/casinogv-0.0.0.3.tar.gz

---
+++ packages/casinogv/casinogv-0.0.0.3.tar.gz/casinogv-0.0.0.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup, find_namespace_packages
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+    
+setup(name='casinogv', 
+packages=find_namespace_packages(include=['casinogv.*']), 
+version='0.0.0.3',
+description="Gibson's casino library",
+author='Sagiv',
+include_package_data=True,
+license="MIT", 
+long_description=long_description,
+long_description_content_type='text/markdown',
+setup_requires=['pytest-runner'])
