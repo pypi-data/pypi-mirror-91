60c09d2419bf60618df426fef1eff6fefd5299bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:06 +0000

    Add package zcoinbase/0.0.9/zcoinbase-0.0.9.tar.gz

---
+++ packages/zcoinbase/zcoinbase-0.0.9.tar.gz/zcoinbase-0.0.9/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+VERSION_NUMBER = '0.0.9'
+
+setup(
+  name='zcoinbase',
+  version=VERSION_NUMBER,
+  packages=find_packages(),
+  url='https://github.com/Zbot21/zcoinbase',
+  download_url='https://github.com/Zbot21/zcoinbase/archive/v{}.tar.gz'.format(VERSION_NUMBER),
+  license='MIT',
+  author='Chris Bellis',
+  author_email='chris@zbots.org',
+  description='A Simple Coinbase Client for the Coinbase Pro API',
+  install_requires=['requests',
+                    'websocket-client',
+                    'sortedcontainers',
+                    'python-dateutil',
+                    'pandas']
+)
