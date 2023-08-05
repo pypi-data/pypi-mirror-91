617a6af21e26b169892ecd0c533b675fee1db4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:02 +0000

    Add package rdfpandas/1.1.0/rdfpandas-1.1.0.tar.gz

---
+++ packages/rdfpandas/rdfpandas-1.1.0.tar.gz/rdfpandas-1.1.0/setup.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name = 'rdfpandas',
+    version = '1.1.0',
+    description = 'RDF support for Pandas',
+    long_description = readme,
+    author = 'Eugene Morozov',
+    author_email = 'emorozov@gmail.com',
+    url = 'https://github.com/cadmiumkitty/rdfpandas',
+    license = 'MIT',
+    packages = find_packages(exclude = ('tests', 'docs')),
+    install_requires = ['pandas>=1.2.0', 'rdflib>=5.0.0']
+)
+
