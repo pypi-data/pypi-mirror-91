b1f3ceba6a2b18cb596da93ccda99583a83b2b90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:01 +0000

    Add package tst-distributions/0.1/tst_distributions-0.1.tar.gz

---
+++ packages/tst-distributions/tst_distributions-0.1.tar.gz/tst_distributions-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tst_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tst_distributions'],
+      author='Jubert Roldan',
+      author_email = 'jubertroldan@gmail.com',
+      
+      zip_safe=False)
