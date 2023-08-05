f7d1abe095a3e0a2e29ea1a9597ec14261fb5f26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:08 +0000

    Add package jiraone/0.1.6/jiraone-0.1.6-py3-none-any.whl

---
+++ packages/jiraone/jiraone-0.1.6-py3-none-any.whl/jiraone/reporting.py
@@ -0,0 +1,403 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Example methods of Generating reports
+
+Provided herein are Report Generator Classes and Methods,
+Easily generate report for the various endpoints
+"""
+from typing import Any, Optional, List, Iterable, Tuple
+from collections import deque
+from jiraone import LOGIN, endpoint, add_log, WORK_PATH
+import json
+import csv
+import sys
+import os
+
+CsvData = List[List[str]]
+
+
+class Projects:
+    """Get report on a Project based on user or user's attributes or groups."""
+
+    @staticmethod
+    def projects_accessible_by_users(*args: Any, project_folder: str = "Project",
+                                     project_file_name: str = "project_file.csv",
+                                     user_extraction_file: str = "project_extract.csv",
+                                     permission: str = "BROWSE", **kwargs):
+        """
+        Send an argument as String equal to a value, example: status=live.
+
+        Multiple arguments separate by comma as the first argument in the function, all other arguments should
+        be keyword args that follows. This API helps to generate full user accessibility to Projects on Jira.
+        It checks the users access and commits the finding to a report file. You can tweak the permission argument
+        with the options mention here
+        https://developer.atlassian.com/cloud/jira/platform/rest/v3/api-group-user-search
+        for endpoint /rest/api/3/user/permission/search
+        """
+        count_start_at = 0
+        headers = ["Project Key", "Project Name", "Issue Count", "Last Issue Update"]
+        csv_writer(folder=project_folder, file_name=project_file_name, data=headers)
+
+        def project():
+            csv_writer(project_folder, project_file_name, data=raw)
+            read_users = csv_reader(folder=project_folder, file_name=file_name)
+            for user in read_users:
+                account_id = user[0]
+                display_name = user[2]
+                active_status = user[3]
+                project_key = keys
+                find = LOGIN.get(endpoint.find_users_with_permission(account_id, project_key, permission))
+                data = json.loads(find.content)
+                if str(data) == "[]":
+                    raw_vision = [display_name, f"Has {permission} Permission: False",
+                                  f"Project: {name}", f"User Status: {active_status}"]
+                    csv_writer(project_folder, project_file_name, data=raw_vision)
+                else:
+                    for d in data:
+                        d_name = d["displayName"]
+                        active = d["active"]
+                        raw_vision = [d_name, f"Has {permission} Permission: {active}",
+                                      f"Project: {name}", f"User Status: {active_status}"]
+
+                        csv_writer(project_folder, project_file_name, data=raw_vision)
+
+        file_name = user_extraction_file
+        USER.get_all_users(file=file_name, folder=project_folder, **kwargs)
+        print("Project User List Extracted")
+        add_log("Project User List Extracted", "info")
+        while True:
+            load = LOGIN.get(endpoint.get_projects(*args, start_at=count_start_at))
+            count_start_at += 50
+            if load.status_code == 200:
+                results = json.loads(load.content)
+                for key in results["values"]:
+                    keys = key["key"]
+                    name = key["name"]
+                    if "insight" in key:
+                        insight = key["insight"]
+                        if "totalIssueCount" and "lastIssueUpdateTime" in insight:
+                            raw = [keys, name, f"{insight['totalIssueCount']}",
+                                   f"{insight['lastIssueUpdateTime']}"]
+                            project()
+                        elif "totalIssueCount" in insight and "lastIssueUpdateTime" not in insight:
+                            raw = [keys, name, f"{insight['totalIssueCount']}",
+                                   "No data available"]
+                            project()
+                    else:
+                        raw = [keys, name, "No data available", "No data available"]
+                        project()
+
+                if count_start_at > results["total"]:
+                    print("Project Reporting Completed")
+                    print("File extraction completed. Your file is located at {}"
+                          .format(path_builder(path=project_folder, file_name=project_file_name)))
+                    add_log("Project Reporting Completed", "info")
+                    break
+            else:
+                sys.stderr.write("Unable to fetch data status {} ".format(load.status_code))
+                add_log(f"Data retrieval failure due to {load.reason}", "error")
+                sys.exit(1)
+
+    @staticmethod
+    def dashboards_shared_with(dashboard_folder: str = "Dashboard",
+                               dashboard_file_name: str = "dashboard_file.csv",
+                               **kwargs):
+        """
+        Retrieve the Dashboard Id/Name/owner and who it is shared with.
+
+        The only requirement is that the user querying this API should have access to all
+        the Dashboard available else it will only return dashboard where the user's view access is allowed.
+        """
+        count_start_at = 0
+        dash_list = deque()  # get the id of dashboard search
+        put_list = deque()  # arrange the different values associated with dashboard
+        dump_list = deque()  # dump the results into our csv writer
+        headers = ["Dashboard Id", "Dashboard Name", "Owner", "Shared Permission"]
+        csv_writer(folder=dashboard_folder, file_name=dashboard_file_name, data=headers, **kwargs)
+
+        while True:
+            load = LOGIN.get(endpoint.search_for_dashboard(start_at=count_start_at))
+            count_start_at += 50
+            if load.status_code == 200:
+                results = json.loads(load.content)
+                for value in results["values"]:
+                    valid = value["id"]
+                    name = value["name"]
+                    raw = [valid, name]
+                    dash_list.append(raw)
+
+                print("Dashboard List Extracted")
+                add_log("Dashboard List Extracted", "info")
+
+                def dash_run():
+                    for pem in shared_permission:
+                        if "role" in pem:
+                            role = f"Role: {pem['role']['name']}"
+                            put_list.append(role)
+                        if "group" in pem:
+                            group = f"Group: {pem['group']['name']}"
+                            put_list.append(group)
+                        if "type" in pem:
+                            types = pem["type"]
+                            if types == "project":
+                                project = f"Project Name: {pem['project']['name']}"
+                                put_list.append(project)
+                            if types == "loggedin" or types == "global":
+                                put_list.append(types)
+
+                    extract = [d for d in put_list]
+                    raw = [value[0], names, owner, extract]
+                    dump_list.append(raw)
+
+                for value in dash_list:
+                    init = LOGIN.get(endpoint.get_dashboard(value[0]))
+                    if init.status_code == 200:
+                        expanse = json.loads(init.content)
+                        names = expanse["name"]
+                        if "owner" in expanse:
+                            owner = expanse["owner"]["displayName"]
+                        shared_permission = expanse["sharePermissions"]
+                        dash_run()
+                        put_list = deque()
+
+                csv_writer(dashboard_folder, dashboard_file_name, data=dump_list, mark="many")
+
+                if count_start_at > results["total"]:
+                    print("Dashboard Reporting Completed")
+                    print("File extraction completed. Your file is located at {}"
+                          .format(path_builder(path=dashboard_folder, file_name=dashboard_file_name)))
+                    add_log("Dashboard Reporting Completed", "info")
+                    break
+
+    @staticmethod
+    def get_all_roles_for_projects(roles_folder: str = "Roles",
+                                   roles_file_name: str = "roles_file.csv",
+                                   user_extraction: str = "role_users.csv",
+                                   **kwargs):
+        """Get the roles available in a project and which user is assigned to which
+        role within the project"""
+        count_start_at = 0
+        headers = ["Project Id ", "Project Key", "Project Name", "Project roles", "User AccountId", "User DisplayName",
+                   "User role in Project"]
+        csv_writer(folder=roles_folder, file_name=roles_file_name, data=headers, **kwargs)
+
+        # get extraction of projects data
+        def role_on() -> Any:
+            project_role_list = deque()
+            for keys in results["values"]:
+                role_list = deque()
+                key = keys["key"]
+                pid = keys["id"]
+                name = keys["name"]
+                roles = LOGIN.get(endpoint.get_roles_for_project(pid))
+                print("Extracting Project Keys {}".format(key))
+                add_log("Extracting Project Keys {}".format(key), "info")
+
+                # find out what roles exist within each project
+                def role_over() -> Tuple:
+                    if roles.status_code == 200:
+                        extract = json.loads(roles.content)
+                        only_keys = extract.keys()
+                        casting = list(only_keys)
+                        only_values = extract.items()
+                        puller = list(only_values)
+                        z = [d for d in puller]
+                        return casting, z
+
+                caster = role_over()
+                raw = [pid, key, name, caster[0]]
+                role_list.append(raw)
+                add_log("Appending data to List Queue", "info")
+
+                for user in read_users:
+                    account_id = user[0]
+                    display_name = user[2]
+
+                    # extract the user role using the appropriate accountId
+                    def get_user_role(user_data) -> Any:
+                        for users in user_data[1]:
+                            check = LOGIN.get(users[1])
+                            if check.status_code == 200:
+                                result_data = json.loads(check.content)
+                                actors = result_data["actors"]
+                                for act in actors:
+                                    if "actorUser" in act:
+                                        if account_id == act["actorUser"]["accountId"]:
+                                            res = f"Role Name: {result_data['name']}"
+                                            project_role_list.append(res)
+
+                        # function to write collected data into a file
+                        def pull_data() -> Any:
+                            role_puller = [j for j in project_role_list]
+                            project_id = data[0]
+                            project_key = data[1]
+                            project_name = data[2]
+                            project_roles = data[3]
+                            raw_dump = [project_id, project_key, project_name, project_roles, account_id, display_name,
+                                        role_puller]
+                            csv_writer(folder=roles_folder, file_name=roles_file_name, data=raw_dump)
+
+                        for data in role_list:
+                            pull_data()
+
+                    get_user_role(caster)
+                    project_role_list = deque()
+
+        USER.get_all_users(folder=roles_folder, file=user_extraction, **kwargs)
+        read_users = csv_reader(folder=roles_folder, file_name=user_extraction, **kwargs)
+        while True:
+            init = LOGIN.get(endpoint.get_projects(start_at=count_start_at))
+            if init.status_code == 200:
+                print("Project Extraction")
+                results = json.loads(init.content)
+                add_log("Project Extraction Initiated", "info")
+                if count_start_at > results["total"]:
+                    break
+                if results["total"] > 0:
+                    role_on()
+            count_start_at += 50
+
+        print("File extraction completed. Your file is located at {}".format(path_builder
+                                                                             (path=roles_folder,
+                                                                              file_name=roles_file_name)))
+        add_log("File extraction completed", "info")
+
+
+class Users:
+    """
+    Below methods helps to Generate the No of Users on Jira Cloud
+
+    You can customize it to determine which user you're looking for.
+    >> The below method here displays active or inactive users, so you'll be getting all users
+    :param: pull (options)
+            -> both: pulls out inactive and active users
+            -> active: pulls out only active users
+            -> inactive: pulls out inactive users
+    :param: user_type (options)
+            -> atlassian: a normal Jira Cloud user
+            -> customer: this will be your JSM customers
+            -> app: this will be the bot users for any Cloud App
+            -> unknown: as the name suggest unknown user type probably from oAuth
+    """
+    user_list = deque()
+
+    def get_all_users(self, pull: str = "both", user_type: str = "atlassian",
+                      file: str = None, folder: str = Any, **kwargs) -> Any:
+        """Generates a list of users."""
+        count_start_at = 0
+        validate = LOGIN.get(endpoint.myself())
+
+        while validate.status_code == 200:
+            extract = LOGIN.get(endpoint.search_users(count_start_at))
+            results = json.loads(extract.content)
+            self.user_activity(pull, user_type, results)
+            count_start_at += 50
+            print("Current Record - At Row", count_start_at)
+            add_log(f"Current Record - At Row {count_start_at}", "info")
+
+            if str(results) == "[]":
+                break
+        else:
+            sys.stderr.write("Unable to connect to {} - Login Failed...".format(LOGIN.base_url))
+            add_log(f"Login Failure on {LOGIN.base_url}, due to {validate.reason}", "error")
+            sys.exit(1)
+
+        if file is not None:
+            self.report(category=folder, filename=file)
+
+    def report(self, category: str = Any, filename: str = "users_report.csv") -> Optional:
+        """Creates a user report file in CSV format."""
+        read = [d for d in self.user_list]
+        csv_writer(folder=category, file_name=filename, data=read, mark="many")
+        add_log(f"Generating report file on {filename}", "info")
+
+    def user_activity(self, status: str = Any, account_type: str = Any, results: List = Any) -> Any:
+        """Determines users activity."""
+
+        # get both active and inactive users
+        def stack(c: Any, f: Any, s: Any) -> Any:
+            if status == "both":
+                if s["accountType"] == account_type:
+                    return c.user_list.append(f)
+            elif status == "active":
+                if s["accountType"] == account_type and s["active"] is True:
+                    return c.user_list.append(f)
+            elif status == "inactive":
+                if s["accountType"] == account_type and s["active"] is False:
+                    return c.user_list.append(f)
+
+        for each_user in results:
+            list_user = [each_user["accountId"], each_user["accountType"], each_user["displayName"],
+                         each_user["active"]]
+            stack(self, list_user, each_user)
+
+    def get_all_users_group(self, group_folder: str = "Groups", group_file_name: str = "group_file.csv",
+                            user_extraction_file: str = "group_extraction.csv", **kwargs):
+        """Get all users and the groups associated to them on the Instance."""
+        headers = ["Name", "AccountId", "Groups", "User status"]
+        csv_writer(folder=group_folder, file_name=group_file_name, data=headers, **kwargs)
+        file_name = user_extraction_file
+        self.get_all_users(file=file_name, folder=group_folder, **kwargs)
+        reader = csv_reader(file_name=file_name, folder=group_folder, **kwargs)
+        for user in reader:
+            account_id = user[0]
+            display_name = user[2]
+            active_status = user[3]
+            load = LOGIN.get(endpoint.get_user_group(account_id))
+            results = json.loads(load.content)
+            get_all = [d["name"] for d in results]
+            raw = [display_name, account_id, get_all, active_status]
+            csv_writer(folder=group_folder, file_name=group_file_name, data=raw)
+
+        print("File extraction completed. Your file is located at {}"
+              .format(path_builder(path=group_folder, file_name=group_file_name)))
+        add_log("Get Users group Completed", "info")
+
+
+def call_users() -> Users:
+    """Should return an instance of Users class."""
+    return Users()
+
+
+def call_projects() -> Projects:
+    """Should return an instance of Projects class."""
+    return Projects()
+
+
+def path_builder(path: str = "Report", file_name: str = Any, **kwargs):
+    """Builds a dir path and file path in a directory."""
+    base_dir = os.path.join(WORK_PATH, path)
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
+        add_log(f"Building Path {path}", "info")
+    base_file = os.path.join(base_dir, file_name)
+    return base_file
+
+
+def csv_writer(folder: str = Any, file_name: str = Any, data: Iterable = object,
+               mark: str = "single", mode: str = "a+", **kwargs) -> Any:
+    """Reads and writes to a file, single or multiple rows."""
+    file = path_builder(path=folder, file_name=file_name)
+    if mode:
+        with open(file, mode) as f:
+            write = csv.writer(f, delimiter=",")
+            if mark == "single":
+                write.writerow(data)
+            if mark == "many":
+                write.writerows(data)
+            add_log(f"Writing to file {file_name}", "info")
+
+
+def csv_reader(folder: str = Any, file_name: str = Any, mode: str = "r", **kwargs) -> CsvData:
+    """Reads a CSV file and returns a list comprehension of the data."""
+    file = path_builder(path=folder, file_name=file_name)
+    if mode:
+        with open(file, mode) as f:
+            read = csv.reader(f, delimiter=",")
+            load = [d for d in read]
+            add_log(f"Read file {file_name}", "info")
+            return load
+
+
+USER = call_users()
+PROJECT = call_projects()
