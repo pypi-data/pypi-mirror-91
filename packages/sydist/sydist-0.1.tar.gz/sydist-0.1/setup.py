8e9d471f51771b273c19dde447b7cc48b64bffe8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:52 +0000

    Add package sydist/0.1/sydist-0.1.tar.gz

---
+++ packages/sydist/sydist-0.1.tar.gz/sydist-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='sydist',
+      version='0.1',
+      description='Gaussian and Binomial Distribution',
+      packages=['distributions'],
+      zip_safe=False)
