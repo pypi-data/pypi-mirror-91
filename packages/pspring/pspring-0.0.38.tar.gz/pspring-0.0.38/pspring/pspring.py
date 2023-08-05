eff68f15a7354f21f31dd0569c5d655256295992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:23 +0000

    Add package pspring/0.0.38/pspring-0.0.38.tar.gz

---
+++ packages/pspring/pspring-0.0.38.tar.gz/pspring-0.0.38/pspring/pspring.py
@@ -0,0 +1,15 @@
+context={}
+
+import os
+
+for item in os.environ:
+    if item.startswith("pspring"):
+        os.environ[item.replace("_",".")] = os.environ.get(item)
+
+from .core.bean import *
+from .core.autowired import *
+from .core.context import *
+from .core.config import *
+from .core.configprovider import *
+from .core.cacheprovider import *
+from .core.jsonconfigprovider import *
