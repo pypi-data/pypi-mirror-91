4af80e945050b30d9c509751ca2008df9192cf91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:30 +0000

    Add package nepali-datetime/1.0.5.1/nepali-datetime-1.0.5.1.tar.gz

---
+++ packages/nepali-datetime/nepali-datetime-1.0.5.1.tar.gz/nepali-datetime-1.0.5.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name="nepali-datetime",
+    version="1.0.5.1",
+    description="Datetime module that operates on Bikram Sambat & Nepal time.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dxillar/nepali-datetime",
+    author="Amit Garu",
+    author_email="amitgaru2@gmail.com",
+    license="MIT",
+    packages=find_packages(exclude=("tests", "docs")),
+    keywords=['nepali', 'bs', 'b.s', 'date', 'datetime', 'time', 'timezone', 'nepal', 'bikram', 'sambat', 'samvat',
+              'nepali-date', 'nepali-datetime', 'nepal-time', 'npt', 'nepal-timezone'],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
