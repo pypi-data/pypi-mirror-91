a1ce60c9b4ff8917db0161cea6401391a1281170
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:27 +0000

    Add package eyecu-good-guys/0.2.7/eyecu_good_guys-0.2.7-py3-none-any.whl

---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.2.7-py3-none-any.whl/good_guys/__init__.py
@@ -0,0 +1,5 @@
+__version__ = '0.2.7'
+
+from . import models
+from . import layers
+from . import activations
---
+++ packages/eyecu-good-guys/eyecu_good_guys-0.2.7-py3-none-any.whl/good_guys/layers/convs/DepthwiseSeparableConv2d.py
@@ -0,0 +1,24 @@
+import torch.nn as N
+
+
+class DepthwiseSeparableConv2d(N.Module):
+
+    def __init__(self,filters : int,kernel_size:int):
+        super().__init__()
+
+        self.padding = (kernel_size-1)//2
+
+        self.l = N.Sequential(
+            N.Conv2d(filters,filters,(kernel_size,1),1,(self.padding,0),groups=filters),
+            N.BatchNorm2d(filters),
+            N.LeakyReLU(inplace=True),
+            N.Conv2d(filters,filters,(1,kernel_size),1,(0,self.padding),groups=filters),
+            N.BatchNorm2d(filters),
+            N.LeakyReLU(inplace=True),
+        )
+
+
+
+    def forward(self,x):
+
+        return self.l(x)
