2aaa6e81cdfdb320785b4ac6a1be17cab3dbe912
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:33 +0000

    Add package Av1an/5.5.post1/Av1an-5.5.post1-py3-none-any.whl

---
+++ packages/Av1an/Av1an-5.5.post1-py3-none-any.whl/av1an/manager/Counter.py
@@ -0,0 +1,40 @@
+from multiprocessing.managers import BaseManager
+from tqdm import tqdm
+
+def Manager():
+    """
+    Thread save manager for frame counter
+    """
+    m = BaseManager()
+    m.start()
+    return m
+
+
+class Counter:
+    """
+    Frame Counter
+    """
+    def __init__(self, total, initial, use_tqdm=True):
+        self.first_update = True
+        self.initial = initial
+        self.left = total - initial
+        self.current = 0
+        self.use_tqdm = use_tqdm
+        if use_tqdm:
+            self.tqdm_bar = tqdm(total=self.left, initial=0, dynamic_ncols=True, unit="fr", leave=True, smoothing=0.01)
+
+    def update(self, value):
+        if self.use_tqdm:
+            if self.first_update:
+                self.tqdm_bar.reset(self.left)
+                self.first_update = False
+            self.tqdm_bar.update(value)
+        else:
+            self.current += value
+
+    def close(self):
+        self.tqdm_bar.close()
+
+    def get_frames(self):
+        return self.current
+
