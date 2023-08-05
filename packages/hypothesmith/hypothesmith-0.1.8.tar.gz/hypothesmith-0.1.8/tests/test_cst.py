b15359484eda9bbe38082597c4290924627eda43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:45 +0000

    Add package hypothesmith/0.1.8/hypothesmith-0.1.8.tar.gz

---
+++ packages/hypothesmith/hypothesmith-0.1.8.tar.gz/hypothesmith-0.1.8/tests/test_cst.py
@@ -0,0 +1,89 @@
+"""Tests for the hypothesmith.cst module."""
+import ast
+from inspect import isabstract
+from operator import attrgetter
+
+import black
+import libcst
+import parso
+import pytest
+from hypothesis import example, given, note, strategies as st
+
+import hypothesmith
+from hypothesmith.cst import compilable
+
+NODE_TYPES = frozenset(
+    v
+    for v in vars(libcst).values()
+    if isinstance(v, type) and issubclass(v, libcst.CSTNode) and not isabstract(v)
+)
+
+
+@pytest.mark.parametrize("node", sorted(NODE_TYPES, key=attrgetter("__name__")))
+@given(data=st.data())
+def test_source_code_from_libcst_node_type(node, data):
+    try:
+        val = data.draw(st.from_type(node))
+    except NameError:
+        pytest.skip("NameError, probably a forward reference")
+    except TypeError as e:
+        if str(e).startswith("super"):
+            pytest.skip("something weird here, back later")
+        if str(e).startswith("Can't instantiate"):
+            pytest.skip("abstract classes somehow leaking into builds()")
+        raise
+    note(val)
+    if not isinstance(val, libcst.Module):
+        val = libcst.Module([val])
+    try:
+        code = val.code
+    except libcst._nodes.base.CSTCodegenError:
+        pytest.skip("codegen not supported yet, e.g. Annotation")
+    note(code)
+
+
+@pytest.mark.skipif(not hasattr(ast, "unparse"), reason="Can't test before available")
+@given(source_code=hypothesmith.from_node())
+def test_ast_unparse_from_nodes(source_code):
+    first = ast.parse(source_code)
+    unparsed = ast.unparse(first)
+    second = ast.parse(unparsed)
+    assert ast.dump(first) == ast.dump(second)
+
+
+@pytest.mark.xfail
+@example("A\u2592", black.FileMode())
+@given(
+    source_code=hypothesmith.from_node(),
+    mode=st.builds(
+        black.FileMode,
+        line_length=st.just(88) | st.integers(0, 200),
+        string_normalization=st.booleans(),
+        is_pyi=st.booleans(),
+    ),
+)
+def test_black_autoformatter_from_nodes(source_code, mode):
+    try:
+        result = black.format_file_contents(source_code, fast=False, mode=mode)
+    except black.NothingChanged:
+        pass
+    else:
+        with pytest.raises(black.NothingChanged):
+            black.format_file_contents(result, fast=False, mode=mode)
+
+
+@given(source_code=hypothesmith.from_node())
+def test_from_node_always_compilable(source_code):
+    compile(source_code, "<string>", "exec")
+
+
+@example("\x00")
+@given(st.text())
+def test_compilable_never_raises(s):
+    compilable(s)
+
+
+@given(source_code=hypothesmith.from_node())
+def test_parso_from_node(source_code):
+    result = parso.parse(source_code).get_code()
+    assert source_code == result
---
+++ packages/hypothesmith/hypothesmith-0.1.8.tar.gz/hypothesmith-0.1.8/tests/test_syntactic.py
@@ -0,0 +1,108 @@
+"""Tests for the hypothesmith.syntactic module."""
+import ast
+import io
+import tokenize
+
+import black
+import blib2to3
+import parso
+import pytest
+from hypothesis import example, given, reject, strategies as st
+
+import hypothesmith
+
+
+def fixup(s):
+    """Avoid known issues with tokenize() by editing the string."""
+    return "".join(x for x in s if x.isprintable()).strip().strip("\\").strip() + "\n"
+
+
+@pytest.mark.xfail
+@example("#")
+@example("\n\\\n")
+@example("#\n\x0cpass#\n")
+@given(source_code=hypothesmith.from_grammar().map(fixup).filter(str.strip))
+def test_tokenize_round_trip_bytes(source_code):
+    try:
+        source = source_code.encode("utf-8-sig")
+    except UnicodeEncodeError:
+        reject()
+    tokens = list(tokenize.tokenize(io.BytesIO(source).readline))
+    outbytes = tokenize.untokenize(tokens)  # may have changed whitespace from source
+    output = list(tokenize.tokenize(io.BytesIO(outbytes).readline))
+    assert [(t.type, t.string) for t in tokens] == [(t.type, t.string) for t in output]
+    # It would be nice if the round-tripped string stabilised.  It doesn't.
+    # assert outbytes == tokenize.untokenize(output)
+
+
+@pytest.mark.xfail
+@example("#")
+@example("\n\\\n")
+@example("#\n\x0cpass#\n")
+@given(source_code=hypothesmith.from_grammar().map(fixup).filter(str.strip))
+def test_tokenize_round_trip_string(source_code):
+    tokens = list(tokenize.generate_tokens(io.StringIO(source_code).readline))
+    outstring = tokenize.untokenize(tokens)  # may have changed whitespace from source
+    output = tokenize.generate_tokens(io.StringIO(outstring).readline)
+    assert [(t.type, t.string) for t in tokens] == [(t.type, t.string) for t in output]
+    # It would be nice if the round-tripped string stabilised.  It doesn't.
+    # assert outstring == tokenize.untokenize(output)
+
+
+@pytest.mark.skipif(not hasattr(ast, "unparse"), reason="Can't test before available")
+@given(source_code=hypothesmith.from_grammar())
+def test_ast_unparse_from_grammar(source_code):
+    first = ast.parse(source_code)
+    unparsed = ast.unparse(first)
+    second = ast.parse(unparsed)
+    assert ast.dump(first) == ast.dump(second)
+
+
+@example("\\", black.FileMode())
+@example("A#\r#", black.FileMode())
+@given(
+    source_code=hypothesmith.from_grammar(),
+    mode=st.builds(
+        black.FileMode,
+        line_length=st.just(88) | st.integers(0, 200),
+        string_normalization=st.booleans(),
+        is_pyi=st.booleans(),
+    ),
+)
+def test_black_autoformatter_from_grammar(source_code, mode):
+    try:
+        result = black.format_file_contents(source_code, fast=False, mode=mode)
+    except black.NothingChanged:
+        pass
+    except blib2to3.pgen2.tokenize.TokenError:
+        # Fails to tokenise e.g. "\\", though compile("\\", "<string>", "exec") works.
+        # See https://github.com/psf/black/issues/1012
+        reject()
+    except black.InvalidInput:
+        # e.g. "A#\r#", see https://github.com/psf/black/issues/970
+        reject()
+    else:
+        with pytest.raises(black.NothingChanged):
+            black.format_file_contents(result, fast=False, mode=mode)
+
+
+@given(source_code=hypothesmith.from_grammar("eval_input"))
+def test_eval_input_generation(source_code):
+    compile(source_code, filename="<string>", mode="eval")
+
+
+@given(source_code=hypothesmith.from_grammar(auto_target=False))
+def test_generation_without_targeting(source_code):
+    compile(source_code, filename="<string>", mode="exec")
+
+
+@given(hypothesmith.syntactic.identifiers())
+def test_names_are_all_identifiers(name):
+    assert name.isidentifier()
+    name.encode()  # should be UTF-8 encodable
+
+
+@given(source_code=hypothesmith.from_grammar())
+def test_parso_from_node(source_code):
+    result = parso.parse(source_code).get_code()
+    assert source_code == result
