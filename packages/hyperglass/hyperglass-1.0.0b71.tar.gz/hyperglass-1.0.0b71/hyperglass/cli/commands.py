b7069027526e05ebb5f0d826ef636383e372727c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:24 +0000

    Add package hyperglass/1.0.0b71/hyperglass-1.0.0b71.tar.gz

---
+++ packages/hyperglass/hyperglass-1.0.0b71.tar.gz/hyperglass-1.0.0b71/hyperglass/cli/commands.py
@@ -0,0 +1,259 @@
+"""CLI Command definitions."""
+
+# Standard Library
+import sys
+from getpass import getuser
+from pathlib import Path
+
+# Third Party
+import inquirer
+from click import group, option, confirm, help_option
+
+# Project
+from hyperglass.util import cpu_count
+
+# Local
+from .echo import error, label, success, cmd_help
+from .util import build_ui
+from .static import LABEL, CLI_HELP, E
+from .formatting import HelpColorsGroup, HelpColorsCommand, random_colors
+
+# Define working directory
+WORKING_DIR = Path(__file__).parent
+
+supports_color = "utf" in sys.getfilesystemencoding().lower()
+
+
+def _print_version(ctx, param, value):
+    # Project
+    from hyperglass import __version__
+
+    if not value or ctx.resilient_parsing:
+        return
+    label("hyperglass version: {v}", v=__version__)
+    ctx.exit()
+
+
+@group(
+    cls=HelpColorsGroup,
+    help=CLI_HELP,
+    context_settings={"help_option_names": ["-h", "--help"], "color": supports_color},
+    help_headers_color=LABEL,
+    help_options_custom_colors=random_colors(
+        "build-ui", "start", "secret", "setup", "system-info", "clear-cache"
+    ),
+)
+@option(
+    "-v",
+    "--version",
+    is_flag=True,
+    callback=_print_version,
+    expose_value=False,
+    is_eager=True,
+    help=cmd_help(E.NUMBERS, "hyperglass version", supports_color),
+)
+@help_option(
+    "-h",
+    "--help",
+    help=cmd_help(E.FOLDED_HANDS, "Show this help message", supports_color),
+)
+def hg():
+    """Initialize Click Command Group."""
+    pass
+
+
+@hg.command(
+    "build-ui", help=cmd_help(E.BUTTERFLY, "Create a new UI build", supports_color)
+)
+def build_frontend():
+    """Create a new UI build.
+
+    Raises:
+        click.ClickException: Raised on any errors.
+    """
+    return build_ui()
+
+
+@hg.command(  # noqa: C901
+    "start",
+    help=cmd_help(E.ROCKET, "Start web server", supports_color),
+    cls=HelpColorsCommand,
+    help_options_custom_colors=random_colors("-b", "-d", "-w"),
+)
+@option("-b", "--build", is_flag=True, help="Render theme & build frontend assets")
+@option(
+    "-d",
+    "--direct",
+    is_flag=True,
+    default=False,
+    help="Start hyperglass directly instead of through process manager",
+)
+@option(
+    "-w",
+    "--workers",
+    type=int,
+    required=False,
+    default=0,
+    help=f"Number of workers. By default, calculated from CPU cores [{cpu_count(2)}]",
+)
+def start(build, direct, workers):  # noqa: C901
+    """Start web server and optionally build frontend assets."""
+    try:
+        # Project
+        from hyperglass.api import start as uvicorn_start
+        from hyperglass.main import start
+    except ImportError as e:
+        error("Error importing hyperglass: {}", str(e))
+
+    kwargs = {}
+    if workers != 0:
+        kwargs["workers"] = workers
+
+    try:
+
+        if build:
+            build_complete = build_ui()
+
+            if build_complete and not direct:
+                start(**kwargs)
+            elif build_complete and direct:
+                uvicorn_start(**kwargs)
+
+        if not build and not direct:
+            start(**kwargs)
+        elif not build and direct:
+            uvicorn_start(**kwargs)
+    except KeyboardInterrupt:
+        error("Stopping hyperglass due to keyboard interrupt.")
+    except BaseException as err:
+        error(str(err))
+
+
+@hg.command(
+    "secret",
+    help=cmd_help(E.LOCK, "Generate agent secret", supports_color),
+    cls=HelpColorsCommand,
+    help_options_custom_colors=random_colors("-l"),
+)
+@option(
+    "-l", "--length", "length", default=32, help="Number of characters [default: 32]"
+)
+def generate_secret(length):
+    """Generate secret for hyperglass-agent.
+
+    Arguments:
+        length {int} -- Length of secret
+    """
+    # Standard Library
+    import secrets
+
+    gen_secret = secrets.token_urlsafe(length)
+    label("Secret: {s}", s=gen_secret)
+
+
+@hg.command(
+    "setup",
+    help=cmd_help(E.TOOLBOX, "Run the setup wizard", supports_color),
+    cls=HelpColorsCommand,
+    help_options_custom_colors=random_colors("-d"),
+)
+@option(
+    "-d",
+    "--use-defaults",
+    "unattended",
+    default=False,
+    is_flag=True,
+    help="Use hyperglass defaults (requires no input)",
+)
+def setup(unattended):
+    """Define application directory, move example files, generate systemd service."""
+    # Project
+    from hyperglass.cli.util import (
+        create_dir,
+        make_systemd,
+        write_to_file,
+        install_systemd,
+        migrate_static_assets,
+    )
+
+    user_path = Path.home() / "hyperglass"
+    root_path = Path("/etc/hyperglass/")
+
+    install_paths = [
+        inquirer.List(
+            "install_path",
+            message="Choose a directory for hyperglass",
+            choices=[user_path, root_path],
+        )
+    ]
+    if not unattended:
+        answer = inquirer.prompt(install_paths)
+        if answer is None:
+            error("A directory for hyperglass is required")
+        install_path = answer["install_path"]
+
+    elif unattended:
+        install_path = user_path
+
+    ui_dir = install_path / "static" / "ui"
+    images_dir = install_path / "static" / "images"
+    favicon_dir = images_dir / "favicons"
+    custom_dir = install_path / "static" / "custom"
+
+    create_dir(install_path)
+
+    for path in (ui_dir, images_dir, custom_dir, favicon_dir):
+        create_dir(path, parents=True)
+
+    migrate_static_assets(install_path)
+
+    if install_path == user_path:
+        user = getuser()
+    else:
+        user = "root"
+
+    do_systemd = True
+    if not unattended and not confirm(
+        "Do you want to generate a systemd service file?"
+    ):
+        do_systemd = False
+
+    if do_systemd:
+        systemd_file = install_path / "hyperglass.service"
+        systemd = make_systemd(user)
+        write_to_file(systemd_file, systemd)
+        install_systemd(install_path)
+
+    build_ui()
+
+
+@hg.command(
+    "system-info",
+    help=cmd_help(
+        E.THERMOMETER, "  Get system information for a bug report", supports_color
+    ),
+    cls=HelpColorsCommand,
+)
+def get_system_info():
+    """Get CPU, Memory, Disk, Python, & hyperglass version."""
+    # Project
+    from hyperglass.cli.util import system_info
+
+    system_info()
+
+
+@hg.command(
+    "clear-cache",
+    help=cmd_help(E.SOAP, "Clear the Redis cache", supports_color),
+    cls=HelpColorsCommand,
+)
+def clear_cache():
+    """Clear the Redis Cache."""
+    # Project
+    from hyperglass.util import sync_clear_redis_cache
+
+    try:
+        sync_clear_redis_cache()
+        success("Cleared Redis Cache")
+    except RuntimeError as err:
+        error(str(err))
---
+++ packages/hyperglass/hyperglass-1.0.0b71.tar.gz/hyperglass-1.0.0b71/hyperglass/log.py
@@ -0,0 +1,167 @@
+"""Logging instance setup & configuration."""
+
+# Standard Library
+import os
+import logging
+from datetime import datetime
+
+# Third Party
+from loguru import logger as _loguru_logger
+from rich.theme import Theme
+from rich.console import Console
+from rich.logging import RichHandler
+
+_FMT_FILE = (
+    "<lvl><b>[{level}]</b> {time:YYYYMMDD} {time:HH:mm:ss} <lw>|</lw> {name}<lw>:</lw>"
+    "<b>{line}</b> <lw>|</lw> {function}</lvl> <lvl><b>→</b></lvl> {message}"
+)
+_DATE_FMT = "%Y%m%d %H:%M:%S"
+_FMT_STDOUT = "{message}"
+_FMT_BASIC = "{message}"
+_LOG_LEVELS = [
+    {"name": "TRACE", "no": 5, "color": "<m>"},
+    {"name": "DEBUG", "no": 10, "color": "<c>"},
+    {"name": "INFO", "no": 20, "color": "<le>"},
+    {"name": "SUCCESS", "no": 25, "color": "<g>"},
+    {"name": "WARNING", "no": 30, "color": "<y>"},
+    {"name": "ERROR", "no": 40, "color": "<y>"},
+    {"name": "CRITICAL", "no": 50, "color": "<r>"},
+]
+
+_RICH_THEME = Theme(
+    {
+        "logging.level.debug": "bold grey50",
+        "logging.level.info": "bold blue",
+        "logging.level.success": "bold green",
+        "logging.level.warning": "bold yellow",
+        "logging.level.error": "bold dark_goldenrod",
+        "logging.level.critical": "bold red",
+    }
+)
+
+_RICH_CONSOLE = Console(theme=_RICH_THEME, log_time_format=_DATE_FMT)
+
+
+def _get_rich(debug: bool = False) -> RichHandler:
+    rich_kwargs = {
+        "level": "INFO",
+        "markup": True,
+        "rich_tracebacks": True,
+        "console": _RICH_CONSOLE,
+    }
+    if debug:
+        rich_kwargs["level"] = "DEBUG"
+    return RichHandler(**rich_kwargs)
+
+
+def setup_lib_logging(debug: bool = False) -> None:
+    """Override the logging handlers for dependency libraries."""
+    for name in (
+        "gunicorn",
+        "gunicorn.access",
+        "gunicorn.error",
+        "uvicorn",
+        "uvicorn.access",
+        "uvicorn.error",
+        "uvicorn.asgi",
+        "netmiko",
+        "scrapli",
+        "httpx",
+    ):
+        logging.getLogger(name).handlers = [_get_rich(debug)]
+
+
+def base_logger():
+    """Initialize hyperglass logging instance."""
+    _loguru_logger.remove()
+    _loguru_logger.add(_get_rich(), format=_FMT_BASIC, level="INFO", enqueue=True)
+    _loguru_logger.configure(levels=_LOG_LEVELS)
+    return _loguru_logger
+
+
+log = base_logger()
+
+logging.addLevelName(25, "SUCCESS")
+
+
+def _log_success(self, message, *a, **kw):
+    """Add custom builtin logging handler for the success level."""
+    if self.isEnabledFor(25):
+        self._log(25, message, a, **kw)
+
+
+logging.Logger.success = _log_success
+
+
+def set_log_level(logger, debug):
+    """Set log level based on debug state."""
+    if debug:
+        os.environ["HYPERGLASS_LOG_LEVEL"] = "DEBUG"
+        logger.remove()
+        logger.add(_get_rich(True), format=_FMT_BASIC, level="DEBUG", enqueue=True)
+        logger.configure(levels=_LOG_LEVELS)
+
+    if debug:
+        logger.debug("Debugging enabled")
+    return True
+
+
+def enable_file_logging(logger, log_directory, log_format, log_max_size):
+    """Set up file-based logging from configuration parameters."""
+
+    if log_format == "json":
+        log_file_name = "hyperglass.log.json"
+        structured = True
+    else:
+        log_file_name = "hyperglass.log"
+        structured = False
+
+    log_file = log_directory / log_file_name
+
+    if log_format == "text":
+        now_str = "hyperglass logs for " + datetime.utcnow().strftime(
+            "%B %d, %Y beginning at %H:%M:%S UTC"
+        )
+        now_str_y = len(now_str) + 6
+        now_str_x = len(now_str) + 4
+        log_break = (
+            "#" * now_str_y,
+            "\n#" + " " * now_str_x + "#\n",
+            "#  ",
+            now_str,
+            "  #",
+            "\n#" + " " * now_str_x + "#\n",
+            "#" * now_str_y,
+        )
+
+        with log_file.open("a+") as lf:
+            lf.write(f'\n\n{"".join(log_break)}\n\n')
+
+    logger.add(
+        log_file,
+        format=_FMT_FILE,
+        rotation=log_max_size,
+        serialize=structured,
+        enqueue=True,
+    )
+
+    logger.debug("Logging to {} enabled", str(log_file))
+
+    return True
+
+
+def enable_syslog_logging(logger, syslog_host, syslog_port):
+    """Set up syslog logging from configuration parameters."""
+
+    # Standard Library
+    from logging.handlers import SysLogHandler
+
+    logger.add(
+        SysLogHandler(address=(str(syslog_host), syslog_port)),
+        format=_FMT_BASIC,
+        enqueue=True,
+    )
+    logger.debug(
+        "Logging to syslog target {}:{} enabled", str(syslog_host), str(syslog_port),
+    )
+    return True
