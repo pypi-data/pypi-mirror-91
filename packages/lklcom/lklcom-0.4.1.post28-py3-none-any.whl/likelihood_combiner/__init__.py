2a8277f2bfd058cc236ff3e0823d6a88a2f29f5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:26 +0000

    Add package lklcom/0.4.1.post28/lklcom-0.4.1.post28-py3-none-any.whl

---
+++ packages/lklcom/lklcom-0.4.1.post28-py3-none-any.whl/likelihood_combiner/__init__.py
@@ -0,0 +1,10 @@
+from .combiner import *
+from .io import *
+from .jfactor import *
+from .plotter import *
+from .reader import *
+from .sensitivity import *
+from .utils import *
+
+from .version import get_version
+__version__ = get_version(pep440=False)
