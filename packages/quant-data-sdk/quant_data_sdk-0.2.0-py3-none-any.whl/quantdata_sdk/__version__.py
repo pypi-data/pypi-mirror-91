64826529ad4844e8faefafd3f9e3afa5b9f6d9ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:29 +0000

    Add package quant-data-sdk/0.2.0/quant_data_sdk-0.2.0-py3-none-any.whl

---
+++ packages/quant-data-sdk/quant_data_sdk-0.2.0-py3-none-any.whl/quantdata_sdk/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the quantumdata-sdk package.
+"""
+
+__title__ = 'quant-data-sdk'
+__description__ = 'quant-data-sdk - sdk for quant data project'
+__version__ = '0.2.0'
+__author__ = 'Rafał Niewiński'
+__author_email__ = 'rafalniewinski95@gmail.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://gitlab.com/rafalniewinski95/quantdata_sdk'
