5265fe944d09ce93e5463a358c90bb2654e3c12b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:28 +0000

    Add package aoaoao/0.0.1/aoaoao-0.0.1.tar.gz

---
+++ packages/aoaoao/aoaoao-0.0.1.tar.gz/aoaoao-0.0.1/setup.py
@@ -0,0 +1,23 @@
+# -*-coding:utf-8-*-
+# Author: yunhao
+# Github: https://github.com/yunhao233
+# CreatDate: 2021/1/10 16:51
+# Description:
+import setuptools
+
+setuptools.setup(
+    name="aoaoao",
+    version="0.0.1",
+    author="ken",
+    author_email="kenbliky@gmail.com",
+    description="Image kernel.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/kenblikylee/imgkernel",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
