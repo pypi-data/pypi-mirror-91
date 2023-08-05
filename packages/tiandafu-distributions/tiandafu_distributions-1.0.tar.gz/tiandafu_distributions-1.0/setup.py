ad3400907ceb67a617e8e9bebbca00ad253c50aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package tiandafu-distributions/1.0/tiandafu_distributions-1.0.tar.gz

---
+++ packages/tiandafu-distributions/tiandafu_distributions-1.0.tar.gz/tiandafu_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tiandafu_distributions',
+      version='1.0',
+      description='Gaussian distributions',
+      packages=['tiandafu_distributions'],
+      author = 'Dafu Tian',
+      author_email = 'weiyiwdeng@gmail.com',
+      zip_safe=False)
