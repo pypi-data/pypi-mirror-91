47285b50ce8475ff411af263b5dd5c05e1c6f554
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:00 +0000

    Add package distr-lib/0.1/distr_lib-0.1.tar.gz

---
+++ packages/distr-lib/distr_lib-0.1.tar.gz/distr_lib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distr_lib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions'],
+      author = 'Alexandru Gradinaru',
+      author_email = 'gradinaru.alex@gmail.com',
+      zip_safe=False)
