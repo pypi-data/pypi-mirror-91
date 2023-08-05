f97a3b68ac5d1244e29193e9f0166524a68ce785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:51 +0000

    Add package dcdevaluation/0.7.4/dcdevaluation-0.7.4.tar.gz

---
+++ packages/dcdevaluation/dcdevaluation-0.7.4.tar.gz/dcdevaluation-0.7.4/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(name     = 'dcdevaluation',
+      version  = '0.7.4'            ,
+      packages = ['dcdevaluation']  ,
+      zip_safe = False              )
