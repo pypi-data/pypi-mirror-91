9065a41e774159cf8da330365fa5ccb557625f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:44 +0000

    Add package vbnigmm/2.4.1/vbnigmm-2.4.1-py3-none-any.whl

---
+++ packages/vbnigmm/vbnigmm-2.4.1-py3-none-any.whl/vbnigmm/distributions/dirichlet.py
@@ -0,0 +1,34 @@
+from .. import backend as tk
+
+from .base import Dist
+from ..linalg.vector import Vector, wrap_vector
+
+
+class Dirichlet(Dist, Vector):
+
+    def __init__(self, alpha, dtype=None):
+        self.alpha = tk.as_array(alpha, dtype)
+
+    @property
+    def dtype(self):
+        return self.alpha.dtype
+
+    @property
+    def sum(self):
+        return tk.sum(self.alpha, axis=-1)
+
+    @property
+    def mean(self):
+        return self.alpha / self.sum[..., None]
+
+    @property
+    def mean_log(self):
+        return tk.digamma(self.alpha) - tk.digamma(self.sum)[..., None]
+
+    def log_pdf(self, x, condition=None):
+        x = wrap_vector(x, self.dtype)
+        return (
+            tk.lgamma(self.sum)
+            - tk.sum(tk.lgamma(self.alpha), axis=-1)
+            + tk.sum((self.alpha - 1) * x.mean_log, axis=-1)
+        )
