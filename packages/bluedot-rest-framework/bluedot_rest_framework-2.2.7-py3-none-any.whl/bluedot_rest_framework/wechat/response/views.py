422b5e0eaabee913a18eadc84c16e49bd0ed7658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:51 +0000

    Add package bluedot-rest-framework/2.2.7/bluedot_rest_framework-2.2.7-py3-none-any.whl

---
+++ packages/bluedot-rest-framework/bluedot_rest_framework-2.2.7-py3-none-any.whl/bluedot_rest_framework/wechat/response/views.py
@@ -0,0 +1,29 @@
+import hashlib
+from bluedot_rest_framework.utils.viewsets import CustomModelViewSet, AllView
+from bluedot_rest_framework.wechat import App
+from .models import WeChatResponseMaterial, WeChatResponseEvent
+from .serializers import WeChatResponseMaterialSerializer, WeChatResponseEventSerializer
+
+
+class WeChatResponseMaterialView(CustomModelViewSet, AllView):
+    model_class = WeChatResponseMaterial
+    serializer_class = WeChatResponseMaterialSerializer
+
+
+class WeChatResponseEventView(CustomModelViewSet):
+    model_class = WeChatResponseEvent
+    serializer_class = WeChatResponseEventSerializer
+
+    def perform_create(self, serializer):
+        if self.request.data.get('material_type') == 2:
+            event_key = hashlib.md5(b'123').hexdigest()[8:-8]
+            result = App(self.request.data.get('appid')).qrcode.create({
+                'action_name': 'QR_LIMIT_STR_SCENE',
+                'action_info': {
+                    'scene': {'scene_str': event_key},
+                }
+            })
+            qrcode_ticket = result['ticket']
+            serializer.save(event_key=event_key, qrcode_ticket=qrcode_ticket)
+        else:
+            serializer.save()
