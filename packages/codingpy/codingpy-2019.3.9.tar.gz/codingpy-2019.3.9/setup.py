e126e6012956555d55a4c637ac7f80d0fe1787dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:22 +0000

    Add package codingpy/2019.3.9/codingpy-2019.3.9.tar.gz

---
+++ packages/codingpy/codingpy-2019.3.9.tar.gz/codingpy-2019.3.9/setup.py
@@ -0,0 +1,38 @@
+# -*-coding:utf-8-*-
+# Author: yunhao
+# Github: https://github.com/yunhao233
+# CreatDate: 2021/1/9 16:01
+# Description:
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='codingpy',  # 包名
+      version='2019.03.09',  # 版本号
+      description='',
+      long_description='',
+      author='',
+      author_email='',
+      url='',
+      license='',
+      install_requires=[],
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.5',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Topic :: Utilities'
+      ],
+      keywords='',
+      packages=find_packages('src'),  # 必填，就是包的代码主目录
+      package_dir={'': 'src'},  # 必填
+      include_package_data=True,
+      )
