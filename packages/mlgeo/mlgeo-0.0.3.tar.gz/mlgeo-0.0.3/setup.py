b693827bee668e522dd6cc56eeb0c81be43d56ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:34 +0000

    Add package mlgeo/0.0.3/mlgeo-0.0.3.tar.gz

---
+++ packages/mlgeo/mlgeo-0.0.3.tar.gz/mlgeo-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+import mlgeo
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mlgeo",
+    version=mlgeo.__version__,
+    author="Nick Machairas",
+    author_email="nick@groundwork.ai",
+    description="Repository for Machine Learning in Geotechnics",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/groundworkai/mlgeo",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy>=1.19'
+    ]
+)
