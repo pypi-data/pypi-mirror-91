07ae493a5642df354ddd585514bf60e8b904f28c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:35 +0000

    Add package appcli/0.10.1/appcli-0.10.1.tar.gz

---
+++ packages/appcli/appcli-0.10.1.tar.gz/appcli-0.10.1/appcli/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+An object-oriented framework for command-line apps.
+"""
+
+__version__ = '0.10.1'
+
+from .app import App, FromParams
+from .model import init, load, reload
+from .params.param import param, Key
+from .params.toggle import toggle_param, pick_toggled, Toggle as toggle
+from .params.inherited import inherited_param
+from .configs.configs import *
+from .configs.layers import Layer, not_found
+from .configs.attrs import config_attr
+from .errors import *
+from .utils import lookup
