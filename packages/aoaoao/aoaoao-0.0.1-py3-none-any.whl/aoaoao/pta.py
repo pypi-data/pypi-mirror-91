280077e04ff0fae5f463c9abe102ad1c884b2c35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:28 +0000

    Add package aoaoao/0.0.1/aoaoao-0.0.1-py3-none-any.whl

---
+++ packages/aoaoao/aoaoao-0.0.1-py3-none-any.whl/aoaoao/pta.py
@@ -0,0 +1,10 @@
+# -*-coding:utf-8-*-
+# Author: yunhao
+# Github: https://github.com/yunhao233
+# CreatDate: 2021/1/10 16:48
+# Description:
+def hello():
+    print("hello")
+
+def add():
+    return "hello world!"
