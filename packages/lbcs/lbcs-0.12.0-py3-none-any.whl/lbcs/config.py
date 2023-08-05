60866f744faac610c89dc3d4050801bda8eb9ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:11 +0000

    Add package lbcs/0.12.0/lbcs-0.12.0-py3-none-any.whl

---
+++ packages/lbcs/lbcs-0.12.0-py3-none-any.whl/lbcs/config.py
@@ -0,0 +1,31 @@
+from configparser import ConfigParser
+from dataclasses import asdict, dataclass, fields
+
+
+@dataclass
+class LBCSConfig:
+    port: str
+    rows: int
+    columns: int
+    debug: bool
+    pixel_pin: str
+    map_algo: str
+    skip: int
+
+    def normalized(self):
+        return self.__class__(
+            **{f.name: f.type(getattr(self, f.name)) for f in fields(self)}
+        )
+
+    def pretty(self):
+        cfg_ = asdict(self)
+        return "\n".join(
+            f"{k}".ljust(max(len(x) for x in cfg_)) + ": " + f"{v}"
+            for k, v in cfg_.items()
+        )
+
+
+def get(fname: str) -> LBCSConfig:
+    cfg = ConfigParser()
+    cfg.read(fname)
+    return LBCSConfig(**dict(cfg["app"])).normalized()
