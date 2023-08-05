a9d547017fb8d3e903b74ab5e9f0d927e7185597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:44 +0000

    Add package treefrog/0.3.0/treefrog-0.3.0-py3-none-any.whl

---
+++ packages/treefrog/treefrog-0.3.0-py3-none-any.whl/treefrog/organize/__init__.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from typing import Sequence
+
+from slippi.game import Game
+
+from ..parse import Parser
+from ..parse.parsers import matchup, month, stage, year
+from ..parse.utils import ParseError
+
+Ordering = Sequence[Parser]
+
+default_ordering = (
+    year,
+    month,
+    matchup,  # TODO: Add player information
+    stage
+)
+
+
+def build_parent(game: Game, ordering: Ordering = default_ordering) -> Path:
+    try:
+        parent = Path()
+
+        for parser in ordering:
+            parent /= str(parser(game))
+
+        return parent
+    except ParseError:
+        return Path("Error")
