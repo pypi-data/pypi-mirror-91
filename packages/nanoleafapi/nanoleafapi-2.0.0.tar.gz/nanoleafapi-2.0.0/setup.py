b5370f28fd9091cb95c3908b46f2606785d500bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:04 +0000

    Add package nanoleafapi/2.0.0/nanoleafapi-2.0.0.tar.gz

---
+++ packages/nanoleafapi/nanoleafapi-2.0.0.tar.gz/nanoleafapi-2.0.0/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nanoleafapi",
+    version="2.0.0",
+    author="MylesMor",
+    author_email="hello@mylesmor.dev",
+    description="A Python 3 wrapper for the Nanoleaf OpenAPI, " +
+                 "for use when controlling the Light Panels, Canvas and Shapes (including Hexagons)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MylesMor/nanoleafapi",
+    packages=setuptools.find_packages(),
+    install_requires=['requests', 'sseclient'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
