647d6ee30eb81e572cfa1ba84ac6e88268d379e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:18 +0000

    Add package photomatrix/0.0.5/photomatrix-0.0.5.tar.gz

---
+++ packages/photomatrix/photomatrix-0.0.5.tar.gz/photomatrix-0.0.5/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="photomatrix",
+    version="0.0.5",
+    author="Francesco Feltrinelli",
+    description="Concat photos together in a matrix",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ffeltrinelli/photomatrix",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha"
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        'importlib-metadata ~= 1.0 ; python_version < "3.8"',
+        'pillow>=7',
+        'fonts',
+        'font-source-sans-pro'
+    ],
+    entry_points={
+        "console_scripts": [
+            "photomatrix = photomatrix.__main__:main",
+        ]
+    }
+)
