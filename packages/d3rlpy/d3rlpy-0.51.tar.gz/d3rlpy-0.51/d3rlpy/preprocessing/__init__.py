da417841c7d2d3a508fa932c228c07f29aa4f515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:07 +0000

    Add package d3rlpy/0.51/d3rlpy-0.51.tar.gz

---
+++ packages/d3rlpy/d3rlpy-0.51.tar.gz/d3rlpy-0.51/d3rlpy/preprocessing/__init__.py
@@ -0,0 +1,13 @@
+from .scalers import create_scaler
+from .scalers import Scaler
+from .scalers import PixelScaler
+from .scalers import MinMaxScaler
+from .scalers import StandardScaler
+
+__all__ = [
+    "create_scaler",
+    "Scaler",
+    "PixelScaler",
+    "MinMaxScaler",
+    "StandardScaler",
+]
