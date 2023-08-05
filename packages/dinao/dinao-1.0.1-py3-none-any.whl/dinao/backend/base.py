7bcff5a69ebe1cb6e4d3dddab1a9fafcb3e2d329
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:36 +0000

    Add package dinao/1.0.1/dinao-1.0.1-py3-none-any.whl

---
+++ packages/dinao/dinao-1.0.1-py3-none-any.whl/dinao/backend/base.py
@@ -0,0 +1,203 @@
+"""Defines a basic and primitive database interface. It is basically a thin wrapper on DB API 2.0."""
+
+import logging
+from abc import ABC, abstractmethod
+from contextlib import contextmanager
+from dataclasses import dataclass
+from typing import List, Tuple
+from urllib.parse import parse_qs, urlparse
+
+from dinao.backend.errors import ConfigurationError
+
+
+@dataclass
+class ColumnDescriptor:
+    """Describes a column in a result set."""
+
+    name: str
+    type_code: int
+    display_size: int = None
+    internal_size: int = None
+    precision: int = None
+    scale: int = None
+    null_ok: bool = None
+
+
+class ResultSet:
+    """Basic interface definition for result sets (a.k.a rows) returned from Database queries."""
+
+    def __init__(self, cursor):
+        """Construct a result set.
+
+        :param cursor: the underlying DB API 2.0 cursor being wrapped by this object.
+        """
+        self._cursor = cursor
+        self._columns = None
+        self._description = None
+
+    def fetchone(self) -> Tuple:
+        """Fetch one result tuple from the underlying cursor.
+
+        If no results are left, None is returned.
+
+        :returns: a tuple representing a result row or None
+        """
+        return self._cursor.fetchone()
+
+    def fetchall(self) -> List[Tuple]:
+        """Fetch the *remaining* result tuples from the underlying cursor.
+
+        If no results are left, an empty list is returned.
+
+        :returns: a list of tuples that are the remaining results of the underlying cursor.
+        """
+        return self._cursor.fetchall()
+
+    @property
+    def description(self) -> Tuple[ColumnDescriptor]:
+        """Return a sequence of column descriptions representing the result set.
+
+        :returns: a tuple of ColumnDescriptors
+        """
+        if not self._description:
+            self._description = tuple([ColumnDescriptor(*d) for d in self._cursor.description])
+        return self._description
+
+    @property
+    def rowcount(self) -> int:
+        """Return the row count of the result set.
+
+        :returns: the integer count of the rows in the result set
+        """
+        return self._cursor.rowcount
+
+
+class Connection(ABC):
+    """Basic interface definition for a database connection."""
+
+    def __init__(self, cnx, auto_commit: bool = True):
+        """Construct a Connection object.
+
+        :param cnx: the inner DB API 2.0 connection this object wraps
+        :param auto_commit: should calls to execute() be automatically committed, defaults to True
+        """
+        self.logger = logging.getLogger(__name__)
+        self._cnx = cnx
+        self._auto_commit = auto_commit
+
+    @property
+    def autocommit(self):
+        """Whether or not commit is called after every call to query(...) and execute(...)."""
+        return self._auto_commit
+
+    @autocommit.setter
+    def autocommit(self, value: bool):
+        self._auto_commit = value
+
+    def commit(self):
+        """Commit changes for this connection / transaction to the database."""
+        self._cnx.commit()
+
+    def rollback(self):
+        """Rollback changes for this connection / transaction to the database."""
+        self._cnx.rollback()
+
+    @abstractmethod
+    def _execute(self, cursor, sql: str, params: tuple = None):
+        pass  # pragma: no cover
+
+    @contextmanager
+    def query(self, sql: str, params: tuple = None) -> ResultSet:
+        """Execute the given SQL as a statement with the given parameters. Provide the results as context.
+
+        :param sql: the SQL statement(s) to execute
+        :param params: the values to bind to the execution of the given SQL
+        :returns: a result set representing the query's results
+        """
+        cursor = self._cnx.cursor()
+        self._execute(cursor, sql, params)
+        try:
+            yield ResultSet(cursor)
+        finally:
+            cursor.close()
+
+    def execute(self, sql: str, params: tuple = None, commit: bool = None) -> int:
+        """Execute the given SQL as a statement with the given parameters and return the affected row count.
+
+        :param sql: the SQL statement(s) to execute
+        :param params: the values to bind to the execution of the given SQL
+        :param commit: commit the changes to the database after execution, defaults to value given in constructor
+        """
+        commit = commit if commit is not None else self._auto_commit
+        cursor = self._cnx.cursor()
+        self._execute(cursor, sql, params)
+        affected = cursor.rowcount
+        if commit:
+            self.commit()
+        cursor.close()
+        return affected
+
+
+class ConnectionPool(ABC):
+    """Basic interface definition for a pool of database connections."""
+
+    def __init__(self, db_url: str):
+        """Construct a connection pool for the given connection URL.
+
+        The db_url is expected to be in the following format::
+
+            "{dialect}+{driver}://{username}:{password}@{hostname}:{port}/{db_name}?{optional_args}"
+
+        :param db_url: a url with the described format
+        """
+        self.logger = logging.getLogger(__name__)
+        self._raw_db_url = db_url
+        self._db_url = urlparse(self._raw_db_url)
+        self._args = parse_qs(self._db_url.query)
+
+    @staticmethod
+    def _strict_bool(value: str):
+        if value.lower() not in ["true", "false"]:
+            raise ValueError(f"Cannot cast '{value}' to bool")
+        return value.lower() == "true"
+
+    def _raise_for_unexpected_args(self):
+        unexpected = ",".join(self._args.keys())
+        if unexpected:
+            raise ConfigurationError(f"Unexpected argument(s): {unexpected}")
+
+    def _get_arg(self, name: str, expected_type, default=None):
+        if name not in self._args:
+            self.logger.debug(f"No '{name}' specified, defaulting to {default}")
+            return default
+        caster = expected_type if expected_type is not bool else self._strict_bool
+        try:
+            if caster != list:
+                assert len(self._args.get(name)) == 1
+                return caster(self._args.pop(name)[0])
+            return self._args.pop(name)
+        except AssertionError as x:
+            raise ConfigurationError(f"Invalid argument '{name}': only a single value must be specified") from x
+        except ValueError as x:
+            raise ConfigurationError(f"Invalid argument '{name}': must be {expected_type.__name__}") from x
+
+    @property
+    @abstractmethod
+    def mung_symbol(self) -> str:
+        """Return the symbol used when replacing variable specifiers in templated SQL."""
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def lease(self) -> Connection:
+        """Lease a connection from the underlying pool."""
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def release(self, cnx: Connection):
+        """Release a connection back to the underlying pool."""
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def dispose(self):
+        """Close the pool and clean up any resources it was using."""
+        pass  # pragma: no cover
---
+++ packages/dinao/dinao-1.0.1-py3-none-any.whl/tests/binding/template_cases.py
@@ -0,0 +1,97 @@
+"""Test cases for templating functionality."""
+
+from dataclasses import dataclass
+
+
+@dataclass
+class MyNestedArg:
+    """Dummy class for testing argument rendering."""
+
+    another_property: str
+
+
+@dataclass
+class MyArg:
+    """Dummy class for testing argument rendering."""
+
+    my_arg_property: MyNestedArg
+
+
+MUNG_SYMBOL = "%s"
+EMPLOYEE = {
+    "first_name": "Tom",
+    "last_name": "Smith",
+    "username": "tsmith",
+    "email": "tsmith@email",
+    "department_id": 589,
+}
+
+# The ordering of these tuples is:
+# - Template init arguments,
+# - Template's expected arguments property,
+# - Keyword arguments to call with template.render(...)
+# - Expected return values from template.render(...)
+GOOD_CASES = (
+    (  # Valid but kind of dumb, we're not validating their SQL with the template obviously
+        ("  #{arg1} #{arg2} #{arg3}  ",),
+        (("arg1",), ("arg2",), ("arg3",)),
+        {"arg1": 1, "arg2": "test", "arg3": 2.0},
+        ("  %s %s %s  ", (1, "test", 2.0)),
+    ),
+    (  # Basic test case
+        ("INSERT INTO my_table (#{my_arg.my_arg_property.another_property}, #{my_other_arg})",),
+        (("my_arg", "my_arg_property", "another_property"), ("my_other_arg",)),
+        {"my_arg": MyArg(MyNestedArg("test")), "my_other_arg": 5.4},
+        ("INSERT INTO my_table (%s, %s)", ("test", 5.4)),
+    ),
+    (  # Repetitive replacement
+        (
+            "INSERT INTO employees VALUES "
+            "  (#{e.first_name}, #{e.last_name}, #{e.username}, #{e.department_id}, #{e.email}) "
+            "ON CONFLICT (username) DO "
+            "UPDATE SET "
+            "  email = #{e.email} "
+            "  first_name = #{e.first_name} "
+            "  last_name = #{e.last_name} "
+            "  department_id = #{e.department_id} ",
+        ),
+        (
+            ("e", "first_name"),
+            ("e", "last_name"),
+            ("e", "username"),
+            ("e", "department_id"),
+            ("e", "email"),
+            ("e", "email"),
+            ("e", "first_name"),
+            ("e", "last_name"),
+            ("e", "department_id"),
+        ),
+        {"e": EMPLOYEE},
+        (
+            "INSERT INTO employees VALUES "
+            "  (%s, %s, %s, %s, %s) "
+            "ON CONFLICT (username) DO "
+            "UPDATE SET "
+            "  email = %s "
+            "  first_name = %s "
+            "  last_name = %s "
+            "  department_id = %s ",
+            ("Tom", "Smith", "tsmith", 589, "tsmith@email", "tsmith@email", "Tom", "Smith", 589),
+        ),
+    ),
+    (  # Test case for not mangling complex strings
+        (
+            "SELECT * FROM table_name WHERE"
+            "  some_column = #{argument.member.sub_member} AND "
+            '  json_column @> \'{"some": [{"nested": [{"json_in_sql": "#{another_argument}"}]}]}\' ',
+        ),
+        (("argument", "member", "sub_member"), ("another_argument",)),
+        {"argument": {"member": {"sub_member": "test"}}, "another_argument": "something"},
+        (
+            "SELECT * FROM table_name WHERE"
+            "  some_column = %s AND "
+            '  json_column @> \'{"some": [{"nested": [{"json_in_sql": "%s"}]}]}\' ',
+            ("test", "something"),
+        ),
+    ),
+)
