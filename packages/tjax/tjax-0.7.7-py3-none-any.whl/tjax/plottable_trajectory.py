1fea7c59c917a8badf461dc97fbc2420beb66487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package tjax/0.7.7/tjax-0.7.7-py3-none-any.whl

---
+++ packages/tjax/tjax-0.7.7-py3-none-any.whl/tjax/plottable_trajectory.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from typing import Generic, Optional, Sequence, TypeVar, Union
+
+import numpy as np
+from chex import Array
+from jax.tree_util import tree_map
+from matplotlib.axes import Axes
+
+from .annotations import PyTree
+from .dataclass import dataclass
+from .leaky_integral import leaky_integrate_time_series
+
+__all__ = ['PlottableTrajectory']
+
+
+Trajectory = TypeVar('Trajectory', bound=PyTree)
+
+
+@dataclass
+class PlottableTrajectory(Generic[Trajectory]):
+    trajectory: Trajectory
+    "The trajectory is PyTree containing the plotted data in its nonstatic attributes."
+    iterations: int
+    "The number of data points in each of the plotted attributes."
+    time_step: float
+    "The period between adjacent data points."
+
+    def plot(self,
+             data: Array,
+             axis: Axes,
+             subplot_title: str,
+             *,
+             legend: int = 0,
+             labels: Optional[Sequence[str]] = None,
+             decay: Optional[float] = None,
+             clip_slice: slice = slice(None)) -> None:
+        """
+        Plot the PlottableTrajectory into a matplotlib axis.
+        Args:
+            data: The data to be plotted.  Typically, this is a sub-element of trajectory.
+            axis: A matplotlib axis to plot into.
+            subplot_title: The title of the subplot.
+            legend: The maximum number of entries for which to generate a legend.
+            decay: If not none, applies a leaky-integral with the given decay to the plotted points.
+            clip_slice: Restrict the plot to a slice.
+        """
+        axis.set_title(subplot_title)
+        axis.ticklabel_format(useOffset=False)
+
+        xs = np.linspace(0.0, self.iterations * self.time_step, self.iterations, endpoint=False)[
+            clip_slice]
+        all_ys = np.asarray(data)
+        if not np.all(np.isfinite(all_ys)):
+            print(f"Graph {subplot_title} contains infinite numbers.")
+
+        plot_indices = list(np.ndindex(all_ys.shape[1:]))
+        if labels is None:
+            labels = [str(x) for x in plot_indices]
+        for plot_index, label in zip(plot_indices, labels):
+            ys = all_ys[(clip_slice, *plot_index)]
+            if decay is not None:
+                ys = leaky_integrate_time_series(ys, decay)
+            axis.plot(xs, ys, label=label)
+        number_of_graph_lines = np.prod(data.shape[1:])
+        if labels is not None or legend > 0 and 0 < number_of_graph_lines <= legend:
+            axis.legend()
+
+    def slice_into(self, s: Union[int, slice]) -> Trajectory:
+        "Return a new trajectory whose nonstatic attributed are sliced."
+        return tree_map(lambda x: x[s], self.trajectory)
