011883690dd2367d5f355b6bc67a42c3fe9a07f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:03 +0000

    Add package dltkai/1.1.1/dltkai-1.1.1.tar.gz

---
+++ packages/dltkai/dltkai-1.1.1.tar.gz/dltkai-1.1.1/dltkai/__init__.py
@@ -0,0 +1,12 @@
+"""A library that provides a Python interface to the DLTK APIs."""
+
+__author__ = 'DLTK'
+__email__ = 'connect@qubitai.tech'
+__copyright__ = 'Copyright (c) 2019-2020 Qubit AI technologies'
+__version__ = '1.0.0'
+__url__ = 'https://github.com/dltk-ai/dltkai-sdk'
+__download_url__ = ''
+__description__ = 'A Python wrapper around the DLTK API'
+
+from .nlp import NaturalLanguage
+from .cv import ComputerVision
---
+++ packages/dltkai/dltkai-1.1.1.tar.gz/dltkai-1.1.1/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="dltkai",
+    version="1.1.1",
+    author="DLTK",
+    author_email="connect@qubitai.tech",
+    description="Python Client for DLTK.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dltk-ai/dltkai-sdk",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'Keras',
+        'tensorflow==2.2.0',
+        'imageai==2.1.5',
+        'nltk==3.4.5',
+        'spacy==2.2.4',
+        'numpy==1.18.1',
+        'scikit-learn==0.22.1',
+        'scipy==1.4.1',
+        'seaborn==0.10.0',
+        'matplotlib==3.1.3',
+        'gensim==3.8.1',
+        'beautifulsoup4==4.8.2',
+        'rake-nltk==1.0.4',
+        'imutils==0.5.3',
+        'opencv-python==4.5.1.48',
+        'pandas==1.0.0',
+        'streamlit==0.64.0'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
