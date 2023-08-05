2cc0489be4129743aeadb7aa787d0ffe524897a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:03 +0000

    Add package wxyz-html/0.4.2/wxyz_html-0.4.2.tar.gz

---
+++ packages/wxyz-html/wxyz_html-0.4.2.tar.gz/wxyz_html-0.4.2/src/wxyz/html/__init__.py
@@ -0,0 +1,18 @@
+""" Widgets for core HTML 5 controls
+"""
+from ._version import __version__  # noqa
+from .widget_color import AlphaColor, AlphaColorPicker, EmptyAlphaColor
+from .widget_file import File, FileBox, JSONFile, TextFile
+from .widget_fullscreen import Fullscreen
+
+__all__ = [
+    "__version__",
+    "AlphaColor",
+    "AlphaColorPicker",
+    "EmptyAlphaColor",
+    "File",
+    "FileBox",
+    "Fullscreen",
+    "JSONFile",
+    "TextFile",
+]
