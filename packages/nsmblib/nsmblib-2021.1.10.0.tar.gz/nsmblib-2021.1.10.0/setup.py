21a2d58a89e1a8981bf3ab00cea6335e0f962d89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:25 +0000

    Add package nsmblib/2021.1.10.0/nsmblib-2021.1.10.0.tar.gz

---
+++ packages/nsmblib/nsmblib-2021.1.10.0.tar.gz/nsmblib-2021.1.10.0/setup.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+from setuptools import setup, Extension
+
+extra_compile_args = ['-DPy_LIMITED_API']
+
+# Workaround for what looks like a weird Python bug?
+if os.name != 'nt':
+    extra_compile_args.append('-Wno-implicit-function-declaration')
+
+setup(
+  name='nsmblib',
+  version='2021.01.10.0',
+  ext_modules=[
+    Extension(
+      'nsmblib',
+      sources=['nsmblibmodule.c', 'list.c'],
+      extra_compile_args=extra_compile_args,
+      py_limited_api=True,
+    )
+  ]
+)
