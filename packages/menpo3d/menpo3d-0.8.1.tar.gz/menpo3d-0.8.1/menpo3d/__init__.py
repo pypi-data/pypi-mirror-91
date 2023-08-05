64c794b407bdda8f23fa387158c40845795a7552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:54 +0000

    Add package menpo3d/0.8.1/menpo3d-0.8.1.tar.gz

---
+++ packages/menpo3d/menpo3d-0.8.1.tar.gz/menpo3d-0.8.1/menpo3d/__init__.py
@@ -0,0 +1,12 @@
+from . import barycentric
+from . import correspond
+from . import io
+from . import rasterize
+from . import unwrap
+from . import visualize
+from . import vtkutils
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
