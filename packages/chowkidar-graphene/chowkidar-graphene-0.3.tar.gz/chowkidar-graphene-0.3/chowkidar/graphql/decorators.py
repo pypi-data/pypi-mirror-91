1fc77747327603811397c8f3a36745529e165b2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:30 +0000

    Add package chowkidar-graphene/0.3/chowkidar-graphene-0.3.tar.gz

---
+++ packages/chowkidar-graphene/chowkidar-graphene-0.3.tar.gz/chowkidar-graphene-0.3/chowkidar/graphql/decorators.py
@@ -0,0 +1,36 @@
+from django.contrib.auth import get_user_model
+from ..utils import exceptions
+
+User = get_user_model()
+
+
+def login_required(resolver):
+    def wrapper(parent, info, *args, **kwargs):
+        userID = getattr(info.context, "userID", None)
+        if userID:
+            info.context.userID = userID
+            resolved = resolver(parent, info, *args, **kwargs)
+            return resolved
+        raise exceptions.PermissionDenied(message='User not authenticated', code='AUTHENTICATION_REQUIRED')
+    return wrapper
+
+
+def resolve_user(resolver):
+    def wrapper(parent, info, *args, **kwargs):
+        userID = getattr(info.context, "userID", None)
+        if userID:
+            try:
+                info.context.user = User.objects.get(id=userID)
+                info.context.userID = userID
+                resolved = resolver(parent, info, *args, **kwargs)
+                return resolved
+            except User.DoesNotExist:
+                pass
+        raise exceptions.PermissionDenied(message='User not authenticated', code='AUTHENTICATION_REQUIRED')
+    return wrapper
+
+
+__all__ = [
+    'login_required',
+    'resolve_user',
+]
