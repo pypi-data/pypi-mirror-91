3b819f4e323f6afb762503350218bf03f086f8ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:47 +0000

    Add package wagtailcolumnblocks/2.2.0/wagtailcolumnblocks-2.2.0.tar.gz

---
+++ packages/wagtailcolumnblocks/wagtailcolumnblocks-2.2.0.tar.gz/wagtailcolumnblocks-2.2.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name='wagtailcolumnblocks',
+    description='Wagtail Column Blocks',
+    use_scm_version=True,
+    author='Squareweave',
+    author_email='hosting+pypi@squareweave.com.au',
+    url='https://github.com/squareweave/wagtailcolumnblocks',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    license='BSD',
+    classifiers=[
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Framework :: Django :: 1.11',
+        'Topic :: Internet :: WWW/HTTP :: Site Management',
+    ],
+
+    packages=['wagtailcolumnblocks'],
+    include_package_data=True,
+
+    install_requires=[
+        'wagtail >= 2.0',
+    ],
+    setup_requires=[
+        'setuptools_scm',
+    ],
+)
