61e91d2c2579f501593d21fc8a62353a654415fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:37 +0000

    Add package LootBotApi/0.5.2/LootBotApi-0.5.2.tar.gz

---
+++ packages/LootBotApi/LootBotApi-0.5.2.tar.gz/LootBotApi-0.5.2/LootBotApi/LootBotApi.py
@@ -0,0 +1,202 @@
+import requests
+from munch import munchify
+import math
+from copy import deepcopy
+
+class Error500(Exception):
+    pass
+
+
+class LootBotApi:
+    def __init__(self,token):
+        self.token = token
+        self.endpoint = f"http://fenixweb.net:3300/api/v2/{self.token}"
+        self.items = self.get_items()
+        self.craft_needed = dict()
+
+    def __request_url(self,url):
+        response_json = requests.get(url).json()
+        response_code = response_json["code"]
+        if response_code != 200:
+            raise Error500(response_json["error"])
+
+        return munchify(response_json["res"])
+
+    def __lower_dict_keys(self,dict):
+        result = {}
+        for key, value in dict.items():
+            try:
+                result[key.lower()] = value
+            except AttributeError:
+                result[key] = value
+        return result
+
+    def get_items(self,rarity = None):
+        if rarity != None:
+            return self.get_item(rarity)
+
+        return self.__request_url(f"{self.endpoint}/items")
+
+    def get_item(self,item):
+        item_return = list(filter(lambda item_search: item_search.name == item, self.items))
+        if item_return == []:
+            item_return = list(filter(lambda item_search: item_search.rarity == item, self.items))
+            if item_return == []:
+                item_return = self.__request_url(f"{self.endpoint}/items/{item}")
+        return item_return
+
+    def get_exact_item(self,item):
+        item_api = self.get_item(item)
+        if type(item_api) is list:
+            for sub_item in item_api:
+                if sub_item.name == item:
+                    return sub_item
+        else:
+            return item_api
+
+    def get_history(self,place = "payments",limit = None,offset = None,fromPlayer = None,toPlayer = None,fromItem = None,toItem = None,both = None,fromPrice = None,toPrice = None,orderBy = "desc"):
+        string = f"{self.endpoint}/history/{place}?"
+        arguments = locals()
+        """
+        The paramater to cannot be called from because it's a python key and so we changed it to "toPlayer" but we have to change it back to "to" for the request
+        This process can probably be done in a better way.
+        """
+        arguments["to"] = arguments.pop("toPlayer")
+        arguments["from"] = arguments.pop("fromPlayer")
+
+        for argument in arguments:
+            if argument in ["self","string"]:
+                continue
+            else:
+                if arguments[argument] != None:
+                    string += f"{argument}={arguments[argument]}&"
+
+
+        string = string[:-1]
+        return self.__request_url(string)
+
+    def get_players(self):
+        return self.__request_url(f"{self.endpoint}/players")
+
+    def get_player(self,player):
+        return self.__request_url(f"{self.endpoint}/players/{player}")[0]
+
+    def get_team(self,team):
+        return self.__request_url(f"{self.endpoint}/team/{team}")
+
+    def get_searches(self,quantity):
+        return self.__request_url(f"{self.endpoint}/search/{quantity}")
+
+    def get_cards(self):
+        return self.__request_url(f"{self.endpoint}/cards")
+
+    def get_info(self):
+        return self.__request_url(f"{self.endpoint}/info")
+
+    def get_global(self):
+        return self.__request_url(f"{self.endpoint}/global")
+
+    def get_shop(self,shop):
+        return self.__request_url(f"{self.endpoint}/shop/{shop}")
+
+    def get_craft_needed(self,item_id):
+        try:
+            craft_needed = self.craft_needed[item_id]
+        except KeyError:
+            craft_needed = self.__request_url(f"{self.endpoint}/crafts/{item_id}/needed")
+            self.craft_needed[item_id] = craft_needed
+
+        return craft_needed
+
+    def get_craft_needed_base(self,item_id):
+        craft_needed = self.get_craft_needed(item_id)
+        return list(filter(lambda item : not item.craftable,craft_needed))
+
+    def get_craft_used(self,item_id):
+        return self.__request_url(f"{self.endpoint}/crafts/{item_id}/used")
+
+    def get_crafts(self):
+        return self.__request_url(f"{self.endpoint}/crafts/id")
+
+    def get_total_craft_points(self,item):
+        item = self.get_exact_item(item)
+        return item.craft_pnt
+
+    def get_average_market_price(self,item):
+        item = self.get_exact_item(item)
+
+        item_name = item.name
+        item_base_price = item.value
+        prezzi = self.get_history(place="market_direct",fromItem=item_name,fromPrice=item_base_price+1)
+        return int(sum(prezzo.price for prezzo in prezzi) / len(prezzi))
+
+    def get_crafting_steps(self,item,num_elements=1,inventory=dict()):
+        def get_crafting(elemento,num_elements,inventory):
+            elements_needed = self.get_craft_needed(elemento)
+            element_name = self.get_exact_item(elemento).name
+            try:
+                item_quantity = inventory[element_name]
+                inventory[element_name] -= 1
+            except KeyError:
+                item_quantity = 0
+
+            if item_quantity >= num_elements:
+                steps_list = []
+            else:
+                steps_list = [element_name]
+
+            for element_needed in elements_needed:
+                if element_needed.craftable: steps_list.extend(get_crafting(element_needed.id,num_elements,inventory))
+            return steps_list
+
+        element = self.get_exact_item(item).id
+        craft_list = get_crafting(element,num_elements,deepcopy(inventory))
+        craft_list.reverse()
+
+        results = []
+        #A dict that contains each element in the list and how many times it's repeated
+        counter = {x:craft_list.count(x) for x in craft_list}
+
+        for craft in counter:
+            try:
+                item_quantity = inventory[craft]
+            except KeyError:
+                item_quantity = 0
+
+            num_elements_tmp = num_elements * counter[craft] - item_quantity
+
+            for i in range(math.ceil(num_elements_tmp / 3)):
+                if num_elements_tmp <= 3:
+                    num_steps = num_elements_tmp
+                else:
+                    num_steps = 3
+                    num_elements_tmp -= 3
+
+                dict_craft = {craft:num_steps}
+                results.append(dict_craft)
+
+        return results
+
+    def get_craft_total_needed_base_items(self,item,num_elements=1,inventory=dict()):
+        def get_crafting(elemento):
+            elements_needed = self.get_craft_needed(elemento)
+            base_elements = [base.name for base in self.get_craft_needed_base(elemento)]
+
+            for element_needed in elements_needed:
+                if element_needed.craftable:
+                     base_elements.extend(get_crafting(element_needed.id))
+            return base_elements
+
+        craft_list = get_crafting(self.get_exact_item(item).id)
+        craft_list = {x:craft_list.count(x) for x in craft_list}
+        delete_keys = list()
+        for craft in craft_list:
+            craft_list[craft] *= num_elements
+            try:
+                craft_list[craft] -= inventory[craft]
+                if craft_list[craft] <= 0: delete_keys.append(craft)
+            except KeyError:
+                continue
+
+        for delete in delete_keys:
+            del craft_list[delete]
---
+++ packages/LootBotApi/LootBotApi-0.5.2.tar.gz/LootBotApi-0.5.2/README.md
@@ -0,0 +1,137 @@
+# LootBotApi
+This is an API wrapper for Loot Bot (https://telegra.ph/Guida-alle-LootBot-API-04-06)
+
+# Installation
+## Download from PyPi
+1. `pip install LootBotApi`
+2. `from LootBotApi import LootBotApi`
+3. `api = LootBotApi(TOKEN)` --> the token can be found using the command /token on https://t.me/lootplusbot
+4. Call the methods from the object created above
+
+
+# Methods
+## Api Wrapper
+Most of this methods will return a list of Munch objects.
+This means that the attributes of the result can either be accessed as dictionary keys or object attributes
+### Items
+* `get_items(rarity = None)` It returns all the items in the game, if rarity is passed it returns all the items of that rarity
+* `get_item(item)` It returns the info about a given item
+``` python
+print(api.get_item("Meccanismo di Ferro").id) #363
+print(api.get_item(363).name) #Meccanismo di Ferro
+```
+### History
+* `get_history(place = "payments",limit = None,offset = None,fromPlayer = None,toPlayer = None,fromItem = None,toItem = None,both = None,fromPrice = None,toPrice = None,orderBy = "desc")` It returns the transactions with the specificied parameters
+
+### Players
+* `get_players()` It returns all the players of LootBot
+* `get_player(player)` It returns the info about the player
+
+### Crafting
+* `get_crafts()` It returns all the crafts in the game
+* `get_craft_needed(item_id)` It returns the items requested to craft the item
+* `get_craft_used(item_id)` It returns the item that you can craft using the item
+``` python
+items = api.get_craft_needed(363)
+for item in items:
+  print(item.name)
+
+#Perno
+#Meccanismo di Legno
+#Ferro
+```
+
+### Shops
+* `get_shop(shop)` It returns the infos about the shop
+
+### Cards
+* `get_cards()` It returns all the cards in the game
+
+### Global
+* `get_global()` It returns the progress of the current global challenge, it's updated every hour
+* `get_info()` It returns infos about the current global challenge
+
+### Team
+* `get_team(team)` it returns the infos about a team
+
+### Ricerche
+* `get_searches(quantity)` It returns the searches made on the bot
+
+## Custom Methods
+These methods are not natively implemented in the API but they are derived by them.
+
+### Crafting
+#### Meaning of inventory
+The inventory paramater excepts a dict structured like this:
+``` python
+{
+  item1: quantity,
+  item2: quantity,
+  .,
+  .,
+  .,
+  itemN: quantity
+}
+```
+
+* `get_total_craft_points(item)` It returns the total craft points that you will get to craft the item
+``` python
+ print(api.get_total_craft_points("Ordigno Polverizzatore")) #113
+```
+* `get_crafting_steps(item,num_elements=1,inventory=dict())` It returns as a list of dicts all the steps to craft a certain element (and in a certain quantity). If the inventory is passed the items already owned will not be listed.
+``` python
+def print_steps(steps):
+  for step in steps:
+      for elemento in step:
+          print(f"Crea {elemento},{step[elemento]}")
+
+steps = api.get_crafting_steps("Scudo Punta Doppia",5)
+print_steps(steps)
+"""
+Crea Scudo Punta Singola,3
+Crea Scudo Punta Singola,2
+Crea Scudo Punta Doppia,3
+Crea Scudo Punta Doppia,2
+"""
+
+inventory = {"Scudo Punta Singola":2}
+steps = api.get_crafting_steps("Scudo Punta Doppia",5,inventory=inventory)
+print_steps(steps)
+"""
+Crea Scudo Punta Singola,3
+Crea Scudo Punta Doppia,3
+Crea Scudo Punta Doppia,2
+"""
+```
+
+* `get_craft_total_needed_base_items(item,num_elements=1,inventory=dict())` It returns a dict containing the name of all the base items needed to craft a certain element (and in a certain quantity). If the inventory is passed the items already owned will not be listed.
+``` python
+print(api.get_craft_total_needed_base_items("Scudo Punta Tripla",2))
+#{'Ambra Nera': 2, 'Materiale Affilante': 2, 'Manico': 2, 'Scaglia di Rubino': 2, 'Metallo': 2, 'Scheggia': 2, 'Nastro Adesivo': 2}
+```
+
+* `get_craft_needed_base(item_id)`  It returns a list of base elements needed to craft the item
+``` python
+print(api.get_craft_needed_base(api.get_exact_item("Scudo Punta Tripla").id))
+#[Munch({'id': 80, 'name': 'Ambra Nera', 'rarity': 'UR', 'craftable': 0}), Munch({'id': 127, 'name': 'Materiale Affilante', 'rarity': 'UR', 'craftable': 0})]
+```
+
+### Market
+* `get_average_market_price(item)` It returns the average price of an item in the market as an integer
+``` python
+ print(api.get_average_market_price("Ferro")) #37394
+```
+
+### Items
+* `get_exact_item(item)` It returns the exact item searched
+``` python
+print(api.get_element("Carta"))
+"""
+[Munch({'id': 1, 'name': 'Carta', 'rarity': 'C', 'rarity_name': 'Comuni', 'value': 410, 'max_value': 205000, 'estimate': 2000, 'spread': 46, 'spread_tot': 0.116, 'craftable': 0, 'reborn': 1, 'power': 0, 'power_armor': 0, 'power_shield': 0, 'dragon_power': 0, 'critical': 0, 'craft_pnt': 0, 'cons_val': 0}), Munch({'id': 8, 'name': 'Carta Stropicciata', 'rarity': 'NC', 'rarity_name': 'Non Comuni', 'value': 810, 'max_value': 405000, 'estimate': 1237, 'spread': 44, 'spread_tot': 0.084, 'craftable': 0, 'reborn': 1, 'power': 0, 'power_armor': 0, 'power_shield': 0, 'dragon_power': 0, 'critical': 0, 'craft_pnt': 0, 'cons_val': 0}), Munch({'id': 35, 'name': 'Aereo di Carta Piccolo', 'rarity': 'NC', 'rarity_name': 'Non Comuni', 'value': 1121, 'max_value': 1121000, 'estimate': 11750, 'spread': 30, 'spread_tot': 0.001, 'craftable': 1, 'reborn': 1, 'power': 0, 'power_armor': 0, 'power_shield': 0, 'dragon_power': 0, 'critical': 0, 'craft_pnt': 0, 'cons_val': 0.1}), Munch({'id': 36, 'name': 'Aereo di Carta Grande', 'rarity': 'R', 'rarity_name': 'Rari', 'value': 1697, 'max_value': 1697000, 'estimate': 13045, 'spread': 28, 'spread_tot': 0.001, 'craftable': 1, 'reborn': 1, 'power': 0, 'power_armor': 0, 'power_shield': 0, 'dragon_power': 0, 'critical': 0, 'craft_pnt': 1, 'cons_val': 0.15}), Munch({'id': 37, 'name': 'Caccia di Carta', 'rarity': 'UR', 'rarity_name': 'Ultra Rari', 'value': 3468, 'max_value': 3468000, 'estimate': 4650, 'spread': 25, 'spread_tot': 0, 'craftable': 1, 'reborn': 1, 'power': 0, 'power_armor': 0, 'power_shield': 0, 'dragon_power': 0, 'critical': 0, 'craft_pnt': 3, 'cons_val': 0.25})]
+"""
+
+print(api.get_exact_item("Carta"))
+"""
+Munch({'id': 1, 'name': 'Carta', 'rarity': 'C', 'rarity_name': 'Comuni', 'value': 410, 'max_value': 205000, 'estimate': 2000, 'spread': 46, 'spread_tot': 0.116, 'craftable': 0, 'reborn': 1, 'power': 0, 'power_armor': 0, 'power_shield': 0, 'dragon_power': 0, 'critical': 0, 'craft_pnt': 0, 'cons_val': 0})
+"""
+```
