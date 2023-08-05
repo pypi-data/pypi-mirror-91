eac31b41011a34d34ce5ccfe0f5b54401801f693
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:13 +0000

    Add package Miscoto/3.1.0/Miscoto-3.1.0.tar.gz

---
+++ packages/Miscoto/Miscoto-3.1.0.tar.gz/Miscoto-3.1.0/miscoto/__init__.py
@@ -0,0 +1,8 @@
+from miscoto.miscoto_instance import run_instance
+from miscoto.miscoto_mincom import run_mincom
+from miscoto.miscoto_scopes import run_scopes
+
+
+__version__="3.1.0"
+"""Define the version of the package.
+"""
