ea7a2bc3ba196509fd95323724bc3cec9f8499c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:27 +0000

    Add package pylint-django/2.4.2/pylint-django-2.4.2.tar.gz

---
+++ packages/pylint-django/pylint-django-2.4.2.tar.gz/pylint-django-2.4.2/SECURITY.md
@@ -0,0 +1,18 @@
+# Security Policy
+
+## Supported Versions
+
+| Version | Supported          |
+| ------- | ------------------ |
+| [latest](https://pypi.org/project/pylint-django/) | :heavy_check_mark: |
+
+## Reporting a Vulnerability
+
+In case you have found a security problem with pylint-django *DO NOT* report
+it into GitHub Issues. Instead go to
+[https://tidelift.com/security](https://tidelift.com/security)
+and follow the instructions there.
+
+At least one of the package maintainers ([@atodorov](http://github.com/atodorov))
+is a lifter at Tidelift and will be notified when you report the security
+problem with them!
---
+++ packages/pylint-django/pylint-django-2.4.2.tar.gz/pylint-django-2.4.2/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: UTF-8 -*-
+"""
+Setup module for Pylint plugin for Django.
+"""
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open('README.rst').read() + "\n" + open('CHANGELOG.rst').read()
+
+setup(
+    name='pylint-django',
+    url='https://github.com/PyCQA/pylint-django',
+    author='landscape.io',
+    author_email='code@landscape.io',
+    description='A Pylint plugin to help Pylint understand the Django web framework',
+    long_description=LONG_DESCRIPTION,
+    version='2.4.2',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'pylint-plugin-utils>=0.5',
+        'pylint>=2.0',
+    ],
+    extras_require={
+        'with_django': ['Django'],
+        'for_tests': ['django_tables2', 'factory-boy', 'coverage', 'pytest', 'Faker<=5.0.1'],
+    },
+    license='GPLv2',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Operating System :: Unix',
+        'Topic :: Software Development :: Quality Assurance',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords=['pylint', 'django', 'plugin'],
+    zip_safe=False,
+    project_urls={
+        'Changelog': 'https://github.com/PyCQA/pylint-django/blob/master/CHANGELOG.rst',
+    },
+)
