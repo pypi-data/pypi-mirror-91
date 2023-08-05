45e4389102a2c0837538ccf05e780f025a3cc9be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:06 +0000

    Add package python-lic-sys/0.0.2/python_lic_sys-0.0.2.tar.gz

---
+++ packages/python-lic-sys/python_lic_sys-0.0.2.tar.gz/python_lic_sys-0.0.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_lic_sys", # Replace with your own username
+    version="0.0.2",
+    author="TechSmit",
+    author_email="techsmitdevloper@gmail.com",
+    description="A Easy Softwear Licensing",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/PythonProgramme/Python-Code",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
