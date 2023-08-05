e3359929f45f6bff51d7ea9f72dd388d458e19ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:26 +0000

    Add package py-cra/0.0.6/py_cra-0.0.6.tar.gz

---
+++ packages/py-cra/py_cra-0.0.6.tar.gz/py_cra-0.0.6/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_desc = (here / 'README.md').read_text(encoding='utf-8')
+
+
+setup(
+        name='py_cra',
+        version='0.0.6',
+        description='Python tool to create an react web app for django or flask backend',
+        long_description=long_desc,
+        long_description_content_type='text/markdown',
+        url='https://github.com/Moaz-Mohammed-Elesawey/py-cra',
+        author='Moaz Mohammed El-Esawey',
+        author_email='mohammedmiaz3141@gmail.com',
+        classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+        ],
+        keywords='react, frontend, python, pycra',
+        package_dir={'': 'src'},
+        packages=find_packages(where='src'),
+        python_requires='>=3.6, <4',
+        project_urls={
+        'Source': 'https://github.com/Moaz-Mohammed-Elesawey/py-cra',
+    },
+)
