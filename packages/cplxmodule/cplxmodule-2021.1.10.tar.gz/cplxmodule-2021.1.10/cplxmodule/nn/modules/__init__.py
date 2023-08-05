67308e13dcf8c76fc6a878c1393614296e80753d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:01 +0000

    Add package cplxmodule/2021.1.10/cplxmodule-2021.1.10.tar.gz

---
+++ packages/cplxmodule/cplxmodule-2021.1.10.tar.gz/cplxmodule-2021.1.10/cplxmodule/nn/modules/__init__.py
@@ -0,0 +1,20 @@
+from .base import CplxToCplx, CplxParameter
+
+from .casting import AsTypeCplx
+from .casting import CplxToInterleavedReal as CplxToReal
+from .casting import InterleavedRealToCplx as RealToCplx
+
+from .linear import CplxLinear, CplxBilinear
+from .linear import CplxReal, CplxImag, CplxIdentity
+
+from .conv import CplxConv1d, CplxConv2d, CplxConv3d
+from .conv import CplxConvTranspose1d, CplxConvTranspose2d, CplxConvTranspose3d
+
+from .container import CplxSequential
+
+from .activation import CplxModReLU, CplxAdaptiveModReLU
+from .activation import CplxModulus, CplxAngle
+
+from .batchnorm import CplxBatchNorm1d, CplxBatchNorm2d, CplxBatchNorm3d
+
+from .extra import CplxDropout
