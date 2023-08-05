027c13c7338a752f1bc2e905628fb93e9dfed32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:04 +0000

    Add package wxyz-tpl-jinja/0.4.2/wxyz_tpl_jinja-0.4.2.tar.gz

---
+++ packages/wxyz-tpl-jinja/wxyz_tpl_jinja-0.4.2.tar.gz/wxyz_tpl_jinja-0.4.2/src/wxyz/tpl_jinja/__init__.py
@@ -0,0 +1,7 @@
+""" Isomorphic widget for rendering jinja2 templates
+"""
+
+from ._version import __version__  # noqa
+from .widget_template import Template
+
+__all__ = ["__version__", "Template"]
