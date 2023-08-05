a1bb324e38193bc1c820fbfef40b98e968866408
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:56 +0000

    Add package nimrodel/0.6.5/nimrodel-0.6.5-py3-none-any.whl

---
+++ packages/nimrodel/nimrodel-0.6.5-py3-none-any.whl/nimrodel/__init__.py
@@ -0,0 +1,33 @@
+name = "nimrodel"
+version = 0,6,5
+desc = "Bottle-wrapper to make python objects accessible via HTTP API"
+versionstr = ".".join(str(n) for n in version)
+author = {
+	"name": "Johannes Krattenmacher",
+	"email": "python@krateng.dev",
+	"github": "krateng"
+}
+requires = [
+	"bottle",
+	"waitress",
+	"doreah>=1.2.1",
+	"parse"
+]
+resources = [
+	"res/*"
+]
+
+
+
+from ._apiobject import API as ObjectAPI
+from ._apifunction import API as EndpointAPI
+from ._apirest import API as RestAPI
+from ._misc import Multi
+
+# short names
+OAPI = ObjectAPI
+EAPI = EndpointAPI
+RAPI = RestAPI
+
+# default
+API = OAPI
