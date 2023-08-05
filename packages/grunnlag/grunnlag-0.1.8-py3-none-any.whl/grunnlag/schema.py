1f9b2a28f08c3a74e768dd642a02c0e8c3f533b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:14 +0000

    Add package grunnlag/0.1.8/grunnlag-0.1.8-py3-none-any.whl

---
+++ packages/grunnlag/grunnlag-0.1.8-py3-none-any.whl/grunnlag/schema.py
@@ -0,0 +1,28 @@
+from grunnlag.gql import CREATE_REPRESENTATION, GET_REPRESENTATION, UPDATE_REPRESENTATION, FILTER_REPRESENTATION
+from grunnlag.managers import RepresentationManager
+from typing import List, Optional
+from bergen.schema import ArnheimModel, User
+from grunnlag.extenders import Array, RepresentationPrettifier
+
+class Representation(RepresentationPrettifier, Array, ArnheimModel):
+    id: Optional[int]
+    name: Optional[str]
+    package: Optional[str]
+    store: Optional[str]
+    shape: Optional[List[int]]
+    image: Optional[str]
+    creator: Optional[User]
+    unique: Optional[str]
+
+    objects = RepresentationManager()
+
+    class Meta:
+        identifier = "representation"
+        get = GET_REPRESENTATION
+        create = CREATE_REPRESENTATION
+        update = UPDATE_REPRESENTATION
+        filter = FILTER_REPRESENTATION
+
+
+
+
