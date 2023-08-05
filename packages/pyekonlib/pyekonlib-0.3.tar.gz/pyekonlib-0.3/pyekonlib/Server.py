17c75e423af3ece9904919d6fbf3bebc721190c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:57 +0000

    Add package pyekonlib/0.3/pyekonlib-0.3.tar.gz

---
+++ packages/pyekonlib/pyekonlib-0.3.tar.gz/pyekonlib-0.3/pyekonlib/Server.py
@@ -0,0 +1,139 @@
+from pyekonlib.Controllers import ServerController
+import socket
+import logging
+import asyncio
+_LOGGER = logging.getLogger(__name__)
+
+class UDPServer(object):
+    def __init__(self, bindingPort, serverId,
+                 onHvacConnected,
+                 onHvacTimeout,
+                 onDeviceChangeCallback,
+                 callLaterFn,
+                 createAsyncTaskFromThreadFn,
+                 createAsyncTaskFromEventLoopFn,
+                 forward_endpoint = None):
+        _LOGGER.debug("Creating UDPServer")
+        self._addressPair = ("0.0.0.0", bindingPort)
+        self._serverId = serverId
+        self._onDeviceChangeCallback = onDeviceChangeCallback
+        self._callLaterFn = callLaterFn
+        self._createAsyncTaskFromThreadFn = createAsyncTaskFromThreadFn
+        self._onHvacConnected = onHvacConnected
+        self._onHvacTimeout = onHvacTimeout
+        self._started = False
+        self._stopRequest = False
+        self._serverController = ServerController( createAsyncTaskFromEventLoopFn , callLaterFn)
+        self._serverController.onReceivedDeviceKey = self.receivedDeviceKey
+        self._serverController.onDeviceData = self.deviceData
+        self._serverController.onDeviceTimeout = self.deviceTimeout
+        self._serverController.sendData = self.sendData
+
+        self._forward_endpoint = forward_endpoint
+
+        # TODO: Change this when supporting multiple devices
+        self._peer = ("0.0.0.0", 1)
+
+        self._dev_transport = self._dev_protocol = None
+        self._srv_transport = self._srv_protocol = None
+
+    class EkonProtocolFactory:
+        def __init__(self, _sync_data_recived_fn):
+            self._sync_data_recived_fn = _sync_data_recived_fn
+            self._transport = None
+
+        def connection_made(self, transport):
+            self._transport = transport
+
+        def datagram_received(self, data, addr):
+            self._sync_data_recived_fn(data, addr)
+
+    #class EkonDeviceProtocolFactory:
+    #    def __init__(self, _sync_data_recived_fn):
+
+    async def start(self):
+        _LOGGER.info("Starting UDP Server reciver task and periodicTimeoutCheck")
+        if self._started:
+            return
+        self._started = True
+        # await self._createAsyncTaskFn(self.reciverTask())
+
+        # Get a reference to the event loop as we plan to use
+        # low-level APIs.
+        loop = asyncio.get_running_loop()
+
+        # One protocol instance will be created to serve all
+        # Behind the scene this is actually implemented as a thread, so calls to asyncio functions
+        # Should be considered from a thread context
+        # client requests.
+        self._dev_transport, self._dev_protocol = await loop.create_datagram_endpoint(
+            lambda: UDPServer.EkonProtocolFactory( self.syncHandleRecivedDataFromDevice ),
+            local_addr=self._addressPair)
+
+        if self._forward_endpoint:
+            loop = asyncio.get_running_loop()
+            self._srv_transport, self._srv_protocol = await loop.create_datagram_endpoint(
+                lambda: UDPServer.EkonProtocolFactory( self.syncHandleRecivedDataFromServer ),
+                remote_addr=self._forward_endpoint)
+
+
+        await self._serverController.startPeriodicTimeoutCheck()
+
+    async def stop(self):
+        _LOGGER.info("Stopping UDP Server reciver task and periodicTimeoutCheck")
+        #self._stopRequest = True
+        self._dev_transport.close()
+        if self._forward_endpoint:
+            self._srv_transport.close()
+        await self._serverController.stopPeriodicTimeoutCheck()
+
+    # This is run in the thread created by the ProtocolFactory
+    def syncHandleRecivedDataFromDevice(self, data, addr):
+        self._peer = addr
+        # Magic of sync->async
+        self._createAsyncTaskFromThreadFn(self._serverController.processData(data))
+        if self._forward_endpoint:
+            # Send data to forwarding server
+            self._srv_transport.sendto(data, self._forward_endpoint)
+
+    # This is run in the thread created by the ProtocolFactory
+    def syncHandleRecivedDataFromServer(self, data, addr):
+        self._dev_transport.sendto(data, self._peer)
+
+    async def receivedDeviceKey(self, srvController, deviceSession):
+        _LOGGER.debug("UDPServer - receivedDeviceKey")
+        # For this use case, only receiving the device key / id doesnt really matter,
+        # Session was already setup by the controller
+        pass
+
+    async def deviceData(self, srvController, deviceSession, airconState):
+        _LOGGER.debug("UDPServer - Got data from device")
+        if deviceSession.firstState:
+            deviceSession.firstState = False
+            # For more simpler interfacing, I define connection as established only after I have the
+            # 1st state of the hvac
+            await self._onHvacConnected(deviceSession, airconState)
+
+        await self._onDeviceChangeCallback(deviceSession, airconState)
+        # TODO: This doesn't work, Do I even need it? i want for client to be able to check freshness of data
+        """elif deviceSession.lastState!=airconState:
+            print("UDPServer device data " + airconState.toString())
+            await self._onDeviceChangeCallback(deviceSession, airconState)
+        else:
+            print ("NoChange.")"""
+
+    async def deviceTimeout(self, srvController, deviceSession):
+        _LOGGER.debug("UDPServer - device timeout")
+        await self._onHvacTimeout(deviceSession)
+
+    async def sendData(self, data):
+        _LOGGER.debug("UDPServer - Sending data to device")
+        # self._sock.sendto(data, self._peer)
+        self._dev_transport.sendto(data, self._peer)
+
+    async def sendNewState(self, state):
+        await self._serverController.updateDeviceState(self._serverController.getCurrentSession(), state)
+
+    async def turnOff(self):
+        await self._serverController.turnOff(self._serverController.getCurrentSession())
+
---
+++ packages/pyekonlib/pyekonlib-0.3.tar.gz/pyekonlib-0.3/test/test_udpserver.py
@@ -0,0 +1,73 @@
+# For homeassistant, impoart this package via github using manifest-requirements, can link to github tag
+# https://developers.home-assistant.io/docs/creating_integration_manifest/#custom-requirements-during-development--testing
+from _ast import Set
+
+from pyekonlib import *
+from pyekonlib.Server import UDPServer
+from pyekonlib.Migration import SetDeviceUDPServer
+from pyekonlib.Frames import *
+import asyncio
+import hexdump
+
+devConnected = False
+
+
+async def hvacStatusRecived(deviceSession, state):
+    print("HVAC State")
+    print(state.toString())
+
+def myAIOCreateTask(task):
+    loop = asyncio.get_event_loop()
+    asyncio_task = loop.create_task(task)
+
+
+async def hvacConnected(deviceSession, firstState):
+    global devConnected
+    print("Device connected! " + hexdump.dump(deviceSession.device.deviceData)  + " state: ")
+    print(firstState.toString())
+    devConnected = True
+
+async def hvacTimeout(deviceSession):
+    print("Disconnected")
+    devConnected = False
+
+# remember that call later accept sync cb
+def callLater(time, func):
+    event_loop = asyncio.get_event_loop()
+    event_loop.call_later(time, func)
+
+
+import socket
+async def aio_main():
+    ekonServer = UDPServer(6343, 0x9C400008,
+                           hvacConnected,
+                           hvacTimeout,
+                           hvacStatusRecived,
+                           callLater,
+                           myAIOCreateTask, # createAsyncTaskFromThreadFn
+                           asyncio.create_task, # createAsyncTaskFromEventLoopFn,
+                           ("3.137.73.173", 6343))
+    newStateScenario = AirconStateData(onoff=True, mode=AirconMode.Fan, targetTemp=220, currentTemp=220, fanSpeed=1)
+    await ekonServer.start()
+
+    print("Turn off frame")
+    hexdump.hexdump( ServerTurnOnOffFrame(False).toBytes() )
+    print("Turn on frame")
+    hexdump.hexdump( ServerTurnOnOffFrame(True).toBytes() )
+    try:
+        SetDeviceUDPServer("192.168.1.20", "192.168.1.10", 6343)
+    except socket.error as e:
+        print(e)
+
+    await asyncio.sleep(5)
+    print("==========aio_main scenerio===========")
+    #await ekonServer.sendNewState(newStateScenario)
+    await asyncio.sleep(5)
+    #print("Turning off")
+    #await ekonServer.turnOff()
+    #newStateScenario.fanSpeed = 1
+    #await ekonServer.sendNewState(newStateScenario)
+    await asyncio.sleep(7200)
+
+
+asyncio.run(aio_main())
