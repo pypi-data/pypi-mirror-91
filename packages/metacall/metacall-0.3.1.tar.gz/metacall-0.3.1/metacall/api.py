fc151463b5a4a18ccd22f16b6194d622fc127b9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:51 +0000

    Add package metacall/0.3.1/metacall-0.3.1.tar.gz

---
+++ packages/metacall/metacall-0.3.1.tar.gz/metacall-0.3.1/metacall/api.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+
+#	MetaCall Python Port by Parra Studios
+#	A frontend for Python language bindings in MetaCall.
+#
+#	Copyright (C) 2016 - 2020 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+#
+#	Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
+
+import os
+import sys
+import json
+
+if sys.platform == 'win32':
+	from metacall.module_win32 import metacall_module_load
+elif sys.platform == 'linux':
+	from metacall.module_linux import metacall_module_load
+elif sys.platform == 'darwin' or sys.platform == 'cygwin':
+	print('\x1b[31m\x1b[1m', 'The platform', sys.platform, 'has not been not tested, but we are using linux module as a fallback.', '\x1b[0m')
+	# TODO: Probably it won't work, but we use it as a fallback, implement other platforms
+	from metacall.module_linux import metacall_module_load
+else:
+	raise ImportError('MetaCall Python Port is not implemented under this platform.')
+
+# Load metacall extension depending on the platform
+module = metacall_module_load()
+
+# Check if library was found and print error message otherwhise
+if module == None:
+	print('\x1b[31m\x1b[1m', 'You do not have MetaCall installed or we cannot find it.', '\x1b[0m')
+	print('\x1b[1m', 'Looking for it in the following paths:', sys.path, '\x1b[0m')
+	print('\x1b[33m\x1b[1m', 'If you do not have it installed, you have three options:', '\x1b[0m')
+	print('\x1b[1m', '	1) Go to https://github.com/metacall/install and install it.', '\x1b[0m')
+	print('\x1b[1m', '	2) Contribute to https://github.com/metacall/distributable by providing support for your platform and architecture.', '\033[0m')
+	print('\x1b[1m', '	3) Be a x10 programmer and compile it by yourself, then define the install folder (if it is different from the default /usr/local/lib) in os.environ[\'LOADER_LIBRARY_PATH\'].', '\x1b[0m')
+	print('\x1b[33m\x1b[1m', 'If you have it installed in an non-standard folder, please define os.environ[\'LOADER_LIBRARY_PATH\'].', '\x1b[0m')
+	raise ImportError('MetaCall Python Port was not found')
+
+# Load from file
+def metacall_load_from_file(tag, paths):
+	return module.metacall_load_from_file(tag, paths)
+
+# Load from memory
+def metacall_load_from_memory(tag, buffer):
+	return module.metacall_load_from_memory(tag, buffer)
+
+# Invocation
+def metacall(function_name, *args):
+	return module.metacall(function_name, *args)
+
+# Wrap metacall inspect and transform the json string into a dict
+def metacall_inspect():
+	data = module.metacall_inspect()
+	if data:
+		dic = json.loads(data)
+		try:
+			del dic['__metacall_host__']
+		except:
+			pass
+		return dic
+	return dict()
+
+# Monkey patching
+import builtins
+import types
+from contextlib import suppress
+import functools
+
+# Save the original Python import
+__python_import__ = builtins.__import__
+
+def __metacall_import__(name, globals=None, locals=None, fromlist=(), level=0):
+	def find_handle(handle_name):
+		metadata = metacall_inspect()
+
+		for loader in metadata.keys():
+			for handle in metadata[loader]:
+				if handle['name'] == handle_name:
+					return handle
+
+		return None
+
+	def generate_module(handle_name, handle):
+		mod = sys.modules.setdefault(handle_name, types.ModuleType(handle_name))
+
+		# Set a few properties required by PEP 302
+		base_path = os.environ.get('LOADER_SCRIPT_PATH', os.getcwd())
+		mod.__file__ = os.path.join(base_path, handle_name)
+		mod.__name__ = handle_name
+		mod.__path__ = base_path
+		# TODO: Using os.__loader__ instead of self until we implement the custom loader class
+		mod.__loader__ = os.__loader__ # self
+		# PEP-366 specifies that package's set __package__ to
+		# their name, and modules have it set to their parent package (if any)
+		# TODO (https://pymotw.com/3/sys/imports.html):
+		# if self.is_package(handle_name):
+		# 	mod.__package__ = handle_name
+		# else:
+		# 	mod.__package__ = '.'.join(handle_name.split('.')[:-1])
+		mod.__package__ = handle_name
+
+		# Add the symbols to the module
+		symbol_dict = dict(functools.reduce(lambda symbols, func: {**symbols, func['name']: lambda *args: metacall(func['name'], *args) }, handle['scope']['funcs'], {}))
+
+		mod.__dict__.update(symbol_dict)
+
+		return mod
+
+	# Map file extension to tags
+	extensions_to_tag = {
+		# Mock Loader
+		'mock': 'mock',
+		# Python Loader
+		'py': 'py',
+		# Ruby Loader
+		'rb': 'rb',
+		# C# Loader
+		'cs': 'cs',
+		'vb': 'cs',
+		'dll': 'cs',
+		# Cobol Loader
+		'cob': 'cob',
+		'cbl': 'cob',
+		'cpy': 'cob',
+		# NodeJS Loader
+		'node': 'node',
+		'js': 'node',
+		# TypeScript Loader
+		'ts': 'ts',
+		'jsx': 'ts',
+		'tsx': 'ts',
+
+		# Note: By default js extension uses NodeJS loader instead of JavaScript V8
+		# Probably in the future we can differenciate between them, but it is not trivial
+	}
+
+	# Try to load it as a Python module
+	mod = None
+
+	with suppress(ImportError):
+		mod = __python_import__(name, globals, locals, fromlist, level)
+
+	if mod:
+		return mod
+
+	# Obtain the extension of the module if any
+	extension = None if name.count('.') == 0 else name.split('.')[-1]
+
+	# Load by extension if there is any (import puppeteer.js)
+	if extension and extension in extensions_to_tag.keys():
+		# Get handle name without extension
+		handle_name = name.split('.')[-2]
+
+		# Check if it is already loaded in MetaCall
+		handle = find_handle(handle_name)
+
+		if handle != None:
+			# Generate the module from cached handle
+			return generate_module(handle_name, handle)
+
+		if metacall_load_from_file(extensions_to_tag[extension], [name]):
+			handle = find_handle(handle_name)
+			if handle != None:
+				# Generate the module from cached handle
+				return generate_module(handle_name, handle)
+	else:
+		# Check if it is already loaded in MetaCall
+		handle = find_handle(name)
+
+		if handle != None:
+			# Generate the module from cached handle
+			return generate_module(name, handle)
+
+		# Otherwhise, try to load it by guessing the loader
+		for tag in list(set(extensions_to_tag.values())):
+			if metacall_load_from_file(tag, [name]):
+				handle = find_handle(name)
+				if handle != None:
+					# Generate the module from cached handle
+					return generate_module(name, handle)
+
+	raise ImportError('MetaCall could not import:', name)
+
+# Override Python import
+builtins.__import__ = __metacall_import__
---
+++ packages/metacall/metacall-0.3.1.tar.gz/metacall-0.3.1/setup.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python3
+
+#	MetaCall Python Port by Parra Studios
+#	A frontend for Python language bindings in MetaCall.
+#
+#	Copyright (C) 2016 - 2020 Vicente Eduardo Ferrer Garcia <vic798@gmail.com>
+#
+#	Licensed under the Apache License, Version 2.0 (the "License");
+#	you may not use this file except in compliance with the License.
+#	You may obtain a copy of the License at
+#
+#		http://www.apache.org/licenses/LICENSE-2.0
+#
+#	Unless required by applicable law or agreed to in writing, software
+#	distributed under the License is distributed on an "AS IS" BASIS,
+#	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#	See the License for the specific language governing permissions and
+#	limitations under the License.
+
+# To use a consistent encoding
+from codecs import open
+import os
+import sys
+import re
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+current_path = os.path.abspath(os.path.dirname(__file__))
+
+# Get the long description from the README file
+with open(os.path.join(current_path, 'README.rst'), encoding='utf-8') as f:
+	long_description = f.read()
+
+# Define set up options
+options = {
+	'name': 'metacall',
+
+	# Versions should comply with PEP440.  For a discussion on single-sourcing
+	# the version across setup.py and the project code, see
+	# https://packaging.python.org/en/latest/single_source_version.html
+	'version': '0.3.1',
+
+	'description': 'A library for providing inter-language foreign function interface calls',
+	'long_description': long_description,
+	'long_description_content_type': 'text/x-rst',
+
+	# The project's main homepage
+	'url': 'https://github.com/metacall/core',
+
+	# Author details
+	'author': 'Vicente Eduardo Ferrer Garcia',
+	'author_email': 'vic798@gmail.com',
+
+	# License
+	'license': 'Apache License 2.0',
+
+	# See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+	'classifiers': [
+		# Project Status
+		#   3 - Alpha
+		#   4 - Beta
+		#   5 - Production/Stable
+		'Development Status :: 4 - Beta',
+
+		# Audience
+		'Intended Audience :: Developers',
+		'Topic :: Software Development :: Interpreters',
+
+		# License
+		'License :: OSI Approved :: Apache Software License',
+
+		# Python versions support
+		#'Programming Language :: Python :: 2',
+		#'Programming Language :: Python :: 2.6',
+		#'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+	],
+
+	# Keywords
+	'keywords': 'metacall python port ffi polyglot faas serverless',
+
+	# Modules
+	'py_modules': ['metacall'],
+
+	# List additional groups of dependencies here (e.g. development
+	# dependencies). You can install these using the following syntax,
+	# for example:
+	# $ pip install -e .[dev,test]
+	'extras_require': {
+		'dev': ['check-manifest'],
+		'test': ['coverage'],
+	},
+
+	# If there are data files included in your packages that need to be
+	# installed, specify them here.  If using Python 2.6 or less, then these
+	# have to be included in MANIFEST.in as well.
+	#package_data: {
+	#    'sample': ['package_data.dat'],
+	#},
+
+	# Although 'package_data' is the preferred approach, in some case you may
+	# need to place data files outside of your packages. See:
+	# http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+	# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+	#data_files: [('my_data', ['data/data_file'])],
+}
+
+# Exclude base packages
+exclude_packages =  ['contrib', 'docs', 'tests', 'CMakeLists.txt']
+
+# TODO: Review helper
+# # Detect if metacall port is already installed
+# port_installed = False
+
+# Append environment variable or default install path when building manually (TODO: Cross-platform paths)
+sys.path.append(os.environ.get('PORT_LIBRARY_PATH', os.path.join(os.path.sep, 'usr', 'local', 'lib')));
+
+# Find is MetaCall is installed as a distributable tarball (TODO: Cross-platform paths)
+rootdir = os.path.join(os.path.sep, 'gnu', 'store')
+regex = re.compile('.*-metacall-.*')
+
+for root, dirs, _ in os.walk(rootdir):
+	for folder in dirs:
+		if regex.match(folder) and not folder.endswith('R'):
+			sys.path.append(os.path.join(rootdir, folder, 'lib'))
+
+# TODO: Review helper
+# # Find if module is installed
+# if sys.version_info[0] < 3:
+# 	# Python 2.7
+# 	import imp
+# 	try:
+# 		imp.find_module('_py_port')
+# 		port_installed = True
+# 	except ImportError:
+# 		try:
+# 			imp.find_module('_py_portd')
+# 			port_installed = True
+# 		except ImportError:
+# 			pass
+# elif sys.version_info[0] >= 3 and sys.version_info[1] <= 3:
+# 	# Python <= 3.3
+# 	import importlib
+# 	py_port = importlib.find_loader('_py_port')
+# 	port_installed = py_port is not None
+# 	if port_installed == False:
+# 		py_port = importlib.find_loader('_py_portd')
+# 		port_installed = py_port is not None
+# elif sys.version_info[0] >= 3 and sys.version_info[1] > 3:
+# 	# Python >= 3.4
+# 	import importlib
+# 	py_port = importlib.util.find_spec("_py_port")
+# 	port_installed = py_port is not None
+# 	if port_installed == False:
+# 		py_port = importlib.util.find_spec("_py_portd")
+# 		port_installed = py_port is not None
+
+# TODO: This code is very interesting for providing commands to the end user.
+#	pip cannot execute arbitrary code as pre/post install hook when the package is being installed.
+#	So it is impossible to install the binaries unless we add extra commands after install.
+#	At this moment there is a common solution for installing binaries depending on Bash/PowerShell
+#	that is OS dependant and not language dependant. By the moment we will use the new way of install
+#	instead of the old one, but we keep the ./helper folder in order to provide future support for
+#	extra commands, although the main idea is to keep the OS dependant install, this can be useful
+#	for updating or doing Python related things. Meanwhile, it will be avoided.
+exclude_packages.append('helper')
+
+# TODO: Review helper
+# if port_installed == True:
+# 	# Exclude helper package if port is already installed
+# 	exclude_packages.append('helper')
+# else:
+# 	# List run-time dependencies here.  These will be installed by pip when
+# 	# your project is installed. For an analysis of "install_requires" vs pip's
+# 	# requirements files see:
+# 	# https://packaging.python.org/en/latest/requirements.html
+# 	options['install_requires'] = ['peppercorn', 'requests']
+
+# 	# To provide executable scripts, use entry points in preference to the
+# 	# "scripts" keyword. Entry points provide cross-platform support and allow
+# 	# pip to create the appropriate form of executable for the target platform.
+# 	options['entry_points'] = {
+# 		'console_scripts': [
+# 			'metacall-install=helper:install',
+# 			'metacall-uninstall=helper:uninstall_prompt',
+# 			'metacall-update=helper:update'
+# 		],
+# 	}
+
+# Define required packages
+options['packages'] = find_packages(exclude=exclude_packages)
+
+# Execute the setup
+setup(**options)
