b094d63ce30ebf6d707f1243ce79737e7a1a155c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:50 +0000

    Add package pyxbe/0.0.3/pyxbe-0.0.3.tar.gz

---
+++ packages/pyxbe/pyxbe-0.0.3.tar.gz/pyxbe-0.0.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+with open('README.md') as f:
+	long_description = f.read()
+
+setup(name='pyxbe',
+      version='0.0.3',
+      description='Library to work with XBE files',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Matt Borgerson',
+      author_email='contact@mborgerson.com',
+      url='https://github.com/mborgerson/pyxbe',
+      license='MIT',
+      packages=['xbe'],
+     )
