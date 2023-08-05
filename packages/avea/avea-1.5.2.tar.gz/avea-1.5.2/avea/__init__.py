1e224f4ebd73b9907f059a60a6760027e9f25b80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:47 +0000

    Add package avea/1.5.2/avea-1.5.2.tar.gz

---
+++ packages/avea/avea-1.5.2.tar.gz/avea-1.5.2/avea/__init__.py
@@ -0,0 +1,6 @@
+"""avea
+
+A python library to control Elgato's Avea Bulb.
+"""
+__version__ = "1.5.2"
+from .avea import *
---
+++ packages/avea/avea-1.5.2.tar.gz/avea-1.5.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="avea",
+    version="1.5.2",
+    author="k0rventen",
+    description="Control an Elgato Avea bulb using python3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license="MIT",
+    url="https://github.com/k0rventen/avea",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        'bluepy',
+    ],
+)
