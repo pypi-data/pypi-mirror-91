5a22d4c2dd447118d6f6868c9e7159a5a5d7118f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:52 +0000

    Add package prettynumbers/0.0.8/prettynumbers-0.0.8.tar.gz

---
+++ packages/prettynumbers/prettynumbers-0.0.8.tar.gz/prettynumbers-0.0.8/pretty_numbers.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+########################################################
+__author__ = "Gerard Keating"
+__date__ = "12 Feb 2013"
+########################################################
+
+def getPrettyTextFromSet(frames):
+    """
+    Given a set of integers returns a more human readable string
+    """
+    if not frames: 
+        return ""
+    if len(frames)==1:
+        framesSet = frames.copy()
+        return str(framesSet.pop())
+    framesList = list(frames)
+    framesList.sort(reverse=True)
+    lastNum = framesList.pop()
+    currentStrStart = lastNum
+    pStr = str(lastNum)
+    while framesList:
+        currNum = framesList.pop()
+        if not lastNum + 1 == currNum:
+            if not lastNum == currentStrStart:
+                pStr = pStr + "-" + str(lastNum)
+            pStr = pStr + "," + str(currNum)
+            currentStrStart = currNum
+        lastNum = currNum
+    if currentStrStart != currNum:
+        pStr = pStr + "-" + str(lastNum)
+    return pStr
+
+def getPrettyTextFromNumbers(frames):
+    """
+    Given iterable of integers returns a more human readable string
+    """
+    framesSet = set(frames)
+    return getPrettyTextFromSet(framesSet)
+
+
+def getPrettyNumbersText(list_of_strings):
+    nums = set()
+    end_text = set()
+    for i in list_of_strings:
+        try:
+            is_digit = i.isdigit()
+        except AttributeError:
+            is_digit = True
+        if is_digit:
+            try:
+                nums.add(int(i))
+            except TypeError:
+                end_text.add(str(i))
+        else:
+            end_text.add(i)
+    result = getPrettyTextFromSet(nums)
+    if not end_text:
+        return result
+    end_text = list(end_text)
+    end_text.sort()
+    end_text = ",".join(end_text)
+    if result:
+        return result + "," + end_text
+    else:
+        return end_text
+
---
+++ packages/prettynumbers/prettynumbers-0.0.8.tar.gz/prettynumbers-0.0.8/setup.py
@@ -0,0 +1,29 @@
+# based on https://github.com/pypa/sampleproject/blob/master/setup.py
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+version = '0.0.8'
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='prettynumbers',
+    version=version,
+    description='Display a range of numbers in a human readable way',
+    long_description=long_description,
+    author='Gerard Keating',
+    author_email='gerardk@gmail.com',
+    license='GNU GENERAL PUBLIC LICENSE',
+    url='https://github.com/vfxGer/pretty-numbers',
+    py_modules=['pretty_numbers'],
+    download_url='https://pypi.python.org/packages/source/d/pretty_numbers-%s.tar.gz?raw=true' % (version),
+    platforms='Cross-platform',
+    classifiers=[
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 3'
+    ],
+)
