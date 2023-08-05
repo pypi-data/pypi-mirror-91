ab2be55b6bc4d8ebcf16f405278dc4337dcbfcfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:13 +0000

    Add package gauss-binom-distributions-patwrz/0.1/gauss_binom_distributions_patwrz-0.1.tar.gz

---
+++ packages/gauss-binom-distributions-patwrz/gauss_binom_distributions_patwrz-0.1.tar.gz/gauss_binom_distributions_patwrz-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='gauss_binom_distributions_patwrz',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gauss_binom_distributions_patwrz'],
+      author = 'Patrycja Wrzosek',
+      zip_safe=False)
