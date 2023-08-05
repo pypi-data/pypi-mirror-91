21a8fd2a3331e823f60d7e98019d3673dc994549
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:04 +0000

    Add package hyssop-aiodb/0.0.1/hyssop_aiodb-0.0.1-py3-none-any.whl

---
+++ packages/hyssop-aiodb/hyssop_aiodb-0.0.1-py3-none-any.whl/hyssop_aiodb/__init__.py
@@ -0,0 +1,13 @@
+# Copyright (C) 2020-Present the hyssop authors and contributors.
+#
+# This module is part of hyssop and is released under
+# the MIT License: http://www.opensource.org/licenses/mit-license.php
+
+'''
+File created: September 4th 2020
+
+Modified By: hsky77
+Last Updated: December 26th 2020 21:29:30 pm
+'''
+
+Version = '0.0.1'
