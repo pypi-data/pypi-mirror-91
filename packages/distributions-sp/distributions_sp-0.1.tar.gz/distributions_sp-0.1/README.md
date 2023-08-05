d24dfef8a6c50c24b4a0f558a00f46b0274102e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:39 +0000

    Add package distributions-sp/0.1/distributions_sp-0.1.tar.gz

---
+++ packages/distributions-sp/distributions_sp-0.1.tar.gz/distributions_sp-0.1/README.md
@@ -0,0 +1,3 @@
+# This is a test package
+
+Author: Shubham Periwal
---
+++ packages/distributions-sp/distributions_sp-0.1.tar.gz/distributions_sp-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_sp',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sp'],
+      zip_safe=False)
