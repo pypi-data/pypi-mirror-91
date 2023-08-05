9b0df1c55abf02a5a178c4bd8ff7a9ba281b94c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:39 +0000

    Add package tangle-node-editor/1.0.5a0/tangle-node-editor-1.0.5a0.tar.gz

---
+++ packages/tangle-node-editor/tangle-node-editor-1.0.5a0.tar.gz/tangle-node-editor-1.0.5a0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tangle-node-editor',
+    version='1.0.5-alpha',
+    packages=find_packages(),
+    url='https://www.github.com/nielsvaes/tangle',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["ez-qt", "ez-settings", "ez-utils", "PySide2", "numpy", "pillow", "pyqtgraph"],
+    license='MIT',
+    author='Niels Vaes',
+    author_email='nielsvaes@gmail.com',
+    description='A Python based node editor',
+
+    entry_points = {
+                   'console_scripts': ['tangle=tangle.app:main'],
+               },
+)
