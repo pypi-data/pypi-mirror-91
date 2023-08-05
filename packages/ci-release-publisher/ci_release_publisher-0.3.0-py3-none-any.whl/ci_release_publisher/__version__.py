c5df59550a1550dd2b9d7ef7afecfe8975290b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:05 +0000

    Add package ci-release-publisher/0.3.0/ci_release_publisher-0.3.0-py3-none-any.whl

---
+++ packages/ci-release-publisher/ci_release_publisher-0.3.0-py3-none-any.whl/ci_release_publisher/__version__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'ci_release_publisher'
+__description__ = 'A script for publishing Travis-CI build artifacts on GitHub Releases'
+__version__ = '0.3.0'
+__url__ = 'https://github.com/nurupo/ci-release-publisher'
+__author__ = 'Maxim Biro'
+__author_email__ = 'nurupo.contributions@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2018-2020 Maxim Biro'
