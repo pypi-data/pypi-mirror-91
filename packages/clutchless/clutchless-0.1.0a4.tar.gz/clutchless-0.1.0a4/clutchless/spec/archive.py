9a128b22f0292dbee26eef44fe8673e34e8147de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:46 +0000

    Add package clutchless/0.1.0a4/clutchless-0.1.0a4.tar.gz

---
+++ packages/clutchless/clutchless-0.1.0a4.tar.gz/clutchless-0.1.0a4/clutchless/spec/archive.py
@@ -0,0 +1,11 @@
+""" Copy metainfo files from Transmission for backup.
+
+Usage:
+    clutchless archive [--dry-run] <destination>
+
+Arguments:
+    <destination>   Directory where metainfo files in Transmission will be copied.
+
+Options:
+    --dry-run       Do not copy any files, only list which files would be moved.
+"""
