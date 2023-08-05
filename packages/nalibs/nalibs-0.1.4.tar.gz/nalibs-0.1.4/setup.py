0dec65423cd3a6dce2c1cb95305bc7c8f7fa2b98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:39 +0000

    Add package nalibs/0.1.4/nalibs-0.1.4.tar.gz

---
+++ packages/nalibs/nalibs-0.1.4.tar.gz/nalibs-0.1.4/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nalibs", # Replace with your own username
+    version="0.1.4",
+    author="Quang Nguyen",
+    author_email="anhquangnet@gmail.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "requests",
+        "PyYAML"
+    ],
+    entry_points={
+        'console_scripts': [
+            'nalibs_hello=nalibs.addfunc:sayHello',
+    ],},
+    python_requires='>=3.6',
+)
