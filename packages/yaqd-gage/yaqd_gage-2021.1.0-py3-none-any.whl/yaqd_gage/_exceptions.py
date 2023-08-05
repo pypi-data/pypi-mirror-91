a3cc139dd872fb5d81b07dd8f6c65a0c2691d667
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:10:04 +0000

    Add package yaqd-gage/2021.1.0/yaqd_gage-2021.1.0-py3-none-any.whl

---
+++ packages/yaqd-gage/yaqd_gage-2021.1.0-py3-none-any.whl/yaqd_gage/_exceptions.py
@@ -0,0 +1,16 @@
+"""Python Exceptions for Gage codes."""
+
+from typing import Any, Dict
+
+
+from ._constants import  error_codes
+
+
+class CompuScopeException(Exception):
+
+    def __init__(self, error_code):
+        if error_code in error_codes:
+            message = f"{error_code}: {error_codes[error_code]}"
+        else:
+            message = f"{error_code}: unrecognized error code"
+        super().__init__(message)
