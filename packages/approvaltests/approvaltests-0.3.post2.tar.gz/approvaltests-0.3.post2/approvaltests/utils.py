f2cd95e812dbc5993bf509bb1b0d2e1f4adf8c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package approvaltests/0.3.post2/approvaltests-0.3.post2.tar.gz

---
+++ packages/approvaltests/approvaltests-0.3.post2.tar.gz/approvaltests-0.3.post2/approvaltests/utils.py
@@ -0,0 +1,32 @@
+import inspect
+import json
+import os
+
+
+def get_adjacent_file(name):
+    calling_file = inspect.stack(1)[1][1]
+    directory = os.path.dirname(os.path.abspath(calling_file))
+    filename = os.path.join(directory, name)
+    return filename
+
+
+def write_to_temporary_file(expected, name):
+    import tempfile
+    with tempfile.NamedTemporaryFile(mode='w+b', suffix='.txt', prefix=name, delete=False) as temp:
+        temp.write(expected.encode('utf-8-sig'))
+        return temp.name
+
+
+def to_json(object):
+    return json.dumps(
+        object,
+        sort_keys=True,
+        indent=4,
+        separators=(',', ': '),
+        default=lambda o: o.__dict__,
+        ensure_ascii=True,
+    )
+
+
+def is_windows_os():
+    return os.name == 'nt'
---
+++ packages/approvaltests/approvaltests-0.3.post2.tar.gz/approvaltests-0.3.post2/setup.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+from distutils.core import setup
+
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+print("Here:", here)
+about = {}
+with open(os.path.join(here, 'approvaltests', 'version.py'), 'r') as f:
+    exec(f.read(), about)
+
+
+setup(
+    name='approvaltests',
+    version= about['version_number'],
+    description='Assertion/verification library to aid testing',
+    author='ApprovalTests Contributors',
+    author_email='jamesrcounts@outlook.com',
+    url='https://github.com/approvals/ApprovalTests.Python',
+    packages=find_packages(exclude=['tests*']),
+    package_data={'approvaltests':['reporters/reporters.json']},
+    install_requires=['pyperclip==1.5.27', 'pytest'],
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
+)
