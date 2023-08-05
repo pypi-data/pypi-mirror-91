a9dc85530f8f567be0d61902e10940a6921a1353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:09 +0000

    Add package odeintw2/0.0.0/odeintw2-0.0.0.tar.gz

---
+++ packages/odeintw2/odeintw2-0.0.0.tar.gz/odeintw2-0.0.0/odeintw2/__init__.py
@@ -0,0 +1,6 @@
+# Copyright (c) 2021, Menglong He 
+# All rights reserved.
+# See the LICENSE file for license information.
+from ._odeintw2 import odeintw2
+
+
---
+++ packages/odeintw2/odeintw2-0.0.0.tar.gz/odeintw2-0.0.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+from distutils.core import setup
+setup(name='odeintw2',
+      author='Menglong He',
+      url='https://github.com/HEMENGLONGIEEE/odeintw2',
+      packages=['odeintw2'],
+      classifiers=[
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: BSD License",
+          "Operating System :: OS Independent",
+      ],
+      keywords="scipy odeint",
+      install_requires=['scipy'])
