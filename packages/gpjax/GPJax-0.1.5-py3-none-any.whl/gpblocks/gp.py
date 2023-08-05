43711f268bf67e305c2d8721d3a2f94b82fe8fd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:02 +0000

    Add package gpjax/0.1.5/GPJax-0.1.5-py3-none-any.whl

---
+++ packages/gpjax/GPJax-0.1.5-py3-none-any.whl/gpblocks/gp.py
@@ -0,0 +1,67 @@
+from .likelihoods import Gaussian, Likelihood
+from .kernel import Kernel
+from .mean_functions import MeanFunction
+from .mean_functions import ZeroMean
+from .utils import get_factorisations
+from objax import TrainVar, Module
+import jax.numpy as jnp
+from jax import nn
+from jax.scipy.linalg import cho_solve
+import jax.random as jr
+from jax.scipy.stats import multivariate_normal
+
+
+class Prior(Module):
+    def __init__(self,
+                 kernel: Kernel,
+                 mean_function: MeanFunction = ZeroMean(),
+                 observation_noise: jnp.ndarray = jnp.array([0.1]),
+                 jitter: float = 1e-6):
+        self.meanf = mean_function
+        self.kernel = kernel
+        self.noise = TrainVar(observation_noise)
+        self.jitter = jitter
+
+    def sample(self, X: jnp.ndarray, key, n_samples: int = 1):
+        Inn = jnp.eye(X.shape[0])
+        mu = self.meanf(X)
+        cov = self.kernel(X, X) + self.jitter * Inn
+        return jr.multivariate_normal(key,
+                                      mu.squeeze(),
+                                      cov,
+                                      shape=(n_samples, ))
+
+    def __mul__(self, other: Likelihood):
+        return Posterior(self, other)
+
+
+class Posterior(Module):
+    def __init__(self, prior: Prior, likelihood: Gaussian):
+        self.kernel = prior.kernel
+        self.meanf = prior.meanf
+        self.likelihood = likelihood
+        self.jitter = prior.jitter
+
+    def marginal_ll(self, X: jnp.ndarray, y: jnp.ndarray) -> jnp.ndarray:
+        Inn = jnp.eye(X.shape[0])
+        mu = self.meanf(X)
+        cov = self.kernel(X, X) + self.jitter * Inn
+        cov += nn.softplus(self.likelihood.noise.value) * Inn
+        # L = jnp.linalg.cholesky(cov)
+        # TODO: Return the logpdf w.r.t. the Cholesky, not the full cov.
+        lpdf = multivariate_normal.logpdf(y.squeeze(), mu.squeeze(), cov)
+        return lpdf
+
+    def neg_mll(self, X: jnp.ndarray, y: jnp.ndarray):
+        return -self.marginal_ll(X, y)
+
+    def predict(self, Xstar, X, y):
+        sigma = nn.softplus(self.likelihood.noise.value)
+        L, alpha = get_factorisations(X, y, sigma, self.kernel, self.meanf)
+        Kfx = self.kernel(Xstar, X)
+        mu = jnp.dot(Kfx, alpha)
+        v = cho_solve(L, Kfx.T)
+        # Calculate kernel matrix for inputs
+        Kxx = self.kernel(Xstar, Xstar)
+        cov = Kxx - jnp.dot(Kfx, v)
+        return mu, cov
---
+++ packages/gpjax/GPJax-0.1.5-py3-none-any.whl/gpblocks/kernel.py
@@ -0,0 +1,60 @@
+import jax.numpy as jnp
+from objax import TrainVar, Module
+from typing import Callable
+from jax import vmap, nn
+
+
+class Kernel(Module):
+    def __init__(self,
+                 parameter_transform: Callable = nn.softplus,
+                 name: str = None):
+        # TODO: Make the transformation a method of the parameter type.
+        self.transform = parameter_transform
+        self.name = name
+
+    @staticmethod
+    def gram(func: Callable, x: jnp.ndarray, y: jnp.ndarray) -> jnp.ndarray:
+        mapx1 = vmap(lambda x, y: func(x=x, y=y),
+                     in_axes=(0, None),
+                     out_axes=0)
+        mapx2 = vmap(lambda x, y: mapx1(x, y), in_axes=(None, 0), out_axes=1)
+        return mapx2(x, y)
+
+    def __call__(self, x: jnp.ndarray, y: jnp.ndarray):
+        raise NotImplementedError
+
+
+class Stationary(Kernel):
+    def __init__(self,
+                 lengthscale: jnp.ndarray = jnp.array([1.]),
+                 variance: jnp.ndarray = jnp.array([1.]),
+                 parameter_transform: Callable = nn.softplus,
+                 name: str = "Stationary"):
+        super().__init__(parameter_transform=parameter_transform, name=name)
+        self.lengthscale = TrainVar(lengthscale)
+        self.variance = TrainVar(variance)
+
+    @staticmethod
+    def dist(x: jnp.array, y: jnp.array) -> float:
+        return x - y
+
+
+class RBF(Stationary):
+    def __init__(self,
+                 lengthscale: jnp.ndarray = jnp.array([1.]),
+                 variance: jnp.ndarray = jnp.array([1.]),
+                 parameter_transform: Callable = nn.softplus,
+                 name: str = "RBF"):
+        super().__init__(parameter_transform=parameter_transform,
+                         lengthscale=lengthscale,
+                         variance=variance,
+                         name=name)
+
+    def k_func(self, x: jnp.ndarray, y: jnp.ndarray) -> jnp.ndarray:
+        ell = self.transform(self.lengthscale.value)
+        sigma = self.transform(self.variance.value)
+        tau = jnp.sum(self.dist(x, y)**2)
+        return sigma * jnp.exp(-jnp.square(tau) / ell)
+
+    def __call__(self, X: jnp.ndarray, Y: jnp.ndarray) -> jnp.ndarray:
+        return self.gram(self.k_func, X, Y).squeeze()
