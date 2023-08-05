9571b16a13a30f9acf8ed2185a8ddd3d0bdd9dd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:29 +0000

    Add package django-logging-middleware/0.0.2b1/django-logging-middleware-0.0.2b1.tar.gz

---
+++ packages/django-logging-middleware/django-logging-middleware-0.0.2b1.tar.gz/django-logging-middleware-0.0.2b1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="django-logging-middleware", # Replace with your own username
+    version="0.0.2beta1",
+    author="Rifat Fazlutdinov",
+    author_email="rifatfazlutdinov@gmail.com",
+    description="A small package to log django requests and responses",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fearsd/django-logging-middleware",
+    packages=setuptools.find_packages(exclude=['tests*']),
+    install_requires=["django>=3.0", "loguru"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
