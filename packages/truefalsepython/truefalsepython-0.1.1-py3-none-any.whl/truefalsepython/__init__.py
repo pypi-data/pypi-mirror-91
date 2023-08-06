36d2b4f5324ccb001bf1edda3aa4064ffcddc0b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:29 +0000

    Add package truefalsepython/0.1.1/truefalsepython-0.1.1-py3-none-any.whl

---
+++ packages/truefalsepython/truefalsepython-0.1.1-py3-none-any.whl/truefalsepython/__init__.py
@@ -0,0 +1,6 @@
+
+from .logical import TRUE, true, T, FALSE, F, false, NULL
+from .little impoty is_number, is_odd, is_even
+
+from .r_funcs import nrow, ncol, colMeans, rowMeans, colSums, rowSums, apply, lapply, sapply, sample, sample_int
+
