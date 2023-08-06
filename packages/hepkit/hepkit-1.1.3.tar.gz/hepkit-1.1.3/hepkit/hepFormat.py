a1dd1aa4db8f8c7e975691f8c5fb3d2adbc4143d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:03 +0000

    Add package hepkit/1.1.3/hepkit-1.1.3.tar.gz

---
+++ packages/hepkit/hepkit-1.1.3.tar.gz/hepkit-1.1.3/hepkit/hepFormat.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def DefaultSetup(mc=999,mr=10):
+  '''
+  Personal preferences for plt.pyplot
+  '''
+  plt.rcParams['font.family'] = 'serif'
+  plt.rcParams['font.weight'] = 'light'
+  plt.rcParams['font.size'] = 16
+  plt.rcParams['figure.figsize'] = [12,8]
+  pd.options.display.max_columns = mc
+  pd.options.display.max_rows = mr
+
+def mc():
+  '''
+  Really cool color palette
+  '''
+  mc = [
+      [35/255, 55/255, 59/255],
+      [237/255, 138/255, 46/255],
+      [131/255, 40/255, 0/255],
+      [0/255, 123/255, 68/255],
+      [107/255, 60/255, 79/255],
+      [118/255, 93/255, 68/255],
+      [62/255, 79/255, 51/255],
+      [147/255, 161/255, 161/255]
+  ]
+  return mc
---
+++ packages/hepkit/hepkit-1.1.3.tar.gz/hepkit-1.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hepkit',
+    version='1.1.3',
+    description='Tools for performing High Energy Physics analysis with Python',
+    license='MIT',
+    packages=['hepkit'],
+    author='Salvatore Davide Porzio',
+    author_email='porziodavide@gmail.com',
+    keywords=['high energy physics','hep','python','pandas','uproot','pyplot'],
+    url='https://github.com/sdporzio/HEPKit'
+)
