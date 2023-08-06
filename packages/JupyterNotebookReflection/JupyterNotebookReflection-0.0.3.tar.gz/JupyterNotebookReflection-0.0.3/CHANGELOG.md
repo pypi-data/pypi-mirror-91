7cf50df17203e909129105b749f399a1fa366194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:19 +0000

    Add package JupyterNotebookReflection/0.0.3/JupyterNotebookReflection-0.0.3.tar.gz

---
+++ packages/JupyterNotebookReflection/JupyterNotebookReflection-0.0.3.tar.gz/JupyterNotebookReflection-0.0.3/CHANGELOG.md
@@ -0,0 +1,14 @@
+Change Log
+==========
+
+0.0.1 (16/01/2021)
+------------------
+- First Release.
+
+0.0.2 (16/01/2021)
+------------------
+- Added README and CHANGELOG files.
+
+0.0.3 (16/01/2021)
+------------------
+- Added *md to manifest file.
---
+++ packages/JupyterNotebookReflection/JupyterNotebookReflection-0.0.3.tar.gz/JupyterNotebookReflection-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='JupyterNotebookReflection',
+    version='0.0.3',
+    description='A module to perform reflection coding in Jupyter notebooks.',
+    url='',
+    author='Charles Varley',
+    license='MIT',
+    keywords=['jupyter','notebook','reflection','introspection'],
+    packages=find_packages(),
+)
