507c4d19bad2251c0a86a98ab77f35bd627eb08d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:36 +0000

    Add package bibmanager/1.3.2/bibmanager-1.3.2.tar.gz

---
+++ packages/bibmanager/bibmanager-1.3.2.tar.gz/bibmanager-1.3.2/bibmanager/VERSION.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2018-2021 Patricio Cubillos.
+# bibmanager is open-source software under the MIT license (see LICENSE).
+
+# bibmanager Version:
+BM_VER =  1  # Major version
+BM_MIN =  3  # Minor version
+BM_REV =  2  # Revision
