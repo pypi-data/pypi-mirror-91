8e6b1333bbd3e3c10adad5779dfd9797e9dfb54b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:18 +0000

    Add package django-adobesign/0.10/django_adobesign-0.10-py2.py3-none-any.whl

---
+++ packages/django-adobesign/django_adobesign-0.10-py2.py3-none-any.whl/django_adobesign/client.py
@@ -0,0 +1,331 @@
+from functools import wraps
+from os import path
+from os.path import join, basename
+
+import requests
+from requests import HTTPError
+from requests_oauthlib import OAuth2Session
+
+from django_adobesign.exceptions import get_adobe_exception
+
+ADOBE_OAUTH_TOKEN_URL = 'https://api.echosign.com/oauth/token'
+ADOBE_OAUTH_REFRESH_TOKEN_URL = 'https://api.echosign.com/oauth/refresh'
+
+
+class AdobeSignOAuthSession(object):
+    def __init__(self, application_id, redirect_uri, account_type, state=None):
+        self.application_id = application_id
+        self.oauth_session = OAuth2Session(
+            client_id=self.application_id,
+            redirect_uri=redirect_uri,
+            scope=self.get_scopes(account_type),
+            state=state)
+
+    def get_authorization_url(self, root_url):
+        url = join(root_url, 'public/oauth')
+        authorization_url, state = self.oauth_session.authorization_url(url)
+        return authorization_url
+
+    @staticmethod
+    def get_scopes(account_type):
+        return [scope.format(account_type) for scope in (
+            'user_login:{}',
+            'agreement_send:{}',
+            'agreement_read:{}',
+            'agreement_write:{}')]
+
+    def create_token(self, code, application_secret):
+        response = self.oauth_session.fetch_token(
+            ADOBE_OAUTH_TOKEN_URL,
+            code=code,
+            client_secret=application_secret,
+            authorization_response='/')
+
+        return response
+
+    @staticmethod
+    def refresh_token(refresh_token, application_id, application_secret):
+        oauth_session = OAuth2Session(client_id=application_id)
+        response = oauth_session.refresh_token(
+            ADOBE_OAUTH_REFRESH_TOKEN_URL,
+            refresh_token=refresh_token,
+            client_id=application_id,
+            client_secret=application_secret,
+            authorization_response="/")
+
+        return response
+
+
+def handle_adobe_exception(function):
+    @wraps(function)
+    def wrapper(*arg, **kwargs):
+        try:
+            return function(*arg, **kwargs)
+        except (requests.exceptions.RequestException, HTTPError) as e:
+            raise get_adobe_exception(e)
+
+    return wrapper
+
+
+class AdobeSignClient(object):
+    '''
+    AdobeSign client use v6 api.
+    See https://secure.na1.echosign.com/public/docs/restapi/v6
+    '''
+
+    def __init__(self, root_url, access_token, api_user=None,
+                 on_behalf_of_user=None, timeout=15):
+        self.root_url = root_url.strip('/')
+        self.access_token = access_token
+        self.on_behalf_of_user = on_behalf_of_user
+        self.api_user = api_user
+        self.timeout = timeout
+
+    def build_url(self, urlpath):
+        return path.join(self.root_url, 'api/rest/v6', urlpath)
+
+    def get_headers(self):
+        header = {'Authorization': 'Bearer {}'.format(self.access_token)}
+        if self.api_user:
+            header['x-api-user'] = self.api_user
+        if self.on_behalf_of_user:
+            header['x-on-behalf-of-user'] = self.on_behalf_of_user
+        return header
+
+    @handle_adobe_exception
+    def upload_document(self, document):
+        """
+            Upload a document and get a transient document id
+        """
+        url = self.build_url(urlpath='transientDocuments')
+        data = {
+            'File-Name': basename(document.name),
+            'Mime-Type': 'application/pdf'
+        }
+
+        response = requests.post(
+            url,
+            headers=self.get_headers(),
+            files={'File': document.bytes},
+            data=data,
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def jsonify_participant(self, name, email, order):
+        return {
+            'name': name,
+            'memberInfos': [
+                {'email': email}],
+            'order': order,
+            'role': 'SIGNER'
+        }
+
+    @handle_adobe_exception
+    def post_agreement(self, transient_document_id, name, participants,
+                       post_sign_redirect_url, post_sign_redirect_delay,
+                       send_mail, state='IN_PROCESS', **extra_data):
+        '''
+        Create signature with only one document
+
+        https://secure.na1.adobesign.com/public/docs/restapi/v6#!/agreements/createAgreement
+
+        This is a primary endpoint which is used to create a new agreement.
+        An agreement can be created using transientDocument, libraryDocument
+        or a URL. You can create an agreement in one of the 3 mentioned
+        states:
+        a) DRAFT - to incrementally build the agreement before
+        sending out,
+        b) AUTHORING - to add/edit form fields in the agreement,
+        c) IN_PROCESS - to immediately send the agreement.
+
+        :param post_sign_redirect_delay: The delay (in seconds) before the user
+        is taken to the success page. If this value is greater than 0, the user
+        will first see the standard Adobe Sign success message, and then after
+        a delay will be redirected to your success page,
+        :param post_sign_redirect_url: A publicly accessible url to which the
+        user will be sent after successfully completing the signing process
+        :param document: file wrapper object
+        :param name: name of document
+        :param participants:  A list of one or more participant set.
+        A participant set may have one or more participant.
+        If any member of the participant set takes the action that has been
+        assigned to the set(Sign/Approve/Acknowledge etc ), the action is
+        considered as the action taken by whole participation set.
+        For regular (non-MegaSign) documents, there is no limit on the number
+        of electronic signatures in a single document. Written signatures are
+        limited to four per document,
+        :param extra_data: extra data to pass (see adobesign documentation)
+        '''
+
+        # Send doc for signature
+        url = self.build_url(urlpath='agreements')
+
+        data = {
+            'fileInfos': [{
+                'transientDocumentId': transient_document_id
+            }],
+            'name': name,
+            'participantSetsInfo': participants,
+            'signatureType': 'ESIGN',
+            'state': state
+        }
+        if post_sign_redirect_url:
+            data['postSignOption'] = {
+                'redirectDelay': post_sign_redirect_delay,
+                'redirectUrl': post_sign_redirect_url
+            }
+
+        if not send_mail:
+            data['emailOption'] = {
+                'sendOptions': {
+                    'completionEmails': 'NONE',
+                    'inFlightEmails': 'NONE',
+                    'initEmails': 'NONE'
+                }
+            }
+
+        data.update(extra_data)
+        response = requests.post(
+            url,
+            headers=self.get_headers(),
+            json=data,
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @handle_adobe_exception
+    def get_agreements(self, page_size, cursor=None, **extra_params):
+        """
+        Return the list of agreements with pagination
+        """
+        path_url = 'agreements'
+        params = {'pageSize': page_size}
+        if cursor:
+            params['cursor'] = cursor
+        params.update(extra_params)
+        url = self.build_url(path_url)
+        response = requests.get(
+            url,
+            headers=self.get_headers(),
+            params=params,
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @handle_adobe_exception
+    def get_members(self, agreement_id, include_next_participant_set):
+        """
+        Return members of a given agreement id
+        :param include_next_participant_set: add to the response the set of
+        next participants
+        """
+        url = self.build_url('agreements/{}/members'.format(agreement_id))
+        params = {
+            'includeNextParticipantSet': include_next_participant_set}
+        response = requests.get(
+            url,
+            params=params,
+            headers=self.get_headers(),
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @handle_adobe_exception
+    def get_signing_url(self, agreement_id):
+        """
+        Return the next signing url for the agreement
+        corresponding to the agreement_id.
+        """
+        url = self.build_url('agreements/{}/signingUrls'.format(agreement_id))
+        response = requests.get(
+            url,
+            headers=self.get_headers(),
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @handle_adobe_exception
+    def get_signer(self, agreement_id, signer_id):
+        """
+        Return the signer with the given signer_id who belongs to the agreement
+        corresponding to the agreement_id.
+        """
+        url = self.build_url('agreements/{}/members/participantSets/{}'
+                             .format(agreement_id, signer_id))
+        response = requests.get(
+            url,
+            headers=self.get_headers(),
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @handle_adobe_exception
+    def update_signer(self, agreement_id, signer_id, participant):
+        """
+        Update and return the signer with the given signer_id who belongs to
+        the agreement corresponding to the agreement_id.
+        """
+        url = self.build_url('agreements/{}/members/participantSets/{}'
+                             .format(agreement_id, signer_id))
+        response = requests.put(
+            url,
+            headers=self.get_headers(),
+            json=participant,
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+
+    @handle_adobe_exception
+    def get_documents(self, agreement_id, **extra_data):
+        """
+        Return all document ids for a given agreement id
+        """
+        url = self.build_url('agreements/{}/documents'.format(agreement_id))
+        response = requests.get(
+            url,
+            headers=self.get_headers(),
+            data=extra_data,
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    @handle_adobe_exception
+    def get_document(self, agreement_id, document_id):
+        """
+        Download a document
+        """
+        url = self.build_url('agreements/{}/documents/{}'
+                             .format(agreement_id, document_id))
+        response = requests.get(
+            url,
+            headers=self.get_headers(),
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.content
+
+    @handle_adobe_exception
+    def get_events(self, agreement_id):
+        """
+        Retrieves the events information for an agreement.
+        """
+        url = self.build_url('agreements/{}/events'.format(agreement_id))
+        response = requests.get(
+            url,
+            headers=self.get_headers(),
+            timeout=self.timeout
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def rebuild_with_token(self, access_token):
+        return AdobeSignClient(self.root_url, access_token, self.api_user,
+                               self.on_behalf_of_user)
---
+++ packages/django-adobesign/django_adobesign-0.10-py2.py3-none-any.whl/django_adobesign/tests/test_adobe_backend.py
@@ -0,0 +1,214 @@
+import pytest
+from adobesign.models import Signer, Signature, SignatureType
+from django.core.files import File
+from django.db.models import FileField
+
+from django_adobesign.backend import AdobeSignBackend
+from django_adobesign.client import AdobeSignClient
+from django_adobesign.exceptions import AdobeSignNoMoreSignerException
+
+
+@pytest.fixture()
+def adobe_sign_backend():
+    adobe_sign_client = AdobeSignClient(root_url='http://fake',
+                                        access_token='ThisIsAToken')
+    return AdobeSignBackend(adobe_sign_client)
+
+
+@pytest.fixture()
+def minimal_signature(mocker):
+    signature_type = SignatureType()
+    signature_type.save()
+    signature = Signature(signature_type=signature_type)
+    signature.document = mocker.Mock(FileField)
+    signature.document.file = mocker.Mock(File)
+    signature.document.file.name = '/tmp/uploaded_file.pdf'
+    signature.document._committed = True
+    signature.save()
+    return signature
+
+
+@pytest.mark.django_db
+def test_get_adobesign_participants_in_right_order(minimal_signature,
+                                                   adobe_sign_backend):
+    signer1 = Signer(full_name='Poney poney', email='poney@plop.com',
+                     signing_order=1)
+    signer2 = Signer(full_name='Pouet pouet', email='pouet@plop.com',
+                     signing_order=2)
+
+    signer2.signature = minimal_signature
+    signer1.signature = minimal_signature
+
+    signer2.save()
+    signer1.save()
+    adobe_sign_signers = adobe_sign_backend.get_adobesign_participants(
+        minimal_signature)
+
+    assert len(adobe_sign_signers) == 2
+
+    expected_signer1 = {
+        'name': 'Poney poney',
+        'memberInfos': [
+            {'email': 'poney@plop.com'}],
+        'order': 1,
+        'role': 'SIGNER'
+    }
+    expected_signer2 = {
+        'name': 'Pouet pouet',
+        'memberInfos': [
+            {'email': 'pouet@plop.com'}],
+        'order': 2,
+        'role': 'SIGNER'
+    }
+    assert adobe_sign_signers[0] == expected_signer1
+    assert adobe_sign_signers[1] == expected_signer2
+
+
+@pytest.mark.django_db
+def test_create_signature(mocker, minimal_signature, adobe_sign_backend):
+    mocker.patch.object(AdobeSignClient, 'upload_document',
+                        return_value={'transientDocumentId': 'doc_id'})
+    mocker.patch.object(AdobeSignClient, 'post_agreement',
+                        return_value={'id': 'test_agreement_id'})
+    mocker.patch.object(
+        AdobeSignClient, 'get_members',
+        return_value={
+            'participantSets': [
+                # Check email are case sensitive case (only side Adobe)
+                {'memberInfos': [{'email': 'PoUeT@plop.com'}],
+                 'id': 'fooid',
+                 'status': 'NOT_YET_VISIBLE',
+                 'order': 2},
+                {'memberInfos': [{'email': 'poney@plop.com'}],
+                 'id': 'barid',
+                 'status': 'CANCELLED',
+                 'order': 1}]})
+
+    # Signers
+    signer1 = Signer(full_name='Poney poney', email='poney@plop.com',
+                     signing_order=1)
+    signer2 = Signer(full_name='Pouet pouet', email='pouet@plop.com',
+                     signing_order=2)
+    signer2.signature = minimal_signature
+    signer1.signature = minimal_signature
+    signer2.save()
+    signer1.save()
+
+    assert signer1.signature_backend_id == ""
+    assert signer2.signature_backend_id == ""
+
+    assert minimal_signature.signature_backend_id == ""
+
+    adobe_sign_backend.create_signature(minimal_signature)
+
+    minimal_signature.refresh_from_db()
+    assert minimal_signature.signature_backend_id == 'test_agreement_id'
+
+    signer1.refresh_from_db()
+    signer2.refresh_from_db()
+    assert signer1.signature_backend_id == "barid"
+    assert signer2.signature_backend_id == "fooid"
+
+
+def test_get_next_signer_urls(mocker, adobe_sign_backend):
+    mocker.patch.object(AdobeSignClient, 'get_signing_url',
+                        side_effect=AdobeSignNoMoreSignerException(
+                            'm', Exception('c')))
+    with pytest.raises(AdobeSignNoMoreSignerException):
+        adobe_sign_backend.get_next_signer_urls('12')
+
+
+def test_get_next_signer_url(mocker, adobe_sign_backend):
+    data = {'signingUrlSetInfos': [{
+        'signingUrls': [{
+            'email': 'pouet@truc.com',
+            'esignUrl': 'url'},
+            {'other': 'potential signer'}
+        ]}
+    ]}
+    mocker.patch.object(AdobeSignClient, 'get_signing_url',
+                        return_value=data)
+    mail, url = adobe_sign_backend.get_next_signer_url('12')
+    assert (mail, url) == ('pouet@truc.com', 'url')
+
+
+@pytest.mark.django_db
+def test_is_last_signer(adobe_sign_backend, minimal_signature):
+    signer1 = Signer(full_name='A', email='a@b.c', signing_order=1)
+    signer2 = Signer(full_name='B', email='d@e.f', signing_order=2)
+    signer2.signature = minimal_signature
+    signer1.signature = minimal_signature
+    signer2.save()
+    signer1.save()
+
+    assert not adobe_sign_backend.is_last_signer(signer1)
+    assert adobe_sign_backend.is_last_signer(signer2)
+
+
+def test_get_refuse_comment(mocker, adobe_sign_backend):
+    data = {
+        "events": [
+            {
+                "type": "CREATED",
+            },
+            {
+                "type": "ACTION_REQUESTED",
+            },
+            {
+                "type": "REJECTED",
+                "comment": "test comment message"
+            }
+        ]
+    }
+    mocker.patch.object(AdobeSignClient, 'get_events',
+                        return_value=data)
+    comment = adobe_sign_backend.get_refuse_comment('12')
+    assert comment == "test comment message"
+
+
+@pytest.mark.django_db
+def test_map_adobe_signer_to_signer(mocker, adobe_sign_backend,
+                                    minimal_signature):
+    # Check signer with same email are good mapped with the order
+    signer1 = Signer(full_name='Poney poney', email='poney@plop.com',
+                     signing_order=1)
+    signer2 = Signer(full_name='Pouet pouet', email='poney@plop.com',
+                     signing_order=2)
+    signer3 = Signer(full_name='Pouet pouet', email='poney@plop.com',
+                     signing_order=3)
+    signer3.signature = minimal_signature
+    signer2.signature = minimal_signature
+    signer1.signature = minimal_signature
+    signer3.save()
+    signer2.save()
+    signer1.save()
+
+    mocker.patch.object(
+        AdobeSignClient, 'get_members',
+        return_value={
+            'participantSets': [
+                # Check email are case sensitive case (only side Adobe)
+                {'memberInfos': [{'email': 'poney@plop.com'}],
+                 'id': 'foo2',
+                 'status': 'NOT_YET_VISIBLE',
+                 'order': 2},
+                {'memberInfos': [{'email': 'poney@plop.com'}],
+                 'id': 'foo1',
+                 'status': 'CANCELLED',
+                 'order': 1},
+                {'memberInfos': [{'email': 'poney@plop.com'}],
+                 'id': 'foo3',
+                 'status': 'CANCELLED',
+                 'order': 3}]})
+
+    adobe_sign_backend.map_adobe_signer_to_signer(minimal_signature)
+    signer1.refresh_from_db()
+    signer2.refresh_from_db()
+    signer3.refresh_from_db()
+    assert signer1.signature_backend_id == "foo1"
+    assert signer2.signature_backend_id == "foo2"
+    assert signer3.signature_backend_id == "foo3"
+    # No status change
+    assert signer1.current_status == "NOT_YET_VISIBLE"
+    assert signer2.current_status == "NOT_YET_VISIBLE"
+    assert signer3.current_status == "NOT_YET_VISIBLE"
