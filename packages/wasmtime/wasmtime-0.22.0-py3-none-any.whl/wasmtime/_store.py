92ec28bf1685f7621fbf5a6fb6e4f6a6120bd264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:59 +0000

    Add package wasmtime/0.22.0/wasmtime-0.22.0-py3-none-any.whl

---
+++ packages/wasmtime/wasmtime-0.22.0-py3-none-any.whl/wasmtime/_store.py
@@ -0,0 +1,73 @@
+from . import _ffi as ffi
+from ctypes import pointer
+from wasmtime import Engine, WasmtimeError
+
+
+class Store:
+    _ptr: "pointer[ffi.wasm_store_t]"
+
+    def __init__(self, engine: Engine = None):
+        if engine is None:
+            engine = Engine()
+        elif not isinstance(engine, Engine):
+            raise TypeError("expected an Engine")
+        self._ptr = ffi.wasm_store_new(engine._ptr)
+        self.engine = engine
+
+    def interrupt_handle(self) -> "InterruptHandle":
+        """
+        Creates a new interrupt handle through which execution of wasm can be
+        interrupted.
+
+        Raises a `WasmtimeError` if this store's configuration has not been
+        configured to enable interruption.
+
+        For more information about this be sure to consult the Rust documentation:
+        https://bytecodealliance.github.io/wasmtime/api/wasmtime/struct.Store.html#method.interrupt_handle
+        """
+
+        return InterruptHandle(self)
+
+    def gc(self) -> None:
+        """
+        Runs a GC over `externref` values that have been passed into this Store,
+        cleaning out anything that is no longer referenced.
+
+        This is not required to be run manually, but can be done so if you'd
+        like more precise control over when unreferenced `externref` values are
+        deallocated.
+        """
+        ffi.wasmtime_store_gc(self._ptr)
+
+    def __del__(self) -> None:
+        if hasattr(self, '_ptr'):
+            ffi.wasm_store_delete(self._ptr)
+
+
+class InterruptHandle:
+    """
+    A handle which can be used to interrupt executing WebAssembly code, forcing
+    it to trap.
+
+    For more information about this be sure to consult the Rust documentation:
+    https://bytecodealliance.github.io/wasmtime/api/wasmtime/struct.Store.html#method.interrupt_handle
+    """
+
+    def __init__(self, store: Store):
+        if not isinstance(store, Store):
+            raise TypeError("expected a Store")
+        ptr = ffi.wasmtime_interrupt_handle_new(store._ptr)
+        if not ptr:
+            raise WasmtimeError("interrupts not enabled on Store")
+        self._ptr = ptr
+
+    def interrupt(self) -> None:
+        """
+        Schedules an interrupt to be sent to interrupt this handle's store's
+        next (or current) execution of wasm code.
+        """
+        ffi.wasmtime_interrupt_handle_interrupt(self._ptr)
+
+    def __del__(self) -> None:
+        if hasattr(self, '_ptr'):
+            ffi.wasmtime_interrupt_handle_delete(self._ptr)
