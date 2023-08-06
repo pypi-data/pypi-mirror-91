f3fd66d9ea1a26c7c2dfb13d089b18f1e64bbebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:06 +0000

    Add package mpl-playback/0.1.0/mpl_playback-0.1.0-py3-none-any.whl

---
+++ packages/mpl-playback/mpl_playback-0.1.0-py3-none-any.whl/mpl_playback/util.py
@@ -0,0 +1,56 @@
+from matplotlib.figure import Figure
+from matplotlib.axes import Axes
+from matplotlib.axes import SubplotBase
+import numpy as np
+
+__all__ = [
+    "listify_dict",
+    "exec_no_show",
+    "extract_by_name",
+]
+
+
+def exec_no_show(filepath, globals=None, locals=None):
+    """
+    exec a file except for plt.show
+    """
+    if globals is None:
+        globals = {}
+    globals.update(
+        {
+            "__file__": filepath,
+            "__name__": "__main__",
+        }
+    )
+    with open(filepath) as file:
+        block = file.read()
+        block = block.replace("plt.show", "")
+        exec(compile(block, filepath, "exec"), globals)
+    return globals
+
+
+def listify_dict(d):
+    """
+    convert a dictionary to a list of names and values filtering only for objects
+    that we care about
+    """
+    names = []
+    objs = []
+    for k, v in d.items():
+        if isinstance(v, (list, tuple, dict, np.ndarray, Axes, SubplotBase, Figure)):
+            if isinstance(v, np.ndarray) and not v.dtype == np.object:
+                continue
+            if "__" in k:
+                continue
+            names.append(k)
+            objs.append(v)
+    return names, objs
+
+
+def extract_by_name(name, globals):
+    """
+    https://stackoverflow.com/a/63331128/835607
+    """
+    loc = {}
+    exec(f"__fig = {name}", globals, loc)
+    return loc["__fig"]
