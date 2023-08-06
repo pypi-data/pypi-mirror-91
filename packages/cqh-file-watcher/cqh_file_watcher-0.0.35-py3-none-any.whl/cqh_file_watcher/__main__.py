0a4570c1fa14b4bac750911aa8dcbd73e2511945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:14 +0000

    Add package cqh-file-watcher/0.0.35/cqh_file_watcher-0.0.35-py3-none-any.whl

---
+++ packages/cqh-file-watcher/cqh_file_watcher-0.0.35-py3-none-any.whl/cqh_file_watcher/__main__.py
@@ -0,0 +1,5 @@
+from cqh_file_watcher.run import main
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/cqh-file-watcher/cqh_file_watcher-0.0.35-py3-none-any.whl/cqh_file_watcher/conf.py
@@ -0,0 +1,118 @@
+doc = """
+                      cqh_file_watcher
+=============================================
+
+something like `File-Watcher` for vscode
+
+
+Usage
+-------------------------------------------------
+
+
+``cqh_file_watcher -c ***.conf``
+
+conf example
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+use pattern
+::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+.. code-block::
+
+    {"command_list":[
+        {
+            "pattern": "*.py",
+            "command": "sudo supervisorctl restart redis"
+        }
+    ],
+    "directory": "/home/vagrant/code/code1"
+    }
+
+no pattern
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+.. code-block::
+
+
+    {"command_list":[
+
+        {
+            "command": "echo things changed"
+        }
+    ],
+    "directory": "/home/vagrant/code/code1"
+    }
+
+directory for command
+::::::::::::::::::::::::::::::::::::::::::::
+
+
+.. code-block::
+
+    {"command_list":[
+        {
+            "pattern": "*.py",
+            "command": "sudo supervisorctl restart redis"
+            "directory":  "/home/vagrant"
+        }
+    ]
+    "directory": "/home/vagrant/code/code1"
+    }
+
+
+add ignore pattern for one
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+.. code-block::
+
+    {"command_list":[
+        {
+            "pattern": "*.py",
+            "ignore_pattern": ["_build/.*"],
+            "command": "sudo supervisorctl restart redis"
+            "directory":  "/home/vagrant"
+        }
+    ]
+    "directory": "/home/vagrant/code/code1"
+    }
+
+add ignore pattern for multi
+:::::::::::::::::::::::::::::::::::::::
+
+
+
+.. code-block::
+
+    {"command_list":[
+        {
+            "pattern": "*.py",
+            "ignore_pattern": ["_build/.*" , "_download/.*", "^css/.*", "^_static/.*"],
+            "command": "sudo supervisorctl restart redis"
+            "directory":  "/home/vagrant"
+        }
+    ]
+    "directory": "/home/vagrant/code/code1"
+    }
+
+use DIRECTORY env
+:::::::::::::::::::::::::::::::::::::::::::::::::
+
+
+.. code-block::
+
+    {"command_list":[
+        {
+            "pattern": "*.py",
+            "ignore_pattern": ["_build/.*" , "_download/.*", "^css/.*", "^_static/.*"],
+            "command": "sudo supervisorctl restart redis"
+            "directory":  "/home/vagrant"
+        }
+    ]
+    "directory": "${DIRECTORY}"
+    }
+
+
+                      """
