30d3b26513f47725b215c65220f2418e6249b079
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:12 +0000

    Add package kpler.sdk/1.0.3/kpler.sdk-1.0.3-py3-none-any.whl

---
+++ packages/kpler.sdk/kpler.sdk-1.0.3-py3-none-any.whl/kpler/sdk/resources/product.py
@@ -0,0 +1,45 @@
+from pandas import DataFrame
+
+from kpler.sdk import Platform
+from kpler.sdk.client import KplerClient
+from kpler.sdk.configuration import Configuration
+
+
+class Product(KplerClient):
+    """
+    The ``Product`` endpoint allows to perform full-text search on products,
+    in order to find names used in Kpler referential.
+    """
+
+    RESOURCE_NAME = "products"
+
+    AVAILABLE_PLATFORMS = [Platform.Dry, Platform.Liquids, Platform.LNG, Platform.LPG]
+
+    def __init__(self, configuration: Configuration, column_ids: bool = True, log_level=None):
+        super().__init__(configuration, self.AVAILABLE_PLATFORMS, column_ids, log_level)
+
+    def search(self, q: str) -> DataFrame:
+        """
+
+        Args:
+            q: str Argument to search by in products names
+
+        Examples:
+            >>>
+            from kpler.sdk.resources.product import Product
+            product_client=Product(config)
+            product_client.search("Arab")
+
+            .. csv-table::
+                :header: "products"
+
+                "Arab"
+                "Arab M"
+                "Arab M Abu Safah"
+                "Arab SLt."
+                "Arab XLt."
+                "Arab Hy."
+                "Arab Lt."
+        """
+        query_parameters = {"q": q, "resources": self.RESOURCE_NAME}
+        return self._search(query_parameters)
