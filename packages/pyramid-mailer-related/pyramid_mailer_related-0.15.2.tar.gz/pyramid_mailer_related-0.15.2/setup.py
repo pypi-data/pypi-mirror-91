9b4675215b931df3eefd1231c0991e91c3098049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:21 +0000

    Add package pyramid-mailer-related/0.15.2/pyramid_mailer_related-0.15.2.tar.gz

---
+++ packages/pyramid-mailer-related/pyramid_mailer_related-0.15.2.tar.gz/pyramid_mailer_related-0.15.2/setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup
+
+docs_extras = [
+    'Sphinx >= 1.8.1',
+    'docutils',
+    'repoze.sphinx.autointerface',
+    'pylons-sphinx-themes >= 1.0.10',
+    ]
+
+tests_require = []
+
+testing_extras = tests_require + [
+    'nose',
+    'coverage',
+    'WebTest',
+    ]
+
+try:
+    with open('README.rst') as f:
+        README = f.read()
+
+    with open('CHANGES.rst') as f:
+        CHANGES = f.read()
+
+except IOError:
+    README = ''
+    CHANGES = ''
+
+setup(
+    name='pyramid_mailer_related',
+    version='0.15.2',
+    license='BSD',
+    author='Dan Jacob',
+    author_email='danjac354@gmail.com',
+    description='Sendmail package for Pyramid',
+    long_description='\n\n'.join([README, CHANGES]),
+    url="https://docs.pylonsproject.org/projects/pyramid-mailer/en/latest/",
+    packages=[
+        'pyramid_mailer',
+    ],
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'pyramid',
+        'repoze.sendmail>=4.1',
+        'transaction',
+    ],
+    tests_require = tests_require,
+    extras_require = {
+        'testing':testing_extras,
+        'docs':docs_extras,
+        },
+    test_suite='pyramid_mailer',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        "Topic :: Communications :: Email",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Framework :: Pyramid",
+    ]
+)
