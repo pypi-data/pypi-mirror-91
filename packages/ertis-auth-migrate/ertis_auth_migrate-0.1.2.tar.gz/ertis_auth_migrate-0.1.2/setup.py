a41af0537101de163fa04992f34d18528482d389
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:40 +0000

    Add package ertis-auth-migrate/0.1.2/ertis_auth_migrate-0.1.2.tar.gz

---
+++ packages/ertis-auth-migrate/ertis_auth_migrate-0.1.2.tar.gz/ertis_auth_migrate-0.1.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("Readme.md", "r") as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="ertis_auth_migrate",
+    version="0.1.2",
+    scripts=["src/ertis_auth_migrate"],
+    author="ismetacar",
+    author_email="dismetacar@gmail.com",
+    description="Ertis Auth initializer CLI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ismetacar/ertis_auth_cli",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
