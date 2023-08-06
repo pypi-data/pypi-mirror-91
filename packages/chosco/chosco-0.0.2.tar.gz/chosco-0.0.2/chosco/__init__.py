6a93273395207f7792b701ef6ca66a56b3d6132e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:44 +0000

    Add package chosco/0.0.2/chosco-0.0.2.tar.gz

---
+++ packages/chosco/chosco-0.0.2.tar.gz/chosco-0.0.2/chosco/__init__.py
@@ -0,0 +1,3 @@
+from chosco.annotation.domain.annotation import Annotation
+from chosco.annotation.domain.annotation_repository import AnnotationRepository
+from chosco.media.retriever.domain.media_retriever import MediaRetriever
