50680f574bf068758bf8ea345996f3e8f59928e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:52 +0000

    Add package seven-cloudapp/1.0.141/seven_cloudapp-1.0.141.tar.gz

---
+++ packages/seven-cloudapp/seven_cloudapp-1.0.141.tar.gz/seven_cloudapp-1.0.141/seven_cloudapp/handlers/client/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+:Author: HuangJingCan
+:Date: 2020-05-19 11:59:38
+:LastEditTime: 2020-11-20 16:17:52
+:LastEditors: HuangJingCan
+:description: 
+"""
+__all__ = ["act", "address", "app", "goods", "lottery", "machine", "prize", "theme", "user"]
