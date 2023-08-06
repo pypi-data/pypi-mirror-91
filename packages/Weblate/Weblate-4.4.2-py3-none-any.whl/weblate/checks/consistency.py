a9c8caba889cdd3787212c58c3a5285982b7ccae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:10 +0000

    Add package Weblate/4.4.2/Weblate-4.4.2-py3-none-any.whl

---
+++ packages/Weblate/Weblate-4.4.2-py3-none-any.whl/weblate/checks/consistency.py
@@ -0,0 +1,136 @@
+#
+# Copyright © 2012 - 2021 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from django.utils.translation import gettext_lazy as _
+
+from weblate.checks.base import TargetCheck
+
+
+class PluralsCheck(TargetCheck):
+    """Check for incomplete plural forms."""
+
+    check_id = "plurals"
+    name = _("Missing plurals")
+    description = _("Some plural forms are not translated")
+
+    def check_target_unit(self, sources, targets, unit):
+        # Is this plural?
+        if len(sources) == 1:
+            return False
+        # Is at least something translated?
+        if targets == len(targets) * [""]:
+            return False
+        # Check for empty translation
+        return "" in targets
+
+    def check_single(self, source, target, unit):
+        """We don't check target strings here."""
+        return False
+
+
+class SamePluralsCheck(TargetCheck):
+    """Check for same plural forms."""
+
+    check_id = "same-plurals"
+    name = _("Same plurals")
+    description = _("Some plural forms are translated in the same way")
+
+    def check_target_unit(self, sources, targets, unit):
+        # Is this plural?
+        if len(sources) == 1 or len(targets) == 1:
+            return False
+        if targets[0] == "":
+            return False
+        return len(set(targets)) == 1
+
+    def check_single(self, source, target, unit):
+        """We don't check target strings here."""
+        return False
+
+
+class ConsistencyCheck(TargetCheck):
+    """Check for inconsistent translations."""
+
+    check_id = "inconsistent"
+    name = _("Inconsistent")
+    description = _(
+        "This string has more than one translation in this project "
+        "or is not translated in some components."
+    )
+    ignore_untranslated = False
+    propagates = True
+
+    def check_target_unit(self, sources, targets, unit):
+        if not unit.translation.component.allow_translation_propagation:
+            return False
+        for other in unit.same_source_units:
+            if unit.target == other.target:
+                continue
+            if unit.translated or other.translated:
+                return True
+        return False
+
+    def check_single(self, source, target, unit):
+        """We don't check target strings here."""
+        return False
+
+
+class TranslatedCheck(TargetCheck):
+    """Check for inconsistent translations."""
+
+    check_id = "translated"
+    name = _("Has been translated")
+    description = _("This string has been translated in the past")
+    ignore_untranslated = False
+
+    def get_description(self, check_obj):
+        unit = check_obj.unit
+        target = self.check_target_unit(unit.source, unit.target, unit)
+        if not target:
+            return super().get_description(check_obj)
+        return _('Last translation was "%s".') % target
+
+    def check_target_unit(self, sources, targets, unit):
+        if unit.translated:
+            return False
+
+        from weblate.trans.models import Change
+
+        states = {Change.ACTION_SOURCE_CHANGE}
+        states.update(Change.ACTIONS_CONTENT)
+
+        changes = unit.change_set.filter(action__in=states).order()
+
+        for action, target in changes.values_list("action", "target"):
+            if action in Change.ACTIONS_CONTENT and target:
+                return target
+            if action == Change.ACTION_SOURCE_CHANGE:
+                break
+
+        return False
+
+    def check_single(self, source, target, unit):
+        """We don't check target strings here."""
+        return False
+
+    def get_fixup(self, unit):
+        target = self.check_target_unit(unit.source, unit.target, unit)
+        if not target:
+            return None
+        return [(".*", target, "u")]
---
+++ packages/Weblate/Weblate-4.4.2-py3-none-any.whl/weblate/utils/docs.py
@@ -0,0 +1,56 @@
+#
+# Copyright © 2012 - 2021 Michal Čihař <michal@cihar.com>
+#
+# This file is part of Weblate <https://weblate.org/>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
+from django.conf import settings
+from django.utils.translation import get_language
+
+import weblate
+
+# Enabled languages in the docs
+LANGMAP = {
+    "zh-hans": "zh_CN",
+    "pt-br": "pt_BR",
+    "uk": "uk",
+    "ru": "ru",
+    "es": "es",
+    "pt": "pt",
+    "nb": "no",
+    "ja": "ja",
+    "fr": "fr",
+}
+
+
+def get_doc_url(page, anchor="", user=None):
+    """Return URL to documentation."""
+    # Should we use tagged release or latest version
+    if "-dev" in weblate.VERSION or (
+        (user is None or not user.is_authenticated) and settings.HIDE_VERSION
+    ):
+        version = "latest"
+    else:
+        version = f"weblate-{weblate.VERSION}"
+    # Language variant
+    code = LANGMAP.get(get_language(), "en")
+    # Generate URL
+    url = f"https://docs.weblate.org/{code}/{version}/{page}.html"
+    # Optionally append anchor
+    if anchor != "":
+        url += "#{}".format(anchor.replace("_", "-"))
+
+    return url
