3409e8544759314baea29ec09e4004aa53544e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:00 +0000

    Add package OASYS1-WOFRY/1.0.23/OASYS1-WOFRY-1.0.23.tar.gz

---
+++ packages/OASYS1-WOFRY/OASYS1-WOFRY-1.0.23.tar.gz/OASYS1-WOFRY-1.0.23/orangecontrib/wofry/util/wofry_objects.py
@@ -0,0 +1,21 @@
+from syned.beamline.beamline import Beamline
+from wofry.propagator.wavefront import Wavefront
+
+class WofryData(object):
+    def __init__(self, beamline=None, wavefront=None):
+        super().__init__()
+        if beamline is None:
+            self.__beamline = Beamline()
+        else:
+            self.__beamline = beamline
+
+        if wavefront is None:
+            self.__wavefront = Wavefront()
+        else:
+            self.__wavefront = wavefront
+
+    def get_beamline(self):
+        return self.__beamline
+
+    def get_wavefront(self):
+        return self.__wavefront
