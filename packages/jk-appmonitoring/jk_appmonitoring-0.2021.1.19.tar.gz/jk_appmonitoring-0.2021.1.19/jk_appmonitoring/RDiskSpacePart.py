f06b2f47ff40e932fb788d073956196d5ca415bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:17 +0000

    Add package jk-appmonitoring/0.2021.1.19/jk_appmonitoring-0.2021.1.19.tar.gz

---
+++ packages/jk-appmonitoring/jk_appmonitoring-0.2021.1.19.tar.gz/jk_appmonitoring-0.2021.1.19/jk_appmonitoring/RDiskSpacePart.py
@@ -0,0 +1,111 @@
+
+
+import typing
+import time
+
+import jk_typing
+import jk_utils
+import jk_prettyprintobj
+
+
+
+
+
+#
+# This class is an informational class. It provides information about backup volumes.
+#
+class RDiskSpacePart(jk_prettyprintobj.DumpMixin):
+
+	################################################################################################################################
+	## Constructor Method
+	################################################################################################################################
+
+	@jk_typing.checkFunctionSignature()
+	def __init__(self, name:str, dirPath:typing.Union[str,None], diskSpaceUsed:int, fsSizeTotal:int, partType:str):
+		self.__name = name
+		self.__dirPath = dirPath
+		self.__nDiskSpaceUsed = diskSpaceUsed
+		self.__nSizeTotal = fsSizeTotal
+		self.__partType = partType
+	#
+
+	################################################################################################################################
+	## Public Properties
+	################################################################################################################################
+
+	#
+	# The type of this disk usage part
+	#
+	# @return	str		Returns the type of this part:
+	#					* "free" if this reflects free disk space
+	#					* "reserved" if this reflects disk space reserved for root
+	#					* "usedDir" if this reflects disk space used based on specified directory trees
+	#					* "usedOther" if this reflects disk space used by other files
+	#
+	@property
+	def partType(self) -> str:
+		return self.__partType
+	#
+
+	@property
+	def name(self) -> str:
+		return self.__name
+	#
+
+	@property
+	def dirPath(self) -> str:
+		return self.__dirPath
+	#
+
+	@property
+	def diskSpaceUsed(self) -> int:
+		return self.__nDiskSpaceUsed
+	#
+
+	@property
+	def diskSpaceUsedPercent(self) -> float:
+		return self.__nDiskSpaceUsed / self.__nSizeTotal * 100
+	#
+
+	@property
+	def diskSpaceUsedFraction(self) -> float:
+		return self.__nDiskSpaceUsed / self.__nSizeTotal
+	#
+
+	@property
+	def fsSizeTotal(self) -> int:
+		if self.__nSizeTotal < 0:
+			self.update()
+		return self.__nSizeTotal
+	#
+
+	################################################################################################################################
+	## Helper Methods
+	################################################################################################################################
+
+	def _dumpVarNames(self) -> list:
+		return [
+			"name",
+			"partType",
+			"dirPath",
+			"diskSpaceUsed",
+			"diskSpaceUsedPercent",
+			"fsSizeTotal",
+		]
+	#
+
+	################################################################################################################################
+	## Public Methods
+	################################################################################################################################
+
+#
+
+
+
+
+
+
+
+
+
+
---
+++ packages/jk-appmonitoring/jk_appmonitoring-0.2021.1.19.tar.gz/jk_appmonitoring-0.2021.1.19/jk_appmonitoring/RFileSystemCollection.py
@@ -0,0 +1,105 @@
+
+
+
+import time
+
+import jk_typing
+import jk_utils
+import jk_prettyprintobj
+
+from .RFileSystem import RFileSystem
+from .RDirectory import RDirectory
+
+
+
+
+
+
+class RFileSystemCollection(jk_prettyprintobj.DumpMixin):
+
+	################################################################################################################################
+	## Constructor Method
+	################################################################################################################################
+
+	@jk_typing.checkFunctionSignature()
+	def __init__(self):
+		self.__fileSystems = {}
+	#
+
+	################################################################################################################################
+	## Public Properties
+	################################################################################################################################
+
+	@property
+	def filesystems(self) -> tuple:
+		ret = sorted(self.__fileSystems.values(), key=lambda x: x.mountPoint)
+		return tuple(ret)
+	#
+
+	@property
+	def hasData(self) -> bool:
+		if self.__fileSystems:
+			for fs in self.__fileSystems:
+				if not fs.hasData:
+					return False
+			return True
+		else:
+			return False
+	#
+
+	################################################################################################################################
+	## Helper Methods
+	################################################################################################################################
+
+	def _dumpVarNames(self) -> list:
+		return [
+			"filesystems",
+		]
+	#
+
+	################################################################################################################################
+	## Public Methods
+	################################################################################################################################
+
+	def clear(self):
+		self.__fileSystems.clear()
+	#
+
+	@jk_typing.checkFunctionSignature()
+	def registerDirectory(self, directory:RDirectory):
+		assert isinstance(directory, RDirectory)
+		if directory.mountPoint not in self.__fileSystems:
+			raise Exception("Mount point not registered: " + directory.mountPoint)
+		self.__fileSystems[directory.mountPoint]._registerDirectory(directory)
+	#
+
+	@jk_typing.checkFunctionSignature()
+	def registerFileSystem(self, fs:RFileSystem):
+		self.__fileSystems[fs.mountPoint] = fs
+	#
+
+	def hasMountPoint(self, mountPoint:str):
+		return mountPoint in self.__fileSystems
+	#
+
+	def update(self):
+		for fs in self.__fileSystems.values():
+			fs.update()
+	#
+
+	def invalidate(self):
+		for fs in self.__fileSystems.values():
+			fs.invalidate()
+	#
+
+#
+
+
+
+
+
+
+
+
+
+
