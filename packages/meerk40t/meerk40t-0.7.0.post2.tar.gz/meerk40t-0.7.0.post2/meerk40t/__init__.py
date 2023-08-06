6036f2f3843517029165e505363c304b1fb5d1c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:40 +0000

    Add package meerk40t/0.7.0.post2/meerk40t-0.7.0.post2.tar.gz

---
+++ packages/meerk40t/meerk40t-0.7.0.post2.tar.gz/meerk40t-0.7.0.post2/meerk40t/__init__.py
@@ -0,0 +1,4 @@
+name = "meerk40t"
+
+from .main import *
+
---
+++ packages/meerk40t/meerk40t-0.7.0.post2.tar.gz/meerk40t-0.7.0.post2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    install_requires=[
+        "pyusb>=1.0.0",
+        "Pillow>=8.0.0",
+        "wxPython>=4.0.0",
+    ],
+)
