a705ad0ff00de1105b00da971c6105ddceeb95ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:01 +0000

    Add package tacview-client/0.1.71/tacview-client-0.1.71.tar.gz

---
+++ packages/tacview-client/tacview-client-0.1.71.tar.gz/tacview-client-0.1.71/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['tacview_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['SQLAlchemy==1.4.0b1',
+ 'asyncpg>=0.20.1,<0.21.0',
+ 'click>=7.1.2,<8.0.0',
+ 'pytz>=2020.5,<2021.0',
+ 'typer>=0.3.2,<0.4.0']
+
+entry_points = \
+{'console_scripts': ['tacview = tacview_client.cli:app']}
+
+setup_kwargs = {
+    'name': 'tacview-client',
+    'version': '0.1.71',
+    'description': '',
+    'long_description': None,
+    'author': 'mcdelaney',
+    'author_email': 'mcdelaney@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mcdelaney/py-tacview-client.git',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<3.9',
+}
+
+
+setup(**setup_kwargs)
