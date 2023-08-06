079684d5e558a224db17a041e9536463402e1e5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:39 +0000

    Add package cuenca/0.4.3.dev2/cuenca-0.4.3.dev2.tar.gz

---
+++ packages/cuenca/cuenca-0.4.3.dev2.tar.gz/cuenca-0.4.3.dev2/cuenca/exc.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+from cuenca_validations.typing import DictStrAny
+
+
+class CuencaException(Exception):
+    ...
+
+
+class NoResultFound(CuencaException):
+    """No results were found"""
+
+
+class MultipleResultsFound(CuencaException):
+    """One result was expected but multiple were returned"""
+
+
+@dataclass
+class CuencaResponseException(CuencaException):
+    json: DictStrAny
+    status_code: int
+
+    def __str__(self) -> str:
+        return repr(self)
