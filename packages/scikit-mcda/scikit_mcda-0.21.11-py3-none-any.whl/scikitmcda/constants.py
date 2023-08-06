0410e9ac4f85a53cbcf01270dfd7229b78f2d650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:38 +0000

    Add package scikit-mcda/0.21.11/scikit_mcda-0.21.11-py3-none-any.whl

---
+++ packages/scikit-mcda/scikit_mcda-0.21.11-py3-none-any.whl/scikitmcda/constants.py
@@ -0,0 +1,17 @@
+MAX = +1
+MIN = -1
+
+ZScore_ = "ZScore"
+MinMax_ = "MinMax"
+Logistic_ = "Logistic"
+Max_ = "Max"
+Sum_ = "Sum"
+RootSumSquared_ = "RootSumSquared"
+
+NORMALIZATION_METHODS = [ZScore_,
+                         MinMax_,
+                         Logistic_,
+                         Max_,
+                         Sum_,
+                         RootSumSquared_]
+                         
