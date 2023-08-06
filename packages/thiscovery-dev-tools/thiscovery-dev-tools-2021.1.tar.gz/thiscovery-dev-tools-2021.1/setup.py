57903f8f2c69a30fa7a4cbdddfd6d31da2c0ea7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:21 +0000

    Add package thiscovery-dev-tools/2021.1/thiscovery-dev-tools-2021.1.tar.gz

---
+++ packages/thiscovery-dev-tools/thiscovery-dev-tools-2021.1.tar.gz/thiscovery-dev-tools-2021.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="thiscovery-dev-tools", # Replace with your own username
+    version="2021.1",
+    author="Thiscovery team",
+    author_email="support@thiscovery.org",
+    description="Thiscovery development tools",
+    install_requires=[
+        'thiscovery-lib',
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/THIS-Institute/thiscovery-dev-tools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
