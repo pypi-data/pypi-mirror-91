3d9064ebfe879ed177084e5b286ddf07a30e3ff4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:43 +0000

    Add package biobrary/0.0.1/biobrary-0.0.1.tar.gz

---
+++ packages/biobrary/biobrary-0.0.1.tar.gz/biobrary-0.0.1/README.md
@@ -0,0 +1,9 @@
+# biolib
+Python3 library for bioinformatics.
+
+## bioinformatic file parse.
+* biopaser  
+      Fasta_parser
+* biocondon
+* amino_acids_mw
+* CircleNond
---
+++ packages/biobrary/biobrary-0.0.1.tar.gz/biobrary-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="biobrary",
+    version="0.0.1",
+    description="library for bioinformatics.",
+    url="https://github.com/benjaminfang/biobrary",
+    author="Benjamin Fang",
+    author_email="benjaminfang.ol@outlook.com",
+    license='MIT',
+    python_requires=">=3.7",
+    classifers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="bioinformatics, file_parser",
+    packages=setuptools.find_packages(),
+
+)
