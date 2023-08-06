e3217ba325a77c28eae7bfd27b7f859aea3b0caf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:34 +0000

    Add package mohammedyasser/0.0.1/mohammedyasser-0.0.1.tar.gz

---
+++ packages/mohammedyasser/mohammedyasser-0.0.1.tar.gz/mohammedyasser-0.0.1/mohammedyasser/__init__.py
@@ -0,0 +1,9 @@
+def add(num1,num2):
+    print(num1+num2)
+def subtract(num1,num2):
+    print(num1-num2)
+
+def multiplicatin(num1,num2):
+    print(num1*num2)
+def division(num1,num2):
+    print(num1/num2)
