e0cb34558a662631d8ed81369e5ce4c0962abffb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:31 +0000

    Add package k-ege/0.0/k_ege-0.0.tar.gz

---
+++ packages/k-ege/k_ege-0.0.tar.gz/k_ege-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='k_ege',
+      version='0.0',
+      description='Gaussian and Binomial distributions',
+      packages=['dist_alx'],
+      author_email='stepan.kazancev.04@bk.ru',
+      zip_safe=False)
