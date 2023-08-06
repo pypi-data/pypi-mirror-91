ae6370e58f815e8e6b3a5ece62fefbfe2eabf291
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:24 +0000

    Add package dwcl/1.0.3/dwcl-1.0.3.tar.gz

---
+++ packages/dwcl/dwcl-1.0.3.tar.gz/dwcl-1.0.3/setup.py
@@ -0,0 +1,27 @@
+import json
+import os
+from setuptools import setup
+
+
+with open("package.json") as f:
+    package = json.load(f)
+
+with open("README.md") as f:
+    long_description = f.read()
+
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+
+setup(
+    name=package_name,
+    version=package["version"],
+    author=package["author"],
+    url="https://github.com/raymon-ai/dwcl",
+    packages=[package_name],
+    include_package_data=True,
+    license=package["license"],
+    description=package.get("description", package_name),
+    install_requires=[],
+    classifiers=["Framework :: Dash",],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
