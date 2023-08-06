f480257b78655831ba8e52028aaa11edb8a54b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:38 +0000

    Add package whale-pipelines/1.5.1/whale-pipelines-1.5.1.tar.gz

---
+++ packages/whale-pipelines/whale-pipelines-1.5.1.tar.gz/whale-pipelines-1.5.1/whale/utils/markdown_delimiters.py
@@ -0,0 +1,9 @@
+COLUMN_DETAILS_DELIMITER = "## Column details"
+PARTITIONS_DELIMITER = "## Partition info"
+USAGE_DELIMITER = "## Usage info"
+UGC_DELIMITER = "-" * 79
+DEFINED_METRICS_DELIMITER = "```metrics"
+METRICS_DELIMITER = "## Metrics"
+SQL_BLOCK_DELIMITER = "```sql\n"
+TAGS_DELIMITER = "## Tags"
+BLOCK_END_DELIMITER = "```"
