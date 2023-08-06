5c2cd083a60c27ee584f3f489f7bf4705cd83014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:03 +0000

    Add package sansio-lsp-client/0.8.0/sansio-lsp-client-0.8.0.tar.gz

---
+++ packages/sansio-lsp-client/sansio-lsp-client-0.8.0.tar.gz/sansio-lsp-client-0.8.0/sansio_lsp_client/__init__.py
@@ -0,0 +1,8 @@
+"""An implementation of the client side of the LSP protocol, useful for embedding easily in your editor."""
+
+from .client import *
+from .events import *
+from .structs import *
+from .utils import *
+
+__version__ = "0.8.0"
