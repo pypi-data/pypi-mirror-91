9831483788d84fd4e72f2534bd800aa81e48cdcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:17 +0000

    Add package heritrix3/0.4.0/heritrix3-0.4.0-py2.py3-none-any.whl

---
+++ packages/heritrix3/heritrix3-0.4.0-py2.py3-none-any.whl/heritrix3/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.0"
+
+# ---------------------------------------------------------------------------
+
+
+from .api import HeritrixAPI  # noqa: F401
+from .api import HeritrixAPIError  # noqa: F401
+from .api import disable_ssl_warnings  # noqa: F401
+
+# ---------------------------------------------------------------------------
