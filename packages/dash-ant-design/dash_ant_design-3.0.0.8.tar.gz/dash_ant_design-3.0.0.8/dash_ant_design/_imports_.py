1740ddd2065f3abf3cd73c0f25104a78310a9d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:05 +0000

    Add package dash-ant-design/3.0.0.8/dash_ant_design-3.0.0.8.tar.gz

---
+++ packages/dash-ant-design/dash_ant_design-3.0.0.8.tar.gz/dash_ant_design-3.0.0.8/dash_ant_design/_imports_.py
@@ -0,0 +1,21 @@
+from .AutoComplete import AutoComplete
+from .Button import Button
+from .DatePicker import DatePicker
+from .Icon import Icon
+from .Tag import Tag
+from .TimePicker import TimePicker
+from .Tooltip import Tooltip
+from .Transfer import Transfer
+from .TreeSelect import TreeSelect
+
+__all__ = [
+    "AutoComplete",
+    "Button",
+    "DatePicker",
+    "Icon",
+    "Tag",
+    "TimePicker",
+    "Tooltip",
+    "Transfer",
+    "TreeSelect"
+]
