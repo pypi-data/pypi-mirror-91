7e4aaba6a02b58da194e7a5011b0c82a0e4f01e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:30:05 +0000

    Add package joshsqlite/0.0.3/joshsqlite-0.0.3.tar.gz

---
+++ packages/joshsqlite/joshsqlite-0.0.3.tar.gz/joshsqlite-0.0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+
+setup_args = dict(
+    name='joshsqlite',
+    version='0.0.3',
+    description='A thread safe DSL for sqlite3.',
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license='MIT',
+    packages=find_packages(),
+    author='Josh Arnold',
+    author_email='dev@josharnold.me',
+    keywords=['Sqlite3', 'Thread', 'Safe', 'DSL', 'Wrapper'],
+    python_requires='>=3.6',
+    url='https://github.com/josharnoldjosh/thread_safe_sqlite',    
+)
+
+
+if __name__ == '__main__':
+    setup(**setup_args)
