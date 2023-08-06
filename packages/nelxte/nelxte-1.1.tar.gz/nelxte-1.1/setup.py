7492c9d9d1a656186b4fe4beda085b1b3eb82340
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:48 +0000

    Add package nelxte/1.1/nelxte-1.1.tar.gz

---
+++ packages/nelxte/nelxte-1.1.tar.gz/nelxte-1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+setup(
+    name='nelxte',
+    packages=find_packages(include=['nelxte']),
+    version='1.1',
+    download_url = 'https://github.com/amay428/nelxte_lib/archive/1.1.tar.gz',
+    description='Nelxte is a fast and open source python library for checking password strength.', 
+    author='Amay Agarwal',
+    license='MIT'
+)
