190d2a5073a480dfbb5f46646c8ecb5c903c6f6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:47 +0000

    Add package experiment-collection-server/0.2.3/experiment_collection_server-0.2.3.tar.gz

---
+++ packages/experiment-collection-server/experiment_collection_server-0.2.3.tar.gz/experiment_collection_server-0.2.3/setup.py
@@ -0,0 +1,26 @@
+import os
+import subprocess
+
+import sys
+
+
+def call(*args, shell=False):
+    proc = subprocess.run(args, shell=shell, check=False)
+    if proc.returncode:
+        print('Error: ', *args)
+
+
+print('use setup_client.py or setup_server.py', file=sys.stderr)
+
+setup_dir = os.path.dirname(os.path.abspath(__file__))
+setup_client = os.path.join(setup_dir, 'setup_client.py')
+setup_server = os.path.join(setup_dir, 'setup_server.py')
+
+if os.path.exists(setup_client):
+    call_args = sys.argv.copy()
+    call_args[0] = setup_client
+    call('python3', *call_args)
+if os.path.exists(setup_server):
+    call_args = sys.argv.copy()
+    call_args[0] = setup_server
+    call('python3', *call_args)
