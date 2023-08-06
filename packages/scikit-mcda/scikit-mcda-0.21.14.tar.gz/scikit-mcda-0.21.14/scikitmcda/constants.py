1a451b809fcb7b9085ea7c0aa815b14cbeded1b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:49 +0000

    Add package scikit-mcda/0.21.14/scikit-mcda-0.21.14.tar.gz

---
+++ packages/scikit-mcda/scikit-mcda-0.21.14.tar.gz/scikit-mcda-0.21.14/scikitmcda/constants.py
@@ -0,0 +1,17 @@
+MAX = +1
+MIN = -1
+
+LinearMinMax_ = "LinearMinMax"
+LinearMax_ = "LinearMax"
+LinearSum_ = "LinearSum"
+Vector_ = "Vector"
+EnhancedAccuracy_ = "EnhancedAccuracy"
+Logarithmic_ = "Logarithmic"
+
+NORMALIZATION_METHODS = [LinearMinMax_,
+                         LinearMax_,
+                         LinearSum_,
+                         Vector_,
+                         EnhancedAccuracy_,
+                         Logarithmic_]
+                         
