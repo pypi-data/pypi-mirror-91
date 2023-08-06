335fa3e6f9b979eb86b9ef39d5a0d627141014f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:35 +0000

    Add package NewsAgent/1.0/NewsAgent-1.0-py3-none-any.whl

---
+++ packages/NewsAgent/NewsAgent-1.0-py3-none-any.whl/NewsAgent/newsitems.py
@@ -0,0 +1,7 @@
+class NewsItem:
+    """
+    A simple news item consisting of a title and body text.
+    """
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body.replace("=92", "'")
