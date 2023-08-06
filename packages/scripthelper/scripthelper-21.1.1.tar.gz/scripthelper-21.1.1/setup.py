570a78da33d56f7cef88bf47a49f116ef96ea901
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:50 +0000

    Add package scripthelper/21.1.1/scripthelper-21.1.1.tar.gz

---
+++ packages/scripthelper/scripthelper-21.1.1.tar.gz/scripthelper-21.1.1/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scripthelper",
+    version="21.1.1",
+    scripts=[],
+    author="Máté Farkas",
+    author_email="fm@farkas-mate.hu",
+    description="Helper module for creating simple Python 3 scripts",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/presidento/scripthelper",
+    packages=["scripthelper"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "tqdm >= 4.31.1",
+        "coloredlogs >= 10.0",
+        "verboselogs >= 1.7",
+        "traceback_with_variables >= 2.0.1",
+        "prettyprinter >= 0.18.0",
+    ],
+)
