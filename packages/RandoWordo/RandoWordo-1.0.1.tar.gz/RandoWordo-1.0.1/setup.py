5c8cd8ff6e90ac142de963fcbf9aaf349f15b7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:21 +0000

    Add package RandoWordo/1.0.1/RandoWordo-1.0.1.tar.gz

---
+++ packages/RandoWordo/RandoWordo-1.0.1.tar.gz/RandoWordo-1.0.1/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+
+setuptools.setup(
+    name ="RandoWordo",
+    version="1.0.1",
+    author = "Ewen",
+    author_email = "1lorimerewe2@gmail.com",
+    description = "a simple random word generator",
+    long_description = "All you need to know is RandWord(textfile.txt), or randoword() if you don't have a pre defined text file \
+        \ the default text file does contair swears and NSFW words",
+    long_description_content_type = "text/markdown",
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        "": ["*.txt"],
+    },
+    packages= setuptools.find_packages(),
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+
+        
+        "Operating System :: OS Independent"
+
+    ],
+    python_requires='<=3.9',
+)
+
