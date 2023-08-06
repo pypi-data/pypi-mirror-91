1fbf8e92a5e542b75817227c9e91b6d5c748a86a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:38 +0000

    Add package pycldf/1.18.0/pycldf-1.18.0-py2.py3-none-any.whl

---
+++ packages/pycldf/pycldf-1.18.0-py2.py3-none-any.whl/pycldf/validators.py
@@ -0,0 +1,43 @@
+import re
+import functools
+
+
+def valid_references(dataset, table, column, row):
+    if dataset.sources:
+        dataset.sources.validate(row[column.name])
+
+
+def valid_regex(pattern, name, dataset, table, column, row):
+    value = row[column.name]
+    if value is not None and not pattern.match(value):
+        raise ValueError('invalid {0}: {1}'.format(name, value))
+
+
+def valid_igt(dataset, table, column, row):
+    gloss, morphemes = row[column.name], None
+    col = table.get_column('http://cldf.clld.org/v1.0/terms.rdf#analyzedWord')
+    if col:
+        morphemes = row[col.name]
+
+    if gloss and morphemes and len(gloss) != len(morphemes):
+        raise ValueError('number of morphemes and glosses does not match')
+
+
+VALIDATORS = [
+    (
+        None,
+        'http://cldf.clld.org/v1.0/terms.rdf#iso639P3code',
+        functools.partial(valid_regex, re.compile(r'[a-z]{3}$'), 'ISO 639-3 code')),
+    (
+        None,
+        'http://cldf.clld.org/v1.0/terms.rdf#glottocode',
+        functools.partial(valid_regex, re.compile(r'[a-z0-9]{4}[0-9]{4}$'), 'glottocode')),
+    (
+        None,
+        'http://cldf.clld.org/v1.0/terms.rdf#gloss',
+        valid_igt),
+    (
+        None,
+        'http://cldf.clld.org/v1.0/terms.rdf#source',
+        valid_references)
+]
