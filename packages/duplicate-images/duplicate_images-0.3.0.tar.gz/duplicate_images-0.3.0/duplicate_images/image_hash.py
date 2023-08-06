28b87a65e2d19e919bab38c5b97087fd69008939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:21 +0000

    Add package duplicate-images/0.3.0/duplicate_images-0.3.0.tar.gz

---
+++ packages/duplicate-images/duplicate_images-0.3.0.tar.gz/duplicate_images-0.3.0/duplicate_images/image_hash.py
@@ -0,0 +1,28 @@
+__author__ = 'Lene Preuss <lene.preuss@gmail.com>'
+
+from typing import Callable
+
+import imagehash
+from PIL import Image
+
+from duplicate_images.image_wrapper import ImageWrapper
+
+MAX_DIMENSION = 200
+
+IMAGE_HASH_ALGORITHM = {
+    'ahash': imagehash.average_hash,
+    'colorhash': imagehash.colorhash,
+    'dhash': imagehash.dhash,
+    'phash': imagehash.phash,
+    'whash': imagehash.whash
+}
+
+
+def is_similar(
+        image: ImageWrapper, other_image: ImageWrapper, hash_func: Callable[[Image.Image], int]
+) -> bool:
+    if not image.valid or not other_image.valid:
+        return False
+    hash_distance = hash_func(image.resized_image) - hash_func(other_image.resized_image)
+    # print(f'hash_distance {image.file.stem}, {other_image.file.stem} -> {hash_distance}')
+    return hash_distance == 0
