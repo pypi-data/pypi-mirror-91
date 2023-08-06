98dd9b0b44288f8f287325bbba3a94a127894b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:46 +0000

    Add package aok/0.2.0/aok-0.2.0.tar.gz

---
+++ packages/aok/aok-0.2.0.tar.gz/aok-0.2.0/aok/_operations.py
@@ -0,0 +1,39 @@
+import typing
+
+from aok import comparisons
+from aok import _definitions
+
+
+def to_comparator(value: typing.Any) -> "_definitions.Comparator":
+    """
+    Converts the value to a default comparator if not already one. If the value
+    is already a comparator, it is returned unchanged.
+    """
+    if isinstance(value, _definitions.Comparator):
+        return value
+    elif isinstance(value, dict):
+        return comparisons.Dict(value)
+    elif isinstance(value, list):
+        return comparisons.List(value)
+    elif isinstance(value, tuple):
+        return comparisons.Tuple(value)
+
+    return comparisons.Equals(value)
+
+
+def cast_compatible(
+    expectation_value: typing.Any,
+    observed_value: typing.Any,
+) -> typing.Any:
+    """Casts by compatible type, which is limited."""
+    ex = expectation_value
+    ob = observed_value
+
+    if isinstance(ob, int) and isinstance(ex, str):
+        return int(ex)
+    elif isinstance(ob, float) and isinstance(ex, str):
+        return float(ex)
+    elif isinstance(ob, bool) and ex in ("true", "false"):
+        return ex == "true"
+
+    return ex
