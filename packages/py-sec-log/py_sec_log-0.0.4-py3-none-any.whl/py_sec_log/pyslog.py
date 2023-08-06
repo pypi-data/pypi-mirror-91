11a18971ba217279602524e9330e5e76ab6dc531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:45 +0000

    Add package py-sec-log/0.0.4/py_sec_log-0.0.4-py3-none-any.whl

---
+++ packages/py-sec-log/py_sec_log-0.0.4-py3-none-any.whl/py_sec_log/pyslog.py
@@ -0,0 +1,36 @@
+import requests, os, sys
+from licensing import methods
+from licensing.methods import Helpers
+import hashlib
+class login:
+    k=Helpers.GetMachineCode()
+    ke=k.encode('utf-8')
+    ey=hashlib.md5(ke)
+    global key
+    key=ey.hexdigest()
+    def auth(auth_id):
+        if auth_id=='':
+            print('Please Enter auth_id')
+            sys.exit()
+        else:
+            global r
+        r=requests.get('https://pastebin.com/'+auth_id)
+    def verify(username, password):
+        if username=='' and password=='' or username=='' or password=='':
+            print('Need Username And Password For Login!')
+            sys.exit()
+        else:
+            pass
+        ins=username+password
+        sec=ins.encode('utf-8')
+        secu=hashlib.md5(sec)
+        info=secu.hexdigest()
+        if info in r.text:
+            if key in r.text:
+                pass
+            else:
+                print('Unexpected Error!')
+                sys.exit()
+        else:
+            print('Invalid Username Or Password!')
+            sys.exit()
