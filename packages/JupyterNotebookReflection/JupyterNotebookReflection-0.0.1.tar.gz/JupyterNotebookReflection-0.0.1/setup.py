13a423c01dc0aeda2f652e78f6309e672c89e896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:57 +0000

    Add package JupyterNotebookReflection/0.0.1/JupyterNotebookReflection-0.0.1.tar.gz

---
+++ packages/JupyterNotebookReflection/JupyterNotebookReflection-0.0.1.tar.gz/JupyterNotebookReflection-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='JupyterNotebookReflection',
+    version='0.0.1',
+    description='A module to perform reflection coding in Jupyter notebooks.',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Charles Varley',
+    license='MIT',
+    keywords=['jupyter','notebook','reflection','introspection'],
+    packages=find_packages(),
+    install_requires=[''],
+)
