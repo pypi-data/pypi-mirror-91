585cba5a2a9fc3f8f6bd62574b6745d7e081e5f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:49 +0000

    Add package example-pkg-naturalett/5.14.dev11/example_pkg_naturalett-5.14.dev11-py3-none-any.whl

---
+++ packages/example-pkg-naturalett/example_pkg_naturalett-5.14.dev11-py3-none-any.whl/version/__init__.py
@@ -0,0 +1,12 @@
+"""
+Metadata about semver.
+Contains information about semver's version, the implemented version
+of the semver specifictation
+.. autodata:: __version__
+.. autodata:: __sha__
+"""
+
+#: Semver version
+__version__ = "v5.13"
+#: Sha hash
+__sha__ = "bb94577dc35455bbaa5f87221313e67eb0fb8413"
