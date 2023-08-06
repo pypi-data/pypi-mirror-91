c402a773f0fea61bf78c0de88c36de5153e73de8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:58 +0000

    Add package nd-distriProb/1.1/nd_distriProb-1.1.tar.gz

---
+++ packages/nd-distriProb/nd_distriProb-1.1.tar.gz/nd_distriProb-1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='nd_distriProb',
+      version='1.1',
+      description='Gaussian and Binomial distributions',
+      packages=['nd_distriProb'],
+      author = 'Nancy Doss',
+      author_email = 'Nancyjaykujmar@hotmail.com',
+      zip_safe=False)
