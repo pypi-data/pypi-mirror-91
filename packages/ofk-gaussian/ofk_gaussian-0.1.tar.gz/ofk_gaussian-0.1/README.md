aac15c081946e48d965c887d8db97beee9164468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:24 +0000

    Add package ofk-gaussian/0.1/ofk_gaussian-0.1.tar.gz

---
+++ packages/ofk-gaussian/ofk_gaussian-0.1.tar.gz/ofk_gaussian-0.1/README.md
@@ -0,0 +1,1 @@
+This is a basic packing exercise for udacity-ml course. It includes a two modules for Binomial and Gaussian distributions with several attributes.
---
+++ packages/ofk-gaussian/ofk_gaussian-0.1.tar.gz/ofk_gaussian-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(name='ofk_gaussian',
+      version='0.1',
+      description='Gaussian distribution',
+      packages=find_packages(),
+      zip_safe=False)
