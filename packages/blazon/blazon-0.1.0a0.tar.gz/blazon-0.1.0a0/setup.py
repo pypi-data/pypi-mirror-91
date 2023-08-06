05ccac2d7fe41655d7cec3f9c4647e347a17ca59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:29 +0000

    Add package blazon/0.1.0a0/blazon-0.1.0a0.tar.gz

---
+++ packages/blazon/blazon-0.1.0a0.tar.gz/blazon-0.1.0a0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['blazon', 'blazon.constraints', 'blazon.environments']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['inflection>=0.3.1,<0.4.0', 'shortuuid>=1.0.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'blazon',
+    'version': '0.1.0a0',
+    'description': '',
+    'long_description': None,
+    'author': 'Brantley Harris',
+    'author_email': 'deadwisdom@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
