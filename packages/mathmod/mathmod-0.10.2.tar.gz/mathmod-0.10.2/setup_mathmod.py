27187e6b46e24c43cdc39d98686dee688d99fdd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:12 +0000

    Add package mathmod/0.10.2/mathmod-0.10.2.tar.gz

---
+++ packages/mathmod/mathmod-0.10.2.tar.gz/mathmod-0.10.2/setup_mathmod.py
@@ -0,0 +1,33 @@
+input("This file is for setting up Mathmod ONLY. It does NOT set up palc.\nPress ENTER to continue, or Ctrl+C to end.")
+
+from setuptools import setup
+
+with open("README_MATHMOD.md", "r") as file:
+    long_desc = file.read()
+
+setup(
+    name='mathmod',
+    version='0.10.2',
+    description='yet another Python math module',
+    long_description=long_desc,
+    license='GNU GPL 3.0',
+    packages=['mathmod'],
+    author='TheTechRobo',
+    author_email='thetechrobo@outlook.com',
+    keywords=['math', 'easy', 'thetechrobo'],
+    url='https://github.com/TheTechRobo/python-text-calculator',
+    
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3'
+    ],
+    project_urls={
+        'Source': 'https://github.com/thetechrobo/python-text-calculator',
+        'Tracker': 'https://github.com/thetechrobo/python-text-calculator/issues',
+    },
+
+    long_description_content_type='text/markdown',
+
+    python_requires='>=3',
+)
