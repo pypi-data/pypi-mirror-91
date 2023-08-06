a19c88f79bf42ae86b709c424f33626707f678a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:17 +0000

    Add package osa/0.2.2/osa-0.2.2.tar.gz

---
+++ packages/osa/osa-0.2.2.tar.gz/osa-0.2.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Environment :: Console
+Environment :: Web Environment
+Intended Audience :: Developers
+Intended Audience :: End Users/Desktop
+License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)
+Natural Language :: English
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Internet
+Topic :: Internet :: WWW/HTTP
+Topic :: Software Development :: Libraries
+Topic :: Software Development :: Object Brokering""".split("\n")
+
+setup(
+    name='osa',
+    version='0.2.2',
+    description='Python fast/slim/convenient SOAP/WSDL client.',
+    author="Sergey A. Bozhenkov",
+    author_email='ba-serge@yandex.ru',
+    url="https://github.com/baserge/osa",
+    download_url="https://github.com/baserge/osa/archive/master.zip",
+    packages=["osa",],
+    license="LGPLv3",
+    classifiers=CLASSIFIERS,
+    )
