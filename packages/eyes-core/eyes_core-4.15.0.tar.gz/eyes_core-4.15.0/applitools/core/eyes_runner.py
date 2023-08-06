5cb99111f75e9531cfdd9af0676440e6f93feadc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:46 +0000

    Add package eyes-core/4.15.0/eyes_core-4.15.0.tar.gz

---
+++ packages/eyes-core/eyes_core-4.15.0.tar.gz/eyes_core-4.15.0/applitools/core/eyes_runner.py
@@ -0,0 +1,20 @@
+from abc import abstractmethod
+
+from applitools.common import TestResultsSummary, logger
+from applitools.common.utils import ABC, iteritems
+
+
+class EyesRunner(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def _get_all_test_results_impl(self, should_raise_exception):
+        pass
+
+    def get_all_test_results(self, should_raise_exception=True):
+        # type: (bool) -> TestResultsSummary
+        logger.debug("get_all_test_results({}) called".format(should_raise_exception))
+        summary = self._get_all_test_results_impl(should_raise_exception)
+        logger.debug(str(summary))
+        return summary
