f2288f0dd3e20f6675fedb973154524cb153b4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:01 +0000

    Add package balinese-library/0.0.2/balinese_library-0.0.2.tar.gz

---
+++ packages/balinese-library/balinese_library-0.0.2.tar.gz/balinese_library-0.0.2/balinese_library/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+path = os.path.dirname(__file__)
+
+from NER_location import ner_location
+from NER_name import ner_name
+from NER_time import ner_time
+from POS_tag import pos_tag
