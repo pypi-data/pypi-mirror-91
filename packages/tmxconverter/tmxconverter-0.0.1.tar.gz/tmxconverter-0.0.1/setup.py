1243643d0235e4cf2c963b8f61a9076ce05644be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:52 +0000

    Add package tmxconverter/0.0.1/tmxconverter-0.0.1.tar.gz

---
+++ packages/tmxconverter/tmxconverter-0.0.1.tar.gz/tmxconverter-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tmxconverter",
+    version="0.0.1",
+    author="Thorsten Hapke",
+    author_email="thorsten.hapke@sap.com",
+    description="Converts TMX files to CSV-files and/or stores to HANA table",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/thhapke/tmxconverter",
+    keywords = ['tmx'],
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas','pyyaml','hdbcli'
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'tmxconverter = tmxconverter.tmxconverter:main'
+        ]
+    },
+    classifiers=[
+    	'Programming Language :: Python :: 3.6',
+    	'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
