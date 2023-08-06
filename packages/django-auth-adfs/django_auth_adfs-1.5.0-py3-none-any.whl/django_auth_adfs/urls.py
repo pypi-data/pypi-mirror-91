cb4546eeade22ca70fb93cd0dd660a3dc1d958bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:35 +0000

    Add package django-auth-adfs/1.5.0/django_auth_adfs-1.5.0-py3-none-any.whl

---
+++ packages/django-auth-adfs/django_auth_adfs-1.5.0-py3-none-any.whl/django_auth_adfs/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+
+from django_auth_adfs import views
+
+app_name = "django_auth_adfs"
+
+urlpatterns = [
+    url(r'^callback$', views.OAuth2CallbackView.as_view(), name='callback'),
+    url(r'^login$', views.OAuth2LoginView.as_view(), name='login'),
+    url(r'^login_no_sso$', views.OAuth2LoginNoSSOView.as_view(), name='login-no-sso'),
+    url(r'^login_force_mfa$', views.OAuth2LoginForceMFA.as_view(), name='login-force-mfa'),
+    url(r'^logout$', views.OAuth2LogoutView.as_view(), name='logout'),
+]
