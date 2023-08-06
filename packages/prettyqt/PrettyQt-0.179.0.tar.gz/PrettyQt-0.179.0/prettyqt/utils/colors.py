a8fe3c6a4c3e2aa386fa00a0b2655cba5845f6a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:20 +0000

    Add package prettyqt/0.179.0/PrettyQt-0.179.0.tar.gz

---
+++ packages/prettyqt/PrettyQt-0.179.0.tar.gz/PrettyQt-0.179.0/prettyqt/utils/colors.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from prettyqt import gui
+from prettyqt.utils import types
+
+
+WINDOW_ICON_COLOR = "darkcyan"
+
+
+def set_window_icon_color(color):
+    global WINDOW_ICON_COLOR
+    WINDOW_ICON_COLOR = color
+
+
+def get_color(color: types.ColorType) -> gui.Color:
+    """Get gui.Color instance for given parameter.
+
+    named colors are 'aliceblue', 'antiquewhite', 'aqua', 'aquamarine',
+    'azure', 'beige', 'bisque', 'black', 'blanchedalmond', 'blue', 'blueviolet',
+    'brown', 'burlywood', 'cadetblue', 'chartreuse', 'chocolate', 'coral',
+    'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan',
+    'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki',
+    'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred',
+    'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey',
+    'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey',
+    'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'fuchsia', 'gainsboro',
+    'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'greenyellow', 'grey',
+    'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender',
+    'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral',
+    'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey',
+    'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray',
+    'lightslategrey', 'lightsteelblue', 'lightyellow', 'lime', 'limegreen', 'linen',
+    'magenta', 'maroon', 'mediumaquamarine', 'mediumblue', 'mediumorchid',
+    'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen',
+    'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose',
+    'moccasin', 'navajowhite', 'navy', 'oldlace', 'olive', 'olivedrab', 'orange',
+    'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise',
+    'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum',
+    'powderblue', 'purple', 'red', 'rosybrown', 'royalblue', 'saddlebrown',
+    'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'silver', 'skyblue',
+    'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue',
+    'tan', 'teal', 'thistle', 'tomato', 'transparent', 'turquoise', 'violet',
+    'wheat', 'white', 'whitesmoke', 'yellow', 'yellowgreen'
+
+    Args:
+        color (ColorType): color to create gui.Icon from
+
+    Returns:
+        gui.Color: color instance
+    """
+    if isinstance(color, (tuple, list)):
+        return gui.Color(*color)
+    return gui.Color(color)
