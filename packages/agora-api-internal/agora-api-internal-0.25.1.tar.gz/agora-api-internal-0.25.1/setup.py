cc61f11aa0635ea53a3e7c9171ff1876c22f8ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:29 +0000

    Add package agora-api-internal/0.25.1/agora-api-internal-0.25.1.tar.gz

---
+++ packages/agora-api-internal/agora-api-internal-0.25.1.tar.gz/agora-api-internal-0.25.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+    name='agora-api-internal',
+    version='0.25.1',
+    description='Agora API for Python',
+    author='Kik Engineering',
+    author_email='engineering@kik.com',
+    url='https://github.com/kinecosystem/agora-api-internal',
+    license='MIT',
+    packages=find_packages(include=["agoraapi", "agoraapi.*"]),
+    long_description=open("../README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=[
+        "protobuf"
+    ],
+)
