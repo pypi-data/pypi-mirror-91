cb63c651b0292ee250a0a43f8e6ee2387f804f62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:13 +0000

    Add package learnpack/1.0.3/learnpack-1.0.3.tar.gz

---
+++ packages/learnpack/learnpack-1.0.3.tar.gz/learnpack-1.0.3/learnpack/test_case1.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/19 4:14 下午
+# @Author  : haohao
+# @FileName: test_case1.py
+
+# content of test_sample.py
+
+
+class learnPack:
+    def learnpack(self,a):
+        print(a)
+
+if __name__ == "__main__":
+    # pytest.main(["-s", "test_case1.py"])
+    learnPack().learnpack('type1')
---
+++ packages/learnpack/learnpack-1.0.3.tar.gz/learnpack-1.0.3/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/1/13 4:42 下午
+# @Author  : haohao
+# @FileName: setup.py
+from setuptools import find_packages,setup
+
+setup(
+    name='learnpack',
+    version='1.0.3',
+    author='haohao',
+    packages = find_packages(),
+)
