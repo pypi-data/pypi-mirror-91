50d660ec1a3dab98da7094bb0c06e0b3be2b92c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:43 +0000

    Add package timemachines/0.1.1/timemachines-0.1.1-py3-none-any.whl

---
+++ packages/timemachines/timemachines-0.1.1-py3-none-any.whl/timemachines/optimizers/optunacube.py
@@ -0,0 +1,31 @@
+import optuna
+from optuna.logging import CRITICAL
+from timemachines.optimizers.objectives import OBJECTIVES
+
+
+def optuna_cube(objective, n_trials,n_dim, with_count=False):
+
+    global feval_count
+    feval_count = 0
+
+    def cube_objective(trial):
+        global feval_count
+        us = [ trial.suggest_float('u'+str(i),0,1) for i in range(n_dim)]
+        feval_count += 1
+        return objective(us)
+
+    optuna.logging.set_verbosity(CRITICAL)
+    study = optuna.create_study()
+    study.optimize(cube_objective,n_trials=n_trials)
+
+    best_x = [ study.best_params['u'+str(i)] for i in range(n_dim) ]
+
+    if with_count:
+        return study.best_value, best_x, feval_count
+    else:
+        return study.best_value, best_x
+
+
+if __name__=='__main__':
+    for objective in OBJECTIVES:
+        print((objective.__name__,optuna_cube(objective, n_trials=100, n_dim=6, with_count=True)))
