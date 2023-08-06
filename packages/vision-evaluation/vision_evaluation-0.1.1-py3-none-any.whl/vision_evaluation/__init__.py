551ad57916a935a782723b26b101528065ae5486
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:22 +0000

    Add package vision-evaluation/0.1.1/vision_evaluation-0.1.1-py3-none-any.whl

---
+++ packages/vision-evaluation/vision_evaluation-0.1.1-py3-none-any.whl/vision_evaluation/__init__.py
@@ -0,0 +1,5 @@
+from .evaluators import MeanAveragePrecisionEvaluatorForMultipleIOUs, MeanAveragePrecisionEvaluatorForSingleIOU, TopKAccuracyEvaluator, ThresholdAccuracyEvaluator, PrecisionEvaluator,\
+    RecallEvaluator, AveragePrecisionEvaluator, EceLossEvaluator
+
+__all__ = ['MeanAveragePrecisionEvaluatorForMultipleIOUs', 'MeanAveragePrecisionEvaluatorForSingleIOU', 'TopKAccuracyEvaluator', 'ThresholdAccuracyEvaluator', 'PrecisionEvaluator', 'RecallEvaluator',
+           "AveragePrecisionEvaluator", "EceLossEvaluator"]
