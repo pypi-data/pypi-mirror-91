bd7c31d91a3bec1d2d8806205f499a694fdb8fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:44 +0000

    Add package wsit/1.0.8/wsit-1.0.8.tar.gz

---
+++ packages/wsit/wsit-1.0.8.tar.gz/wsit-1.0.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requires.txt') as fp:
+    requirements = fp.read()
+
+setup(name='wsit',
+      version='1.0.8',
+      author='VMS Software, Inc.',
+      description='Web Services Integration Toolkit',
+      long_description=long_description,
+      url="https://vmssoftware.com/products/web-services-integration-toolkit/",
+      license='MIT',
+      long_description_content_type="text/markdown",
+      packages=find_packages(),
+      install_requires=requirements,
+      include_package_data=False,
+      zip_safe=False)
+
