80b5a1b94b2fd659416eafb6dac6d09288190354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:33 +0000

    Add package matrix-package-krp/0.3/matrix_package_krp-0.3.tar.gz

---
+++ packages/matrix-package-krp/matrix_package_krp-0.3.tar.gz/matrix_package_krp-0.3/setup.py
@@ -0,0 +1,13 @@
+# TODO: Fill out this file with information about your package
+
+# HINT: Go back to the object-oriented programming lesson "Putting Code on PyPi" and "Exercise: Upload to PyPi"
+
+# HINT: Here is an example of a setup.py file
+# https://packaging.python.org/tutorials/packaging-projects/
+from setuptools import setup
+
+setup(name='matrix_package_krp',
+      version='0.3',
+      description='Basic Matrix Operations',
+      packages=['matrix_package_krp'],
+      zip_safe=False)
