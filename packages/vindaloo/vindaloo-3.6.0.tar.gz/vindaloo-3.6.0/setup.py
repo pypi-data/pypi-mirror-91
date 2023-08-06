9b33483d7af306bc655e332516bf2561a63b8dab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:47 +0000

    Add package vindaloo/3.6.0/vindaloo-3.6.0.tar.gz

---
+++ packages/vindaloo/vindaloo-3.6.0.tar.gz/vindaloo-3.6.0/setup.py
@@ -0,0 +1,35 @@
+import re
+
+from setuptools import setup
+
+
+with open("vindaloo/vindaloo.py", "rt", encoding="utf8") as f:
+    version = re.search(r'VERSION = \'(.*?)\'', f.read()).group(1)
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='vindaloo',
+    version=version,
+    description='K8S deployer',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/seznam/vindaloo",
+    author='Daniel Milde',
+    author_email='daniel.milde@firma.seznam.cz',
+    install_requires=[
+        'argcomplete>=1.9.5',
+        'pystache',
+    ],
+    entry_points={
+        'console_scripts': [
+            'vindaloo = vindaloo.vindaloo:run'
+        ]
+    },
+    packages=['vindaloo'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires='>=3.5',
+)
