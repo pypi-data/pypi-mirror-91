fa6d999303017080a5766d99857b51326f4f0f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:57 +0000

    Add package zosapi/0.2/zosapi-0.2.tar.gz

---
+++ packages/zosapi/zosapi-0.2.tar.gz/zosapi-0.2/zosapi/zosapi.py
@@ -0,0 +1,244 @@
+import clr, os, winreg
+from itertools import islice
+
+class PythonStandaloneApplication(object):
+    '''
+    A class to connect Pytyhon to Zemax OpticStudio API via PythonNET
+    
+    Properties
+    ----------
+    ZOSAPI
+        Namespace which contains enumerated variables
+    TheApplication
+        Interface which contains all information about the current ZOS-API connection
+    TheSystem
+        Interface which represents a complete optical system which corresponds to a single ZMX file
+    
+    Methods
+    -------
+    reshape          : System.Double[,], int, int, bool
+        Converts a System.Double[,] to a 2D list for plotting or post processing
+    transpose        : System.Double[,]
+        Transposes a 2D list; useful for converting mutli-dimensional line series (i.e. FFT PSF)
+    '''
+    class LicenseException(Exception):
+        pass
+    class ConnectionException(Exception):
+        pass
+    class InitializationException(Exception):
+        pass
+    class SystemNotPresentException(Exception):
+        pass
+
+    def __init__(self, path=None):
+        # determine location of ZOSAPI_NetHelper.dll & add as reference
+        aKey = winreg.OpenKey(winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER), r"Software\Zemax", 0, winreg.KEY_READ)
+        zemaxData = winreg.QueryValueEx(aKey, 'ZemaxRoot')
+        NetHelper = os.path.join(os.sep, zemaxData[0], r'ZOS-API\Libraries\ZOSAPI_NetHelper.dll')
+        winreg.CloseKey(aKey)
+        clr.AddReference(NetHelper)
+        import ZOSAPI_NetHelper
+        
+        path = r'C:\Program Files\Zemax OpticStudio 181120\\'
+        
+        # Find the installed version of OpticStudio
+        #if len(path) == 0:
+        if path is None:
+            isInitialized = ZOSAPI_NetHelper.ZOSAPI_Initializer.Initialize()
+        else:
+            # Note -- uncomment the following line to use a custom initialization path
+            isInitialized = ZOSAPI_NetHelper.ZOSAPI_Initializer.Initialize(path)
+        
+        # determine the ZOS root directory
+        if isInitialized:
+            dir = ZOSAPI_NetHelper.ZOSAPI_Initializer.GetZemaxDirectory()
+        else:
+            raise PythonStandaloneApplication.InitializationException("Unable to locate Zemax OpticStudio.  Try using a hard-coded path.")
+
+        # add ZOS-API referencecs
+        clr.AddReference(os.path.join(os.sep, dir, "ZOSAPI.dll"))
+        clr.AddReference(os.path.join(os.sep, dir, "ZOSAPI_Interfaces.dll"))
+        import ZOSAPI
+
+        # create a reference to the API namespace
+        self.ZOSAPI = ZOSAPI
+
+        # Create the initial connection class
+        self.TheConnection = ZOSAPI.ZOSAPI_Connection()
+
+        if self.TheConnection is None:
+            raise PythonStandaloneApplication.ConnectionException("Unable to initialize .NET connection to ZOSAPI")
+
+        self.TheApplication = self.TheConnection.CreateNewApplication()
+        if self.TheApplication is None:
+            raise PythonStandaloneApplication.InitializationException("Unable to acquire ZOSAPI application")
+
+        if self.TheApplication.IsValidLicenseForAPI == False:
+            raise PythonStandaloneApplication.LicenseException("License is not valid for ZOSAPI use")
+
+        self.TheSystem = self.TheApplication.PrimarySystem
+        if self.TheSystem is None:
+            raise PythonStandaloneApplication.SystemNotPresentException("Unable to acquire Primary system")
+
+    def __del__(self):
+        if self.TheApplication is not None:
+            self.TheApplication.CloseApplication()
+            self.TheApplication = None
+        
+        self.TheConnection = None
+    
+    def reshape(self, data, x, y, transpose = False):
+        """Converts a System.Double[,] to a 2D list for plotting or post processing
+        
+        Parameters
+        ----------
+        data      : System.Double[,] data directly from ZOS-API 
+        x         : x width of new 2D list [use var.GetLength(0) for dimension]
+        y         : y width of new 2D list [use var.GetLength(1) for dimension]
+        transpose : transposes data; needed for some multi-dimensional line series data
+        
+        Returns
+        -------
+        res       : 2D list; can be directly used with Matplotlib or converted to
+                    a numpy array using numpy.asarray(res)
+        """
+        if type(data) is not list:
+            data = list(data)
+        var_lst = [y] * x;
+        it = iter(data)
+        res = [list(islice(it, i)) for i in var_lst]
+        if transpose:
+            return self.transpose(res);
+        return res
+        
+    def transpose(self, data):
+        """Transposes a 2D list (Python3.x or greater).  
+        
+        Useful for converting mutli-dimensional line series (i.e. FFT PSF)
+        
+        Parameters
+        ----------
+        data      : Python native list (if using System.Data[,] object reshape first)    
+        
+        Returns
+        -------
+        res       : transposed 2D list
+        """
+        if type(data) is not list:
+            data = list(data)
+        return list(map(list, zip(*data)))
+
+# create a wrapper class so we don't have to write out 'PythonStandaloneApplication' each time
+class App(PythonStandaloneApplication):
+    '''
+    Wrapper class to shorten the name for initializing the PythonStandaloneApplication class
+    '''
+    def __init__(self):
+        super().__init__()
+        pass
+        
+class Interactive(object):
+    def __init__(self, instance = 0):
+        # determine the Zemax working directory
+        aKey = winreg.OpenKey(winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER), r"Software\Zemax", 0, winreg.KEY_READ)
+        zemaxData = winreg.QueryValueEx(aKey, 'ZemaxRoot')
+        NetHelper = os.path.join(os.sep, zemaxData[0], r'ZOS-API\Libraries\ZOSAPI_NetHelper.dll')
+        winreg.CloseKey(aKey)
+
+        # add the NetHelper DLL for locating the OpticStudio install folder
+        clr.AddReference(NetHelper)
+        import ZOSAPI_NetHelper
+
+        pathToInstall = ''
+        # uncomment the following line to use a specific instance of the ZOS-API assemblies
+        #pathToInstall = r'C:\C:\Program Files\Zemax OpticStudio'
+
+        # connect to OpticStudio
+        success = ZOSAPI_NetHelper.ZOSAPI_Initializer.Initialize(pathToInstall);
+
+        zemaxDir = ''
+        if success:
+            zemaxDir = ZOSAPI_NetHelper.ZOSAPI_Initializer.GetZemaxDirectory();
+            print('Found OpticStudio at:   %s' + zemaxDir);
+        else:
+            raise Exception('Cannot find OpticStudio')
+
+        # load the ZOS-API assemblies
+        clr.AddReference(os.path.join(os.sep, zemaxDir, r'ZOSAPI.dll'))
+        clr.AddReference(os.path.join(os.sep, zemaxDir, r'ZOSAPI_Interfaces.dll'))
+        import ZOSAPI
+
+        # create a reference to the API namespace
+        self.ZOSAPI = ZOSAPI
+
+        self.TheConnection = self.ZOSAPI.ZOSAPI_Connection()
+        if self.TheConnection is None:
+            raise Exception("Unable to intialize NET connection to ZOSAPI")
+
+        self.TheApplication = self.TheConnection.ConnectAsExtension(instance)
+        if self.TheApplication is None:
+            raise Exception("Unable to acquire ZOSAPI application")
+
+        if self.TheApplication.IsValidLicenseForAPI == False:
+            raise Exception("License is not valid for ZOSAPI use.  Make sure you have enabled 'Programming > Interactive Extension' from the OpticStudio GUI.")
+
+        self.TheSystem = self.TheApplication.PrimarySystem
+        if self.TheSystem is None:
+            raise Exception("Unable to acquire Primary system")
+    def __del__(self):
+        if self.TheApplication is not None:
+            self.TheApplication.CloseApplication()
+            self.TheApplication = None
+            del self.TheApplication
+        
+        self.TheConnection = None
+        del self.TheConnection
+    def reshape(self, data, x, y, transpose = False):
+        """Converts a System.Double[,] to a 2D list for plotting or post processing
+        
+        Parameters
+        ----------
+        data      : System.Double[,] data directly from ZOS-API 
+        x         : x width of new 2D list [use var.GetLength(0) for dimension]
+        y         : y width of new 2D list [use var.GetLength(1) for dimension]
+        transpose : transposes data; needed for some multi-dimensional line series data
+        
+        Returns
+        -------
+        res       : 2D list; can be directly used with Matplotlib or converted to
+                    a numpy array using numpy.asarray(res)
+        """
+        if type(data) is not list:
+            data = list(data)
+        var_lst = [y] * x;
+        it = iter(data)
+        res = [list(islice(it, i)) for i in var_lst]
+        if transpose:
+            return self.transpose(res);
+        return res
+        
+    def transpose(self, data):
+        """Transposes a 2D list (Python3.x or greater).  
+        
+        Useful for converting mutli-dimensional line series (i.e. FFT PSF)
+        
+        Parameters
+        ----------
+        data      : Python native list (if using System.Data[,] object reshape first)    
+        
+        Returns
+        -------
+        res       : transposed 2D list
+        """
+        if type(data) is not list:
+            data = list(data)
+        return list(map(list, zip(*data)))
+
+    #print('Connected to OpticStudio')
+
+    # The connection should now be ready to use.  For example:
+    #print('Serial #: ', TheApplication.SerialCode)
+
+    # Insert Code Here
+
+
