4a305e068214ed4ccc8a9be46b6e1ada11c275f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:46 +0000

    Add package pisensors/0.0.4/pisensors-0.0.4.tar.gz

---
+++ packages/pisensors/pisensors-0.0.4.tar.gz/pisensors-0.0.4/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pisensors",
+    version="0.0.4",
+    author="Ismael Raya",
+    author_email="phornee@gmail.com",
+    description="Raspberry Pi Sensors script for Temperature & Humidity",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Phornee/pisensors",
+    packages=setuptools.find_packages(),
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        '': ['*.yml'],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'baseutils_phornee>=0.0.3',
+        'dbutils_phornee>=0.0.3',
+        'adafruit-circuitpython-dht>=3.5.1'
+    ],
+    python_requires='>=3.6',
+)
