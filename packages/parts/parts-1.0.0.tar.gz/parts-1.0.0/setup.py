f87622693a8431f3531a65b153e2fa883a003328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:40 +0000

    Add package parts/1.0.0/parts-1.0.0.tar.gz

---
+++ packages/parts/parts-1.0.0.tar.gz/parts-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="parts",
+    version="1.0.0",
+    packages=["parts",],
+    install_requires=[],
+    license="MIT",
+    url="https://github.com/lapets/parts",
+    author="Andrei Lapets",
+    author_email="a@lapets.io",
+    description="Minimal Python library that provides common "+\
+                "functions related to partitioning lists.",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
