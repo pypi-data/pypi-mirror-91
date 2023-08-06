e0ff5a00fbb246c24b188f0258267820abb2f3ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:48 +0000

    Add package ecs-composex/0.11.0/ecs_composex-0.11.0-py2.py3-none-any.whl

---
+++ packages/ecs-composex/ecs_composex-0.11.0-py2.py3-none-any.whl/ecs_composex/acm/acm_stack.py
@@ -0,0 +1,172 @@
+﻿#  -*- coding: utf-8 -*-
+#   ECS ComposeX <https://github.com/lambda-my-aws/ecs_composex>
+#   Copyright (C) 2020-2021  John Mille <john@lambda-my-aws.io>
+#  #
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#  #
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#  #
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
+Main module for ACM
+"""
+
+from copy import deepcopy
+from warnings import warn
+
+from troposphere import Ref, Tags
+from troposphere.certificatemanager import (
+    Certificate as AcmCert,
+    DomainValidationOption,
+)
+
+from ecs_composex.acm.acm_aws import lookup_cert_config
+from ecs_composex.acm.acm_params import RES_KEY, MOD_KEY
+from ecs_composex.common import (
+    NONALPHANUM,
+    keyisset,
+)
+from ecs_composex.common.compose_resources import set_resources
+from ecs_composex.dns.dns_params import PUBLIC_DNS_ZONE_ID
+from ecs_composex.resources_import import import_record_properties
+
+
+class Certificate(object):
+    """
+    Class specifically for ACM Certificate
+    """
+
+    def __init__(self, name, definition, settings):
+        self.name = name
+        self.logical_name = NONALPHANUM.sub("", name)
+        self.definition = deepcopy(definition)
+        self.lookup = (
+            None
+            if not keyisset("Lookup", self.definition)
+            else self.definition["Lookup"]
+        )
+        self.use = (
+            None if not keyisset("Use", self.definition) else self.definition["Use"]
+        )
+        if not self.lookup and not self.use:
+            self.properties = (
+                {}
+                if not keyisset("Properties", self.definition)
+                else self.definition["Properties"]
+            )
+        else:
+            self.properties = {}
+        self.settings = (
+            {}
+            if not keyisset("Settings", self.definition)
+            else self.definition["Settings"]
+        )
+        self.parameters = (
+            {}
+            if not keyisset("MacroParameters", self.definition)
+            else self.definition["MacroParameters"]
+        )
+
+        self.cfn_resource = None
+
+    def define_parameters_props(self):
+        if not keyisset("DomainNames", self.parameters):
+            raise KeyError(
+                "For MacroParameters, you need to define at least DomainNames"
+            )
+        validations = [
+            DomainValidationOption(
+                DomainName=domain_name,
+                HostedZoneId=Ref(PUBLIC_DNS_ZONE_ID)
+                if not keyisset("HostedZoneId", self.parameters)
+                else self.parameters["HostedZoneId"],
+            )
+            for domain_name in self.parameters["DomainNames"]
+        ]
+        props = {
+            "DomainValidationOptions": validations,
+            "DomainName": self.parameters["DomainNames"][0],
+            "ValidationMethod": "DNS",
+            "Tags": Tags(
+                Name=self.parameters["DomainNames"][0], ZoneId=Ref(PUBLIC_DNS_ZONE_ID)
+            ),
+            "SubjectAlternativeNames": self.parameters["DomainNames"][1:],
+        }
+        return props
+
+    def create_acm_cert(self):
+        """
+        Method to set the ACM Certificate definition
+        """
+        if self.properties:
+            props = import_record_properties(self.properties, AcmCert)
+        elif self.parameters:
+            props = self.define_parameters_props()
+        else:
+            raise ValueError(
+                "Failed to determine how to create the ACM certificate",
+                self.logical_name,
+            )
+
+        self.cfn_resource = AcmCert(f"{self.logical_name}AcmCert", **props)
+
+
+def define_acm_certs(new_resources, dns_settings, root_stack):
+    """
+    Function to create the certificates
+
+    :param list<Certificate> new_resources:
+    :param dns_settings:
+    :param ecs_composex.common.stacks.ComposeXStack root_stack:
+    """
+    for resource in new_resources:
+        resource.create_acm_cert()
+        root_stack.stack_template.add_resource(resource.cfn_resource)
+
+
+def create_acm_mappings(resources, settings):
+    """
+    Function
+
+    :param list resources:
+    :return:
+    """
+    mappings = {}
+    for res in resources:
+        cert_config = lookup_cert_config(res.logical_name, res.lookup, settings.session)
+        if cert_config:
+            mappings.update(cert_config)
+    return mappings
+
+
+def init_acm_certs(settings, dns_settings, root_stack):
+    set_resources(settings, Certificate, RES_KEY)
+    new_resources = [
+        settings.compose_content[RES_KEY][cert_name]
+        for cert_name in settings.compose_content[RES_KEY]
+        if not settings.compose_content[RES_KEY][cert_name].lookup
+    ]
+    lookup_resources = [
+        settings.compose_content[RES_KEY][cert_name]
+        for cert_name in settings.compose_content[RES_KEY]
+        if settings.compose_content[RES_KEY][cert_name].lookup
+    ]
+    if new_resources:
+        define_acm_certs(new_resources, dns_settings, root_stack)
+    if new_resources and dns_settings.create_public_zone:
+        warn(
+            "Validation via DNS can only work if the zone is functional and you cannot associate a pending cert."
+            "CFN Will fail if the ACM cert validation is not complete."
+        )
+    if lookup_resources:
+        mappings = create_acm_mappings(lookup_resources, settings)
+        if mappings:
+            root_stack.stack_template.add_mapping(MOD_KEY, mappings)
---
+++ packages/ecs-composex/ecs_composex-0.11.0-py2.py3-none-any.whl/ecs_composex/iam/__init__.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+#  ECS ComposeX <https://github.com/lambda-my-aws/ecs_composex>
+#  Copyright (C) 2020-2021  John Mille <john@lambda-my-aws.io>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Entrypoint for IAM"""
+
+import re
+from troposphere import Sub, Ref, Join
+from troposphere.iam import Role
+
+from ecs_composex.common import LOG
+
+POLICY_PATTERN = r"((^([a-zA-Z0-9-_.\/]+)$)|(^(arn:aws:iam::(aws|[0-9]{12}):policy\/)[a-zA-Z0-9-_.\/]+$))"
+POLICY_RE = re.compile(POLICY_PATTERN)
+
+ROLE_ARN_ARG = "RoleArn"
+
+
+def validate_iam_role_arn(arn):
+    """
+    Function to validate IAM ROLE ARN format
+    :param str arn:
+    :return: resource match
+    :rtype: re.match
+    """
+    arn_valid = re.compile(r"^arn:aws(?:-[a-z]+)?:iam::[0-9]{12}:role/[\S]+$")
+    if not arn_valid.match(arn):
+        raise ValueError(
+            "The role ARN needs to be a valid ARN of format",
+            r"^arn:aws(?:-[a-z]+)?:iam::[0-9]{12}:role/[\S]+$",
+        )
+    return arn_valid.match(arn)
+
+
+def service_role_trust_policy(service_name):
+    """
+    Simple function to format the trust relationship for a Role and an AWS Service
+    used from lambda-my-aws/ozone
+
+    :param service_name: name of the ecs_service
+    :type service_name: str
+
+    :return: policy document
+    :rtype: dict
+    """
+    statement = {
+        "Effect": "Allow",
+        "Principal": {"Service": [Sub(f"{service_name}.${{AWS::URLSuffix}}")]},
+        "Action": ["sts:AssumeRole"],
+        "Condition": {"Bool": {"aws:SecureTransport": "true"}},
+    }
+    policy_doc = {"Version": "2012-10-17", "Statement": [statement]}
+    return policy_doc
+
+
+def define_iam_policy(policy):
+    policy_def = policy
+    if not POLICY_RE.match(policy):
+        raise ValueError(
+            f"policy name {policy} does not match expected regexp", POLICY_PATTERN
+        )
+    if isinstance(policy, str) and not policy.startswith("arn:aws:iam::"):
+        policy_def = Sub(
+            f"arn:${{AWS::Partition}}:iam::${{AWS::AccountId}}:policy/{policy}"
+        )
+    elif isinstance(policy, (Sub, Ref, Join)):
+        LOG.debug(f"policy {policy}")
+    return policy_def
+
+
+def add_role_boundaries(iam_role, policy):
+    """
+    Function to set permission boundary onto an IAM role
+
+    :param iam_role: the IAM Role to add the boundary to
+    :type iam_role: troposphere.iam.Role
+    :param policy: the name or ARN of the policy
+    :type policy: str
+    """
+    if not isinstance(iam_role, Role):
+        raise TypeError(f"{iam_role} is of type", type(iam_role), "expected", Role)
+    if isinstance(policy, str):
+        policy = define_iam_policy(policy)
+    if hasattr(iam_role, "PermissionsBoundary"):
+        LOG.warning(
+            f"IAM Role {iam_role.title} already has PermissionsBoundary set. Overriding"
+        )
+    setattr(iam_role, "PermissionsBoundary", policy)
