b662165f297cbedd93bb7f35dac83261a4d5cfef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:22 +0000

    Add package wurm/0.1.0/wurm-0.1.0.tar.gz

---
+++ packages/wurm/wurm-0.1.0.tar.gz/wurm-0.1.0/wurm/queries.py
@@ -0,0 +1,173 @@
+from dataclasses import dataclass, fields
+from typing import Any
+
+from . import sql
+from .connection import execute, ensure_created, WurmError
+from .typemaps import from_stored, to_stored
+
+@dataclass(frozen=True)
+class Comparison:
+    op: str
+    value: Any
+
+def gt(value):
+    """Helper function for selecting fields whose value is greater than
+    *value*
+
+    :samp:`{table}.query({myfield}=gt(7))` roughly translates to
+    :samp:`SELECT * FROM {table} WHERE {myfield} > 7`
+
+    :param value: value being compared to the field
+    :returns: object used in queries for comparison"""
+    return Comparison('>', value)
+
+def lt(value):
+    """Helper function for selecting fields whose value is lesser than
+    *value*
+
+    :samp:`{table}.query({myfield}=lt(7))` roughly translates to
+    :samp:`SELECT * FROM {table} WHERE {myfield} < 7`
+
+    :param value: value being compared to the field
+    :returns: object used in queries for comparison"""
+    return Comparison('<', value)
+
+def ge(value):
+    """Helper function for selecting fields whose value is greater than
+    or equal to *value*
+
+    :samp:`{table}.query({myfield}=ge(7))` roughly translates to
+    :samp:`SELECT * FROM {table} WHERE {myfield} >= 7`
+
+    :param value: value being compared to the field
+    :returns: object used in queries for comparison"""
+    return Comparison('>=', value)
+
+def le(value):
+    """Helper function for selecting fields whose value is lesser than
+    or equal to *value*
+
+    :samp:`{table}.query({myfield}=le(7))` roughly translates to
+    :samp:`SELECT * FROM {table} WHERE {myfield} <= 7`
+
+    :param value: value being compared to the field
+    :returns: object used in queries for comparison"""
+    return Comparison('<=', value)
+
+def eq(value):
+    """Helper function for selecting fields whose value is equal to
+    *value*
+
+    :samp:`{table}.query({myfield}=7)` roughly translates to
+    :samp:`SELECT * FROM {table} WHERE {myfield} = 7`
+
+    :param value: value being compared to the field
+    :returns: object used in queries for comparison"""
+    return Comparison('=', value)
+
+def ne(value):
+    """Helper function for selecting fields whose value is not equal to
+    *value*
+
+    :samp:`{table}.query({myfield}=ne(7))` roughly translates to
+    :samp:`SELECT * FROM {table} WHERE {myfield} != 7`
+
+    :param value: value being compared to the field
+    :returns: object used in queries for comparison"""
+    return Comparison('!=', value)
+
+def ensure_comparison(value):
+    if isinstance(value, Comparison):
+        return value
+    return eq(value)
+
+def encode_query_value(table, fieldname, value):
+    for field in fields(table):
+        if field.name == fieldname:
+            return to_stored(field.type, value)
+    raise WurmError(f'invalid query: {table.__name__}.{fieldname} does not exist')
+
+def decode_row(table, row):
+    rowid, *py_values = (
+        from_stored(field.type, stored_value)
+        for stored_value, field
+        in zip(row, fields(table))
+        )
+    item = table(*py_values)
+    item.rowid = rowid
+    return item
+
+class Query:
+    """Represents one or more queries on a specified table.
+
+    :samp:`Query({table}, filters)` is equivalent to :samp:`{table}.query(**filters)``"""
+    def __init__(self, table: type, filters: dict):
+        self.table = table
+        self.filters = {key: ensure_comparison(value) for key, value in filters.items()}
+        self.comparisons = ' and '.join(f'{key}{value.op}?' for key, value in self.filters.items())
+        self.values = tuple(encode_query_value(table, key, value.value) for key, value in self.filters.items())
+    def __len__(self):
+        """Returns the number of rows matching this query.
+
+        .. note:: This method accesses the connected database.
+
+        :returns: number of matches
+        :rtype: int
+        """
+        ensure_created(self.table)
+        c, = execute(sql.count(self.table, self.comparisons), self.values).fetchone()
+        return c
+    def select_with_limit(self, limit=None):
+        """Create an iterator over the results of this query.
+
+        This accesses the database.
+
+        :param limit: The number of results to limit this query to.
+        :type limit: int or None
+        :returns: an iterator over the objects matching this query."""
+        ensure_created(self.table)
+        if limit is not None:
+            values = self.values + (limit,)
+        else:
+            values = self.values
+        for row in execute(sql.select(self.table, self.comparisons, limit is not None), values):
+            yield decode_row(self.table, row)
+    def __iter__(self):
+        """Iterate over the results of this query.
+
+        .. note:: This method accesses the connected database.
+
+        Equivalent to :meth:`select_with_limit` without specifying *limit*."""
+        return self.select_with_limit()
+    def _only_first(self, *, of):
+        try:
+            i, = self.select_with_limit(of)
+        except ValueError as e:
+            raise WurmError(e.args[0].replace('values to unpack', 'rows returned')) from None
+        return i
+    def first(self):
+        """Return the first result of this query.
+
+        .. note:: This method accesses the connected database.
+
+        :raises WurmError: if this query returns zero results"""
+        return self._only_first(of=1)
+    def one(self):
+        """Return the only result of this query.
+
+        .. note:: This method accesses the connected database.
+
+        :raises WurmError: if this query returns zero results or more than one"""
+        return self._only_first(of=2)
+    def delete(self):
+        """Delete the objects matching this query.
+
+        .. warning:: Calling this on an empty query deletes all rows
+           in the database
+
+        .. note:: This method accesses the connected database.
+
+        :returns: the number of rows deleted
+        :rtype: int
+        """
+        execute(sql.delete(self.table, self.comparisons), self.values).rowcount
---
+++ packages/wurm/wurm-0.1.0.tar.gz/wurm-0.1.0/wurm/sql.py
@@ -0,0 +1,43 @@
+from dataclasses import fields
+
+from .typemaps import sql_type_for
+
+def create(table):
+    return f'create table if not exists {table.__table_name__}({", ".join(make_field(f) for f in fields(table)[1:])})'
+
+def make_field(f):
+    return f'{f.name} {sql_type_for(f.type)}'
+
+def count(table, where=None):
+    if not where:
+        where_clause = ''
+    else:
+        where_clause = f'where {where}'
+    return f'select count(*) from {table.__table_name__} {where_clause}'
+
+def select(table, where=None, limit=False):
+    if where:
+        where_clause = f'where {where}'
+    else:
+        where_clause = ''
+    if limit:
+        limit_clause = 'limit ?'
+    else:
+        limit_clause = ''
+    return f'select rowid, * from {table.__table_name__} {where_clause} {limit_clause}'
+
+def insert(table, *, includes_rowid=False):
+    tfields = fields(table)
+    if not includes_rowid:
+        tfields = tfields[1:]
+    return f'insert into {table.__table_name__} ({", ".join(f.name for f in tfields)}) values({", ".join("?" * len(tfields))})'
+
+def update(table):
+    return f'update {table.__table_name__} set {", ".join(f"{field.name}=?" for field in fields(table)[1:]) } where rowid=?'
+
+def delete(table, where=None):
+    if where:
+        where_clause = f'where {where}'
+    else:
+        where_clause = ''
+    return f'delete from {table.__table_name__} {where_clause}'
