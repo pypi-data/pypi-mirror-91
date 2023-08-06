78264bbac553c6301073a0bad87d3e08893f8b87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:40 +0000

    Add package cp-tool/1.0.0/cp_tool-1.0.0-py3-none-any.whl

---
+++ packages/cp-tool/cp_tool-1.0.0-py3-none-any.whl/cpTool/__main__.py
@@ -0,0 +1,54 @@
+import os
+from customModules import readmeFIleGenerate as readme
+import api
+
+def  main():
+    # def createDir():
+
+    #     creating_directory = input("Enter dir name : ")
+    #     path = os.path.join(main_directory, creating_directory)
+    #     os.mkdir(path)
+
+
+    # while (1):
+    #     try:
+    #         createDir()
+    #         break
+
+    #     except OSError as error:
+
+    #         print(error)
+
+
+    main_directory = os.getcwd()
+
+    repo_name = input("Enter your repository name : ")
+
+    filePath = os.path.join(main_directory, 'test.md')
+
+    isFileExists = os.path.exists(filePath)
+
+    # if isFileExists :
+    #     file = open('test.md','a')
+    #     file.write("Hello\n")
+    #     file.close()
+    # else :
+    #     file = open('test.md','w')
+    #     file.write("Hello\n")
+    #     file.close()
+
+
+    user_info = api.getUserInfo()
+
+    repos =  api.getSubmissions()
+
+    readme.createReadmeFile(repo_name, user_info, repos)
+
+    print("Done!")
+
+    print("Thanks for using cp-tool")
+
+    
+
+if __name__ == "__main__":
+    main()
