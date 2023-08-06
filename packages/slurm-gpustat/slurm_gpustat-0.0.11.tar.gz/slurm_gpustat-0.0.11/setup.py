193a349747d242e5cff708412a3f10372258dc22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:49 +0000

    Add package slurm-gpustat/0.0.11/slurm_gpustat-0.0.11.tar.gz

---
+++ packages/slurm-gpustat/slurm_gpustat-0.0.11.tar.gz/slurm_gpustat-0.0.11/setup.py
@@ -0,0 +1,41 @@
+"""
+Commands to upload:
+python3 setup.py sdist bdist_wheel
+twine upload --skip-existing dist/*
+"""
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+
+setuptools.setup(
+    name="slurm_gpustat",
+    version="0.0.11",
+    entry_points={
+        "console_scripts": [
+            "slurm_gpustat=slurm_gpustat.slurm_gpustat:main",
+        ],
+    },
+    author="Samuel Albanie",
+    description="A simple SLURM gpu summary tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/albanie/slurm_gpustat",
+    packages=["slurm_gpustat"],
+    python_requires=">=3.6",
+    install_requires=[
+        "numpy",
+        "colored",
+        "seaborn",
+        "beartype",
+        "humanize",
+        "humanfriendly",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: POSIX :: Linux',
+    ],
+)
