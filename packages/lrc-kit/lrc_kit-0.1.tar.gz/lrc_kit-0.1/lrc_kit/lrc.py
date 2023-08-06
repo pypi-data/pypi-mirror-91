fb2af2245d5be09ff4d6361b214f40654c6945e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:50 +0000

    Add package lrc-kit/0.1/lrc_kit-0.1.tar.gz

---
+++ packages/lrc-kit/lrc_kit-0.1.tar.gz/lrc_kit-0.1/lrc_kit/lrc.py
@@ -0,0 +1,22 @@
+from lrc_kit.parser import parse_lyrics
+import json
+
+class LRC:
+    def __init__(self, lyrics, metadata={}):
+        if isinstance(lyrics, str):
+            self.lyrics, self.metadata = parse_lyrics(lyrics)
+        else:
+            self.lyrics = lyrics
+            self.metadata = metadata
+    
+    def offset(self, min_offset=0, sec_offset=0, millis_offset=0):
+        for line in self.lyrics:
+            line.offset(min_offset=min_offset, sec_offset=sec_offset, millis_offset=millis_offset)
+    
+    def export(self, fp_or_str):
+        if isinstance(fp_or_str, str):
+            fp = open(fp_or_str, 'w')
+        str_lyrics = '\n'.join(str(l) for l in self.lyrics)
+        str_metadata = '\n'.join(f'[{key}:{value}]' for key, value in self.metadata.items()) + '\n'
+        fp.write(str_metadata)
+        fp.write(str_lyrics)
