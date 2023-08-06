31d1762cf60eb6d075dbc5c113c458997c85a4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:18 +0000

    Add package ipyspeck/0.6.0/ipyspeck-0.6.0-py2.py3-none-any.whl

---
+++ packages/ipyspeck/ipyspeck-0.6.0-py2.py3-none-any.whl/ipyspeck/_version.py
@@ -0,0 +1,8 @@
+# Module version
+version_info = (0, 6, 0)
+
+# Module version stage suffix map
+_specifier_ = {'alpha': 'a', 'beta': 'b', 'candidate': 'rc', 'final': ''}
+
+# Module version accessible using ipyspeck.__version__
+__version__ = '%s.%s.%s'%(version_info[0], version_info[1], version_info[2])
