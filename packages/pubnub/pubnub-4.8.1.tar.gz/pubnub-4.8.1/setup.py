b4bdf817f77b4f7fbd3edf75e76899dbebf8c95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:55 +0000

    Add package pubnub/4.8.1/pubnub-4.8.1.tar.gz

---
+++ packages/pubnub/pubnub-4.8.1.tar.gz/pubnub-4.8.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pubnub',
+    version='4.8.1',
+    description='PubNub Real-time push service in the cloud',
+    author='PubNub',
+    author_email='support@pubnub.com',
+    url='http://pubnub.com',
+    packages=find_packages(exclude=("examples*", 'tests*')),
+    license='MIT',
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ),
+    install_requires=[
+        'pycryptodomex>=3.3',
+        'requests>=2.4',
+        'six>=1.10',
+        'cbor2'
+    ],
+    zip_safe=False,
+)
