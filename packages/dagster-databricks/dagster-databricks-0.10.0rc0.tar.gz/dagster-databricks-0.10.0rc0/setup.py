7b3a82e397affe37518b5b30f16eed5185de5e12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:23 +0000

    Add package dagster-databricks/0.10.0rc0/dagster-databricks-0.10.0rc0.tar.gz

---
+++ packages/dagster-databricks/dagster-databricks-0.10.0rc0.tar.gz/dagster-databricks-0.10.0rc0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+
+def get_version():
+    version = {}
+    with open("dagster_databricks/version.py") as fp:
+        exec(fp.read(), version)  # pylint: disable=W0122
+
+    return version["__version__"]
+
+
+if __name__ == "__main__":
+    setup(
+        name="dagster-databricks",
+        version=get_version(),
+        author="Elementl",
+        author_email="hello@elementl.com",
+        license="Apache-2.0",
+        description="Package for Databricks-specific Dagster framework solid and resource components.",
+        url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-databricks",
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        packages=find_packages(exclude=["test"]),
+        include_package_data=True,
+        install_requires=["dagster", "dagster-pyspark", "databricks_api"],
+        zip_safe=False,
+    )
