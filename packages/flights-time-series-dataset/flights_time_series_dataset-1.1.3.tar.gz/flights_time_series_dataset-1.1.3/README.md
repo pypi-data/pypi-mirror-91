abc5b74bd9187d055d0cf6aa07d4c36aefe2fd15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:23 +0000

    Add package flights-time-series-dataset/1.1.3/flights_time_series_dataset-1.1.3.tar.gz

---
+++ packages/flights-time-series-dataset/flights_time_series_dataset-1.1.3.tar.gz/flights_time_series_dataset-1.1.3/README.md
@@ -0,0 +1,14 @@
+# Oze dataset
+
+[![PyPI version](https://badge.fury.io/py/flights-time-series-dataset.svg)](https://badge.fury.io/py/flights-time-series-dataset) [![travis](https://travis-ci.org/krypton-unite/flights_time_series_dataset.svg?branch=master)](https://travis-ci.org/github/krypton-unite/flights-time-series-dataset) [![codecov](https://codecov.io/gh/krypton-unite/flights_time_series_dataset/branch/master/graph/badge.svg)](https://codecov.io/gh/krypton-unite/flights-time-series-dataset) [![GitHub license](https://img.shields.io/github/license/krypton-unite/flights_time_series_dataset)](https://github.com/krypton-unite/flights_time_series_dataset)
+
+## Description
+Flights time series dataset example for [Time series predictor](https://github.com/krypton-unite/time_series_predictor)
+
+## Installation
+
+```terminal
+pip install flights-time-series-dataset
+```
+
+## Usage
---
+++ packages/flights-time-series-dataset/flights_time_series_dataset-1.1.3.tar.gz/flights_time_series_dataset-1.1.3/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+with open('HISTORY.md') as history_file:
+    HISTORY = history_file.read()
+
+setup(
+    name='flights_time_series_dataset',
+    version='1.1.3',
+    description='Flights time series dataset for time-series-predictor.',
+    long_description_content_type="text/markdown",
+    long_description=README + '\n\n' + HISTORY,
+    license='The Unlicense',
+    packages=find_packages(exclude=("tests",)),
+    author='Daniel Kaminski de Souza',
+    author_email='daniel@kryptonunite.com',
+    keywords=['Time series dataset'],
+    url='https://github.com/krypton-unite/flights_time_series_dataset.git',
+    download_url='https://pypi.org/project/flights-time-series-dataset/',
+    python_requires=">=3.7",
+    install_requires = [
+        'time-series-dataset',
+        'time-series-dataset-generator',
+        'pandas',
+        'seaborn'
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'pytest-cov'
+        ],
+        'dev': [
+            'bumpversion',
+            'twine',
+            'wheel',
+            'pylint',
+            'autopep8'
+        ]
+    }
+)
