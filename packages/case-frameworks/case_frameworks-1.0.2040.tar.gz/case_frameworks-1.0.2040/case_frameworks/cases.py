570a99f1c72ee2a83524cfd797ac708331a05303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:37 +0000

    Add package case-frameworks/1.0.2040/case_frameworks-1.0.2040.tar.gz

---
+++ packages/case-frameworks/case_frameworks-1.0.2040.tar.gz/case_frameworks-1.0.2040/case_frameworks/cases.py
@@ -0,0 +1,24 @@
+# Cases includes classes containing frameworks and practise cases
+
+# Imports useful python functions
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import scipy as sc
+import sklearn as skl
+import csv as csv
+import openpyxl as pyxl
+import pathlib
+import os
+import pydrive
+import random as rd
+
+
+class frameworks:
+    """frameworks displays a variety of case frameworks depending on the function call.
+    """
+
+
+class practise:
+    """practise enables the user to practise the case recorded in this class. Each case is an independent function.
+    """
