c0312830148884a24b4c1541965a26921dd5c990
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:54 +0000

    Add package papermerge-core/2.0.0rc9/papermerge-core-2.0.0rc9.tar.gz

---
+++ packages/papermerge-core/papermerge-core-2.0.0rc9.tar.gz/papermerge-core-2.0.0rc9/papermerge/core/signal_definitions.py
@@ -0,0 +1,66 @@
+from django.dispatch import Signal
+
+
+automates_matching = Signal(
+    providing_args=[
+        "user_id",
+        "level",
+        "message",
+        "document_id",
+        "page_num",
+        # text against which matching is performed
+        # i.e. extracted text of the page
+        "text",
+        "namespace"
+    ]
+)
+
+page_ocr = Signal(
+    providing_args=[
+        "user_id",
+        "level",
+        "message",
+        "document_id",
+        "page_num",
+        "lang",
+        "namespace",
+        # status is a string: started, complete
+        "status"
+    ]
+)
+
+post_page_ocr = Signal(
+    providing_args=[
+        "user_id",
+        "document_id",
+        "page_num",
+        "lang",
+        "namespace",
+        "text"
+    ]
+)
+
+# Sent by core.views.documents.create_folder
+# Sent AFTER one single folder was created
+folder_created = Signal(
+    providing_args=[
+        "user_id",
+        "level",
+        "message",
+        "folder_id"
+    ]
+)
+
+# Sent by core.views.nodes.nodes_view
+# Sent AFTER one of more nodes were batch deleted
+nodes_deleted = Signal(
+    providing_args=[
+        "user_id",
+        "level",
+        "message",
+        # node tags is a list of
+        # '<a href="{node.id}">{node.title}</a>'
+        "node_tags"
+    ]
+)
+
