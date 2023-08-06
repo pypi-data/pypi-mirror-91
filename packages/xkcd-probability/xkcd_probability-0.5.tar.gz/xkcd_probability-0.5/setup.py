30f240377d2afc4289c7685da9690c83f0b6ac9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:23 +0000

    Add package xkcd-probability/0.5/xkcd_probability-0.5.tar.gz

---
+++ packages/xkcd-probability/xkcd_probability-0.5.tar.gz/xkcd_probability-0.5/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='xkcd_probability',
+      version='0.5',
+      description='Gaussian & Binomial distributions',
+      packages=['xkcd_probability'],
+      author="T.I. Szatmari",
+      author_email="test-email@gmail.com",
+      zip_safe=False)
