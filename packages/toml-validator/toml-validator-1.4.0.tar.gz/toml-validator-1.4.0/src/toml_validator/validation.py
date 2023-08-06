a0e550f35fef32eb61668964f04cf08a51edbf39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:18 +0000

    Add package toml-validator/1.4.0/toml-validator-1.4.0.tar.gz

---
+++ packages/toml-validator/toml-validator-1.4.0.tar.gz/toml-validator-1.4.0/src/toml_validator/validation.py
@@ -0,0 +1,41 @@
+"""TOML Validator validations."""
+import sys
+
+import tomlkit
+from tomlkit.exceptions import ParseError
+from tomlkit.exceptions import TOMLKitError
+
+
+def validate_extension(filename: str) -> bool:
+    """Validates extension in filename.
+
+    Args:
+        filename (str): name of the file.
+
+    Returns:
+        bool: if extension is valid.
+    """
+    valid_extensions = [".toml"]
+    for extension in valid_extensions:
+        if filename.endswith(extension):
+            return True
+    sys.exit('Error: "FILANAME" {} does not have a valid extension.'.format(filename))
+
+
+def validate_toml(filename: str) -> str:
+    """It validates the TOML.
+
+    Args:
+        filename (str): name of the file.
+
+    Returns:
+        str: error messages.
+    """
+    with open(filename) as toml:
+        lines = toml.read()
+
+    try:
+        tomlkit.parse(lines)
+        return ""
+    except (TOMLKitError, ParseError) as errors:
+        return str(errors)
