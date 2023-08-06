926629cdbcab9306e95dc5c86057115ac67080e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:07 +0000

    Add package litncov/0.0.4/litncov-0.0.4.tar.gz

---
+++ packages/litncov/litncov-0.0.4.tar.gz/litncov-0.0.4/setup.py
@@ -0,0 +1,14 @@
+
+from os import path as os_path
+from setuptools import setup
+
+setup(name='litncov',
+      version='0.0.4',
+      description='A ncov report library and tool for LIT(Luoyang Institute of Science and Technology)',
+      url='http://github.com/icepie/lit-ncov-report',
+      author='Tea',
+      author_email='icepie.dev@gmail.com',
+      license='MIT',
+      packages=['litncov'],
+      install_requires=['click', 'requests', 'gb2260'],
+      python_requires='>=3.6')
