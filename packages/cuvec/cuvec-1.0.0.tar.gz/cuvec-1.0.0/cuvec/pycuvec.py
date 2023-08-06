e16ebd90394ffe1d8b41db994bece81376c1dbe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:15 +0000

    Add package cuvec/1.0.0/cuvec-1.0.0.tar.gz

---
+++ packages/cuvec/cuvec-1.0.0.tar.gz/cuvec-1.0.0/cuvec/pycuvec.py
@@ -0,0 +1,32 @@
+"""Thin wrappers around `cuvec` C++/CUDA module"""
+import array
+
+import numpy as np
+
+from .cuvec import (
+    Vector_b,
+    Vector_B,
+    Vector_c,
+    Vector_d,
+    Vector_f,
+    Vector_h,
+    Vector_H,
+    Vector_i,
+    Vector_I,
+    Vector_q,
+    Vector_Q,
+)
+
+typecodes = [i for i in array.typecodes if i not in "ulL"]
+vec_types = {
+    np.dtype('int8'): Vector_b,
+    np.dtype('uint8'): Vector_B,
+    np.dtype('S1'): Vector_c,
+    np.dtype('int16'): Vector_h,
+    np.dtype('uint16'): Vector_H,
+    np.dtype('int32'): Vector_i,
+    np.dtype('uint32'): Vector_I,
+    np.dtype('int64'): Vector_q,
+    np.dtype('uint64'): Vector_Q,
+    np.dtype('float32'): Vector_f,
+    np.dtype('float64'): Vector_d}
