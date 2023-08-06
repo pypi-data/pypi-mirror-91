6112ebde608150eec0f62281193cc1df19fef537
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:26 +0000

    Add package py-telegram/1.0.0.9/py_telegram-1.0.0.9.tar.gz

---
+++ packages/py-telegram/py_telegram-1.0.0.9.tar.gz/py_telegram-1.0.0.9/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="py_telegram",
+    version="1.0.0.9",
+    author="Gabriel Heinzer",
+    author_email="dev@gabrielheinzer.ch",
+    description="This module should help you controlling the telegram bot API.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/programmer372/python-telegram-api",
+    packages=setuptools.find_packages(),
+    # classifiers=[
+        #"Programming Language :: Python :: 3",
+        #"License :: OSI Approved :: MIT License",
+        #"Operating System :: OS Independent",
+    # ],
+    python_requires=">=3.0",
+    include_package_data=True
+)
