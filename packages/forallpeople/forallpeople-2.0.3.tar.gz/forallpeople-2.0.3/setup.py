ac190d3b0e550d18074471c4a733f9e9f206ec10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:44 +0000

    Add package forallpeople/2.0.3/forallpeople-2.0.3.tar.gz

---
+++ packages/forallpeople/forallpeople-2.0.3.tar.gz/forallpeople-2.0.3/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['forallpeople']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'forallpeople',
+    'version': '2.0.3',
+    'description': 'SI units library for daily calculation work intended for daily calculation work',
+    'long_description': None,
+    'author': 'Connor Ferster',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
