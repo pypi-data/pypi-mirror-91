a5b669866d8ae309a3c160e5eeea18c4bb86ff82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:29 +0000

    Add package genuine-fake/1.2.10/Genuine-fake-1.2.10.tar.gz

---
+++ packages/genuine-fake/Genuine-fake-1.2.10.tar.gz/Genuine-fake-1.2.10/genuine/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Andile Jaden Mbele'
+__email__ = 'andilembele020@gmail.com'
+__github__ = 'https://github.com/xeroxzen/genuine-fake'
+__package__ = 'genuine-fake'
