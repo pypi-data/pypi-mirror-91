84e9befd49518ef27ba169f7749d3c8be1e47701
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:53 +0000

    Add package danny-probability/0.0/danny_probability-0.0.tar.gz

---
+++ packages/danny-probability/danny_probability-0.0.tar.gz/danny_probability-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='danny_probability',
+      version='0.0',
+      description='Gaussian and Bonimial distribution',
+      package=['danny_probability'],
+      author='Daniel Shih',
+      zip_safe=False)
