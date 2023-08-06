820c13cd0e545b788e43597248bf6d5efc82dd3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:56 +0000

    Add package coralogix-logger/2.0.4/coralogix_logger-2.0.4.tar.gz

---
+++ packages/coralogix-logger/coralogix_logger-2.0.4.tar.gz/coralogix_logger-2.0.4/AUTHORS.rst
@@ -0,0 +1,9 @@
+Coralogix Python SDK is written by Coralogix Ltd. and maintained by Coralogix Ltd.
+
+Contributors
+````````````
+
+- Coralogix Ltd. <info@coralogix.com> `@coralogix <https://github.com/coralogix>`_
+- Amnon Shahar <amnon@coralogix.com> `@amnons77 <https://github.com/amnons77>`_
+- Eldar Aliiev <eldar@coralogix.com> `@EldarAliiev <https://github.com/EldarAliiev>`_
+- Lior Redlus <lior@coralogix.com> `@redlus <https://github.com/redlus>`_
---
+++ packages/coralogix-logger/coralogix_logger-2.0.4.tar.gz/coralogix_logger-2.0.4/coralogix/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Coralogix Python SDK
+Author: Coralogix Ltd.
+Email: info@coralogix.com
+"""
+
+__name__ = 'coralogix_logger'
+__author__ = 'Coralogix Ltd.'
+__email__ = 'info@coralogix.com'
+__copyright__ = 'Copyright 2020, Coralogix Ltd.'
+__credits__ = ['Ariel Assaraf', 'Lior Redlus', 'Amnon Shahar']
+__license__ = 'Apache Version 2.0'
+__version__ = '2.0.4'
+__maintainer__ = 'Coralogix Ltd.'
+__date__ = '12 January 2021'
+__status__ = 'Stable'
