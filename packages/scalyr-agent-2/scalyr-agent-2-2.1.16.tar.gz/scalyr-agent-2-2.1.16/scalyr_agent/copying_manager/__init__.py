4e8567aa0b1f60c3cea78d9e1a7a096165309730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:25 +0000

    Add package scalyr-agent-2/2.1.16/scalyr-agent-2-2.1.16.tar.gz

---
+++ packages/scalyr-agent-2/scalyr-agent-2-2.1.16.tar.gz/scalyr-agent-2-2.1.16/scalyr_agent/copying_manager/__init__.py
@@ -0,0 +1,4 @@
+from .copying_manager import CopyingManager, CopyingManagerWorker
+from .worker import CopyingManagerWorkerSession
+
+__all__ = ["CopyingManager", "CopyingManagerWorkerSession", "CopyingManagerWorker"]
