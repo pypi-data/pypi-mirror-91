96ec17ca5ed0ed98f3e72e1f3c09a3d5847deed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:20 +0000

    Add package LibWiser/0.9.14/LibWiser-0.9.14.tar.gz

---
+++ packages/LibWiser/LibWiser-0.9.14.tar.gz/LibWiser-0.9.14/LibWiser/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM OASYS SETUP.PY
+short_version = '0.9.14'
+version = '0.9.14'
+full_version = '0.9.14'
+git_revision = '5438e813982abf5269dd45c6a8c082865b68675f'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
