7a454e31724ac849376653ebaeee96f98315eb26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:17 +0000

    Add package metaspace2020/1.8.3/metaspace2020-1.8.3.tar.gz

---
+++ packages/metaspace2020/metaspace2020-1.8.3.tar.gz/metaspace2020-1.8.3/README.md
@@ -0,0 +1,25 @@
+# METASPACE Python client
+
+Python package that provides programmatic access to [METASPACE](https://metaspace2020.eu).
+
+## Applications
+
+* Running batch jobs
+* Exploratory analysis of submitted data
+* Quick access to things hidden from web interface (such as location of files on S3 and other metadata)
+
+## Installation
+
+You need to have **Python 3.6 or higher** to use this library.
+
+```
+pip install metaspace2020
+```
+
+## Documentation
+
+The package documentation is available at [metaspace2020.readthedocs.io](https://metaspace2020.readthedocs.io)
+
+## Acknowledgements and license
+
+[See the top-level README](../../README.md#acknowledgements)
---
+++ packages/metaspace2020/metaspace2020-1.8.3.tar.gz/metaspace2020-1.8.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+def read_version():
+    with open('metaspace/__init__.py') as f:
+        version = f.readline().split('=')[1]
+        for s in ['\'', '"', '\n', ' ']:
+            version = version.replace(s, '')
+        return version
+
+
+setup(
+    name='metaspace2020',
+    version=read_version(),
+    description='Python library for connecting to the METASPACE platform',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/metaspace2020/metaspace/tree/master/metaspace/python-client',
+    author='Alexandrov Team, EMBL',
+    author_email='contact@metaspace2020.eu',
+    packages=find_packages(exclude=['*tests*']),
+    install_requires=[
+        'pandas',
+        'plotly>=1.12',
+        'numpy',
+        'matplotlib',
+        'pyMSpec',
+        'pillow',
+        'requests',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Intended Audience :: Science/Research",
+    ],
+)
