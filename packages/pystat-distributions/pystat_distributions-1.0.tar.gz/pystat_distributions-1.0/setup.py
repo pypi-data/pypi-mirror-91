911ed2b43e5e03038e596f7f6eed227703cb2191
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:09 +0000

    Add package pystat-distributions/1.0/pystat_distributions-1.0.tar.gz

---
+++ packages/pystat-distributions/pystat_distributions-1.0.tar.gz/pystat_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='pystat_distributions',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['pystat_distributions'],
+      author = 'Tara Haddadin',
+      author_email = 'tara_haddadin@yahoo.com',
+      zip_safe=False)
