f3e29b2b2a2fe27f52eb393e1d5397d7afb22018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:22 +0000

    Add package cp-tool/2.0.2/cp-tool-2.0.2.tar.gz

---
+++ packages/cp-tool/cp-tool-2.0.2.tar.gz/cp-tool-2.0.2/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cp-tool",
+    version="2.0.2",
+    author="jspw",
+    author_email="mhshifat757@gmail.com",
+    description="cp-tool is an auto generator tool for solved problems at different online judges",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jspw/cp-tool",
+    license="MIT License",
+    packages=setuptools.find_packages(),
+    install_requires=["requests"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+       "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
