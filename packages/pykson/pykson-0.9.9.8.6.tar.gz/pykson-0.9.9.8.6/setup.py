8fcc67bb44b1586c852d1136387e35608b607a6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:48 +0000

    Add package pykson/0.9.9.8.6/pykson-0.9.9.8.6.tar.gz

---
+++ packages/pykson/pykson-0.9.9.8.6.tar.gz/pykson-0.9.9.8.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(name='pykson',
+                 version='0.9.9.8.6',
+                 author='Sina Rezaei',
+                 author_email='sinarezaei1991@gmail.com',
+                 long_description_content_type="text/markdown",
+                 long_description=long_description,
+                 description='Pykson: A JSON Serializer/Deserializer for Python',
+                 url='https://github.com/sinarezaei/pykson',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 install_requires=required,
+                 python_requires='>=3.6',
+                 zip_safe=False)
