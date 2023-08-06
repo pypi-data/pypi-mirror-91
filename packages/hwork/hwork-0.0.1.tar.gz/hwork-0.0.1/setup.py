4ae4b07cf6cc095ddd5f901c73daf881c5541970
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:34 +0000

    Add package hwork/0.0.1/hwork-0.0.1.tar.gz

---
+++ packages/hwork/hwork-0.0.1.tar.gz/hwork-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='hwork',
+        author="Satyarth Praveen",
+        author_email="sap89750@gmail.com",
+        url="https://github.com/satyarth934/test_pip_pkg.git",
+        version='0.0.1',
+        description='Say hello!',
+        py_modules=['hwork'],
+        package_dir={'': 'src'},
+)
+
