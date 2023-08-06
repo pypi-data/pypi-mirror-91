24bc53b1da1f15fc030acb2fce61a682b50ee2e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:52 +0000

    Add package windows-ha-util/0.1.2/windows_ha_util-0.1.2-py3-none-any.whl

---
+++ packages/windows-ha-util/windows_ha_util-0.1.2-py3-none-any.whl/windows_ha_util/discovery.py
@@ -0,0 +1,36 @@
+import socket # for connecting
+import requests
+
+def is_port_open(host, port):
+    """
+    determine whether `host` has the `port` open
+    """
+    # creates a new socket
+    s = socket.socket()
+    try:
+        # tries to connect to host using that port
+        s.settimeout(0.1)
+
+        s.connect((host, port))
+        # make timeout if you want it a little faster ( less accuracy )
+    except:
+        # cannot connect, port is closed
+        # return false
+        return False
+    else:
+        # the connection was established, port is open!
+        return True
+
+def discover() -> []: 
+    hosts = []
+    for host in range(0,100): 
+        print(host)
+        host_ip = f"192.168.1.{host}"
+        if is_port_open(host_ip, 3040):
+            id = requests.get(f"http://{host_ip}:3040/api/id").json()['id']
+            hosts.append({"ip": host_ip, "id": id})
+    return hosts
+
+if __name__ == "__main__":
+    discover()
+print("done")
