87ba76070286be92a79d87c8bad4377d1471146d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:49 +0000

    Add package kafkaesk/0.5.5/kafkaesk-0.5.5.tar.gz

---
+++ packages/kafkaesk/kafkaesk-0.5.5.tar.gz/kafkaesk-0.5.5/kafkaesk/ext/logging/__init__.py
@@ -0,0 +1,5 @@
+from .handler import PydanticKafkaeskHandler
+from .handler import PydanticLogModel
+from .handler import PydanticStreamHandler
+
+__all__ = ("PydanticLogModel", "PydanticKafkaeskHandler", "PydanticStreamHandler")
