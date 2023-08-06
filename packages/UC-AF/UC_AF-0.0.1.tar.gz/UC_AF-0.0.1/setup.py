510126590a584a56bb53c9796938e242dcda4f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:10 +0000

    Add package UC-AF/0.0.1/UC_AF-0.0.1.tar.gz

---
+++ packages/UC-AF/UC_AF-0.0.1.tar.gz/UC_AF-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='UC_AF',
+    version='0.0.1',
+    packages=['src', 'src.main'],
+    url='',
+    license='',
+    author='WSIT',
+    author_email='',
+    description=''
+)
