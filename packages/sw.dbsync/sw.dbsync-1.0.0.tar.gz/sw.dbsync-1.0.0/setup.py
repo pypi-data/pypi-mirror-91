2eae34161fa8eae9d00059c52ec4b3a14bb7c5b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:47 +0000

    Add package sw.dbsync/1.0.0/sw.dbsync-1.0.0.tar.gz

---
+++ packages/sw.dbsync/sw.dbsync-1.0.0.tar.gz/sw.dbsync-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['dbsync']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'sw.dbsync',
+    'version': '1.0.0',
+    'description': 'Implementation of a database synchronization schema for PEP 249 compliant databases.',
+    'long_description': '',
+    'author': 'Simon Wrede',
+    'author_email': 'simwr872@student.liu.se',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
