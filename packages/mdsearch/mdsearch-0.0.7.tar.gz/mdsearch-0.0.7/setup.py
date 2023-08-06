797df0ca05085a1eedd742dc95217501dc85b16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:11 +0000

    Add package mdsearch/0.0.7/mdsearch-0.0.7.tar.gz

---
+++ packages/mdsearch/mdsearch-0.0.7.tar.gz/mdsearch-0.0.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mdsearch", # Replace with your own username
+    version="0.0.7",
+    author="Low-level Maseter Do Search",
+    author_email="3120201103@bit.edu.cn",
+    description="mdsearch is a python package for paper search",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lydia07/mdsearch",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries", 
+        "Topic :: Software Development :: Libraries :: Python Modules", 
+        "Intended Audience :: Developers", 
+    ],
+    python_requires='>=3.6',
+    install_requires=['elasticsearch5', 'flask==1.1.2'],
+)
