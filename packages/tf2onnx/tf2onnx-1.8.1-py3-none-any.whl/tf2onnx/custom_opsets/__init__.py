79f783225ebc08f55c106935bd26073e1d671c1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:27 +0000

    Add package tf2onnx/1.8.1/tf2onnx-1.8.1-py3-none-any.whl

---
+++ packages/tf2onnx/tf2onnx-1.8.1-py3-none-any.whl/tf2onnx/custom_opsets/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+""" custom tf2onnx mapping functions. """
+
+from . import ms
+from . import onnx_ml
+from . import string_ops
---
+++ packages/tf2onnx/tf2onnx-1.8.1-py3-none-any.whl/tf2onnx/version.py
@@ -0,0 +1,3 @@
+
+version = '1.8.1'
+git_version = '4e49f39edba73b36c9fed9d271d45ea96ab02355'
