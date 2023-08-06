6d72db5d8b48ff6da8aa57f8a98fe398abeb9938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:48 +0000

    Add package ladm/0.1.2rc1/ladm-0.1.2rc1-py3-none-any.whl

---
+++ packages/ladm/ladm-0.1.2rc1-py3-none-any.whl/ladm/main/util/allowed_system_dependencies.py
@@ -0,0 +1,8 @@
+def get_allowed_system_dependencies():
+    allowed_system_dependencies = [
+        "libffi-dev",
+        "default-jre",
+        "openjdk-11-jre-headless",
+        "openjdk-8-jre-headless"
+    ]
+    return allowed_system_dependencies
