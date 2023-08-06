577087c52e039e1862974b369eec9128845321a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:29 +0000

    Add package torchkeras/2.1.1/torchkeras-2.1.1.tar.gz

---
+++ packages/torchkeras/torchkeras-2.1.1.tar.gz/torchkeras-2.1.1/setup.py
@@ -0,0 +1,29 @@
+# -*- coding:utf-8 -*-
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="torchkeras",
+    version="2.1.1",
+    author="PythonAiRoad",
+    author_email="lyhue1991@163.com",
+    description="pytorch❤️ keras",
+    long_description=long_description,
+    install_requires=[           
+         'pandas',
+         'pytorch_lightning',
+         'prettytable'
+         'tqdm'
+       ],
+    long_description_content_type="text/markdown",
+    url="https://github.com/lyhue1991/torchkeras",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5'
+)
+
