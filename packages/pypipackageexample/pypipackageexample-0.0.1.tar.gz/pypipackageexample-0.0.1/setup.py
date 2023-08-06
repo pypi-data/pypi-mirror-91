beae558724b6cc852c3232168ed608c409116d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:53 +0000

    Add package pypipackageexample/0.0.1/pypipackageexample-0.0.1.tar.gz

---
+++ packages/pypipackageexample/pypipackageexample-0.0.1.tar.gz/pypipackageexample-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name= 'pypipackageexample', 
+    version= '0.0.1',
+    license= 'MIT',
+    description= "paquete de prueba para una clase de Udemy",
+    author= "Jose Escribano Vega",
+    autthor_email= "escribanovegajose@gmail.com", 
+    url= 'https://github.com/escribanoVegaJose/pypipackageexample'
+)
