4951412e99eab09df313d743c9f839b791b9dc87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:21 +0000

    Add package SpecAnalysis-cader-Cademan051/1.5.5/SpecAnalysis_cader_Cademan051-1.5.5-py3-none-any.whl

---
+++ packages/SpecAnalysis-cader-Cademan051/SpecAnalysis_cader_Cademan051-1.5.5-py3-none-any.whl/Spec/Spec.py
@@ -0,0 +1,29 @@
+from Data_Handler import *
+
+
+class MainWindow(Spectrum):
+    def __init__(self):
+         pg.setConfigOption("background", "w")
+         pg.setConfigOption("foreground", "k")
+         Spectrum.__init__(self)
+
+
+
+#Creates GUI Application
+if __name__ == '__main__':
+    app = QApplication.instance()
+    if app==None:
+        app=QApplication([])
+    
+    main = MainWindow()
+    main.setGeometry(100,100,1300,800)
+    main.setWindowTitle("Spectrum")
+
+    
+    main.show()
+    sys.exit(app.exec_())
+    
+                                               
+    
+    
+     
