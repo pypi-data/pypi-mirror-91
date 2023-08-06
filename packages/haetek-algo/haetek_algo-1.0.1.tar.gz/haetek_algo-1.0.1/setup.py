c0ea6d02280b1d1ac3d7eb0e0008fc1752b20065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:18 +0000

    Add package haetek-algo/1.0.1/haetek_algo-1.0.1.tar.gz

---
+++ packages/haetek-algo/haetek_algo-1.0.1.tar.gz/haetek_algo-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='haetek_algo',
+    version='1.0.1',
+    py_modules=['haetek_algo'],
+    author='haetek_shenzhen',
+    author_email='haetek123@163.com',
+    url='https://haetek.com/',
+    description='Haetek helps you chase girls with Python'
+)
