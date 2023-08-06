b981917ffcdcd3ca1aa5b72f094d3f6d4b2d4015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:02 +0000

    Add package django-username-email/2.3.1/django-username-email-2.3.1.tar.gz

---
+++ packages/django-username-email/django-username-email-2.3.1.tar.gz/django-username-email-2.3.1/cuser/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from .settings import CUSER_SETTINGS
+
+
+class CuserConfig(AppConfig):
+    name = 'cuser'
+    verbose_name = CUSER_SETTINGS['app_verbose_name']
---
+++ packages/django-username-email/django-username-email-2.3.1.tar.gz/django-username-email-2.3.1/setup.py
@@ -0,0 +1,53 @@
+from os import path
+from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='django-username-email',
+
+    version='2.3.1',
+
+    description='Custom Django User model that makes email the USERNAME_FIELD.',
+    long_description=long_description,
+
+    url='https://github.com/tmm/django-username-email/',
+
+    author='Tom Meagher',
+    author_email='tom@meagher.co',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Operating System :: OS Independent',
+    ],
+    keywords='user email username',
+
+    packages=find_packages(),
+    include_package_data=True,
+
+    install_requires=[
+        'django',
+    ]
+)
