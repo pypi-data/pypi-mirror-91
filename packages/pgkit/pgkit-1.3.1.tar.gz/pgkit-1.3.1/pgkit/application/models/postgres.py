688fd20fb15af0f15d023dd28c6a6d911f18698a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:39 +0000

    Add package pgkit/1.3.1/pgkit-1.3.1.tar.gz

---
+++ packages/pgkit/pgkit-1.3.1.tar.gz/pgkit-1.3.1/pgkit/application/models/postgres.py
@@ -0,0 +1,21 @@
+from pgkit.application.utils import execute_sync
+
+
+class Postgres:
+    def __init__(self, name, host, port, version, dbname, username, password, slot):
+        self.name = name
+        self.host = host
+        self.port = port
+        self.version = version
+        self.dbname = dbname
+        self.username = username
+        self.password = password
+        self.slot = slot
+
+    def run_cmd(self, cmd):
+        execute_sync(f'psql -h {self.host} -p {self.port} -U {self.username} -d {self.dbname} -c {cmd}',
+                     env=[('PGPASSWORD', self.password)])
+
+    def force_switch_wal(self):
+        pg_command = 'select * from pg_switch_xlog()' if float(self.version) < 10 else 'select * from pg_switch_wal()'
+        self.run_cmd(pg_command)
