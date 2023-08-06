6cdd264bf3288e52aa427eef4006714fcc0f0977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:48 +0000

    Add package gstools/1.3.0rc1/gstools-1.3.0rc1-cp36-cp36m-win32.whl

---
+++ packages/gstools/gstools-1.3.0rc1-cp36-cp36m-win32.whl/gstools/transform/__init__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+GStools subpackage providing transformations.
+
+.. currentmodule:: gstools.transform
+
+Field-Transformations
+^^^^^^^^^^^^^^^^^^^^^
+
+.. autosummary::
+   binary
+   discrete
+   boxcox
+   zinnharvey
+   normal_force_moments
+   normal_to_lognormal
+   normal_to_uniform
+   normal_to_arcsin
+   normal_to_uquad
+
+----
+"""
+
+from gstools.transform.field import (
+    binary,
+    discrete,
+    boxcox,
+    zinnharvey,
+    normal_force_moments,
+    normal_to_lognormal,
+    normal_to_uniform,
+    normal_to_arcsin,
+    normal_to_uquad,
+)
+
+__all__ = [
+    "binary",
+    "discrete",
+    "boxcox",
+    "zinnharvey",
+    "normal_force_moments",
+    "normal_to_lognormal",
+    "normal_to_uniform",
+    "normal_to_arcsin",
+    "normal_to_uquad",
+]
