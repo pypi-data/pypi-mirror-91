cfd54dc6f80814cb9845bc02b929cf353e5aac13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:07:59 +0000

    Add package pieparty/1.5.3/pieparty-1.5.3.tar.gz

---
+++ packages/pieparty/pieparty-1.5.3.tar.gz/pieparty-1.5.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages 
+ 
+#with open('requirements.txt') as f: 
+#    requirements = f.readlines() 
+
+requirements = 'matplotlib'
+ 
+long_description = '' 
+  
+setup( 
+        name ='pieparty', 
+        version ='1.5.3', 
+        author ='Stefan Kurtenbach', 
+        author_email ='stefan.kurtenbach@med.miami.edu', 
+        #url ='https://github.com/amcruise/practice_party', 
+        description ='scRNA seq visualization tool.', 
+        long_description = long_description, 
+        long_description_content_type ="text/markdown", 
+        license ='MIT', 
+        packages = find_packages(),
+        entry_points = {
+         'console_scripts': [
+             'pieparty=pieparty.piemain:main',
+          ],
+        },
+        classifiers =[ 
+            "Programming Language :: Python :: 3", 
+            "License :: OSI Approved :: MIT License", 
+            "Operating System :: OS Independent", 
+        ], 
+        keywords ='scRNA, UMAP, tSNE', 
+        install_requires = requirements, 
+        zip_safe = False
+) 
