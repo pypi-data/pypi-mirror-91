88307537fe6262e0cb5589c7a412b376942cf411
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:52 +0000

    Add package windows-ha-util/0.1.2/windows-ha-util-0.1.2.tar.gz

---
+++ packages/windows-ha-util/windows-ha-util-0.1.2.tar.gz/windows-ha-util-0.1.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['windows_ha_util']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-nmap>=0.6.1,<0.7.0', 'requests>=2.25.1,<3.0.0']
+
+setup_kwargs = {
+    'name': 'windows-ha-util',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': None,
+    'author': 'Jacob Clarke',
+    'author_email': 'jacobclarke718@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
