f2515397444116ec1479c09dd05c549ca66443ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:17 +0000

    Add package funcverbnet/0.2.1/funcverbnet-0.2.1.tar.gz

---
+++ packages/funcverbnet/funcverbnet-0.2.1.tar.gz/funcverbnet-0.2.1/HISTORY.rst
@@ -0,0 +1,95 @@
+=======
+History
+=======
+
+0.1.0 (2020-10-15)
+------------------
+* First release on PyPI.
+
+0.1.1 (2020-11-10)
+------------------
+* [add] add basic data and methods
+
+0.1.2 (2020-11-10)
+------------------
+* [add] add basic data and methods
+
+0.1.3 (2020-11-12)
+------------------
+* [modify] modify the problem of JSON data upload
+
+0.1.4 (2020-11-17)
+------------------
+* [modify] modify some methods and f_verb.json
+
+0.1.5 (2020-12-1)
+------------------
+* [add] add sentences
+
+0.1.6 (2020-12-1)
+------------------
+* [add] add dependency
+
+0.1.7 (2020-12-1)
+------------------
+* [add] add dependency
+
+0.1.8 (2020-12-1)
+------------------
+* [add] add package_data
+
+0.1.9 (2020-12-3)
+------------------
+* [add] add package_data
+
+0.1.10 (2020-12-3)
+------------------
+* [add] add package_data
+
+0.1.11 (2020-12-3)
+------------------
+* [add] add package_data
+
+0.1.12 (2020-12-10)
+-------------------
+* [add] add sentence classification
+
+0.1.13 (2020-12-10)
+-------------------
+* [update] update model path
+
+0.1.14 (2020-12-12)
+-------------------
+* [modify] modify error
+
+0.1.15 (2020-12-15)
+-------------------
+* [error] modify error
+
+0.1.16 (2020-12-15)
+-------------------
+* [error] modify error
+
+0.1.17 (2020-12-15)
+-------------------
+* [update] update train data
+
+0.1.18 (2020-12-15)
+-------------------
+* [update] update python version
+
+0.1.19 (2020-12-18)
+-------------------
+* [add] add train data
+
+0.1.20 (2020-12-18)
+-------------------
+* [add] add train data
+
+0.2.0 (2021-1-14)
+-------------------
+* [add] add the methods of antisense verbs
+
+0.2.1 (2021-1-14)
+-------------------
+* [update] modify error
---
+++ packages/funcverbnet/funcverbnet-0.2.1.tar.gz/funcverbnet-0.2.1/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0',
+                'pandas', 'fasttext~=0.9.2',]
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Software Engineering Laboratory of Fudan University",
+    author_email='lmwtclmwtc@outlook.com',
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Provides a knowledge system constructed from functionality categories, verbs, and phrase patterns, as well as functionality for fine-grained analysis of functionality descriptions based on this knowledge system",
+    entry_points={
+        'console_scripts': [
+            'funcverbnet=funcverbnet.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="BSD license",
+    long_description=readme + '\n\n' + history,
+    packages=find_packages(include=['funcverbnet', 'funcverbnet.*']),
+    # include_package_data=True,
+    package_data={
+        # If any package contains *.json files, include them:
+        'funcverbnet': ['data/*.json', "data/*.zip", "data/*.csv", "model/*.model"],
+    },
+    keywords='funcverbnet',
+    name='funcverbnet',
+
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/FudanSELab/funcverbnet',
+    version='0.2.1',
+    zip_safe=False,
+)
