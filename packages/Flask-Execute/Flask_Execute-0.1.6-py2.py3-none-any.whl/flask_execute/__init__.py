bf1257df61a4149df9492104374cff69d433525c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:18 +0000

    Add package Flask-Execute/0.1.6/Flask_Execute-0.1.6-py2.py3-none-any.whl

---
+++ packages/Flask-Execute/Flask_Execute-0.1.6-py2.py3-none-any.whl/flask_execute/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+__pkg__ = 'Flask-Execute'
+__url__ = 'https://github.com/bprinty/Flask-Execute'
+__info__ = 'Simple Celery integration for Flask applications.'
+__author__ = 'Blake Printy'
+__email__ = 'bprinty@gmail.com'
+__version__ = '0.1.6'
+
+
+from .plugin import Celery            ## noqa
+from .plugin import current_task      ## noqa
