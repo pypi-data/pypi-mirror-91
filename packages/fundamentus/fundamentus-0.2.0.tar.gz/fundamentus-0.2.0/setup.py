17bd3c545afa553563c610cbc43ffa70ac306e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:28 +0000

    Add package fundamentus/0.2.0/fundamentus-0.2.0.tar.gz

---
+++ packages/fundamentus/fundamentus-0.2.0.tar.gz/fundamentus-0.2.0/setup.py
@@ -0,0 +1,51 @@
+
+from setuptools import setup
+from setuptools import find_packages
+
+
+description='Fundamentus: API to load data from Fundamentus website: https://www.fundamentus.com.br/',
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='fundamentus',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+
+    version='0.2.0',
+
+#   scripts=[
+#       'bin/fundamentus.csv.py',
+#       'bin/magic_formula.simple.py',
+#   ],
+
+    url='https://github.com/mv/fundamentus-api/',
+    author='Mv-Marcus Vinicius Ferreira',
+    author_email='ferreira.mv@gmail.com',
+
+    license='MIT',
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    install_requires=[
+       'requests >= 2.25.1',
+       'requests-cache >= 0.5.2',
+       'pandas >= 1.1.5',
+       'lxml >= 4.6.2',
+       'tabulate >= 0.8.7',
+    ],
+
+    python_requires='>=3.7',
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+
+
+)
