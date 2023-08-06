2690dc19076bbe1539052120ebbeda6f2da32ca6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:13 +0000

    Add package jittor/1.2.2.16/jittor-1.2.2.16.tar.gz

---
+++ packages/jittor/jittor-1.2.2.16.tar.gz/jittor-1.2.2.16/python/jittor/src/executor.h
@@ -0,0 +1,27 @@
+// ***************************************************************
+// Copyright (c) 2021 Jittor. All Rights Reserved.
+// Maintainers: Dun Liang <randonlang@gmail.com>. 
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE.txt', which is part of this source code package.
+// ***************************************************************
+#pragma once
+#include "common.h"
+#include "mem/allocator.h"
+#ifdef HAS_CUDA
+#include <cuda_runtime.h>
+#include "helper_cuda.h"
+#endif
+
+namespace jittor {
+
+struct Executor {
+    Allocator* allocator;
+    bool last_is_cuda = false;
+    void run_sync(vector<Var*> vars, bool device_sync);
+};
+
+extern Executor exe;
+
+void load_fused_op(FusedOp& fused_op, vector<int>& fuse_ops, vector<Op*>& ops, int ll, int rr, int64 tt);
+    
+} // jittor
