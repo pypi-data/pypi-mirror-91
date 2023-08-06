24f1a9f5c4a25c541879a5b81323d7f4b27b4803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:44 +0000

    Add package databricks-bundle/0.6.0a14/databricks_bundle-0.6.0a14-py3-none-any.whl

---
+++ packages/databricks-bundle/databricks_bundle-0.6.0a14-py3-none-any.whl/databricksbundle/notebook/decorator/DuplicateColumnsChecker.py
@@ -0,0 +1,43 @@
+import collections
+from typing import Tuple
+from injecta.container.ContainerInterface import ContainerInterface
+from pyspark.sql.dataframe import DataFrame
+
+class DuplicateColumnsChecker:
+
+    def __init__(
+        self,
+        container: ContainerInterface,
+    ):
+        self.__container = container
+
+    def check(self, df: DataFrame, fun: callable, sources: Tuple[callable]):
+        fieldNames = [field.name.lower() for field in df.schema.fields]
+        duplicateFields = dict()
+
+        for fieldName, count in collections.Counter(fieldNames).items():
+            if count > 1:
+                duplicateFields[fieldName] = []
+
+        if duplicateFields == dict():
+            return
+
+        g = fun.__globals__
+
+        fields2Tables = dict()
+
+        for source in sources:
+            sourceDf = g[source.__name__ + '_df']
+            for field in sourceDf.schema.fields:
+                fieldName = field.name.lower()
+
+                if fieldName not in fields2Tables:
+                    fields2Tables[fieldName] = []
+
+                fields2Tables[fieldName].append(source.__name__)
+
+        for duplicateField in duplicateFields:
+            self.__container.get('databricksbundle.logger').error(f'Duplicate field {duplicateField}', extra={'source_dataframes': fields2Tables[duplicateField]})
+
+        fieldsString = ', '.join(duplicateFields)
+        raise Exception(f'Duplicate output column(s): {fieldsString}. Disable by setting @transformation(checkDuplicateColumns=False)')
---
+++ packages/databricks-bundle/databricks_bundle-0.6.0a14-py3-none-any.whl/databricksbundle/notebook/function/ArgumentsResolver.py
@@ -0,0 +1,22 @@
+from typing import List
+from injecta.service.class_.InspectedArgument import InspectedArgument
+from databricksbundle.notebook.function.ArgumentResolver import ArgumentResolver
+
+class ArgumentsResolver:
+
+    def __init__(
+        self,
+        argumentResolver: ArgumentResolver,
+    ):
+        self.__argumentResolver = argumentResolver
+
+    def resolve(self, inspectedArguments: List[InspectedArgument], decoratorArgs: tuple):
+        def resolve(functionArgument: InspectedArgument, decoratorArgument):
+            return self.__argumentResolver.resolve(functionArgument, decoratorArgument)
+
+        decoratorArgsCount = len(decoratorArgs)
+
+        if decoratorArgsCount > len(inspectedArguments):
+            raise Exception('There are more decorator arguments than function arguments')
+
+        return tuple(resolve(functionArgument, decoratorArgs[idx] if idx < decoratorArgsCount else None) for idx, functionArgument in enumerate(inspectedArguments))
