beb12b26a7ad13989c9382397817399d2303f0d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:13 +0000

    Add package wavefront-opentracing-sdk-python/2.2.2/wavefront-opentracing-sdk-python-2.2.2.tar.gz

---
+++ packages/wavefront-opentracing-sdk-python/wavefront-opentracing-sdk-python-2.2.2.tar.gz/wavefront-opentracing-sdk-python-2.2.2/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""Wavefront Python SDK.
+
+This is a Wavefront Python SDK.
+"""
+
+import os
+
+import setuptools
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       'README.md')) as fd:
+    LONG_DESCRIPTION = fd.read()
+
+
+setuptools.setup(
+    name='wavefront-opentracing-sdk-python',
+    version='2.2.2',
+    author='Wavefront by VMware',
+    author_email='chitimba@wavefront.com',
+    url='https://github.com/wavefrontHQ/wavefront-opentracing-sdk-python',
+    license='Apache-2.0',
+    description='Wavefront Opentracing Python SDK',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    keywords=[
+        'OpenTracing',
+        'OpenTracing SDK',
+        'Wavefront',
+        'Wavefront SDK',
+        ],
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ],
+    include_package_data=True,
+    packages=setuptools.find_packages(exclude=('*.tests', '*.tests.*',
+                                               'tests.*', 'tests')),
+    install_requires=[
+        'opentracing>=2.0',
+        'wavefront-pyformance>=1.0',
+        'wavefront-sdk-python>=1.7.4',
+        ],
+    test_require=[
+        'freezegun>=0.3.11',
+        'mock>=2.0',
+        ],
+)
