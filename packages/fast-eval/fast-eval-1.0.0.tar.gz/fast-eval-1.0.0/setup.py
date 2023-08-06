63d64803c882f877ae5427bf11b60eec48c69d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:43 +0000

    Add package fast-eval/1.0.0/fast-eval-1.0.0.tar.gz

---
+++ packages/fast-eval/fast-eval-1.0.0.tar.gz/fast-eval-1.0.0/setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-
+from setuptools import setup, find_packages
+
+setup(
+    name='fast-eval',
+    packages=find_packages(exclude=["examples/*"]),
+    version='1.0.0',
+    description='Simple tool to provide automation to assessment processes.',
+    author=u'Virgile Daugé',
+    author_email='virgile.dauge@pm.me',
+    url='https://github.com/Virgile-Dauge/fast-eval',
+    # download_url='',
+    keywords=['assessment', 'evaluation'],
+    install_requires=['rich'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 3.6',
+        ],
+    entry_points={
+        'console_scripts': [
+            'fast-eval=fast_eval.__main__:main',
+        ],
+    },
+    python_requires='>=3.6',
+)
