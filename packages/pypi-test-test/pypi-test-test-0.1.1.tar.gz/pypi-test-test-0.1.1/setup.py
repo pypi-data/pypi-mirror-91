cab9c417a99a831289e2aa142aaf250fe7f08da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:21 +0000

    Add package pypi-test-test/0.1.1/pypi-test-test-0.1.1.tar.gz

---
+++ packages/pypi-test-test/pypi-test-test-0.1.1.tar.gz/pypi-test-test-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pypi_test_test']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pypi-test-test',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'amitkummer',
+    'author_email': 'amit.kummer@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
