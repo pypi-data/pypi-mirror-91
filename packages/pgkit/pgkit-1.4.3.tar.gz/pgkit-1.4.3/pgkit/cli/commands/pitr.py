8bd382fd179b9e8a3136535850e905f27960c691
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:25 +0000

    Add package pgkit/1.4.3/pgkit-1.4.3.tar.gz

---
+++ packages/pgkit/pgkit-1.4.3.tar.gz/pgkit-1.4.3/pgkit/cli/commands/pitr.py
@@ -0,0 +1,28 @@
+import click
+from pgkit.application.db import DB
+import pgkit.application.pitr as PITR
+
+
+@click.group()
+def pitr():
+    pass
+
+
+@pitr.command()
+@click.argument('name', required=True)
+@click.argument('delay', required=True, type=int)
+def backup(name, delay):
+    config = DB.get_config(name)
+    PITR.backup(**{**config, 'replica_delay': delay})
+
+
+@pitr.command()
+def restore():
+    pass
+
+@pitr.command()
+@click.argument('name', required=True)
+def status(name):
+    config = DB.get_config(name)
+    PITR.print_status(**config)
+
