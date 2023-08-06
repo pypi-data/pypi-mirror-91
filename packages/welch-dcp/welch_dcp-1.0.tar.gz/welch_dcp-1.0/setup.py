db3262a02ddaa485ee90a695a940a37e7ae5b976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:48 +0000

    Add package welch-dcp/1.0/welch_dcp-1.0.tar.gz

---
+++ packages/welch-dcp/welch_dcp-1.0.tar.gz/welch_dcp-1.0/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+
+
+setup(name='welch_dcp',
+		version='1.0',
+		author='Hamada Gasmallah',
+		packages=['welch'],
+		install_requires=[
+			'numpy'
+		],
+		keywords=["Signal processing"],
+		classifiers=[
+		'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+		'Intended Audience :: Developers',      # Define that your audience are developers
+		'Topic :: Software Development :: Libraries',
+		'License :: OSI Approved :: MIT License',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.7',
+		'Programming Language :: Python :: 3.8',
+		]
+      )
+
+
+
---
+++ packages/welch-dcp/welch_dcp-1.0.tar.gz/welch_dcp-1.0/welch/__init__.py
@@ -0,0 +1,1 @@
+from .welch import *
