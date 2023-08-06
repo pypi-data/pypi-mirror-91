113ce5fc468dc3103d84273a6c1a98a26343a5a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:01 +0000

    Add package django-phone-login-fix-version/0.0.9/django-phone-login-fix-version-0.0.9.tar.gz

---
+++ packages/django-phone-login-fix-version/django-phone-login-fix-version-0.0.9.tar.gz/django-phone-login-fix-version-0.0.9/setup.py
@@ -0,0 +1,85 @@
+import os
+from distutils.util import convert_path
+from fnmatch import fnmatchcase
+
+from setuptools import find_packages, setup
+
+try:
+    from pypandoc import convert_file
+
+    def read_md(f):
+        return convert_file(f, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+
+    def read_md(f):
+        return open(f, 'r').read()
+
+version = __import__('phone_login').__version__
+
+
+# Provided as an attribute, so you can append to these instead
+# of replicating them:
+standard_exclude = ["*.py", "*.pyc", "*~", ".*", "*.bak", "Makefile"]
+standard_exclude_directories = [
+    ".*", "CVS", "_darcs", "./build",
+    "./dist", "EGG-INFO", "*.egg-info",
+    "./example"
+]
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+REQUIREMENTS = ["django-phonenumber-field==5.0.0", "django-sendsms==0.3.1"]
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-phone-login-fix-version',
+    version=version,
+    include_package_data=True,
+    license='BSD License',
+    description='Django Phone Login: Login with OTP (fix version)',
+    long_description=read_md('README.md'),
+    url='https://github.com/PyCormick/django-phone-login',
+    author='Daniil Agarev',
+    author_email='severustime@gmail.com',
+    install_requires=REQUIREMENTS,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    packages=get_packages('phone_login'),
+    package_data=get_package_data('phone_login'),
+)
