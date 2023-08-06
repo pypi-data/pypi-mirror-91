cf21b6888a26845ff5fb8059b8fcb71c7dd51020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:54 +0000

    Add package shiftencode/0.0.4/shiftencode-0.0.4.tar.gz

---
+++ packages/shiftencode/shiftencode-0.0.4.tar.gz/shiftencode-0.0.4/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="shiftencode", # Replace with your own username
+    version="0.0.4",
+    author="Dip Ghosh",
+    author_email="dipghoshraj@gmail.com",
+    description="shift-encode is a encoding and decoding libery for text and messages",
+    long_description= open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dipghoshraj/shiftencode",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
