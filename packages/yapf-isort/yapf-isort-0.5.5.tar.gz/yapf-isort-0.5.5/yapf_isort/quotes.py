b4d88235769726994636513950348a5475236baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:16 +0000

    Add package yapf-isort/0.5.5/yapf-isort-0.5.5.tar.gz

---
+++ packages/yapf-isort/yapf-isort-0.5.5.tar.gz/yapf-isort-0.5.5/yapf_isort/quotes.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+#
+#  quotes.py
+"""
+Reformatter for quotes.
+"""
+#
+#  Copyright © 2020 Dominic Davis-Foster <dominic@davis-foster.co.uk>
+#  License: Apache Software License 2.0
+#  See the LICENSE file for details.
+#
+
+# stdlib
+import ast
+import json
+import re
+import sys
+from io import StringIO
+from typing import List
+
+# 3rd party
+import asttokens  # type: ignore
+
+__all__ = ["Visitor", "double_repr", "reformat_quotes"]
+
+
+class Visitor(ast.NodeVisitor):  # noqa: D101
+
+	def __init__(self):
+		super().__init__()
+		self.string_nodes: List[ast.Str] = []
+
+	if sys.version_info[:2] < (3, 8):
+
+		def visit_Str(self, node: ast.Str) -> None:  # noqa: D102
+			self.string_nodes.append(node)
+	else:
+
+		def visit_Constant(self, node: ast.Constant) -> None:  # noqa: D102
+			if isinstance(node.value, str):
+				self.string_nodes.append(node)
+			else:
+				self.generic_visit(node)
+
+	def visit(self, node: ast.AST) -> List[ast.Str]:  # noqa: D102
+		super().visit(node)
+		return self.string_nodes
+
+
+def double_repr(string: str):
+	"""
+	Like :func:`repr`, but tries to use double quotes instead.
+	"""
+
+	# figure out which quote to use; double is preferred
+	if '"' in string and "'" not in string:
+		return repr(string)
+	else:
+		return json.dumps(string, ensure_ascii=False)
+
+
+def reformat_quotes(source: str) -> str:
+	"""
+	Reformats quotes in the given source, and returns the reformatted source.
+
+	:param source:
+	"""
+
+	offset = 0
+	buf = StringIO()
+	visitor = Visitor()
+	atok = asttokens.ASTTokens(source, parse=True)
+
+	def key_func(value):
+		return atok.get_text_range(value)[0]
+
+	try:
+		for string_node in sorted(visitor.visit(atok.tree), key=key_func):
+			text_range = atok.get_text_range(string_node)
+
+			if text_range == (0, 0):
+				continue
+
+			buf.write(source[offset:text_range[0]])
+
+			if source[text_range[0]:text_range[1]] in {'""', "''"}:
+				buf.write("''")
+			elif not re.match("^[\"']", source[text_range[0]:text_range[1]]):
+				buf.write(source[text_range[0]:text_range[1]])
+			elif len(string_node.s) == 1:
+				buf.write(repr(string_node.s))
+			elif '\n' in source[text_range[0]:text_range[1]]:
+				buf.write(source[text_range[0]:text_range[1]])
+			elif '\n' in string_node.s or "\\n" in string_node.s:
+				buf.write(source[text_range[0]:text_range[1]])
+			else:
+				buf.write(double_repr(string_node.s))
+
+			offset = text_range[1]
+
+		buf.write(source[offset:])
+
+		return buf.getvalue()
+
+	except NotImplementedError as e:  # pragma: no cover
+		print(f"An error occurred: {e}")
+		return source
