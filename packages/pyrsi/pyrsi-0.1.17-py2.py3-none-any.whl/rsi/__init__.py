efdb6c6b2f6ca01fc4b64ce7433f8711ffa15070
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:41 +0000

    Add package pyrsi/0.1.17/pyrsi-0.1.17-py2.py3-none-any.whl

---
+++ packages/pyrsi/pyrsi-0.1.17-py2.py3-none-any.whl/rsi/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for Python RSI."""
+__author__ = """Ventorvar"""
+__email__ = 'ventorvar@gmail.com'
+__version__ = '0.1.17'
+
+
+
+# TODO: future endpoints to check out
+# https://robertsspaceindustries.com/api/stats/getCrowdfundStats
+# https://robertsspaceindustries.com/api/account/badge/getBadges
+
+from .rsi import RSISite
