95c1f00df61c2d7fb80d385ed8948a54d230b862
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:46 +0000

    Add package yeonji/0.6.0/yeonji-0.6.0.tar.gz

---
+++ packages/yeonji/yeonji-0.6.0.tar.gz/yeonji-0.6.0/yeonji/__pkginfo__.py
@@ -0,0 +1,27 @@
+from . import modulenames
+
+name = "yeonji"
+author = {
+	"name":"Johannes Krattenmacher",
+	"email":"python@krateng.dev",
+	"github":"krateng"
+}
+desc = "Collection of command line tools"
+version = 0,6,0
+
+
+requires = [
+	"tabulate",
+	"send2trash",
+	"doreah>=1.6.11"
+]
+
+resources = [
+]
+
+
+
+commands = {
+	mod:mod + ":main"
+	for mod in modulenames
+}
