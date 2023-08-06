0832bf2853c02f1b7ac1b7290ab3e238401de689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:59 +0000

    Add package Doric/1.1.0/Doric-1.1.0.tar.gz

---
+++ packages/Doric/Doric-1.1.0.tar.gz/Doric-1.1.0/Doric/__init__.py
@@ -0,0 +1,34 @@
+
+
+from .ProgNet import ProgBlock
+from .ProgNet import ProgColumnGenerator
+from .ProgNet import ProgColumn
+from .ProgNet import ProgNet
+from .ProgNet import ProgInertBlock
+
+from .ProgBlocks import ProgDenseBlock
+from .ProgBlocks import ProgDenseBNBlock
+
+from .MultiBlock import MultiBlock as ProgMultiBlock
+from .MultiBlock import PassBlock as ProgPassBlock
+from .MultiBlockReducers import ConcatReducer
+from .MultiBlockReducers import SumReducer
+
+from .UtilityBlocks import ProgSkip
+from .UtilityBlocks import ProgLambda
+from .UtilityBlocks import ProgReshape
+
+from .UtilityFunctions import enumerateFloatTensor
+from .UtilityFunctions import getNBiggestWeights
+from .UtilityFunctions import zeroLaterals
+
+from .extra_blocks import ProgConv2DBlock
+from .extra_blocks import ProgConvTranspose2DBNBlock
+from .extra_blocks import ProgConv2DBNBlock
+from .extra_blocks import ProgDeformConv2DBlock
+from .extra_blocks import ProgDeformConv2DBNBlock
+
+DORIC_VERSION = "1.1.0"
+
+
+#===============================================================================
