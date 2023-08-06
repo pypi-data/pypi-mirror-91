10cabe035709028e2a9a4b275aa76542140772aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:24 +0000

    Add package python-iugu/0.1.9/python_iugu-0.1.9-py3-none-any.whl

---
+++ packages/python-iugu/python_iugu-0.1.9-py3-none-any.whl/python_iugu/__init__.py
@@ -0,0 +1,23 @@
+from python_iugu.service import (
+    InvoiceService,
+    SubscriptionService,
+    CustomerService
+)
+
+from python_iugu.request import (
+    customer_request,
+    invoice_request,
+    subscription_request,
+    utils_request
+)
+from python_iugu.version import __version__
+from python_iugu.client import config
+
+from python_iugu.model import (
+    invoice,
+    custom_variable,
+    customer,
+    item,
+    payment_method,
+    subscription
+)
---
+++ packages/python-iugu/python_iugu-0.1.9-py3-none-any.whl/python_iugu/service/__init__.py
@@ -0,0 +1,3 @@
+from python_iugu.service.invoice import InvoiceService
+from python_iugu.service.customer import CustomerService
+from python_iugu.service.subscription import SubscriptionService
