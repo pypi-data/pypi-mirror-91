4c751e0eb522e8f60af7f48fc1c2ba159c34bf3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package streamlit-webrtc/0.4.1/streamlit-webrtc-0.4.1.tar.gz

---
+++ packages/streamlit-webrtc/streamlit-webrtc-0.4.1.tar.gz/streamlit-webrtc-0.4.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['streamlit_webrtc']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aiortc>=1.0.0', 'streamlit>=0.63']
+
+extras_require = \
+{':python_version < "3.8"': ['typing_extensions>=3.7.4,<4.0.0']}
+
+setup_kwargs = {
+    'name': 'streamlit-webrtc',
+    'version': '0.4.1',
+    'description': '',
+    'long_description': '# streamlit-webrtc\n',
+    'author': 'Yuichiro Tsuchiya',
+    'author_email': 't.yic.yt@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
