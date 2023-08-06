6f25fa8e25b508d9063a778546a32f83fb36e384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:33 +0000

    Add package pybooklib/1.1/pybooklib-1.1.tar.gz

---
+++ packages/pybooklib/pybooklib-1.1.tar.gz/pybooklib-1.1/pybooklib/pylib_cli.py
@@ -0,0 +1,15 @@
+import click
+
+from pybooklib import pylib
+
+
+@click.group()
+def cli():
+    "Host your own PyLib Server"
+    pass
+
+
+@cli.command()
+def run():
+    "Runs PyLib server"
+    pylib.run()
