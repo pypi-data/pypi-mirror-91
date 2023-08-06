7286d8c8a989544320b950bfc9cb030cc2c5d966
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:45 +0000

    Add package heare-config/0.0.1/heare-config-0.0.1.tar.gz

---
+++ packages/heare-config/heare-config-0.0.1.tar.gz/heare-config-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='heare-config',
+      version='0.0.1',
+      description='Heare.io Configuration Utilities',
+      author='Sean Fitzgerald',
+      author_email='seanfitz@heare.io',
+      url='https://github.com/heare-io/heare0config',
+      packages=['heare.config'],
+      )
