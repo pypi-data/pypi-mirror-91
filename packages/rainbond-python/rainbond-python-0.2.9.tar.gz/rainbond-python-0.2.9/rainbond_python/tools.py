3921385e74c36cdad4fc5a8ee93777aca7c1d78d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:55 +0000

    Add package rainbond-python/0.2.9/rainbond-python-0.2.9.tar.gz

---
+++ packages/rainbond-python/rainbond-python-0.2.9.tar.gz/rainbond-python-0.2.9/rainbond_python/tools.py
@@ -0,0 +1,18 @@
+from datetime import datetime, timedelta
+
+
+def handle_date(date: str, date_type='start'):
+    try:
+        if len(date) == 0:
+            date_data = None
+        elif date.isdigit():
+            date_time = date[:10]
+            date_data = datetime.fromtimestamp(int(date_time))
+        else:
+            date_data = datetime.strptime(date, "%Y-%m-%d")
+        if date_type == 'end':
+            date_data += timedelta(hours=23, minutes=59, seconds=59)
+    except Exception as err:
+        date_data = None
+        logging.warning('日期字符串解析异常: {0}'.format(err))
+    return date_data
