48dc48b8d6f02752e298b59fbed7cc487a89cf26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:10 +0000

    Add package etch-a-tv/0.1.1/etch_a_tv-0.1.1.tar.gz

---
+++ packages/etch-a-tv/etch_a_tv-0.1.1.tar.gz/etch_a_tv-0.1.1/setup.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['etch_a_tv']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyAudio>=0.2.11,<0.3.0',
+ 'numpy>=1.19.5,<2.0.0',
+ 'pygame-gui>=0.5.7,<0.6.0',
+ 'pygame>=2.0.1,<3.0.0',
+ 'scipy>=1.6.0,<2.0.0']
+
+entry_points = \
+{'console_scripts': ['etch_a_tv = etch_a_tv:__main__.main']}
+
+setup_kwargs = {
+    'name': 'etch-a-tv',
+    'version': '0.1.1',
+    'description': 'Draw images over the radio',
+    'long_description': None,
+    'author': 'Michaela',
+    'author_email': 'git@michaela.lgbt',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
