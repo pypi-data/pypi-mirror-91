67518338796f015b7462a13c221d4113e77ab9ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:25 +0000

    Add package poetry-mkdocs-shenxzh/0.1.0/poetry_mkdocs_shenxzh-0.1.0-py3-none-any.whl

---
+++ packages/poetry-mkdocs-shenxzh/poetry_mkdocs_shenxzh-0.1.0-py3-none-any.whl/poetry_mkdocs_shenxzh/hello.py
@@ -0,0 +1,12 @@
+import time
+
+
+class Hello:
+    def __init__(self):
+        pass
+
+    def sayHello(self):
+        """
+        Just print hello world!
+        """
+        print("Hello World!")
