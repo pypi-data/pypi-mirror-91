a69f87a0281088941d4db3a8e4346e08b22f240c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:32 +0000

    Add package pyyatl/0.1.1/PyYATL-0.1.1.tar.gz

---
+++ packages/pyyatl/PyYATL-0.1.1.tar.gz/PyYATL-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['yatl']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'pyyatl',
+    'version': '0.1.1',
+    'description': 'YAML Templating Language',
+    'long_description': '',
+    'author': 'Dan Hipschman',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/d5h-foss/yatl',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
