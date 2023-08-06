a99043c36950fd6bd69fa398b3db6e058cc92dba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:16 +0000

    Add package libscientific/1.2.5/libscientific-1.2.5-py3-none-any.whl

---
+++ packages/libscientific/libscientific-1.2.5-py3-none-any.whl/libscientific/pls.py
@@ -0,0 +1,341 @@
+# pls libscientific python binding
+#
+# Copyright (C) <2019>  Giuseppe Marco Randazzo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+import ctypes
+import libscientific.matrix as mx
+import libscientific.vector as vect
+import libscientific.tensor as t
+from libscientific.loadlibrary import LoadLibrary
+
+lsci = LoadLibrary()
+
+
+class PLSMODEL(ctypes.Structure):
+    _fields_ = [
+        ("xscores", ctypes.POINTER(mx.matrix)),
+        ("xloadings", ctypes.POINTER(mx.matrix)),
+        ("xweights", ctypes.POINTER(mx.matrix)),
+        ("yscores", ctypes.POINTER(mx.matrix)),
+        ("yloadings", ctypes.POINTER(mx.matrix)),
+        ("cweights", ctypes.POINTER(mx.matrix)),
+        ("b", ctypes.POINTER(vect.dvector)),
+        ("xvarexp", ctypes.POINTER(vect.dvector)),
+        ("xcolaverage", ctypes.POINTER(vect.dvector)),
+        ("xcolscaling", ctypes.POINTER(vect.dvector)),
+        ("ycolaverage", ctypes.POINTER(vect.dvector)),
+        ("ycolscaling", ctypes.POINTER(vect.dvector)),
+        ("recalculated_y", ctypes.POINTER(mx.matrix)),
+        ("recalc_residuals", ctypes.POINTER(mx.matrix)),
+        ("predicted_y", ctypes.POINTER(mx.matrix)),
+        ("pred_residuals", ctypes.POINTER(mx.matrix)),
+        ("recalc_residuals", ctypes.POINTER(mx.matrix)),
+        ("r2y_recalculated", ctypes.POINTER(mx.matrix)),
+        ("r2y_validation", ctypes.POINTER(mx.matrix)),
+        ("q2y", ctypes.POINTER(mx.matrix)),
+        ("sdep", ctypes.POINTER(mx.matrix)),
+        ("sdec", ctypes.POINTER(mx.matrix)),
+        ("bias", ctypes.POINTER(mx.matrix)),
+        ("roc_recalculated", ctypes.POINTER(t.tensor)),
+        ("roc_validation", ctypes.POINTER(t.tensor)),
+        ("roc_auc_recalculated", ctypes.POINTER(mx.matrix)),
+        ("roc_auc_validation", ctypes.POINTER(mx.matrix)),
+        ("roc_auc_validation", ctypes.POINTER(mx.matrix)),
+        ("precision_recall_recalculated", ctypes.POINTER(t.tensor)),
+        ("precision_recall_validation", ctypes.POINTER(t.tensor)),
+        ("precision_recall_ap_recalculated", ctypes.POINTER(mx.matrix)),
+        ("precision_recall_ap_validation", ctypes.POINTER(mx.matrix)),
+        ("yscrambling", ctypes.POINTER(mx.matrix))]
+
+
+lsci.NewPLSModel.argtypes = [ctypes.POINTER(ctypes.POINTER(PLSMODEL))]
+lsci.NewPLSModel.restype = None
+
+
+def NewPLSModel():
+    """
+    NewPLSModel: Allocate in memory an empty libscientific PLS model
+    """
+    mpls = ctypes.POINTER(PLSMODEL)()
+    lsci.NewPLSModel(ctypes.pointer(mpls))
+    return mpls
+
+
+lsci.DelPLSModel.argtypes = [ctypes.POINTER(ctypes.POINTER(PLSMODEL))]
+lsci.DelPLSModel.restype = None
+
+
+def DelPLSModel(mpls):
+    """
+    DelPLSModel: Delete an allocated libscientific PLS model
+    """
+    lsci.DelPLSModel(ctypes.pointer(mpls))
+
+
+lsci.PLS.argtypes = [ctypes.POINTER(mx.matrix),
+                     ctypes.POINTER(mx.matrix),
+                     ctypes.c_size_t,
+                     ctypes.c_size_t,
+                     ctypes.c_size_t,
+                     ctypes.POINTER(PLSMODEL),
+                     ctypes.POINTER(ctypes.c_int)]
+lsci.PLS.restype = None
+
+
+def PLS_(x, y, nlv, xscaling, yscaling, mpls):
+    """
+    PLS: Calculate the PLS model using a matrix x and a matrix y
+         according to the NIPALS algorithm
+
+    PLS(matrix *mx,
+        matrix *my,
+        size_t nlv,
+        size_t xautoscaling,
+        size_t yautoscaling,
+        PLSMODEL* model,
+        ssignal *s)
+    """
+    print(nlv)
+    ssignal = ctypes.c_int(0)
+    lsci.PLS(x,
+             y,
+             nlv,
+             xscaling,
+             yscaling,
+             mpls,
+             ctypes.pointer(ssignal))
+
+
+lsci.PLSBetasCoeff.argtypes = [ctypes.POINTER(PLSMODEL),
+                               ctypes.c_size_t,
+                               ctypes.POINTER(ctypes.POINTER(vect.dvector))]
+lsci.PLSBetasCoeff.restype = None
+
+
+def PLSBetasCoeff(mpls, nlv, bcoeff):
+    """
+    PLSBetasCoeff calculation
+    """
+    lsci.PLSBetasCoeff(mpls, nlv, ctypes.pointer(ctypes.pointer(bcoeff)))
+
+
+
+lsci.PLSScorePredictor.argtypes = [ctypes.POINTER(mx.matrix),
+                                   ctypes.POINTER(PLSMODEL),
+                                   ctypes.c_size_t,
+                                   ctypes.POINTER(ctypes.POINTER(mx.matrix))]
+lsci.PLSScorePredictor.restype = None
+
+
+def PLSScorePredictor(x, mpls, nlv, pscores):
+    """
+    PLSScorePredictor: Predict scores for a matrix m in the computed PLS modes
+    """
+    lsci.PLSScorePredictor(x,
+                           mpls,
+                           nlv,
+                           ctypes.pointer(pscores))
+
+
+# void PLSYPredictor(matrix *tscore, PLSMODEL *model, size_t nlv, matrix **y);
+
+lsci.PLSYPredictor.argtypes = [ctypes.POINTER(mx.matrix),
+                               ctypes.POINTER(PLSMODEL),
+                               ctypes.c_size_t,
+                               ctypes.POINTER(ctypes.POINTER(mx.matrix))]
+lsci.PLSYPredictor.restype = None
+
+
+def PLSYPredictor(xscores, mpls, nlv, predicted_y):
+    """
+    PLSYPredictor: Predict the Y according the predicted scores and the calculated pls model.
+                   This function is dependent on PLSScorePredictor.
+    """
+    lsci.PLSYPredictor(xscores, mpls, nlv, ctypes.pointer(ctypes.pointer(predicted_y)))
+
+
+# void PLSYPredictorAllLV(matrix *mx, PLSMODEL *model, matrix **tscores, matrix **y);
+
+lsci.PLSYPredictorAllLV.argtypes = [ctypes.POINTER(mx.matrix),
+                                    ctypes.POINTER(PLSMODEL),
+                                    ctypes.POINTER(ctypes.POINTER(mx.matrix)),
+                                    ctypes.POINTER(ctypes.POINTER(mx.matrix))]
+lsci.PLSYPredictorAllLV.restype = None
+
+
+def PLSYPredictorAllLV(x, mpls, predicted_scores, predicted_y):
+    """
+    PLSYPredictorAllLV: Predict the Y according the original feature matrix and the calculated pls model.
+                   This function is NOT dependent on PLSScorePredictor.
+    """
+    lsci.PLSYPredictorAllLV(x,
+                            mpls,
+                            ctypes.pointer(predicted_scores),
+                            ctypes.pointer(predicted_y))
+
+
+
+lsci.PrintPLSModel.argtypes = [ctypes.POINTER(PLSMODEL)]
+lsci.PrintPLSModel.restype = None
+
+
+def PrintPLS(mpls):
+    """
+    PrintPLS: Print to video the PLS Model
+    """
+    lsci.PrintPLSModel(mpls)
+
+
+class PLS(object):
+    """
+    Partial least squares
+        Arguments:
+            nlv (int) : number of latent variable
+            xscaling (int) : scaling type for x matrix
+            yscaling (int) : scaling type for y matrix
+            N.B.: xscaling and yscaling can be:
+                0 -> No scaling
+                1 -> Standard deviation scaling
+                2 -> Root mean squared scaling
+                3 -> Pareto scaling
+                4 -> Range scaling
+                5 -> Level scaling
+    """
+    def __init__(self, nlv, xscaling=1, yscaling=0):
+        self.mpls = NewPLSModel()
+        self.nlv = nlv
+        self.xscaling = xscaling
+        self.yscaling = yscaling
+
+    def __del__(self):
+        if self.mpls is not None:
+            DelPLSModel(self.mpls)
+            del self.mpls
+        self.mpls = None
+
+    def fit(self, x_, y_, cross_validation=None):
+        x = None
+        xalloc = False
+        if "Matrix" not in str(type(x_)):
+            x = mx.NewMatrix(x_)
+            xalloc = True
+        else:
+            x = x_
+
+        y = None
+        yalloc = False
+        if "Matrix" not in str(type(y_)):
+            y = mx.NewMatrix(y_)
+            yalloc = True
+        else:
+            y = y_
+        PLS_(x,
+             y,
+             self.nlv,
+             self.xscaling,
+             self.yscaling,
+             self.mpls)
+
+        if xalloc is True:
+            mx.DelMatrix(x)
+            del x
+
+        if yalloc is True:
+            mx.DelMatrix(y)
+            del y
+
+    def get_tscores(self):
+        return mx.MatrixToList(self.mpls[0].xscores)
+
+    def get_uscores(self):
+        return mx.MatrixToList(self.mpls[0].yscores)
+
+    def get_ploadings(self):
+        return mx.MatrixToList(self.mpls[0].xloadings)
+
+    def get_qloadings(self):
+        return mx.MatrixToList(self.mpls[0].yloadings)
+
+    def get_weights(self):
+        return mx.MatrixToList(self.mpls[0].xweights)
+
+    def get_exp_variance(self):
+        return vect.DVectorToList(self.mpls[0].xvarexp)
+
+    def predict(self, x_, nlv_=None):
+        x = mx.NewMatrix(x_)
+        pscores_ = mx.initMatrix()
+        py_ = mx.initMatrix()
+
+        nlv = None
+        if nlv_ is None:
+            nlv = self.nlv
+        else:
+            nlv = self.nlv
+
+        PLSYPredictorAllLV(x, self.mpls, pscores_, py_)
+        pscores = mx.MatrixToList(pscores_)
+        py = mx.MatrixToList(py_)
+        mx.DelMatrix(x)
+        del x
+        mx.DelMatrix(pscores_)
+        del pscores_
+        mx.DelMatrix(py_)
+        del py_
+        return py, pscores
+
+
+if __name__ == '__main__':
+    def mx_to_video(m, decimals=5):
+        for row in m:
+            print("\t".join([str(round(x, decimals)) for x in row]))
+    import random
+    random.seed(123456)
+    x = [[random.random() for j in range(4)] for i in range(10)]
+    y = [[random.random() for j in range(1)] for i in range(10)]
+    xp = [[random.random() for j in range(4)] for i in range(10)]
+
+    print("Original Matrix")
+    print("X")
+    mx_to_video(x)
+    print("Y")
+    mx_to_video(y)
+    print("XP")
+    mx_to_video(xp)
+    print("Computing PLS ...")
+    model = PLS(nlv=2, xscaling=1, yscaling=0)
+    model.fit(x, y)
+    print("Showing the PLS T scores")
+    tscores = model.get_tscores()
+    mx_to_video(tscores, 3)
+
+    print("Showing the PLS U scores")
+    uscores = model.get_uscores()
+    mx_to_video(uscores, 3)
+
+    print("Showing the PLS P loadings")
+    ploadings = model.get_ploadings()
+    mx_to_video(ploadings, 3)
+
+    print("Showing the X Variance")
+    print(model.get_exp_variance())
+
+
+    print("Predict XP")
+    py, pscores = model.predict(xp)
+    print("Predicted Y for all LVs")
+    mx_to_video(py, 3)
+    print("Predicted Scores")
+    mx_to_video(pscores, 3)
---
+++ packages/libscientific/libscientific-1.2.5-py3-none-any.whl/libscientific/vector.py
@@ -0,0 +1,313 @@
+# vector libscientific python binding
+#
+# Copyright (C) <2019>  Giuseppe Marco Randazzo
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import ctypes
+from libscientific.loadlibrary import LoadLibrary
+from libscientific import misc
+
+lsci = LoadLibrary()
+
+
+class strvector(ctypes.Structure):
+    _fields_ = [
+        ("data",    ctypes.POINTER(ctypes.POINTER(ctypes.c_char))),
+        ("size",     ctypes.c_size_t)]
+
+
+class dvector(ctypes.Structure):
+    _fields_ = [
+        ("data",    ctypes.POINTER(ctypes.c_double)),
+        ("size",     ctypes.c_size_t)]
+
+
+lsci.initDVector.argtypes = [ctypes.POINTER(ctypes.POINTER(dvector))]
+lsci.initDVector.restype = None
+
+
+def initDVector():
+    """
+    initDVector: Allocate in memory an empty libscientific double vector
+    """
+    d = ctypes.POINTER(dvector)()
+    lsci.initDVector(ctypes.pointer(d))
+    return d
+
+
+lsci.NewDVector.argtypes = [ctypes.POINTER(ctypes.POINTER(dvector)),
+                            ctypes.c_size_t]
+lsci.NewDVector.restype = None
+
+
+def NewDVector(vlst):
+    """
+    NewDVector: Allocate in memory a libscientific double vector from a list
+    """
+    size = len(vlst)
+    d = ctypes.POINTER(dvector)()
+    lsci.NewDVector(ctypes.pointer(d), size)
+
+    for i in range(size):
+        val = None
+        try:
+            val = float(vlst[i])
+        except ValueError:
+            val = None
+        if val is None:
+            lsci.setDVectorValue(d, i, misc.missing_value())
+        else:
+            lsci.setDVectorValue(d, i, val)
+    return d
+
+
+lsci.DVectorResize.argtypes = [ctypes.POINTER(ctypes.POINTER(dvector)),
+                               ctypes.c_size_t]
+lsci.DVectorResize.restype = None
+
+
+def DVectorResize(d, size):
+    """
+    DVectorResize: Resize an already allocated or reallocate a libscientific
+                   double vector
+    """
+    lsci.DVectorResize(ctypes.pointer(d), size)
+
+
+lsci.DelDVector.argtypes = [ctypes.POINTER(ctypes.POINTER(dvector))]
+lsci.DelDVector.restype = None
+
+
+def DelDVector(d):
+    """
+    DelDVector: Delete an allocated libscientific double vector
+    """
+    lsci.DelDVector(ctypes.pointer(d))
+
+
+lsci.PrintDVector.argtypes = [ctypes.POINTER(dvector)]
+lsci.PrintDVector.restype = None
+
+
+def PrintDVector(d):
+    """
+    PrintDVector: Print to video a libscientific double vector
+    """
+    lsci.PrintDVector(d)
+
+
+lsci.DVectorHasValue.argtypes = [ctypes.POINTER(dvector), ctypes.c_double]
+lsci.DVectorHasValue.restype = ctypes.c_int
+
+
+def DVectorHasValue(v, val):
+    """
+    DVectorHasValue: Check if a libscientific double vector contains an exact
+                     value "val" and return 0 or 1 respectivelly for have or not have.
+    """
+    return lsci.DVectorHasValue(v, val)
+
+
+lsci.DVectorSet.argtypes = [ctypes.POINTER(dvector), ctypes.c_double]
+lsci.DVectorSet.restype = None
+
+
+def DVectorSet(d, val):
+    """
+    DVectorSet: Set all values of a libscientific double vector to "val"
+    """
+    lsci.DVectorSet(d, val)
+
+
+lsci.setDVectorValue.argtypes = [ctypes.POINTER(dvector),
+                                 ctypes.c_size_t,
+                                 ctypes.c_double]
+lsci.setDVectorValue.restype = None
+
+
+def setDVectorValue(d, row, value):
+    """
+    setDVectorValue: Set/modify a value in the row of a libscientific
+                     double vector
+    """
+    lsci.setDVectorValue(d, row, value)
+
+
+lsci.getDVectorValue.argtypes = [ctypes.POINTER(dvector),
+                                 ctypes.c_size_t]
+lsci.getDVectorValue.restype = ctypes.c_double
+
+
+def getDVectorValue(d, row):
+    """
+    getDVectorValue: Get a value in the row of a libscientific
+                     double vector
+    """
+    return lsci.getDVectorValue(d, row)
+
+
+def DVectorToList(d):
+    dlst = []
+    try:
+        for i in range(d[0].size):
+            dlst.append(d[0].data[i])
+    except TypeError:
+        for i in range(d.size):
+            dlst.append(d.data[i])
+    return dlst
+
+
+lsci.DVectorAppend.argtypes = [ctypes.POINTER(ctypes.POINTER(dvector)),
+                               ctypes.c_double]
+lsci.DVectorAppend.restype = None
+
+def DVectorAppend(d, val):
+    """
+    Append a value to a double vector d
+    """
+    return lsci.DVectorAppend(d, val);
+
+
+lsci.DVectorRemoveAt.argtypes = [ctypes.POINTER(ctypes.POINTER(dvector)),
+                                 ctypes.c_size_t]
+lsci.DVectorRemoveAt.restype = None
+
+def DVectorRemoveAt(d, indx):
+    """
+    Remove a value from a double vector d at index indx
+    """
+    return lsci.DVectorRemoveAt(d, indx);
+
+lsci.DVectorCopy.argtypes = [ctypes.POINTER(dvector),
+                             ctypes.POINTER(ctypes.POINTER(dvector))]
+lsci.DVectorCopy.restype = None
+
+def DVectorCopy(src):
+    """
+    Create a copy of dvector d to a
+    """
+    dst = initDVector()
+    lsci.DVectorCopy(src, ctypes.pointer(dst))
+    return dst
+
+
+"""
+TODO: Implement this functions
+
+/* Append to a dvector an other dvector */
+dvector *DVectorExtend(dvector *d1, dvector *d2);
+
+/*Vector operations*/
+double DVectorDVectorDotProd(dvector *v1, dvector *v2); /* product between two vector */
+
+double DvectorModule(dvector *v); /* get the Dvector Module */
+void DVectNorm(dvector *v, dvector *nv); /* vector normalizing */
+void DVectorDVectorDiff(dvector *v1, dvector *v2, dvector **v3);
+void DVectorDVectorSum(dvector *v1, dvector *v2, dvector **v3);
+void DVectorMinMax(dvector *v, double *min, double *max);
+void DVectorMean(dvector *d, double *mean);
+void DVectorMedian(dvector *d, double *mean);
+void DVectorSDEV(dvector *d, double *sdev);
+void DVectorSort(dvector *v);
+
+
+"""
+
+
+class DVector(object):
+    """
+    Translate a list  into a libscientific double vector
+    """
+    def __init__(self, d_):
+        self.d = NewDVector(d_)
+
+    def __del__(self):
+        DelDVector(self.d)
+        del self.d
+
+    def __getitem__(self, key):
+        return self.data_ptr()[key]
+
+    def __setitem__(self, key, value):
+        setDVectorValue(self.d, key, value)
+
+    def size(self):
+        return self.d[0].size
+
+    def data_ptr(self):
+        return self.d[0].data
+    
+    def append(self, value):
+        return DVectorAppend(self.d, value)
+    
+    def extend(self, lst):
+        for item in lst:
+            DVectorAppend(self.d, item)
+    
+    def tolist(self):
+        return DVectorToList(self.d)
+
+    def fromlist(self, vlst_):
+        DelDVector(self.d)
+        del self.d
+        self.d = NewDVector(vlst_)
+
+    def debug(self):
+        PrintDVector(self.d)
+
+
+if __name__ in "__main__":
+    from random import random
+    a = [random() for j in range(10)]
+    d = DVector(a)
+    d.debug()
+    print("get value")
+    print(d[1])
+    print("set value")
+    d[1] = -2
+    dlst = d.tolist()
+    print("print the list converted from the double vector")
+    for item in dlst:
+        print(item)
+    
+    print("Add at the end the value -123")
+    DVectorAppend(d.d, -123)
+    d.debug()
+    
+    print("Append in a different way -123 at the end")
+    d.append(-123)
+    d.debug()
+    
+    print("remove at index 1, then value -2")
+    DVectorRemoveAt(d.d, 1)
+    d.debug()
+    
+    print("Extend d with b")
+    print("b:")
+    b = [random() for j in range(4)]
+    print(b)
+    d.extend(b)
+    print("d extended:")
+    d.debug()
+    
+    print("Create a copy of d.d in q")
+    q = DVectorCopy(d.d)
+    PrintDVector(q)
+    DelDVector(q)
+    
+    print("Check if the double vector d have the value -123")
+    print(DVectorHasValue(d.d, -13.0000))
+    
+    
