a3770c6651ba85522e30de9ca61aa7d7c3e33f6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:40 +0000

    Add package packit/0.27.1/packit-0.27.1.tar.gz

---
+++ packages/packit/packit-0.27.1.tar.gz/packit-0.27.1/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+
+# WARNING! You must also specify all deps in requirements.txt!
+setup(setup_requires=['pbr>=4.0', 'wheel>=0.24', 'glob2>=0.4.1', 'packaging>=16.8'], pbr=True, long_description_content_type="text/markdown")
+
