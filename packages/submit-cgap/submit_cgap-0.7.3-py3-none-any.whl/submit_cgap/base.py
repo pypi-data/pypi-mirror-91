eb3e49f94ea2a39cfe83962630d80675222aaee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:51 +0000

    Add package submit-cgap/0.7.3/submit_cgap-0.7.3-py3-none-any.whl

---
+++ packages/submit-cgap/submit_cgap-0.7.3-py3-none-any.whl/submit_cgap/base.py
@@ -0,0 +1,59 @@
+import contextlib
+import functools
+import os
+
+from dcicutils.qa_utils import local_attrs
+
+
+# TODO: Integrate this better with dcicutils.env_utils
+
+LOCAL_SERVER = "http://localhost:8000"
+LOCAL_PSEUDOENV = 'fourfront-cgaplocal'
+
+PRODUCTION_SERVER = 'https://cgap.hms.harvard.edu'
+PRODUCTION_ENV = 'fourfront-cgap'
+
+DEFAULT_ENV_VAR = 'SUBMITCGAP_ENV'
+
+
+def _compute_default_env():
+    return os.environ.get(DEFAULT_ENV_VAR, PRODUCTION_ENV)
+
+
+DEFAULT_ENV = _compute_default_env()
+
+
+class KeyManager:
+
+    # This is not necessarily the actual keydicts filename. It can e overridden by environment variable CGAP_KEYS_FILE
+    DEFAULT_KEYDICTS_FILENAME = os.path.expanduser('~/.cgap-keys.json')
+
+    _KEYDICTS_FILENAME = DEFAULT_KEYDICTS_FILENAME
+
+    @classmethod
+    def keydicts_filename(cls):
+        return cls._KEYDICTS_FILENAME
+
+    @classmethod
+    @contextlib.contextmanager
+    def alternate_keydicts_filename(cls, filename):
+        if filename is None:
+            yield  # If no alternate filename given, change nothing
+        else:
+            with local_attrs(cls, _KEYDICTS_FILENAME=filename):
+                yield
+
+    @classmethod
+    @contextlib.contextmanager
+    def alternate_keydicts_filename_from_environ(cls):
+        filename = os.environ.get('CGAP_KEYS_FILE') or None  # Treats empty string as undefined
+        with cls.alternate_keydicts_filename(filename=filename):
+            yield
+
+
+def UsingCGAPKeysFile(fn):
+    @functools.wraps(fn)
+    def wrapped(*args, **kwargs):
+        with KeyManager.alternate_keydicts_filename_from_environ():
+            return fn(*args, **kwargs)
+    return wrapped
---
+++ packages/submit-cgap/submit_cgap-0.7.3-py3-none-any.whl/submit_cgap/scripts/show_upload_info.py
@@ -0,0 +1,25 @@
+import argparse
+from ..submission import show_upload_info
+from ..base import UsingCGAPKeysFile
+
+
+EPILOG = __doc__
+
+
+@UsingCGAPKeysFile
+def main(simulated_args_for_testing=None):
+    parser = argparse.ArgumentParser(  # noqa - PyCharm wrongly thinks the formatter_class is invalid
+        description="Submits a data bundle part",
+        epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument('uuid', help='uuid identifier')
+    parser.add_argument('--server', '-s', help="an http or https address of the server to use", default=None)
+    parser.add_argument('--env', '-e', help="a CGAP beanstalk environment name for the server to use", default=None)
+    args = parser.parse_args(args=simulated_args_for_testing)
+
+    show_upload_info(uuid=args.uuid, server=args.server, env=args.env)
+
+
+if __name__ == '__main__':
+    main()
