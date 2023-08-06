3210c873724f4d63a9dc11b0b8cdc17c077a8546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:29 +0000

    Add package redpay/1.3.3/redpay-1.3.3-py3-none-any.whl

---
+++ packages/redpay/redpay-1.3.3-py3-none-any.whl/redpay/charge_ach.py
@@ -0,0 +1,34 @@
+from redpay.session import Session
+
+
+class ChargeACH:
+    def __init__(self, app, key, endpoint) -> None:
+        self.app = app
+        self.key = key
+        self.endpoint = endpoint
+        self.route = "/ach"
+
+    def Process(self, request):
+        # Create a session with the server
+        session = Session(self.app, self.key, self.endpoint, self.route)
+
+        accountType = "C"
+        if "accountType" in request:
+            accountType = request["accountType"]
+
+        currency = "USD"
+        if "currency" in request:
+            currency = request["currency"]
+
+        # Contruct charge ach packet
+        req = {
+            "account": request["account"],
+            "routing": request["routing"],
+            "accountType": accountType,
+            "action": "A",
+            "amount": request["amount"],
+            "cardHolderName": request["accountHolder"],
+            "currency": currency
+        }
+
+        return session.Send(req)
