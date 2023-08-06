d5fc7eed742a45dca6ed6aa6619cbfcfd5e3386f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:33 +0000

    Add package nemonet/0.5.4/nemonet-0.5.4.tar.gz

---
+++ packages/nemonet/nemonet-0.5.4.tar.gz/nemonet-0.5.4/nemonet/cli.py
@@ -0,0 +1,22 @@
+# Created by Jan Rummens at 8/01/2021
+import sys
+from nemonet.runner.vision_runner import Runner
+import typer
+
+app = typer.Typer()
+
+@app.command("scenario")
+def start(scenario: str):
+    try:
+        runner = Runner()
+        runner.execute_scenario(scenario)
+    except ValueError:
+        typer.echo(f"invalid commandline")
+        raise typer.Exit(code=1)
+
+
+
+
+if __name__ == '__main__':
+    app()
+
