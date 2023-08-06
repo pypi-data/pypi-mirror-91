4597b3c63b248cd3b8e4718042b25ad9cbdcafba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:37 +0000

    Add package pangpuriye/0.1.7/pangpuriye-0.1.7-py3-none-any.whl

---
+++ packages/pangpuriye/pangpuriye-0.1.7-py3-none-any.whl/pangpuriye/tests/test_ocr.py
@@ -0,0 +1,15 @@
+import unittest
+from pangpuriye.ai.ocr import OCR
+import numpy as np
+import os
+
+ocr = OCR()
+
+class TestOCR(unittest.TestCase):
+
+    def test_something(self):
+        
+        pass
+
+if __name__ == "__main__":
+    pass
