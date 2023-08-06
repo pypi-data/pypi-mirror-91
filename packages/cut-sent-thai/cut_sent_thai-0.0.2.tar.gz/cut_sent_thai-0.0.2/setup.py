ea7742aca561c4ab61a93eac8064f2b5784bdc21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:46 +0000

    Add package cut-sent-thai/0.0.2/cut_sent_thai-0.0.2.tar.gz

---
+++ packages/cut-sent-thai/cut_sent_thai-0.0.2.tar.gz/cut_sent_thai-0.0.2/setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="cut_sent_thai", # Replace with your own username
+    version="0.0.2",
+    author="sumeth",
+    author_email="author@example.com",
+    description="tokenize sentence in Thai",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=['cut_sent_thai'],
+    license='MIT',
+    include_package_data=True,
+    install_requires= [
+        'numpy',
+        'tensorflow > =2.2.0',
+        'pythainlp >= 2.2.0'
+        ],
+    python_requires='>=3.6',
+    zip_safe=False
+)
