ccb8345702337c54277b94f7a90e107db2b80d76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:21 +0000

    Add package flights-time-series-dataset/1.1.8/flights_time_series_dataset-1.1.8-py3-none-any.whl

---
+++ packages/flights-time-series-dataset/flights_time_series_dataset-1.1.8-py3-none-any.whl/flights_time_series_dataset/__init__.py
@@ -0,0 +1,7 @@
+"""
+__init__.py
+"""
+
+from .flights_dataset import FlightsDataset
+from .flight_series_dataset import FlightSeriesDataset
+from .convert_year_month_array_to_datetime import convert_year_month_array_to_datetime
