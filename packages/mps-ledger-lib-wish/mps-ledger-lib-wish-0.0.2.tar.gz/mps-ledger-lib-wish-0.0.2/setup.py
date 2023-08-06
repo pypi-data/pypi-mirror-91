f5fb85078d32c8137d2b4883b23c60438fe1b9d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:20 +0000

    Add package mps-ledger-lib-wish/0.0.2/mps-ledger-lib-wish-0.0.2.tar.gz

---
+++ packages/mps-ledger-lib-wish/mps-ledger-lib-wish-0.0.2.tar.gz/mps-ledger-lib-wish-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mps-ledger-lib-wish",
+    version="0.0.2",
+    author_email="merchant_payments_eng@contextlogic.com",
+    description="A library for Ledger system in Wish",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ContextLogic/mps-ledger-lib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7',
+)
