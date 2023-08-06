4e095409c7f91632d0bcee4eaa51c12d0114dec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:26 +0000

    Add package spheres/0.3.0.8/spheres-0.3.0.8.tar.gz

---
+++ packages/spheres/spheres-0.3.0.8.tar.gz/spheres-0.3.0.8/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spheres",
+    version="0.3.0.8",
+    author="Matthew Weiss",
+    author_email="heyredhat@gmail.com",
+    description="toolbox for higher spin and symmetrization",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/heyredhat/spheres",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires = ['numpy', 'qutip', 'matplotlib', 'vpython', 'sympy',\
+                        'pytket', 'pytket-qiskit', 'qiskit', 'strawberryfields']
+)
