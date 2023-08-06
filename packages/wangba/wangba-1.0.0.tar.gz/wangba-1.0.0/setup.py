6872b85a9364f8cd466f85c50e29ffa7c310a96e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:27:41 +0000

    Add package wangba/1.0.0/wangba-1.0.0.tar.gz

---
+++ packages/wangba/wangba-1.0.0.tar.gz/wangba-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+ 
+setup(
+    name='wangba',
+    version='1.0.0',
+    description='just for test',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+    author='cao',
+    url='https://github.com',
+    author_email='284440954@qq.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=False,
+    zip_safe=True,
+)
