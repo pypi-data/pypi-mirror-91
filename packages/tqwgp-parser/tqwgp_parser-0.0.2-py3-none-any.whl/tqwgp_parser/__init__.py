8b5c1aebfb67621bfbb5106c66555c9ab5a5d4ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:09 +0000

    Add package tqwgp-parser/0.0.2/tqwgp_parser-0.0.2-py3-none-any.whl

---
+++ packages/tqwgp-parser/tqwgp_parser-0.0.2-py3-none-any.whl/tqwgp_parser/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    tqwgp-parser
+    ~~~~~~~~~~~~~~~~~~~~~
+    TQWGP parser package.
+
+    :copyright: (c) 2021 Yoan Tournade.
+"""
+import hy
+from .parser import parse_quote, parse_invoices
+
+# # Expose main parsing functions.
+# parse_quote = doc_parser.parse_quote
+# parse_invoices = doc_parser.parse_invoices
