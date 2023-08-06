1b676ab89a3a7ebb6b770681210edddf4e13e390
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:16 +0000

    Add package pywikibot/5.5.0/pywikibot-5.5.0.tar.gz

---
+++ packages/pywikibot/pywikibot-5.5.0.tar.gz/pywikibot-5.5.0/pywikibot/specialbots/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Module containing special bots reusable by scripts."""
+#
+# (C) Pywikibot team, 2020
+#
+# Distributed under the terms of the MIT license.
+#
+from pywikibot.specialbots._unlink import (
+    EditReplacement, InteractiveUnlink, BaseUnlinkBot
+)
+from pywikibot.specialbots._upload import UploadRobot
