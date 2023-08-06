1fbc7782c6b1139ae0e808bb5d3761b2c6a2f45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:58 +0000

    Add package cs-models/0.0.161/cs_models-0.0.161-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.161-py3-none-any.whl/cs_models/resources/Intervention/schemas.py
@@ -0,0 +1,16 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class InterventionResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    intervention_uid = fields.String(validate=not_blank, required=True)
+    marketing_category = fields.String(allow_none=True)
+    preferred_term = fields.String(validate=not_blank, required=True)
+    intervention_type = fields.String()
+    updated_at = fields.DateTime()
