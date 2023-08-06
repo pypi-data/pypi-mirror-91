d7bd6aa91da629763642f05094c10fb1f075c3ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:35 +0000

    Add package weaverbird/0.1.1/weaverbird-0.1.1.tar.gz

---
+++ packages/weaverbird/weaverbird-0.1.1.tar.gz/weaverbird-0.1.1/weaverbird/steps/rename.py
@@ -0,0 +1,14 @@
+from typing import List, Tuple
+
+from pandas import DataFrame
+from pydantic import Field
+
+from weaverbird.steps.base import BaseStep
+
+
+class RenameStep(BaseStep):
+    name = Field('rename', const=True)
+    to_rename: List[Tuple[str, str]] = Field(..., alias='toRename')
+
+    def execute(self, df: DataFrame, domain_retriever=None, execute_pipeline=None) -> DataFrame:
+        return df.rename(columns=dict(self.to_rename))
