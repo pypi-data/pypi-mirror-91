2bb86abe536cf08a0dc187ca46e6308fdc63cafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:58 +0000

    Add package SanyTool/1.1.1/SanyTool-1.1.1.tar.gz

---
+++ packages/SanyTool/SanyTool-1.1.1.tar.gz/SanyTool-1.1.1/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: thao
+# Mail: thao92@126.com
+# Created Time:  2021-01-06 09:58:34
+#############################################
+
+
+from setuptools import setup, find_packages
+import sys
+import importlib
+importlib.reload(sys)
+
+setup(
+    name = "SanyTool",
+    version = "1.1.1",
+    keywords = ["pip", "SanyTool", "thao"],
+    description = "Common tools for model development",
+    long_description = "Common tools for model development",
+    license = "MIT Licence",
+
+    url = "http://gitlab.sanywind.net/sanydata/sanytool",
+    author = "thao",
+    author_email = "thao92@126.com",
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ['numpy', 'pandas', 'arrow', 'sklearn', 'sanydata']
+#     install_requires = []
+)
