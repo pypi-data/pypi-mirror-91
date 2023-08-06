63e2518ab796e3baa1875a58ee60b27314240a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:29 +0000

    Add package malojaserver/2.12.5/malojaserver-2.12.5-py3-none-any.whl

---
+++ packages/malojaserver/malojaserver-2.12.5-py3-none-any.whl/maloja/proccontrol/setup.py
@@ -0,0 +1,101 @@
+import pkg_resources
+from distutils import dir_util
+from doreah import settings
+from doreah.io import col, ask, prompt
+from doreah import auth
+import os
+
+from ..globalconf import data_dir, dir_settings
+
+
+# EXTERNAL API KEYS
+apikeys = {
+	"LASTFM_API_KEY":"Last.fm API Key",
+	"FANARTTV_API_KEY":"Fanart.tv API Key",
+	"SPOTIFY_API_ID":"Spotify Client ID",
+	"SPOTIFY_API_SECRET":"Spotify Client Secret"
+}
+
+
+
+def copy_initial_local_files():
+	folder = pkg_resources.resource_filename("maloja","data_files")
+	for cat in dir_settings:
+		dir_util.copy_tree(os.path.join(folder,cat),dir_settings[cat],update=False)
+
+charset = list(range(10)) + list("abcdefghijklmnopqrstuvwxyz") + list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+def randomstring(length=32):
+	import random
+	key = ""
+	for i in range(length):
+		key += str(random.choice(charset))
+	return key
+
+def setup():
+
+	copy_initial_local_files()
+	SKIP = settings.get_settings("SKIP_SETUP")
+
+	print("Various external services can be used to display images. If not enough of them are set up, only local images will be used.")
+	for k in apikeys:
+		key = settings.get_settings(k)
+		if key is None:
+			print("\t" + "Currently not using a " + apikeys[k] + " for image display.")
+		elif key == "ASK":
+			print("\t" + "Please enter your " + apikeys[k] + ". If you do not want to use one at this moment, simply leave this empty and press Enter.")
+			key = prompt("",types=(str,),default=None,skip=SKIP)
+			settings.update_settings(data_dir['settings']("settings.ini"),{k:key},create_new=True)
+		else:
+			print("\t" + apikeys[k] + " found.")
+
+
+	# OWN API KEY
+	if os.path.exists(data_dir['clients']("authenticated_machines.tsv")):
+		pass
+	else:
+		answer = ask("Do you want to set up a key to enable scrobbling? Your scrobble extension needs that key so that only you can scrobble tracks to your database.",default=True,skip=SKIP)
+		if answer:
+			key = randomstring(64)
+			print("Your API Key: " + col["yellow"](key))
+			with open(data_dir['clients']("authenticated_machines.tsv"),"w") as keyfile:
+				keyfile.write(key + "\t" + "Default Generated Key")
+		else:
+			pass
+
+
+	# PASSWORD
+	defaultpassword = settings.get_settings("DEFAULT_PASSWORD")
+	forcepassword = settings.get_settings("FORCE_PASSWORD")
+	# this is mainly meant for docker, supply password via environment variable
+
+	if forcepassword is not None:
+		# user has specified to force the pw, nothing else matters
+		auth.defaultuser.setpw(forcepassword)
+		print("Password has been set.")
+	elif auth.defaultuser.checkpw("admin"):
+		# if the actual pw is admin, it means we've never set this up properly (eg first start after update)
+		if defaultpassword is None:
+			# non-docker installation or user didn't set environment variable
+			defaultpassword = randomstring(32)
+			newpw = prompt("Please set a password for web backend access. Leave this empty to generate a random password.",skip=SKIP,secret=True)
+			if newpw is None:
+				newpw = defaultpassword
+				print("Generated password:",newpw)
+			auth.defaultuser.setpw(newpw)
+		else:
+			# docker installation (or settings file, but don't do that)
+			# we still 'ask' the user to set one, but for docker this will be skipped
+			newpw = prompt("Please set a password for web backend access. Leave this empty to use the default password.",skip=SKIP,default=defaultpassword,secret=True)
+			auth.defaultuser.setpw(newpw)
+
+
+	if settings.get_settings("NAME") is None:
+		name = prompt("Please enter your name. This will be displayed e.g. when comparing your charts to another user. Leave this empty if you would not like to specify a name right now.",default="Generic Maloja User",skip=SKIP)
+		settings.update_settings(data_dir['settings']("settings.ini"),{"NAME":name},create_new=True)
+
+	if settings.get_settings("SEND_STATS") is None:
+		answer = ask("I would like to know how many people use Maloja. Would it be okay to send a daily ping to my server (this contains no data that isn't accessible via your web interface already)?",default=True,skip=SKIP)
+		if answer:
+			settings.update_settings(data_dir['settings']("settings.ini"),{"SEND_STATS":True,"PUBLIC_URL":None},create_new=True)
+		else:
+			settings.update_settings(data_dir['settings']("settings.ini"),{"SEND_STATS":False},create_new=True)
---
+++ packages/malojaserver/malojaserver-2.12.5-py3-none-any.whl/maloja/thirdparty/__init__.py
@@ -0,0 +1,219 @@
+# these different interfaces are for the different roles a third party service
+# can fulfill. implementing them adds some generic functionality to attempt to
+# actually perform the role, but this will have to be overwritten in most cases.
+# functionality is separated into different layers to allow partial override
+
+# also yes, we're using singleton classes for the different providers
+# pls don't sue me
+
+import xml.etree.ElementTree as ElementTree
+import json
+import urllib.parse, urllib.request
+import base64
+from doreah.settings import get_settings
+from doreah.logging import log
+
+
+
+services = {
+	"proxyscrobble":[],
+	"import":[],
+	"metadata":[]
+}
+
+
+def proxy_scrobble_all(artists,title,timestamp):
+	for service in services["proxyscrobble"]:
+		service.scrobble(artists,title,timestamp)
+
+def get_image_track_all(track):
+	for service in services["metadata"]:
+		res = service.get_image_track(track)
+		if res is not None:
+			log("Got track image for " + str(track) + " from " + service.name)
+			return res
+		else:
+			log("Could not get track image for " + str(track) + " from " + service.name)
+def get_image_artist_all(artist):
+	for service in services["metadata"]:
+		res = service.get_image_artist(artist)
+		if res is not None:
+			log("Got artist image for " + str(artist) + " from " + service.name)
+			return res
+		else:
+			log("Could not get artist image for " + str(artist) + " from " + service.name)
+
+
+
+class GenericInterface:
+	def active_proxyscrobble(self):
+		return False
+	def active_import(self):
+		return False
+	def active_metadata(self):
+		return False
+
+	settings = {}
+
+	proxyscrobble = {}
+	scrobbleimport = {}
+	metadata = {}
+
+	def __init__(self):
+		# populate from settings file once on creation
+		# avoid constant disk access, restart on adding services is acceptable
+		for key in self.settings:
+			self.settings[key] = get_settings(self.settings[key])
+		try: self.authorize()
+		except: pass
+
+	def __init_subclass__(cls,abstract=False):
+		if not abstract:
+			s = cls()
+			if s.active_proxyscrobble():
+				services["proxyscrobble"].append(s)
+				log(cls.name + " registered as proxy scrobble target")
+			if s.active_import():
+				services["import"].append(s)
+				log(cls.name + " registered as scrobble import source")
+			if s.active_metadata():
+				services["metadata"].append(s)
+				log(cls.name + " registered as metadata provider")
+
+	def authorize(self):
+		return True
+		# per default. no authorization is necessary
+
+# proxy scrobbler
+class ProxyScrobbleInterface(GenericInterface,abstract=True):
+
+	proxyscrobble = {
+		"required_settings":[],
+		"activated_setting":None
+	}
+
+	def active_proxyscrobble(self):
+		return (
+			all(self.settings[key] not in [None,"ASK"] for key in self.proxyscrobble["required_settings"]) and
+			get_settings(self.proxyscrobble["activated_setting"])
+		)
+
+	def scrobble(self,artists,title,timestamp):
+		response = urllib.request.urlopen(
+			self.proxyscrobble["scrobbleurl"],
+			data=utf(self.proxyscrobble_postdata(artists,title,timestamp)))
+		responsedata = response.read()
+		if self.proxyscrobble["response_type"] == "xml":
+			data = ElementTree.fromstring(responsedata)
+			return self.proxyscrobble_parse_response(data)
+
+# scrobble import
+class ImportInterface(GenericInterface,abstract=True):
+
+	scrobbleimport = {
+		"required_settings":[],
+		"activated_setting":None
+	}
+
+	def active_import(self):
+		return (
+			all(self.settings[key] not in [None,"ASK"] for key in self.scrobbleimport["required_settings"]) and
+			get_settings(self.scrobbleimport["activated_setting"])
+		)
+
+
+# metadata
+class MetadataInterface(GenericInterface,abstract=True):
+
+	metadata = {
+		"required_settings":[],
+		"activated_setting":None
+	}
+
+	def active_metadata(self):
+		return (
+			all(self.settings[key] not in [None,"ASK"] for key in self.metadata["required_settings"]) and
+			self.identifier in get_settings("METADATA_PROVIDERS")
+		)
+
+	def get_image_track(self,track):
+		artists, title = track
+		artiststring = urllib.parse.quote(", ".join(artists))
+		titlestring = urllib.parse.quote(title)
+		response = urllib.request.urlopen(
+			self.metadata["trackurl"].format(artist=artiststring,title=titlestring,**self.settings)
+		)
+
+		responsedata = response.read()
+		if self.metadata["response_type"] == "json":
+			data = json.loads(responsedata)
+			imgurl = self.metadata_parse_response_track(data)
+		else:
+			imgurl = None
+		if imgurl is not None: imgurl = self.postprocess_url(imgurl)
+		return imgurl
+
+	def get_image_artist(self,artist):
+		artiststring = urllib.parse.quote(artist)
+		response = urllib.request.urlopen(
+			self.metadata["artisturl"].format(artist=artiststring,**self.settings)
+		)
+
+		responsedata = response.read()
+		if self.metadata["response_type"] == "json":
+			data = json.loads(responsedata)
+			imgurl = self.metadata_parse_response_artist(data)
+		else:
+			imgurl = None
+		if imgurl is not None: imgurl = self.postprocess_url(imgurl)
+		return imgurl
+
+	# default function to parse response by descending down nodes
+	# override if more complicated
+	def metadata_parse_response_artist(self,data):
+		res = data
+		for node in self.metadata["response_parse_tree_artist"]:
+			try:
+				res = res[node]
+			except:
+				return None
+		return res
+
+	def metadata_parse_response_track(self,data):
+		res = data
+		for node in self.metadata["response_parse_tree_track"]:
+			try:
+				res = res[node]
+			except:
+				return None
+		return res
+
+	def postprocess_url(self,url):
+		url = url.replace("http:","https:",1)
+		return url
+
+
+
+
+### useful stuff
+
+def utf(st):
+	return st.encode(encoding="UTF-8")
+def b64(inp):
+	return base64.b64encode(inp)
+
+
+
+### actually create everything
+
+__all__ = [
+	"lastfm",
+	"spotify",
+	"musicbrainz"
+]
+from . import *
+
+
+services["metadata"].sort(
+	key=lambda provider : get_settings("METADATA_PROVIDERS").index(provider.identifier)
+)
