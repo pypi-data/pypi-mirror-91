7be83d484a7acdf0ef1f140cc506c3dd76d66bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:40 +0000

    Add package citrine/0.90.3/citrine-0.90.3-py3-none-any.whl

---
+++ packages/citrine/citrine-0.90.3-py3-none-any.whl/citrine/exceptions.py
@@ -0,0 +1,109 @@
+"""Citrine-specific exceptions."""
+from typing import Optional, List
+from uuid import UUID
+
+from requests import Response
+
+
+class CitrineException(Exception):
+    """The base exception class for Citrine-Python exceptions."""
+
+    pass
+
+
+class NonRetryableException(CitrineException):
+    """Indicates that a non-retryable error occurred."""
+
+    pass
+
+
+class RetryableException(CitrineException):
+    """Indicates an error occurred but it is retryable."""
+
+    pass
+
+
+class UnauthorizedRefreshToken(NonRetryableException):
+    """The token used to refresh authentication is invalid."""
+
+    pass
+
+
+class NonRetryableHttpException(NonRetryableException):
+    """An exception originating from an HTTP error from a Citrine API."""
+
+    def __init__(self, path: str, response: Optional[Response] = None):
+        super().__init__(path)
+        self.url = path
+        if response is not None:
+            self.response_text = response.text
+            self.code = response.status_code
+            try:
+                resp_json = response.json()
+                if isinstance(resp_json, dict):
+                    from citrine.resources.api_error import ApiError
+                    self.api_error = ApiError.from_dict(resp_json)
+                else:
+                    self.api_error = None
+            # TODO: throw specific exception in DictSerializable when deserialization
+            #  fails due to from JSON keys
+            except (TypeError, ValueError):
+                self.api_error = None
+        else:
+            self.response_text = None
+            self.code = None
+            self.api_error = None
+
+
+class NotFound(NonRetryableHttpException):
+    """A particular url was not found. (http status 404)."""
+
+    pass
+
+
+class Unauthorized(NonRetryableHttpException):
+    """The user is unauthorized to make this api call. (http status 401)."""
+
+    pass
+
+
+class BadRequest(NonRetryableHttpException):
+    """The user is trying to perform an invalid operation. (http status 400)."""
+
+    pass
+
+
+class WorkflowConflictException(NonRetryableException):
+    """There is a conflict preventing the workflow from being executed. (http status 409)."""
+
+    pass
+
+
+class WorkflowNotReadyException(RetryableException):
+    """The workflow is not ready to be executed. i.e. still validating. (http status 425)."""
+
+    pass
+
+
+class PollingTimeoutError(NonRetryableException):
+    """Polling for an asynchronous result has exceeded the timeout."""
+
+    pass
+
+
+class JobFailureError(NonRetryableException):
+    """The asynchronous job completed with the given failure message."""
+
+    def __init__(self, *, message: str, job_id: UUID, failure_reasons: List[str]):
+        super().__init__(message)
+        self.job_id = job_id
+        self.failure_reasons = failure_reasons
+
+
+class ModuleRegistrationFailedException(NonRetryableException):
+    """A module failed to register."""
+
+    def __init__(self, moduleType: str, exc: Exception):
+        err = 'The "{0}" failed to register. {1}: {2}'.format(
+            moduleType, exc.__class__.__name__, str(exc))
+        super().__init__(err)
---
+++ packages/citrine/citrine-0.90.3-py3-none-any.whl/citrine/informatics/objectives.py
@@ -0,0 +1,98 @@
+"""Tools for working with Objectives."""
+from typing import Optional
+
+from citrine._session import Session
+from citrine._serialization import properties
+from citrine._serialization.serializable import Serializable
+from citrine._serialization.polymorphic_serializable import PolymorphicSerializable
+
+
+__all__ = ['Objective', 'ScalarMaxObjective', 'ScalarMinObjective']
+
+
+class Objective(PolymorphicSerializable['Objective']):
+    """
+    [ALPHA] A Citrine Objective describes a goal for a score associated with a single descriptor.
+
+    Abstract type that returns the proper type given a serialized dict.
+
+
+    """
+
+    _response_key = None
+
+    @classmethod
+    def get_type(cls, data):
+        """Return the subtype."""
+        return {
+            'ScalarMax': ScalarMaxObjective,
+            'ScalarMin': ScalarMinObjective
+        }[data['type']]
+
+
+class ScalarMaxObjective(Serializable['ScalarMaxObjective'], Objective):
+    """
+    [ALPHA] Simple single-response maximization objective with optional bounds.
+
+    Parameters
+    ----------
+    descriptor_key: str
+        the key from which to pull the values
+    lower_bound: float
+        the lower bound on the space, e.g. 0 for a non-negative property
+    upper_bound: float
+        the upper bound on the space, e.g. 0 for a non-positive property
+
+    """
+
+    descriptor_key = properties.String('descriptor_key')
+    lower_bound = properties.Optional(properties.Float, 'lower_bound')
+    upper_bound = properties.Optional(properties.Float, 'upper_bound')
+    typ = properties.String('type', default='ScalarMax')
+
+    def __init__(self,
+                 descriptor_key: str,
+                 lower_bound: Optional[float] = None,
+                 upper_bound: Optional[float] = None,
+                 session: Optional[Session] = None):
+        self.descriptor_key = descriptor_key
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.session  = session
+
+    def __str__(self):
+        return '<ScalarMaxObjective {!r}>'.format(self.descriptor_key)
+
+
+class ScalarMinObjective(Serializable['ScalarMinObjective'], Objective):
+    """
+    [ALPHA] Simple single-response minimization objective with optional bounds.
+
+    Parameters
+    ----------
+    descriptor_key: str
+        the key from which to pull the values
+    lower_bound: float
+        the lower bound on the space, e.g. 0 for a non-negative property
+    upper_bound: float
+        the upper bound on the space, e.g. 0 for a non-positive property
+
+    """
+
+    descriptor_key = properties.String('descriptor_key')
+    lower_bound = properties.Optional(properties.Float, 'lower_bound')
+    upper_bound = properties.Optional(properties.Float, 'upper_bound')
+    typ = properties.String('type', default='ScalarMin')
+
+    def __init__(self,
+                 descriptor_key: str,
+                 lower_bound: Optional[float] = None,
+                 upper_bound: Optional[float] = None,
+                 session: Optional[Session] = None):
+        self.descriptor_key = descriptor_key
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.session  = session
+
+    def __str__(self):
+        return '<ScalarMinObjective {!r}>'.format(self.descriptor_key)
