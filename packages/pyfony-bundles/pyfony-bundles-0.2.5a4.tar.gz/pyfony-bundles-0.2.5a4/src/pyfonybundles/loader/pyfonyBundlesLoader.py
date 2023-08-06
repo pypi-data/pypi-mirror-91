cf789492d243638b9bfabdfec34954910ba64bc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:55 +0000

    Add package pyfony-bundles/0.2.5a4/pyfony-bundles-0.2.5a4.tar.gz

---
+++ packages/pyfony-bundles/pyfony-bundles-0.2.5a4.tar.gz/pyfony-bundles-0.2.5a4/src/pyfonybundles/loader/pyfonyBundlesLoader.py
@@ -0,0 +1,51 @@
+from typing import List
+from injecta.dtype.classLoader import loadClass
+from pyfonybundles.Bundle import Bundle
+from pyfonybundles.loader import entryPointsReader, pyprojectReader
+
+def getEntryPoints():
+    entryPoints = entryPointsReader.getByKey('pyfony.bundle')
+
+    for entryPoint in entryPoints:
+        _checkName(entryPoint.name, entryPoint.value)
+
+    return entryPoints
+
+def loadBundles() -> List[Bundle]:
+    bundles = [entryPoint.load()() for entryPoint in getEntryPoints()]
+
+    rawConfig = pyprojectReader.read(pyprojectReader.getPath())
+
+    if _entryPointDefined(rawConfig):
+        bundle = _loadDirectly(rawConfig)()
+        bundles.append(bundle)
+
+    return bundles
+
+def _entryPointDefined(rawConfig):
+    return (
+        'tool' in rawConfig
+        and 'poetry' in rawConfig['tool']
+        and 'plugins' in rawConfig['tool']['poetry']
+        and 'pyfony.bundle' in rawConfig['tool']['poetry']['plugins']
+    )
+
+def _loadDirectly(rawConfig):
+    entryPoints = rawConfig['tool']['poetry']['plugins']['pyfony.bundle']
+
+    for name, val in entryPoints.items():
+        _checkName(name, val)
+
+    return _parse(entryPoints['create'])
+
+def _checkName(name: str, value):
+    if name != 'create':
+        raise Exception(f'Unexpected entry point name "{name}" for {value}')
+
+def _parse(val):
+    moduleName, classAndMethod = val.split(':')
+    className, functionName = classAndMethod.split('.')
+
+    class_ = loadClass(moduleName, className) # pylint: disable = invalid-name
+
+    return getattr(class_, functionName)
