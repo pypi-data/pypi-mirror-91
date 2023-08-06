6ceaca71e5882bab660e52f4f96376d748bd544d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:16 +0000

    Add package Python-ColorText/1.1.1/Python-ColorText-1.1.1.tar.gz

---
+++ packages/Python-ColorText/Python-ColorText-1.1.1.tar.gz/Python-ColorText-1.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Python-ColorText",
+    version="1.1.1",
+    license="GPL v3",
+    author="Sebastian Montoya",
+    author_email="sebastianmontoya209@gmail.com",
+    description="Color Strings in Python easily",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Sebastian-byte/ColorText",
+    packages=setuptools.find_packages(),
+    install_requires = [
+       "colorama",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
