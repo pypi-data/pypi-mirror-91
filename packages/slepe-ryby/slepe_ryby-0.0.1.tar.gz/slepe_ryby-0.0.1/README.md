51aa0e92c4a7bf31d75d96e20c01824cf0b3b74a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:18 +0000

    Add package slepe-ryby/0.0.1/slepe_ryby-0.0.1.tar.gz

---
+++ packages/slepe-ryby/slepe_ryby-0.0.1.tar.gz/slepe_ryby-0.0.1/README.md
@@ -0,0 +1,21 @@
+# slepe_ryby
+Ślepe Ryby
+
+
+## Installation
+Run the following to install:
+
+```python
+pip install slepe_ryby
+```
+
+## Usage
+```python
+from slepe_ryby import say_hello
+
+# Generate "Hello World!"
+say_hello()
+
+# Generate "Hello Everybody!"
+say_hello("Everybody")
+```
---
+++ packages/slepe-ryby/slepe_ryby-0.0.1.tar.gz/slepe_ryby-0.0.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name="slepe_ryby",
+    version="0.0.1",
+    description="Say hello!",
+    py_modules=["slepe_ryby"],
+    package_dir={'':'src'},
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+    ],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pawlaczyk/slepe_ryby",
+    author="Dominika Pawlaczyk",
+    autor_email="dominika.pawlaczyk9@gmail.com",
+)
