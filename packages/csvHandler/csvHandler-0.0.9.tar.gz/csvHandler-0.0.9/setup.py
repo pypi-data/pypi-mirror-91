948dc1e43409328e29a587ae8561b878a78ce2a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:15 +0000

    Add package csvHandler/0.0.9/csvHandler-0.0.9.tar.gz

---
+++ packages/csvHandler/csvHandler-0.0.9.tar.gz/csvHandler-0.0.9/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='csvHandler',
+    packages=find_packages(),
+    version='0.0.9',
+    description='csv in memory paginator',
+    author='own-backup',
+    license='MIT',
+    install_requires=['pandas'],
+    tests_require=['pytest']
+)
