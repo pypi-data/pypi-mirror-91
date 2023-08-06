f6615e3cb66d250f5520c4dc45834c19c298048a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:03 +0000

    Add package vect/0.0.7/vect-0.0.7.tar.gz

---
+++ packages/vect/vect-0.0.7.tar.gz/vect-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+    
+setup(
+
+    name = "vect",
+    version = "0.0.7",
+    description = "This powerful tool can help you in vector related work in python",
+    author = "Alejandro Ríos",
+    long_description = readme,
+    url = "https://github.com/ALEJORIOS/vect.git",
+    keywords = ["vect", "vector", "algebra"],
+    packages = ["vect","vect"],
+)
