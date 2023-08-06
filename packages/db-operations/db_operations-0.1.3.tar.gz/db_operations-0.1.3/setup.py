10a5a169251314e41582754f4b104a07b4105da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:11 +0000

    Add package db-operations/0.1.3/db_operations-0.1.3.tar.gz

---
+++ packages/db-operations/db_operations-0.1.3.tar.gz/db_operations-0.1.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='db_operations',
+      version='0.1.3',
+      author='K.lz',
+      author_email='565150134@qq.com',
+      description='Simple operation of database',
+      url='https://github.com/kimkimheel',
+      packages=['db_operations'],
+      install_requires=['pymysql','pymssql','cx_Oracle'],
+      zip_safe=False)
