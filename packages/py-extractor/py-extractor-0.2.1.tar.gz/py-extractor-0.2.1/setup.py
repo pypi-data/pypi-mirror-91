a8600289e7f05f33bdce29c18b003939b9594b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:55 +0000

    Add package py-extractor/0.2.1/py-extractor-0.2.1.tar.gz

---
+++ packages/py-extractor/py-extractor-0.2.1.tar.gz/py-extractor-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py-extractor',
+    version='0.2.1',
+    description='extract pdf',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+    ],
+    author='super',
+    url='https://github.com/yangyueguang/parser',
+    author_email='2829969299@qq.com',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    keywords=["pdf", "extractor", "extracter", "parser"],
+    long_description="extract pdf include edit or not",
+    platforms="any",
+    install_requires=['numpy', 'pandas', 'scikit-image', 'matplotlib', 'opencv-python', 'pillow', 'python-docx']
+)
