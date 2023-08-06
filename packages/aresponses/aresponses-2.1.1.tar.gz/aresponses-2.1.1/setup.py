3adf161111e6d0304d9be13ee25f1cce914b65ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:22 +0000

    Add package aresponses/2.1.1/aresponses-2.1.1.tar.gz

---
+++ packages/aresponses/aresponses-2.1.1.tar.gz/aresponses-2.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+__version__ = "2.1.1"
+
+setup(
+    name="aresponses",
+    packages=["aresponses"],
+    version=__version__,
+    description="""
+    Asyncio testing server. Similar to the responses library used for 'requests'
+    """.strip(),
+    author="Bryce Drennan, CircleUp",
+    author_email="aresponses@brycedrennan.com   ",
+    url="https://github.com/circleup/aresponses",
+    download_url="https://github.com/circleup/aresponses/tarball/" + __version__,
+    keywords=["asyncio", "testing", "responses"],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.6",
+    install_requires=["aiohttp==3.*,>=3.1.0", "pytest-asyncio"],
+    # the following makes a plugin available to pytest
+    entry_points={"pytest11": ["name_of_plugin = aresponses.main"]},
+)
