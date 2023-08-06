3d58f224eabf3a21976dc6099aa757c60018bf9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:56 +0000

    Add package aiu/0.1.1a6/aiu-0.1.1a6-py3-none-any.whl

---
+++ packages/aiu/aiu-0.1.1a6-py3-none-any.whl/aiu/version.py
@@ -0,0 +1,7 @@
+__name__ = "aiu"
+__version__ = "0.1.1a6"
+
+name = __name__
+version = __version__
+
+user_agent_string = "{}/{} - See: https://github.com/oduwsdl/archiveit_utilities".format(name, version)
