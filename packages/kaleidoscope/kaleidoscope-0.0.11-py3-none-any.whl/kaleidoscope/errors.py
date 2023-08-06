8a1e33bda69b006a7abdc804dc9c709cb3e55331
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:51 +0000

    Add package kaleidoscope/0.0.11/kaleidoscope-0.0.11-py3-none-any.whl

---
+++ packages/kaleidoscope/kaleidoscope-0.0.11-py3-none-any.whl/kaleidoscope/errors.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of Kaleidoscope.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+
+"""Exceptions raised by Kaleidoscope"""
+
+
+class KaleidoscopeError(Exception):
+    """Base class for Kaleidoscope errors."""
+    pass
