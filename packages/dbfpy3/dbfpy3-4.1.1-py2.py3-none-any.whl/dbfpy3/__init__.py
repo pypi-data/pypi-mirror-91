4716030ae006d2eba5825eb83ad4e91c1f3b19b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:30 +0000

    Add package dbfpy3/4.1.1/dbfpy3-4.1.1-py2.py3-none-any.whl

---
+++ packages/dbfpy3/dbfpy3-4.1.1-py2.py3-none-any.whl/dbfpy3/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for dbfpy3."""
+from . import dbf, fields, record, header, utils, code_page
+
+__author__ = """Frank Xu"""
+__email__ = 'franky.xhl@gmail.com'
+__version__ = '4.1.1'
+__all__ = ['dbf']
