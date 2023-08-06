6345d7a35ecba6ce7ff123fd999343b60ed1c153
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:53 +0000

    Add package scArches/0.3.3/scArches-0.3.3.tar.gz

---
+++ packages/scArches/scArches-0.3.3.tar.gz/scArches-0.3.3/scarches/__init__.py
@@ -0,0 +1,16 @@
+from . import metrics, models, dataset, trainers, zenodo, plotting
+from .surgery import scvi_operate, trvae_operate
+
+__author__ = ', '.join([
+    'Marco Wagenstetter',
+    'Mohsen Naghipourfar',
+    'Mohammad Lotfollahi',
+    'Sergei Rybakov'
+])
+
+__email__ = ', '.join([
+    'marco.wagenstetter@tum.de',
+    'naghipourfar@ce.sharif.edu',
+    'mohammad.lotfollahi@helmholtz-muenchen.de',
+    'sergei.rybakov@helmholtz-muenchen.de'
+])
