b313958063df9a061c5283675fd4e5b0695c1428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:09 +0000

    Add package dirtools2/0.2.6/dirtools2-0.2.6.tar.gz

---
+++ packages/dirtools2/dirtools2-0.2.6.tar.gz/dirtools2-0.2.6/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dirtools2",
+    version="0.2.6",
+    author="otapi",
+    description="Exclude/ignore files in a directory (using .gitignore like syntax), compute hash, search projects for an entire directory tree and gzip compression.",
+    license="MIT",
+    keywords="exclude exclusion directory hash compression gzip",
+    url="https://github.com/otapi/dirtools2",
+    py_modules=["dirtools2"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["globster"],
+    tests_require=["pyfakefs"],
+    test_suite="test_dirtools",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+    scripts=["dirtools2.py"],
+)
