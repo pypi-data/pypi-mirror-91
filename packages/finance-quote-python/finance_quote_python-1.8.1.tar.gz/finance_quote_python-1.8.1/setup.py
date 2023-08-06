1d95eca544ca0c6577719f056777e7987205d706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:50 +0000

    Add package finance-quote-python/1.8.1/finance_quote_python-1.8.1.tar.gz

---
+++ packages/finance-quote-python/finance_quote_python-1.8.1.tar.gz/finance_quote_python-1.8.1/setup.py
@@ -0,0 +1,17 @@
+"""
+Setup configuration for the package.
+"""
+from setuptools import setup, find_packages
+
+setup(name='finance_quote_python',
+      version='1.8.1',
+      description='Finance::Quote implementation in Python',
+      url='https://gitlab.com/alensiljak/finance-quote-python',
+      author='Alen Siljak',
+      #author_email='alen.siljak@gmx.com',
+      license='GPL3',
+      #packages=['finance_quote_python'],
+      packages=find_packages(),
+      zip_safe=False,
+      install_requires=['requests']
+      )
