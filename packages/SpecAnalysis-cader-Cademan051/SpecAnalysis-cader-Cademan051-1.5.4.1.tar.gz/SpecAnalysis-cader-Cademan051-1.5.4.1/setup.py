52f540c18e9db49c6f61d1053b3acf5d860cdf33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:31:15 +0000

    Add package SpecAnalysis-cader-Cademan051/1.5.4.1/SpecAnalysis-cader-Cademan051-1.5.4.1.tar.gz

---
+++ packages/SpecAnalysis-cader-Cademan051/SpecAnalysis-cader-Cademan051-1.5.4.1.tar.gz/SpecAnalysis-cader-Cademan051-1.5.4.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+from distutils.core import setup
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name='SpecAnalysis-cader-Cademan051',
+    version='1.5.4.1',
+    packages=find_packages(),
+    install_requires=[
+          'numpy',
+          'PyQt5',
+          'pyqtgraph',
+          'numpy',
+          'matplotlib',
+          'scipy',
+          'rpy2',
+      ],
+    include_package_data=True,
+    package_data={'':['*.txt'],
+                  '':['*.dat'],
+                  '':['*.png']},
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
