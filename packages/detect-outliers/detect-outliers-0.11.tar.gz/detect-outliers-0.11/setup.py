cdbe97b959a95fe096411f8d237d5c14c5116d16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:15 +0000

    Add package detect-outliers/0.11/detect-outliers-0.11.tar.gz

---
+++ packages/detect-outliers/detect-outliers-0.11.tar.gz/detect-outliers-0.11/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'detect-outliers',
+      version = '0.11',
+      description = 'Complete EDA report',
+      packages = ['EdaFirstPhase'],
+      zip_safe = False
+      )
+
