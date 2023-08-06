345c34eb38266c3d6a996ad6f15983c46ded8601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:06 +0000

    Add package binstore/1.1.5/binstore-1.1.5.tar.gz

---
+++ packages/binstore/binstore-1.1.5.tar.gz/binstore-1.1.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+from distutils.core import setup, Extension, DEBUG
+
+sfc_module = Extension('binstore', sources = ['module_binstore.c'])
+setup(name = 'binstore',
+    packages = setuptools.find_packages(),
+    version = '1.1.5',
+    license = 'MIT',
+    description = 'C extension to implement storage of objects based on value-based binning where binned value determines index into array where object is stored',
+    long_description = 'C extension to implement storage of objects based on value-based vinning where binned value determines index into array where object is stored',
+    author = 'John Herrema',
+    author_email = 'jherrema@gmail.com',
+    keywords = ['binning'],
+    ext_modules = [sfc_module],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7']
+
+      )
