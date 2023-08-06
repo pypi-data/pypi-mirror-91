fbd5ddc215a87d6547f0360cadfe5a256255bb7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:06 +0000

    Add package witcher/0.0.12/witcher-0.0.12-py3-none-any.whl

---
+++ packages/witcher/witcher-0.0.12-py3-none-any.whl/witcher/__init__.py
@@ -0,0 +1,11 @@
+######################################
+####### Update : 2021-01-17     ######
+######################################
+
+
+from ._version import version_info, __version__
+from witcher import Recommender
+from witcher import Stock_Market
+from witcher import File_Chooser
+from witcher import Stock_Market
+
