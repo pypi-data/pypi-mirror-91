ae2418376f1ad76ac4db87f25c5f06224cebfa99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:31 +0000

    Add package tkcalc/1.0.4/tkcalc-1.0.4.tar.gz

---
+++ packages/tkcalc/tkcalc-1.0.4.tar.gz/tkcalc-1.0.4/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(name="tkcalc",
+    author="Elia S. Toselli",
+    author_email="elia.toselli@outlook.it",
+    url="https://test.pypi.org/project/tkcalc",
+    version="1.0.4",
+    packages=['tkcalc'],
+    requires="tkinter",
+    description="Calcolatrice di base con tkinter")
