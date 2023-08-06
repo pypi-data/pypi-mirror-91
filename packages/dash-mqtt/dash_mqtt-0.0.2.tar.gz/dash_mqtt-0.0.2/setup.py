532007bf26443ac6cef4cf88797f4195f3d7a6f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:30 +0000

    Add package dash-mqtt/0.0.2/dash_mqtt-0.0.2.tar.gz

---
+++ packages/dash-mqtt/dash_mqtt-0.0.2.tar.gz/dash_mqtt-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import json
+from setuptools import setup
+import pathlib
+
+
+with open('package.json') as f:
+    package = json.load(f)
+
+package_name = package["name"].replace(" ", "_").replace("-", "_")
+
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name=package_name,
+    version=package["version"],
+    author=package['author'],
+    long_description=long_description,
+    long_description_content_type='text/markdown', 
+    url= 'https://github.com/aegis1980/dash-mqtt',
+    packages=[package_name],
+    include_package_data=True,
+    license=package['license'],
+    description=package.get('description', package_name),
+    install_requires=[],
+    classifiers = [
+        'Framework :: Dash',
+    ],    
+)
