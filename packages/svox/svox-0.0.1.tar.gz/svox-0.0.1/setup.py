bb0eca7f4ee9a50243d2ffb582c142fa818b095c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:41 +0000

    Add package svox/0.0.1/svox-0.0.1.tar.gz

---
+++ packages/svox/svox-0.0.1.tar.gz/svox-0.0.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+__version__ = '0.0.1'
+
+CUDA_FLAGS = []
+INSTALL_REQUIREMENTS = []
+
+ext_modules = [
+    CUDAExtension('svox.csrc', [
+        'svox/csrc/svox.cpp',
+        'svox/csrc/svox_kernel.cu',
+    ]),
+]
+
+setup(
+    name='svox',
+    version=__version__,
+    author='Alex Yu',
+    author_email='alexyu99126@gmail.com',
+    description='Sparse voxel N^3-tree data structure using CUDA',
+    long_description='Sparse voxel N^3-tree data structure using CUDA',
+    ext_modules=ext_modules,
+    setup_requires=['pybind11>=2.5.0'],
+    packages=['svox', 'svox.csrc'],
+    cmdclass={'build_ext': BuildExtension},
+    zip_safe=False,
+)
