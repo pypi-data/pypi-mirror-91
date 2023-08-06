5b95d0b44d7c95a7df5c7fe83c3c9ac908f1403d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:02 +0000

    Add package wait-for-it/2.2.0/wait_for_it-2.2.0-py3-none-any.whl

---
+++ packages/wait-for-it/wait_for_it-2.2.0-py3-none-any.whl/wait_for_it/test_wait_for_it.py
@@ -0,0 +1,178 @@
+"""wait_for_it cli test module"""
+import socket
+import subprocess
+
+from unittest.mock import call, Mock, patch
+
+import socketserver
+from threading import Event, Thread
+from unittest import TestCase
+
+from click.testing import CliRunner
+from parameterized import parameterized
+from .wait_for_it import (
+    cli,
+    _determine_host_and_port_for,
+    _MalformedServiceSyntaxException,
+)
+
+_ANY_FREE_PORT = 0
+
+
+class _DummyTcpServerThread(Thread):
+    """
+    A TCP server as a Thread that takes any free port, accepts connections,
+    but doesn't do any meaningful traffic.
+    """
+
+    class _DummyHandler(socketserver.BaseRequestHandler):
+        pass
+
+    def __init__(self):
+        super().__init__()
+        self.started = Event()
+
+    def run(self):
+        with socketserver.TCPServer(
+            ("127.0.0.1", _ANY_FREE_PORT), self._DummyHandler
+        ) as self._server:
+            self.host, self.port = self._server.server_address
+            self.started.set()
+            self._server.serve_forever()
+
+    def stop(self):
+        self._server.shutdown()
+
+
+def _start_server_thread():
+    server = _DummyTcpServerThread()
+    server.start()
+    server.started.wait()
+    return server
+
+
+def _occupy_free_tcp_port(host):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, _ANY_FREE_PORT))
+    _, port = sock.getsockname()
+    return host, port, sock
+
+
+class CliTest(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # https://click.palletsprojects.com/en/7.x/testing/
+        cls._runner = CliRunner()
+
+    def test_help(self):
+        result = self._runner.invoke(cli, ["--help"])
+        assert "Usage:" in result.output
+        assert result.exit_code == 0
+
+    @parameterized.expand([(["echo", "one", "two"], 0), (["false"], 1)])
+    def test_command_invocation_forwards_exit_code(
+        self, command_argv, expected_exit_code
+    ):
+        with patch.object(
+            subprocess, "run", return_value=Mock(returncode=expected_exit_code)
+        ) as mock_subprocess_run:
+            result = self._runner.invoke(cli, command_argv)
+        assert mock_subprocess_run.call_args == call(tuple(command_argv))
+        assert result.exit_code == expected_exit_code
+
+    @parameterized.expand([("parallel", ["-p"]), ("serial", [])])
+    def test_service_available(self, _label, extra_argv):
+        server = _start_server_thread()
+        try:
+            result = self._runner.invoke(
+                cli,
+                [
+                    "-t1",
+                    "-s",
+                    f"{server.host}:{server.port}",
+                    "-s",
+                    f"{server.host}:{server.port}",
+                ]
+                + extra_argv,
+            )
+            assert result.output.count(" is available after ") == 2
+            assert result.exit_code == 0
+        finally:
+            server.stop()
+
+    @parameterized.expand([("parallel", ["-p"]), ("serial", [])])
+    def test_service_available_implied_host(self, _label, extra_argv):
+        server = _start_server_thread()
+        try:
+            result = self._runner.invoke(
+                cli,
+                [
+                    "-t1",
+                    "-s",
+                    f":{server.port}",
+                    "-s",
+                    f":{server.port}",
+                ]
+                + extra_argv,
+            )
+            assert result.output.count(" is available after ") == 2
+            assert result.exit_code == 0
+        finally:
+            server.stop()
+
+    @parameterized.expand(
+        [
+            ("parallel", ["-p"], 2, "127.0.0.1"),
+            ("parallel", ["-p"], 2, "0.0.0.0"),
+            ("parallel", ["-p"], 2, "localhost"),
+            ("parallel", ["-p"], 2, ""),
+            ("serial", [], 1, "127.0.0.1"),
+            ("serial", [], 1, "0.0.0.0"),
+            ("serial", [], 1, "localhost"),
+            ("serial", [], 1, ""),
+        ]
+    )
+    def test_service_unavailable(self, _label, extra_argv, expected_report_count, host):
+        _, port, sock = _occupy_free_tcp_port(host)
+        try:
+            result = self._runner.invoke(
+                cli,
+                ["-t1", "-s", f"{host}:{port}", "-s", f"{host}:{port}"] + extra_argv,
+            )
+            assert result.output.count("Timeout occurred") == expected_report_count
+            assert result.exit_code == 1
+        finally:
+            sock.close()
+
+
+class DetermineHostAndPortForTest(TestCase):
+    @parameterized.expand(
+        [
+            (":1234", None, 1234),
+            ("[::1]:1234", "::1", 1234),
+            ("domain.ext", "domain.ext", 80),
+            ("domain.ext:0", "domain.ext", 80),
+            ("domain.ext:1", "domain.ext", 1),
+            ("domain.ext:65535", "domain.ext", 65535),
+            ("http://domain.ext", "domain.ext", 80),
+            ("http://domain.ext/path/", "domain.ext", 80),
+            ("https://domain.ext", "domain.ext", 443),
+            ("https://domain.ext/path/", "domain.ext", 443),
+        ]
+    )
+    def test_supported(self, service, expected_host, expected_port):
+        actual_host, actual_port = _determine_host_and_port_for(service)
+        assert actual_host == expected_host
+        assert actual_port == expected_port
+
+    @parameterized.expand(
+        [
+            ("::1:1234",),  # needs "[::1]:1234", instead
+            ("domain.ext:-1",),
+            ("domain.ext:65536",),
+            ("domain.ext:1.2",),
+        ]
+    )
+    def test_rejected(self, service):
+        with self.assertRaises(_MalformedServiceSyntaxException):
+            _determine_host_and_port_for(service)
---
+++ packages/wait-for-it/wait_for_it-2.2.0-py3-none-any.whl/wait_for_it/wait_for_it.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+import asyncio
+import os
+import signal
+import socket
+import subprocess
+import sys
+import time
+from contextlib import contextmanager
+from enum import Enum
+from urllib.parse import urlparse
+
+import click
+
+from wait_for_it import __version__
+
+
+class _WaitForItException(Exception):
+    """Base class for all exceptions custom to wait-for-it"""
+
+
+class _MalformedServiceSyntaxException(_WaitForItException):
+    def __init__(self, service):
+        super().__init__(f"{service!r} is not a supported syntax for a service")
+
+
+def _determine_host_and_port_for(service):
+    scheme, _, host = service.rpartition(r"//")
+    try:
+        url = urlparse(f"{scheme}//{host}", scheme="http")
+        host = url.hostname
+        port = url.port or (443 if url.scheme == "https" else 80)
+    except ValueError:
+        raise _MalformedServiceSyntaxException(service)
+    return host, port
+
+
+async def _wait_until_available(host, port):
+    family = 0 if socket.has_ipv6 else socket.AF_INET
+    while True:
+        try:
+            _reader, writer = await asyncio.open_connection(host, port, family=family)
+            writer.close()
+            await writer.wait_closed()
+            break
+        except (socket.gaierror, ConnectionError, OSError, TypeError):
+            pass
+        await asyncio.sleep(1)
+
+
+async def _wait_until_available_and_report(reporter, host, port):
+    reporter.on_before_start()
+    await _wait_until_available(host, port)
+    reporter.on_success()
+
+
+@click.command()
+@click.help_option("-h", "--help")
+@click.version_option(__version__, "-v", "--version", message="Version %(version)s")
+@click.option(
+    "-q",
+    "--quiet",
+    default=False,
+    is_flag=True,
+    help="Do not output any status messages",
+)
+@click.option(
+    "-p",
+    "--parallel",
+    default=False,
+    is_flag=True,
+    help="Test services in parallel rather than in serial",
+)
+@click.option(
+    "-t",
+    "--timeout",
+    type=int,
+    metavar="seconds",
+    default=15,
+    show_default=True,
+    help="Timeout in seconds, 0 for no timeout",
+)
+@click.option(
+    "-s",
+    "--service",
+    metavar="host:port",
+    multiple=True,
+    help="Services to test, in one of the formats: "
+    "':port', "
+    "'hostname:port', "
+    "'v4addr:port', "
+    "'[v6addr]:port' or "
+    "'https://...'",
+)
+@click.argument("commands", nargs=-1)
+def cli(**kwargs):
+    """Wait for service(s) to be available before executing a command."""
+    try:
+        _cli_internal(**kwargs)
+    except _WaitForItException as e:
+        _Messenger.tell_failure(str(e))
+        sys.exit(1)
+
+
+def _cli_internal(service, quiet, parallel, timeout, commands):
+    if quiet:
+        sys.stdout = open(os.devnull, "w")
+
+    if parallel:
+        _connect_all_parallel(service, timeout)
+    else:
+        _connect_all_serial(service, timeout)
+
+    if commands:
+        result = subprocess.run(commands)
+        sys.exit(result.returncode)
+
+
+class _Messenger:
+    class _MessageType(Enum):
+        SUCCESS = "[+] "
+        FAILURE = "[-] "
+        NEUTRAL = "[*] "
+
+    @classmethod
+    def _tell(cls, message_type, message):
+        prefix = message_type.value
+        print(f"{prefix}{message}")
+
+    @classmethod
+    def tell_success(cls, message):
+        cls._tell(cls._MessageType.SUCCESS, message)
+
+    @classmethod
+    def tell_failure(cls, message):
+        cls._tell(cls._MessageType.FAILURE, message)
+
+    @classmethod
+    def tell_neutral(cls, message):
+        cls._tell(cls._MessageType.NEUTRAL, message)
+
+
+class _ConnectionJobReporter:
+    def __init__(self, host, port, timeout):
+        if host is None:
+            host = ""
+        host_is_an_ipv6_address = ":" in host
+        self._friendly_name = (
+            f"[{host}]:{port}" if host_is_an_ipv6_address else f"{host}:{port}"
+        )
+        self._timeout = timeout
+        self._started_at = None
+        self.job_successful = None
+
+    def on_before_start(self):
+        if self._timeout:
+            message = f"Waiting {self._timeout} seconds for {self._friendly_name}"
+        else:
+            message = f"Waiting for {self._friendly_name} without a timeout"
+
+        _Messenger.tell_neutral(message)
+        self._started_at = time.time()
+
+    def on_success(self):
+        seconds = round(time.time() - self._started_at)
+        _Messenger.tell_success(
+            f"{self._friendly_name} is available after {seconds} seconds"
+        )
+        self.job_successful = True
+
+    def on_timeout(self):
+        _Messenger.tell_failure(
+            f"Timeout occurred after waiting {self._timeout} seconds"
+        )
+
+
+@contextmanager
+def _exit_on_timeout(timeout, on_exit):
+    def _handle_timeout(signum, frame):
+        on_exit()
+        sys.exit(1)
+
+    if timeout > 0:
+        signal.signal(signal.SIGALRM, _handle_timeout)
+        signal.alarm(timeout)
+
+    yield
+
+    if timeout > 0:
+        signal.alarm(0)  # disarm sys-exit timer
+
+
+async def _connect_all_parallel_async(services, timeout):
+    connect_job_awaitables = []
+    reporters = []
+
+    for service in services:
+        host, port = _determine_host_and_port_for(service)
+        reporter = _ConnectionJobReporter(host, port, timeout)
+        reporters.append(reporter)
+        connect_job_awaitables.append(
+            _wait_until_available_and_report(reporter, host, port)
+        )
+
+    def _report_on_all_unsuccessful_jobs():
+        for reporter in reporters:
+            if reporter.job_successful:
+                continue
+            reporter.on_timeout()
+
+    with _exit_on_timeout(timeout, on_exit=_report_on_all_unsuccessful_jobs):
+        await asyncio.wait(connect_job_awaitables)
+
+
+def _connect_all_parallel(services, timeout):
+    asyncio.run(_connect_all_parallel_async(services, timeout))
+
+
+def _connect_all_serial(services, timeout):
+    for service in services:
+        connect(service, timeout)
+
+
+def connect(service, timeout):
+    host, port = _determine_host_and_port_for(service)
+    reporter = _ConnectionJobReporter(host, port, timeout)
+
+    with _exit_on_timeout(timeout, on_exit=reporter.on_timeout):
+        asyncio.run(_wait_until_available_and_report(reporter, host, port))
+
+
+if __name__ == "__main__":
+    cli()
