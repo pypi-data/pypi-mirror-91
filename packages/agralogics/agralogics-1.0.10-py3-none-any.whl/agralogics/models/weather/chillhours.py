dc00bb88527671afc5637039250325ae9b02f6a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:08 +0000

    Add package agralogics/1.0.10/agralogics-1.0.10-py3-none-any.whl

---
+++ packages/agralogics/agralogics-1.0.10-py3-none-any.whl/agralogics/models/weather/chillhours.py
@@ -0,0 +1,47 @@
+from agralogics.models.base import BaseModel
+
+
+class ChillDictModel(BaseModel):
+    def __init__(self, dict_model, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.dict_model = dict_model
+
+
+    def get_chill_increment(self, air_temperature):
+        for temp_range in self.dict_model.keys():
+            temp_value = self.dict_model[temp_range]
+            left, right = temp_range.split('_')
+            if left == 'lt' and air_temperature <= int(right):
+                return temp_value
+            elif left == 'gt' and air_temperature > int(right):
+                return temp_value
+            elif left != 'gt' and left != 'lt' and int(left) < air_temperature <= int(right):
+                return temp_value
+
+
+    def calculate(self, lat=None, lon=None, start_dt=None, end_dt=None):
+        data = self.get_hourly_data(lat=lat, lon=lon, start_dt=start_dt, end_dt=end_dt, include=['air_temperature'])
+        hourly_data = data['hourly']
+        chill_hours = 0
+        for data in hourly_data:
+            air_temperature = data['air_temperature']
+            air_temperature_fahrenheit = self.convert_to_fahrenheit(air_temperature)
+            increment = self.get_chill_increment(air_temperature_fahrenheit)
+            chill_hours += increment
+
+        return chill_hours
+
+
+class ChillModelUtah(ChillDictModel):
+    def __init__(self, *args, **kwargs):
+        model = {
+            "lt_34": 0,
+            "34_36": 0.5,
+            "36_48": 1,
+            "48_54": 0.5,
+            "54_60": 0,
+            "60_65": -0.5,
+            "gt_65": -1.0
+        }
+
+        super().__init__(model, *args, **kwargs)
