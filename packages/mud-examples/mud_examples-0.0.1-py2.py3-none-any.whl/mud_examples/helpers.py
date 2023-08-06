562c60fb9d3365a383002c8096a407ce3e35794e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:08 +0000

    Add package mud-examples/0.0.1/mud_examples-0.0.1-py2.py3-none-any.whl

---
+++ packages/mud-examples/mud_examples-0.0.1-py2.py3-none-any.whl/mud_examples/helpers.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+def fit_log_linear_regression(input_values, output_values):
+    x, y = np.log10(input_values), np.log10(output_values)
+    X, Y = np.vander(x, 2), np.array(y).reshape(-1, 1)
+    slope, intercept = (np.linalg.pinv(X) @ Y).ravel()
+    regression_line = 10**(slope * x + intercept)
+    return regression_line, slope
