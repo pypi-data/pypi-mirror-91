b627b8c8bf759c67316355b74b4ed1ba5f8b3872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:11 +0000

    Add package filo/1.1.3/filo-1.1.3.tar.gz

---
+++ packages/filo/filo-1.1.3.tar.gz/filo-1.1.3/filo/__init__.py
@@ -0,0 +1,9 @@
+"""File Management."""
+
+from .general import list_files, list_all, move_files, move_all
+from .general import batch_file_rename, data_to_line, line_to_data
+from .general import make_iterable
+
+from .series import Series
+
+__version__ = '1.1.1'
