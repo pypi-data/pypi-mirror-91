c0fbe1aaa22a8d87f25a7d9358807e926b32338d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:17 +0000

    Add package fa-api/1.0.1/fa_api-1.0.1.tar.gz

---
+++ packages/fa-api/fa_api-1.0.1.tar.gz/fa_api-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from os import path
+
+curr_dir = path.abspath(path.dirname(__file__))
+with open(path.join(curr_dir, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="fa_api",
+    version="1.0.1",
+    author="Georgiy Demenchuk",
+    author_email="demenchuk.george@protonmail.com",
+    license="GPLv3",
+    url="https://github.com/GeorgiyDemo/fa_api",
+    keywords="python api wrapper ruz fa-university faru",
+    description="API wrapper for ruz.fa.ru",
+    packages=["fa_api"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["requests>=2.23.0",],
+)
