e83cc9e200ee748aee6b8d3a138b3b122596b082
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:35 +0000

    Add package dans-netbox-plugins/0.55/dans-netbox-plugins-0.55.tar.gz

---
+++ packages/dans-netbox-plugins/dans-netbox-plugins-0.55.tar.gz/dans-netbox-plugins-0.55/animal_sounds/__init__.py
@@ -0,0 +1,18 @@
+# importing PluginConfig
+from extras.plugins import PluginConfig
+
+# defining config class for animal sounds
+class AnimalSoundsConfig(PluginConfig):
+    name = "netbox_animal_sounds"
+    verbose_name = "Animal Sounds"
+    description = "Tracks animal sounds, by animal."
+    version = "0.1"
+    author = "Daniel Murphy"
+    author_email = "dev@danielmurphy.email"
+    base_url = "animal-sounds"
+    required_settings = []
+    # default settings that will affect our plugin behavior
+    default_settings = {}
+
+# instantiating our config
+config = AnimalSoundsConfig
