74a4dc7a5caa8b89718cbae237a502e18ad9c32c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:07 +0000

    Add package Reality-six/0.2.1/Reality_six-0.2.1-py3-none-any.whl

---
+++ packages/Reality-six/Reality_six-0.2.1-py3-none-any.whl/Reality/__init__.py
@@ -0,0 +1,34 @@
+import sys
+import time
+import calendar
+import psutil
+
+
+
+
+def platform():
+  return sys.platform
+def timestamp():
+  t=time.gmtime()
+  tc=calendar.timegm(t)
+  return tc
+def py():
+  return sys.version
+def running(processName):
+    '''
+    Check if there is any running process that contains the given name processName.
+    '''
+    for proc in psutil.process_iter():
+        try:
+            if processName.lower() in proc.name().lower():
+              return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+          pass
+    return False
+def group(obj:list or tuple,seperator:str):
+  return seperator.join(obj)
+def allapps():
+  lst=[]
+  for proc in psutil.process_iter():
+    lst.append(proc.name())
+  return lst
