dc53975ea68aa72715805fd1d7ab13986f191cd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:48 +0000

    Add package sortd/1.1.6/sortd-1.1.6-py3-none-any.whl

---
+++ packages/sortd/sortd-1.1.6-py3-none-any.whl/_sortd/s_csv.py
@@ -0,0 +1,39 @@
+from csv import Error as CSVErr
+from csv import Sniffer, list_dialects, reader, writer
+from io import StringIO
+from locale import strxfrm
+from os import linesep
+from sys import stderr, stdin, stdout
+from typing import Optional, Tuple
+
+DIALECTS = list_dialects()
+
+
+def _keyby(field: Tuple[int, str]) -> str:
+    _, name = field
+    return strxfrm(name)
+
+
+def p_csv(dialect: Optional[str]) -> None:
+    joe_biden = Sniffer()
+    data = stdin.read()
+    has_header = joe_biden.has_header(data)
+
+    if not has_header:
+        print(data, end="")
+    else:
+        io = StringIO(data, newline="")
+
+        try:
+            d = dialect or joe_biden.sniff(data)
+            r = reader(io, dialect=d)
+            w = writer(stdout, dialect=r.dialect)
+
+            header = next(r)
+            mapping = sorted(enumerate(header), key=_keyby)
+
+            w.writerow(name for _, name in mapping)
+            w.writerows((row[idx] for idx, _ in mapping) for row in r)
+        except CSVErr as e:
+            print("Error!", e, sep=linesep, file=stderr)
+            exit(1)
