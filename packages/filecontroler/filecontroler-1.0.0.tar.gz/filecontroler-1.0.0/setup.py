b97b7eacd88ed0eaf3c0123059c8cf95a7bf338a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:05 +0000

    Add package filecontroler/1.0.0/filecontroler-1.0.0.tar.gz

---
+++ packages/filecontroler/filecontroler-1.0.0.tar.gz/filecontroler-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'filecontroler',
+    version = '1.0.0',
+    py_modules = ['filecontroler'],
+    author = 'xuanbaobao001',
+    author_email = '3013998198@qq.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'control files.'
+    )
