997e6a00e9defe155d9c38dd28c8ce5f4fa39260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:10 +0000

    Add package func-af/0.0.1/func_af-0.0.1.tar.gz

---
+++ packages/func-af/func_af-0.0.1.tar.gz/func_af-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='func_af',
+    version='0.0.1',
+    packages=['src', 'src.main'],
+    url='',
+    license='',
+    author='WSIT',
+    author_email='',
+    description=''
+)
