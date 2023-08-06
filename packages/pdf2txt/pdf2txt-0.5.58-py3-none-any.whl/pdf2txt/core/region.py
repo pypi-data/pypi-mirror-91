41d8dbcf084182d196bb2d57d9f940b2051a2fc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:24 +0000

    Add package pdf2txt/0.5.58/pdf2txt-0.5.58-py3-none-any.whl

---
+++ packages/pdf2txt/pdf2txt-0.5.58-py3-none-any.whl/pdf2txt/core/region.py
@@ -0,0 +1,42 @@
+from copy import copy
+from pdf2txt.core.space_region import SpaceRegionExtractor
+from pdf2txt.core.alignement_region import AlignementRegionExtractor
+from pdf2txt import utils
+from pdf2txt.settings import DEFAULT_X_SEPARATOR_WIDTH, DEFAULT_Y_SEPARATOR_HEIGHT, MIN_AREA_HIEGHT, MIN_AREA_WIDTH
+
+
+class RegionExtractor:
+
+    def __init__(self):
+
+        self.space_region=SpaceRegionExtractor()
+        self.alignement_region=AlignementRegionExtractor()
+
+
+    def extract_regions(self, page, method='images'):
+
+        from pdf2txt.core.page import Region
+
+
+        if method=='objects':
+            regions = self.space_region.extract_regions_from_objects(page)
+
+
+            page_surface = utils.calculate_area(page.bbox)
+
+            regions2 = copy(regions)
+            for region in regions:
+                area_surface_ratio = round(utils.calculate_area(region) / page_surface, 2)
+                if area_surface_ratio > 0.2:
+                    a_regions = self.alignement_region.extract_regions(region)
+                    if len(a_regions) > 1:
+                        regions2.remove(region)
+                        regions2.extend(a_regions)
+
+
+            return regions2
+
+        elif method=='images':
+            regions= self.space_region.extract_regions_from_image(page)
+
+            return [Region(page, r) for r in regions]
