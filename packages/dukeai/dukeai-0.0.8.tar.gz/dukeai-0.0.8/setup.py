66cfa3d249b6456bec4a4640c793323ce8740104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:42 +0000

    Add package dukeai/0.0.8/dukeai-0.0.8.tar.gz

---
+++ packages/dukeai/dukeai-0.0.8.tar.gz/dukeai-0.0.8/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+                    name="dukeai",
+                    version="0.0.8",
+                    author="shubham kothari",
+                    author_email="shubham@duke.ai",
+                    description="Dukeai Development Package",
+                    long_description=long_description,
+                    url="https://duke.ai",
+                    install_requires=['boto3', 'requests'],
+                    packages=setuptools.find_packages(),
+                    classifiers=[
+                                    "Programming Language :: Python :: 3",
+                                    "License :: OSI Approved :: MIT License",
+                                    "Operating System :: OS Independent",
+                                ],
+                )
+
