c8a49236b0e03f7f4a66f9c5c683f1b95110596a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:40 +0000

    Add package reana-client/0.7.2/reana-client-0.7.2.tar.gz

---
+++ packages/reana-client/reana-client-0.7.2.tar.gz/reana-client-0.7.2/reana_client/config.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of REANA.
+# Copyright (C) 2017, 2018 CERN.
+#
+# REANA is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+"""REANA client configuration."""
+
+import os
+
+import pkg_resources
+
+reana_yaml_valid_file_names = ["reana.yaml", "reana.yml"]
+"""REANA specification valid file names."""
+
+reana_yaml_schema_file_path = pkg_resources.resource_filename(
+    __name__, "schemas/reana_analysis_schema.json"
+)
+"""REANA specification schema location."""
+
+default_user = "00000000-0000-0000-0000-000000000000"
+"""Default user to use when submitting workflows to REANA Server."""
+
+ERROR_MESSAGES = {
+    "missing_access_token": "Please provide your access token by using"
+    " the -t/--access-token flag, or by setting the"
+    " REANA_ACCESS_TOKEN environment variable."
+}
+
+JSON = "json"
+"""Json output format."""
+
+TIMECHECK = 5
+"""Time between workflow status check."""
+
+URL = "url"
+"""Url output format."""
+
+WORKFLOW_ENGINES = ["serial", "cwl", "yadage"]
+"""Supported workflow engines."""
---
+++ packages/reana-client/reana-client-0.7.2.tar.gz/reana-client-0.7.2/tests/conftest.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of REANA.
+# Copyright (C) 2017, 2018 CERN.
+#
+# REANA is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pytest configuration for REANA client."""
+
+from __future__ import absolute_import, print_function
+
+import pytest
+
+
+@pytest.fixture()
+def create_yaml_workflow_schema():
+    """Return dummy yaml workflow schema."""
+    reana_yaml_schema = """
+        version: 0.4.0
+        inputs:
+          files:
+            - code/helloworld.py
+            - inputs/names.txt
+          parameters:
+            sleeptime: 2
+            inputfile: inputs/names.txt
+            helloworld: code/helloworld.py
+            outputfile: outputs/greetings.txt
+        outputs:
+          files:
+           - outputs/greetings.txt
+        workflow:
+          type: serial
+          specification:
+            steps:
+              - environment: 'python:2.7'
+                commands:
+                  - python "${helloworld}" --sleeptime ${sleeptime} \
+                  --inputfile "${inputfile}" --outputfile "${outputfile}"
+        """
+    return reana_yaml_schema
