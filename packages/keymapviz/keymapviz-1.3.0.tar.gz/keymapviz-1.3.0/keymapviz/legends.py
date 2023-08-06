2b1bde84fbb11700066cc9d6d7976f3d93426ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:12:11 +0000

    Add package keymapviz/1.3.0/keymapviz-1.3.0.tar.gz

---
+++ packages/keymapviz/keymapviz-1.3.0.tar.gz/keymapviz-1.3.0/keymapviz/legends.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+legends = {
+    'XXXXXXX': '',
+    '_______': '',
+    'KC_TRNS': '',
+    'KC_NO': '',
+    'LCTL(KC_Z)': 'UNDO',
+    'LCTL(KC_Y)': 'REDO',
+    'LCTL(KC_C)': 'COPY',
+    'LCTL(KC_V)': 'PASTE',
+    'LCTL(KC_X)': 'CUT',
+    'KC_PSCREEN': 'PSc',
+
+    'MO(LOWER)': 'LOWER',
+    'MO(RAISE)': 'RAISE',
+    'RSFT_T(KC_ENT)': 'ET/SFT',
+    'LALT(KC_TAB)': 'ALT-TAB',
+    'LCTL(KC_G)': 'Ctrl-G',
+    'LSFT(KC_SPACE)': 'Sft-SPC',
+}
