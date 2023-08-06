e5a91b8f996297c6217ba84a834d0da022ab4ec2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:19 +0000

    Add package pybenutils/0.0.7.1/pybenutils-0.0.7.1-py3-none-any.whl

---
+++ packages/pybenutils/pybenutils-0.0.7.1-py3-none-any.whl/pybenutils/utils_logger/config_logger.py
@@ -0,0 +1,42 @@
+import os
+import sys
+import logging
+import inspect
+import logging.config
+from collections import defaultdict
+from logging.handlers import RotatingFileHandler
+
+INITIALIZED = defaultdict(bool)
+
+
+def set_logger(logger_name):
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+
+    fh = RotatingFileHandler('{}.log'.format(logger_name), 'w+', encoding='utf-8')
+    formatter = logging.Formatter(
+        '[%(asctime)s] | %(module)-20s | %(funcName)-20s | %(levelname)-7s : %(message)s')
+    fh.setFormatter(formatter)
+
+    ch = logging.StreamHandler(stream=sys.stdout)
+    ch.setLevel(logging.DEBUG)
+    ch.setFormatter(formatter)
+
+    eh = logging.StreamHandler(stream=sys.stderr)
+    eh.setLevel(logging.ERROR)
+    eh.setFormatter(formatter)
+
+    logger.addHandler(ch)
+    logger.addHandler(fh)
+
+    INITIALIZED[logger_name] = True
+
+    return logger
+
+
+def get_logger(logger_name=''):
+    if not logger_name:
+        frame = inspect.stack()[1]
+        module = inspect.getmodule(frame[0])
+        logger_name = os.path.basename(module.__file__)
+    return logging.getLogger(logger_name) if INITIALIZED[logger_name] else set_logger(logger_name)
