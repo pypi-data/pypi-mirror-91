7797629f4ee5c565ef9ba7b22e7b908601d02495
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:46 +0000

    Add package django-errors/0.4b2/django_errors-0.4b2-py3.9.egg

---
+++ packages/django-errors/django_errors-0.4b2-py3.9.egg/django_errors/urls.py
@@ -0,0 +1,16 @@
+"""
+    Django Errors URL Pattern List.
+"""
+
+# pylint: disable=W0401, W0614, E1120
+
+from django.conf.urls import url
+from django.views.decorators.cache import never_cache
+from . import views
+
+urlpatterns = [
+    url(r'^403', views.custom_403, name="403"),
+    url(r'^404', views.custom_404, name="404"),
+    url(r'^400', never_cache(views.custom_400), name="400"),
+    url(r'^500', never_cache(views.custom_500), name="500"),
+]
