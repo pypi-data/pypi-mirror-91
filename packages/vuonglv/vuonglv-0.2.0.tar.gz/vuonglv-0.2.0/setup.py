be4df6156f71cc9dc2537512cc4146f7a8bc64fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:07 +0000

    Add package vuonglv/0.2.0/vuonglv-0.2.0.tar.gz

---
+++ packages/vuonglv/vuonglv-0.2.0.tar.gz/vuonglv-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['vuonglv']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'vuonglv',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'vuonglv',
+    'author_email': 'it.vuonglv@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/vuonglv/vuonglv-0.2.0.tar.gz/vuonglv-0.2.0/vuonglv/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.1.0'
+
+infor = {
+    "name": "Le Van Vuong",
+    "nickname": "vuonglv",
+    "location": "Ha Noi, Viet nam",
+    "dev": "backend",
+    "email": "it.vuonglv@gmail.com",
+    "github": "https://www.github.com/vuonglv1612"
+}
+
+from pprint import pprint
+
+pprint(infor)
