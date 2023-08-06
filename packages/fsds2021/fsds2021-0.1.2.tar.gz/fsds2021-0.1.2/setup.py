839af1ff754f0fd4590e944857aa186dccd5dcec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:27 +0000

    Add package fsds2021/0.1.2/fsds2021-0.1.2.tar.gz

---
+++ packages/fsds2021/fsds2021-0.1.2.tar.gz/fsds2021-0.1.2/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['Click>=7.0', ]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="James Irving",
+    author_email='james.irving.phd@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="Revamped version of fsds",
+    entry_points={
+        'console_scripts': [
+            'fsds2021=fsds2021.cli:main',
+        ],
+    },
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='fsds2021',
+    name='fsds2021',
+    packages=find_packages(include=['fsds2021', 'fsds2021.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/jirvingphd/fsds2021',
+    version='0.1.2',
+    zip_safe=False,
+)
