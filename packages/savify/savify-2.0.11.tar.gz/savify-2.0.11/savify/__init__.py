2b529b940d1e4151d48cd0c4ef2b57b65af8a55b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:24 +0000

    Add package savify/2.0.11/savify-2.0.11.tar.gz

---
+++ packages/savify/savify-2.0.11.tar.gz/savify-2.0.11/savify/__init__.py
@@ -0,0 +1,24 @@
+"""
+Savify
+
+Download Spotify songs to mp3 with full metadata and cover art!
+
+:Copyright: © 2020, Laurence Rawlings.
+:License: MIT (see /LICENSE).
+"""
+
+from .savify import *
+from .types import *
+
+__title__ = 'Savify'
+__author__ = """Laurence Rawlings"""
+__email__ = 'contact@laurencerawlings.com'
+__version__ = '2.0.11'
+__license__ = 'MIT'
+__docformat__ = 'restructuredtext en'
+
+__all__ = ['savify', 'types']
+
+def cli():
+    from .cli import main
+    main()
