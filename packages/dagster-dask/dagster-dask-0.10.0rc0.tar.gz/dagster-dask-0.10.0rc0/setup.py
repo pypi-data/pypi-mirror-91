8300c6555d2361632a5ccb7eb62e555a0d2900c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:22 +0000

    Add package dagster-dask/0.10.0rc0/dagster-dask-0.10.0rc0.tar.gz

---
+++ packages/dagster-dask/dagster-dask-0.10.0rc0.tar.gz/dagster-dask-0.10.0rc0/setup.py
@@ -0,0 +1,44 @@
+from setuptools import find_packages, setup
+
+
+def get_version():
+    version = {}
+    with open("dagster_dask/version.py") as fp:
+        exec(fp.read(), version)  # pylint: disable=W0122
+
+    return version["__version__"]
+
+
+if __name__ == "__main__":
+    setup(
+        name="dagster-dask",
+        version=get_version(),
+        author="Elementl",
+        author_email="hello@elementl.com",
+        license="Apache-2.0",
+        description="Package for using Dask as Dagster's execution engine.",
+        url="https://github.com/dagster-io/dagster/tree/master/python_modules/libraries/dagster-dask",
+        classifiers=[
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        packages=find_packages(exclude=["test"]),
+        install_requires=[
+            "bokeh",
+            "dagster",
+            "dask[dataframe]>=1.2.2,<=2.30.0",
+            "distributed>=1.28.1,<=2.30.1",
+            # resolve issue with aiohttp pin of chardet for aiohttp<=3.7.3, req'd by dask-kubernetes
+            # https://github.com/dagster-io/dagster/issues/3539
+            "chardet<4.0",
+        ],
+        extras_require={
+            "yarn": ["dask-yarn"],
+            "pbs": ["dask-jobqueue"],
+            "kube": ["dask-kubernetes"],
+        },
+        zip_safe=False,
+    )
