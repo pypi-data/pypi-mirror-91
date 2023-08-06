cf0f49876f09bcf454ea25a5e255a1924d90c80a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:18 +0000

    Add package atelParser/1.0.0/atelParser-1.0.0.tar.gz

---
+++ packages/atelParser/atelParser-1.0.0.tar.gz/atelParser-1.0.0/CHANGES.md
@@ -0,0 +1,11 @@
+
+## Release Notes
+
+**v1.0.0 - January 13, 2021**
+
+* **ENHANCEMENT** full documentation
+
+**v0.4.0 - May 4, 2020**
+
+* Now compatible with Python 3.*
+* **Fixed**: adding requests, pymysql and pandas as dependencies
---
+++ packages/atelParser/atelParser-1.0.0.tar.gz/atelParser-1.0.0/setup.py
@@ -0,0 +1,62 @@
+from setuptools import setup, find_packages
+import os
+
+moduleDirectory = os.path.dirname(os.path.realpath(__file__))
+exec(open(moduleDirectory + "/atelParser/__version__.py").read())
+
+
+def readme():
+    with open(moduleDirectory + '/README.md') as f:
+        return f.read()
+
+install_requires = [
+    'pyyaml',
+    'atelParser',
+    'fundamentals',
+    'astrocalc',
+    'HMpTy',
+    'multiprocess',
+    'requests',
+    'pymysql==0.9.0',
+    'pandas',
+    'numpy'
+]
+
+# READ THE DOCS SERVERS
+exists = os.path.exists("/home/docs/")
+if exists:
+    c_exclude_list = ['healpy', 'astropy',
+                      'numpy', 'sherlock', 'wcsaxes', 'HMpTy', 'ligo-gracedb']
+    for e in c_exclude_list:
+        try:
+            install_requires.remove(e)
+        except:
+            pass
+
+setup(name="atelParser",
+      version=__version__,
+      description="scrape and parse content of ATels posted on The Astronomer's Telegram website, identify individual objects by name and coordinates",
+      long_description=readme(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.7',
+          'Topic :: Utilities',
+      ],
+      keywords=['astronomy, parse, atel'],
+      url='https://github.com/thespacedoctor/atelParser',
+      download_url='https://github.com/thespacedoctor/atelParser/archive/v%(__version__)s.zip' % locals(
+      ),
+      author='David Young',
+      author_email='davidrobertyoung@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=install_requires,
+      test_suite='nose2.collector.collector',
+      tests_require=['nose2', 'cov-core'],
+      entry_points={
+          'console_scripts': ['atel=atelParser.cl_utils:main'],
+      },
+      zip_safe=False)
