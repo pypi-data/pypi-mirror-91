2960c93bee4a5615450796d2da71813d706e2acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:26:44 +0000

    Add package pgkit/1.3.3/pgkit-1.3.3.tar.gz

---
+++ packages/pgkit/pgkit-1.3.3.tar.gz/pgkit-1.3.3/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pgkit',
+ 'pgkit.application',
+ 'pgkit.application.models',
+ 'pgkit.application.templates',
+ 'pgkit.cli',
+ 'pgkit.cli.commands']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['Jinja2>=2.11.2,<3.0.0', 'click>=7.1.2,<8.0.0', 'tinydb>=4.3.0,<5.0.0']
+
+entry_points = \
+{'console_scripts': ['pgkit = pgkit.cli.cli:main']}
+
+setup_kwargs = {
+    'name': 'pgkit',
+    'version': '1.3.3',
+    'description': '',
+    'long_description': None,
+    'author': 'Sadegh Hayeri',
+    'author_email': 'hayerisadegh@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
