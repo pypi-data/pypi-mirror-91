d2b17ef89812bc0d662f06e2311031213597254d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:31 +0000

    Add package grvlms-discovery/1.0.9/grvlms-discovery-1.0.9.tar.gz

---
+++ packages/grvlms-discovery/grvlms-discovery-1.0.9.tar.gz/grvlms-discovery-1.0.9/grvlmsdiscovery/templates/discovery/apps/settings/grvlms/development.py
@@ -0,0 +1,8 @@
+from ..devstack import *
+
+{% include "discovery/apps/settings/partials/common.py" %}
+
+# The following urls should be accessible from the outside by a discovery web user in
+# order to use the /login endpoint
+BACKEND_SERVICE_EDX_OAUTH2_PROVIDER_URL = SOCIAL_AUTH_EDX_OIDC_URL_ROOT
+SOCIAL_AUTH_EDX_OIDC_KEY = "{{ DISCOVERY_OAUTH2_KEY_DEV }}"
