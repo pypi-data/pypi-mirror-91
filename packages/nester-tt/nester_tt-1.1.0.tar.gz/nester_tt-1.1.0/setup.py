c273b965086aa1f9bcedfd4884fdc4952dbc0e91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:15 +0000

    Add package nester-tt/1.1.0/nester_tt-1.1.0.tar.gz

---
+++ packages/nester-tt/nester_tt-1.1.0.tar.gz/nester_tt-1.1.0/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open ("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+    
+setuptools.setup (
+    name = 'nester_tt',
+    version = '1.1.0',
+    py_modules = ['nester_tt'],
+    author = 'hotchups',
+    author_email = 'miyoungkim.1126@gmail.com',
+    description = 'A simple printer of nested lists',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    )
