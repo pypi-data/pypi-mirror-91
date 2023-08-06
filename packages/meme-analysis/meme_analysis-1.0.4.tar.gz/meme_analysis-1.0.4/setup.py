8443791de701ad00f07c06a14029ada8aea39ace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:45 +0000

    Add package meme-analysis/1.0.4/meme_analysis-1.0.4.tar.gz

---
+++ packages/meme-analysis/meme_analysis-1.0.4.tar.gz/meme_analysis-1.0.4/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="meme_analysis",# 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
+    version="1.0.4",#程序版本
+    author="Xiaoran Li(李笑然)", # 项目作者
+    author_email="lixira@outlook.com",#作者邮件
+    description="Sememe Analysis by SIST(Shizuoka Institute of Science and Technology) NLP Lab XiaoranLi", # 项目的一句话描述
+    long_description=long_description,#加长版描述？
+    long_description_content_type="text/markdown",#描述使用Markdown
+    url="https://github.com/SauronLee/meme_analysis",# 项目地址
+    packages=setuptools.find_packages(),#无需修改
+    classifiers=[
+        "Programming Language :: Python :: 3",#使用Python3
+        "License :: OSI Approved :: Apache Software License",#开源协议
+        "Operating System :: OS Independent",
+    ],
+)
