7f38ddb6e91288338b8b33eaa6a9a06c52689f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:20 +0000

    Add package pymadoka/0.1/pymadoka-0.1-a.tar.gz

---
+++ packages/pymadoka/pymadoka-0.1-a.tar.gz/pymadoka-0.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymadoka',
+    version='0.1',
+    py_modules=['pymadoka'],
+    author = "Manuel Durán",
+    author_email = "manuelduran@gmail.com",
+    description = ("A library to control Daikin BRC1H (Madoka) thermostats"),
+    license = "MIT",
+    url = "https://github.com/mduran80/pymadoka",
+    keywords = "thermostat homeautomation bluetooth",
+    packages=find_packages()+ find_packages(where="./features"),
+    install_requires=[
+        'click',
+        'bleak',
+        'pyyaml'
+    ],
+    entry_points='''
+        [console_scripts]
+        pymadoka=pymadoka.cli:cli
+    ''',
+)
