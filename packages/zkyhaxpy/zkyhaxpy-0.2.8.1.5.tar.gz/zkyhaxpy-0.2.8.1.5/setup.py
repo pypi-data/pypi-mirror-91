6d3142ae4a555f9c5b0b3945df2ef46501a38c19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:56 +0000

    Add package zkyhaxpy/0.2.8.1.5/zkyhaxpy-0.2.8.1.5.tar.gz

---
+++ packages/zkyhaxpy/zkyhaxpy-0.2.8.1.5.tar.gz/zkyhaxpy-0.2.8.1.5/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zkyhaxpy", # Replace with your own username
+    version="0.2.8.1.5",
+    author="Surasak Choedpasuporn",
+    author_email="surasak.cho@gmail.com",
+    description="A swiss-knife Data Science package for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/surasakcho/zkyhaxpy",
+    packages=setuptools.find_packages(),    
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
