5f879cb3b4a71fb25de43e603e42a2a790fe8df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:24 +0000

    Add package itk-texturefeatures/3.4.0/itk_texturefeatures-3.4.0-cp38-cp38-win_amd64.whl

---
+++ packages/itk-texturefeatures/itk_texturefeatures-3.4.0-cp38-cp38-win_amd64.whl/itk/Configuration/TextureFeatures_snake_case.py
@@ -0,0 +1,1 @@
+snake_case_functions = ('coocurrence_texture_features_image_filter', 'image_file_reader', 'image_file_writer', 'image_source', 'image_to_image_filter', 'run_length_texture_features_image_filter', )
