c41cd8d22515c370c1e4f79092cdd33b6fc8a345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:36 +0000

    Add package async-imgkit/0.0.1/async-imgkit-0.0.1.tar.gz

---
+++ packages/async-imgkit/async-imgkit-0.0.1.tar.gz/async-imgkit-0.0.1/async_imgkit/async_imgkit.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import asyncio
+import re
+import subprocess
+import sys
+from imgkit.source import Source
+from async_imgkit.config import Config
+from imgkit import IMGKit
+import io
+import codecs
+
+
+class AsyncIMGKit(IMGKit):
+    """
+
+    """
+
+    def __init__(self):
+        return None
+
+    @classmethod
+    async def create(cls, url_or_file, source_type, options=None, toc=None, cover=None,
+                 css=None, config=None, cover_first=None):
+        imgkit = cls()
+        imgkit.source = Source(url_or_file, source_type)
+        imgkit.config = await Config.create() if not config else config
+        try:
+            imgkit.wkhtmltoimage = imgkit.config.wkhtmltoimage.decode('utf-8')
+        except AttributeError:
+            imgkit.wkhtmltoimage = imgkit.config.wkhtmltoimage
+
+        imgkit.xvfb = imgkit.config.xvfb
+
+        imgkit.options = {}
+        if imgkit.source.isString():
+            imgkit.options.update(imgkit._find_options_in_meta(url_or_file))
+
+        if options:
+            imgkit.options.update(options)
+
+        imgkit.toc = toc if toc else {}
+        imgkit.cover = cover
+        imgkit.cover_first = cover_first
+        imgkit.css = css
+        imgkit.stylesheets = []
+        return imgkit
+
+
+    async def to_img(self, path=None):
+        args = " ".join(self.command(path))
+
+        result = await asyncio.create_subprocess_shell(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+
+        # If the source is a string then we will pipe it into wkhtmltoimage.
+        # If we want to add custom CSS to file then we read input file to
+        # string and prepend css to it and then pass it to stdin.
+        # This is a workaround for a bug in wkhtmltoimage (look closely in README)
+        if self.source.isString() or (self.source.isFile() and self.css):
+            string = self.source.to_s().encode('utf-8')
+        elif self.source.isFileObj():
+            string = self.source.source.read().encode('utf-8')
+        else:
+            string = None
+        stdout, stderr = await result.communicate(input=string)
+        stderr = stderr or stdout
+        try:
+            stderr = stderr.decode('utf-8')
+        except UnicodeDecodeError:
+            stderr = ''
+        exit_code = result.returncode
+
+        if 'cannot connect to X server' in stderr:
+            raise IOError('%s\n'
+                          'You will need to run wkhtmltoimage within a "virtual" X server.\n'
+                          'Go to the link below for more information\n'
+                          'http://wkhtmltopdf.org' % stderr)
+
+        if 'Error' in stderr:
+            raise IOError('wkhtmltoimage reported an error:\n' + stderr)
+
+        if exit_code != 0:
+            xvfb_error = ''
+            if 'QXcbConnection' in stderr:
+                xvfb_error = 'You need to install xvfb(sudo apt-get install xvfb, yum install xorg-x11-server-Xvfb, etc), then add option: {"xvfb": ""}.'
+            raise IOError("wkhtmltoimage exited with non-zero code {0}. error:\n{1}\n\n{2}".format(exit_code, stderr, xvfb_error))
+
+        # Since wkhtmltoimage sends its output to stderr we will capture it
+        # and properly send to stdout
+        if '--quiet' not in args:
+            sys.stdout.write(stderr)
+
+        if not path:
+            return stdout
+        else:
+            try:
+                with codecs.open(path, mode='rb') as f:
+                    text = f.read(4)
+                    if text == '':
+                        raise IOError('Command failed: %s\n'
+                                      'Check whhtmltoimage output without \'quiet\' '
+                                      'option' % args)
+                    return True
+            except IOError as e:
+                raise IOError('Command failed: %s\n'
+                              'Check whhtmltoimage output without \'quiet\' option\n'
+                              '%s ' % (args), e)
---
+++ packages/async-imgkit/async-imgkit-0.0.1.tar.gz/async-imgkit-0.0.1/setup.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools.command.test import test
+import os
+import sys
+import async_imgkit
+
+
+def long_description():
+    try:
+        import pypandoc
+        long_desc = pypandoc.convert_file('README.md', 'rst')
+        long_desc += '\n' + pypandoc.convert_file('AUTHORS.md', 'rst')
+    except Exception as e:
+        print(e)
+        long_desc = async_imgkit.__doc__.strip()
+    return long_desc
+
+
+setup(
+    name='async-imgkit',
+    version=async_imgkit.__version__,
+    description=async_imgkit.__doc__.strip(),
+    long_description=long_description(),
+    download_url='https://github.com/guilhermef/async_imgkit',
+    license="MIT",
+    install_requires=[
+        "imgkit",
+    ],
+    extras_require={
+        'tests': [
+            'pytest',
+            'aiounittest',
+            'pypandoc',
+            'coverage',
+            'nose',
+        ],
+    },
+    packages=['async_imgkit'],
+    author="guilhermef",
+    author_email="guivideojob@gmail.com",
+    url="https://github.com/guilhermef/async_imgkit",
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Text Processing',
+        'Topic :: Text Processing :: General',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Text Processing :: Markup :: HTML',
+        'Topic :: Text Processing :: Markup :: XML',
+        'Topic :: Utilities'
+    ]
+)
