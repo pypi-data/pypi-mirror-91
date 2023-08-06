d1297f4e82f18442ef03db355ae48c5bf6f60430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:24 +0000

    Add package volumentations/0.1.8/volumentations-0.1.8-py3-none-any.whl

---
+++ packages/volumentations/volumentations-0.1.8-py3-none-any.whl/volumentations/core/utils.py
@@ -0,0 +1,13 @@
+"""Utils used by volumentations."""
+from abc import ABCMeta, abstractmethod
+
+from ..core.six import add_metaclass, string_types
+
+
+def format_args(args_dict):
+    formatted_args = []
+    for k, v in args_dict.items():
+        if isinstance(v, string_types):
+            v = "'{}'".format(v)
+        formatted_args.append("{}={}".format(k, v))
+    return ", ".join(formatted_args)
