a651a98e1a7395085f16f4858f2d26b249c00eb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:02 +0000

    Add package wait-for-it/2.2.0/wait-for-it-2.2.0.tar.gz

---
+++ packages/wait-for-it/wait-for-it-2.2.0.tar.gz/wait-for-it-2.2.0/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+from wait_for_it import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="wait-for-it",
+    version=__version__,
+    author="Travis Clarke",
+    author_email="travis.m.clarke@gmail.com",
+    description="Wait for service(s) to be available before executing a command.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/clarketm/wait-for-it",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.7",
+    setup_requires=["setuptools>=38.6.0"],  # for long_description_content_type
+    py_modules=["wait_for_it"],
+    install_requires=["click"],
+    extras_require={"dev": ["black", "click", "flake8", "parameterized", "pytest", "pytest-cov", "twine"]},
+    entry_points={"console_scripts": ["wait-for-it=wait_for_it.wait_for_it:cli"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX",
+        "Topic :: Internet",
+        "Topic :: Utilities",
+    ],
+)
