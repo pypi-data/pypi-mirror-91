423a18e59fbe4e0bfd66a25cdbfca952b197d720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:27 +0000

    Add package pipx/0.16.0.0/pipx-0.16.0.0-py3-none-any.whl

---
+++ packages/pipx/pipx-0.16.0.0-py3-none-any.whl/pipx/constants.py
@@ -0,0 +1,89 @@
+import os
+import sys
+from pathlib import Path
+from textwrap import dedent
+from typing import NewType
+
+DEFAULT_PIPX_HOME = Path.home() / ".local/pipx"
+DEFAULT_PIPX_BIN_DIR = Path.home() / ".local/bin"
+PIPX_HOME = Path(os.environ.get("PIPX_HOME", DEFAULT_PIPX_HOME)).resolve()
+PIPX_LOCAL_VENVS = PIPX_HOME / "venvs"
+PIPX_LOG_DIR = PIPX_HOME / "logs"
+DEFAULT_PIPX_SHARED_LIBS = PIPX_HOME / "shared"
+PIPX_SHARED_LIBS = Path(
+    os.environ.get("PIPX_SHARED_LIBS", DEFAULT_PIPX_SHARED_LIBS)
+).resolve()
+PIPX_SHARED_PTH = "pipx_shared.pth"
+LOCAL_BIN_DIR = Path(os.environ.get("PIPX_BIN_DIR", DEFAULT_PIPX_BIN_DIR)).resolve()
+PIPX_VENV_CACHEDIR = PIPX_HOME / ".cache"
+TEMP_VENV_EXPIRATION_THRESHOLD_DAYS = 14
+
+ExitCode = NewType("ExitCode", int)
+# pipx shell exit codes
+EXIT_CODE_OK = ExitCode(0)
+EXIT_CODE_INJECT_ERROR = ExitCode(1)
+EXIT_CODE_INSTALL_VENV_EXISTS = ExitCode(1)
+EXIT_CODE_LIST_PROBLEM = ExitCode(1)
+EXIT_CODE_UNINSTALL_VENV_NONEXISTENT = ExitCode(1)
+EXIT_CODE_UNINSTALL_ERROR = ExitCode(1)
+EXIT_CODE_REINSTALL_VENV_NONEXISTENT = ExitCode(1)
+
+
+def is_windows() -> bool:
+    try:
+        WindowsError  # noqa
+    except NameError:
+        return False
+    else:
+        return True
+
+
+WINDOWS: bool = is_windows()
+
+
+def strtobool(val: str) -> bool:
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return True
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return False
+    else:
+        return False
+
+
+def use_emjois() -> bool:
+    platform_emoji_support = not is_windows() and sys.getdefaultencoding() == "utf-8"
+    return strtobool(str(os.getenv("USE_EMOJI", platform_emoji_support)))
+
+
+emoji_support = use_emjois()
+
+completion_instructions = dedent(
+    """
+Add the appropriate command to your shell's config file
+so that it is run on startup. You will likely have to restart
+or re-login for the autocompletion to start working.
+
+bash:
+    eval "$(register-python-argcomplete pipx)"
+
+zsh:
+    To activate completions for zsh you need to have
+    bashcompinit enabled in zsh:
+
+    autoload -U bashcompinit
+    bashcompinit
+
+    Afterwards you can enable completion for pipx:
+
+    eval "$(register-python-argcomplete pipx)"
+
+tcsh:
+    eval `register-python-argcomplete --shell tcsh pipx`
+
+fish:
+    # Not required to be in the config file, only run once
+    register-python-argcomplete --shell fish pipx >~/.config/fish/completions/pipx.fish
+
+"""
+)
