fbc2b4e48d65f3e83eeb80ba24cfe8d346776987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:44 +0000

    Add package nondim-slurry/0.0.18/nondim-slurry-0.0.18.tar.gz

---
+++ packages/nondim-slurry/nondim-slurry-0.0.18.tar.gz/nondim-slurry-0.0.18/slurpy/__init__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 23 17:31:26 2020
+
+@author: Jenny Wong
+
+The slurpy package contains the routines needed to solve the slurry equations,
+postprocess data and plot the results
+
+Modules
+-------
+coreproperties.py
+    Contains common physical and chemical properties of the Earth's core
+data_utils.py
+    Functions for data input/output
+getparameters.py
+    Functions to calculate parameters for the slurry
+lookup.py
+    Lookup PREM and liquidus data
+plot_utils.py
+    Functions for plotting results
+slurry.py
+    Solves the slurry equations
+lookupdata
+    Directory containing data from PREM and liquidus data from Davies et al. (2015)
+"""
