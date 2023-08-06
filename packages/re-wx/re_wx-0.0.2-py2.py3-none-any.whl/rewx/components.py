488df1565a825406b735f9fe7433f063e2e551db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:15 +0000

    Add package re-wx/0.0.2/re_wx-0.0.2-py2.py3-none-any.whl

---
+++ packages/re-wx/re_wx-0.0.2-py2.py3-none-any.whl/rewx/components.py
@@ -0,0 +1,70 @@
+"""
+All components and wrappers currently
+supported by rewx.
+"""
+import wx
+import wx.adv
+import wx.lib.scrolledpanel
+import wx.media
+
+
+ActivityIndicator = wx.ActivityIndicator
+Button = wx.Button
+BitmapButton = wx.BitmapButton
+CalendarCtrl = wx.adv.CalendarCtrl
+CheckBox = wx.CheckBox
+# CollapsiblePane = wx.CollapsiblePane
+ComboBox = wx.ComboBox
+Dropdown = ComboBox
+Frame = wx.Frame
+Gauge = wx.Gauge
+ListBox = wx.ListBox
+ListCtrl = wx.ListCtrl
+Panel = wx.Panel
+RadioBox = wx.RadioBox
+RadioButton = wx.RadioButton
+ScrolledPanel = wx.lib.scrolledpanel.ScrolledPanel
+Slider = wx.Slider
+SpinCtrl = wx.SpinCtrl
+SpinCtrlDouble = wx.SpinCtrlDouble
+StaticBitmap = wx.StaticBitmap
+StaticBox = wx.StaticBox
+StaticLine = wx.StaticLine
+StaticText = wx.StaticText
+TextCtrl = wx.TextCtrl
+ToggleButton = wx.ToggleButton
+MediaCtrl = wx.media.MediaCtrl
+
+
+class Grid(wx.Panel):
+    """
+    Wrapper type for creating a panel
+    with a Grid Sizer
+    """
+    pass
+
+class Block(wx.Panel):
+    """
+    Wrapper type for creating a panel
+    with a BoxSizer
+    """
+
+class TextArea(wx.TextCtrl):
+    """
+    TextCtrl Wrapper with it's style pre-baked
+    to be TE_MULTILINE
+    """
+    pass
+
+class SVG(wx.StaticBitmap):
+    """
+    Wrapper for converting an SVG to a scaled StaticBitmap
+    """
+    pass
+
+class SVGButton(wx.BitmapButton):
+    """
+    Wrapper for creating an SVG backed BitmapButton.
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
