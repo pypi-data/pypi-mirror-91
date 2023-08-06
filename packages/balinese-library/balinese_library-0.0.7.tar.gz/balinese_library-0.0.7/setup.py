6be6233ac6cb98d23eea16fae1df311d39ac5014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:16 +0000

    Add package balinese-library/0.0.7/balinese_library-0.0.7.tar.gz

---
+++ packages/balinese-library/balinese_library-0.0.7.tar.gz/balinese_library-0.0.7/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='balinese_library',
+  version='0.0.7',
+  description='A very basic balinese library about information extraction in balinese language',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  packages=['balinese_library'],
+  package_data={'': ['*.txt', 'storage/*.txt']},
+  author='Ngurah Agus Sanjaya ER',
+  author_email='agus_sanjaya@unud.ac.id',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='Information Extraction, Balinese', 
+  install_requires=['nltk'] 
+)
