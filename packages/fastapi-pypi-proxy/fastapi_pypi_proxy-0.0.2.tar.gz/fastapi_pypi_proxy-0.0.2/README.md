68e98fa2d2bb6e8f4ff992bf18de1386bf8beb18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:34 +0000

    Add package fastapi-pypi-proxy/0.0.2/fastapi_pypi_proxy-0.0.2.tar.gz

---
+++ packages/fastapi-pypi-proxy/fastapi_pypi_proxy-0.0.2.tar.gz/fastapi_pypi_proxy-0.0.2/README.md
@@ -0,0 +1,20 @@
+# fastapi-pypi-proxy
+A pypi proxy using fastapi
+
+
+# install
+```
+pip install fastapi-pypi-proxy
+```
+
+# run
+
+```
+python example.py
+```
+
+# docker
+
+```
+docker-compose up -d
+```
---
+++ packages/fastapi-pypi-proxy/fastapi_pypi_proxy-0.0.2.tar.gz/fastapi_pypi_proxy-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name = "fastapi_pypi_proxy",
+    version = "0.0.2",
+    description="A pypi proxy using fastapi",
+    author="xyzou",
+    author_email="zouxinyin1992@163.com",
+    url="https://github.com/singingplayer/fastapi-pypi-proxy",
+    python_requires='>3.6.0',
+    packages=["fastapi_pypi_proxy"],
+    install_requires=required,
+)
