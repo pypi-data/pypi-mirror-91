bd9c9aed59ad302322f0682daff25c694c3a38e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:17 +0000

    Add package streamlit-echarts/0.2.0/streamlit_echarts-0.2.0-py3-none-any.whl

---
+++ packages/streamlit-echarts/streamlit_echarts-0.2.0-py3-none-any.whl/streamlit_echarts/frontend/build/precache-manifest.46f8036259fcfc6ae1d9a6e6183b5f71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fe81d398b9f8f669ba59c6da2a5c28",
+    "url": "./index.html"
+  },
+  {
+    "revision": "6e0e81743e5defa51882",
+    "url": "./static/css/main.d984dddf.chunk.css"
+  },
+  {
+    "revision": "d72888bd0a1f05215766",
+    "url": "./static/js/2.47649f32.chunk.js"
+  },
+  {
+    "revision": "3fa3d8b82d7218267a1f5ab43be25480",
+    "url": "./static/js/2.47649f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e0e81743e5defa51882",
+    "url": "./static/js/main.6227860d.chunk.js"
+  },
+  {
+    "revision": "177d041b66bb68f23701",
+    "url": "./static/js/runtime-main.34185f1e.js"
+  }
+]);
