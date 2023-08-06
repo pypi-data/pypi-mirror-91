2fb6acc64db42d4984d1c7922e48097eabb56ee0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:44 +0000

    Add package pyshp/2.1.3/pyshp-2.1.3.tar.gz

---
+++ packages/pyshp/pyshp-2.1.3.tar.gz/pyshp-2.1.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+
+def read_file(file):
+    with open(file, 'rb') as fh:
+        data = fh.read()
+    return data.decode('utf-8')
+
+setup(name='pyshp',
+      version='2.1.3',
+      description='Pure Python read/write support for ESRI Shapefile format',
+      long_description=read_file('README.md'),
+      long_description_content_type='text/markdown',
+      author='Joel Lawhead',
+      author_email='jlawhead@geospatialpython.com',
+      url='https://github.com/GeospatialPython/pyshp',
+      download_url='https://github.com/GeospatialPython/pyshp/archive/2.1.1.tar.gz',
+      py_modules=['shapefile'],
+      license='MIT',
+      zip_safe=False,
+      keywords='gis geospatial geographic shapefile shapefiles',
+      python_requires='>= 2.7',
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.5',
+                   'Programming Language :: Python :: 3.6',
+                   'Programming Language :: Python :: 3.7',
+                   'Programming Language :: Python :: 3.8',
+                   'Topic :: Scientific/Engineering :: GIS',
+                   'Topic :: Software Development :: Libraries',
+                   'Topic :: Software Development :: Libraries :: Python Modules'])
