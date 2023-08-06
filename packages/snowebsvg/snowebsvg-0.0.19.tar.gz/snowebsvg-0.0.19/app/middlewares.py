1cd788d5a93efe9ee4f259a6bbe011acf6fa5bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:00 +0000

    Add package snowebsvg/0.0.19/snowebsvg-0.0.19.tar.gz

---
+++ packages/snowebsvg/snowebsvg-0.0.19.tar.gz/snowebsvg-0.0.19/app/middlewares.py
@@ -0,0 +1,13 @@
+from snowebsvg import settings
+
+
+class ThemeMiddleware(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if 'theme' not in request.session:
+            request.session['theme'] = settings.SVG_DEFAULT_THEME
+        if 'theme' in request.GET:
+            request.session['theme'] = request.GET['theme']
+        return self.get_response(request)
