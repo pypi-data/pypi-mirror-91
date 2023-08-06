8258a9536aa2608c5eefd98865bdedf81d427063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:10 +0000

    Add package wikimedia-spicerack/0.0.47/wikimedia-spicerack-0.0.47.tar.gz

---
+++ packages/wikimedia-spicerack/wikimedia-spicerack-0.0.47.tar.gz/wikimedia-spicerack-0.0.47/spicerack/tests/fixtures/cookbook/cookbooks/group2/cookbook2.py
@@ -0,0 +1,20 @@
+"""Group2 Cookbook2."""
+import argparse
+
+
+__title__ = __doc__
+
+
+def argument_parser():
+    """As required by spicerack._cookbook."""
+    parser = argparse.ArgumentParser('Argparse')
+    parser.add_argument('-k', action='store_true')
+    parser.add_argument('--argument')
+
+    return parser
+
+
+def run(args, spicerack):
+    """As required by spicerack._cookbook."""
+    print([args, spicerack.verbose, spicerack.dry_run])
+    return 0
