95eb0016f3005df5eb9316aadc4e577ad68c29e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:59 +0000

    Add package ezapi-yelp/0.4.4/ezapi_yelp-0.4.4.tar.gz

---
+++ packages/ezapi-yelp/ezapi_yelp-0.4.4.tar.gz/ezapi_yelp-0.4.4/tests/test_category_endpoints.py
@@ -0,0 +1,30 @@
+import pytest
+
+from . import polite
+from .testdata import default_kwargs
+
+
+@polite
+@pytest.mark.parametrize("kwargs", default_kwargs)
+def test_all_categories(yelp_fusion, kwargs):
+    """
+    test GET /categories
+
+    Args:
+        yelp_fusion (yelp.YelpFusion): a YelpFusion object
+    """
+
+    assert yelp_fusion.all_categories(**kwargs) is not None
+
+
+@polite
+@pytest.mark.parametrize("kwargs", default_kwargs)
+def test_category_details(yelp_fusion, kwargs):
+    """
+    test GET /categories/{alias}
+
+    Args:
+        yelp_fusion (yelp.YelpFusion): a YelpFusion object
+    """
+
+    assert yelp_fusion.category_details("hotdogs", **kwargs) is not None
