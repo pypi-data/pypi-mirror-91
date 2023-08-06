e67b729b539cd2c62efe54fce87ab8dc36e244ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:30 +0000

    Add package si-utils/0.1.10/si_utils-0.1.10.tar.gz

---
+++ packages/si-utils/si_utils-0.1.10.tar.gz/si_utils-0.1.10/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['si_utils',
+ 'si_utils._vendor',
+ 'si_utils._vendor.boltons',
+ 'si_utils._vendor.toml']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['loguru>=0.5,<0.6']
+
+extras_require = \
+{'dev-utils': ['tomlkit>=0.7.0,<0.8.0', 'semver>=2.13.0,<3.0.0'],
+ 'sentry': ['sentry-sdk>=0.19,<0.20'],
+ 'yaml': ['ruamel.yaml>=0.16,<0.17', 'deepmerge>=0.1,<0.2', 'pydantic>=1,<2']}
+
+setup_kwargs = {
+    'name': 'si-utils',
+    'version': '0.1.10',
+    'description': 'an opinionated set of utilities designed to be easily included in any number of projects',
+    'long_description': None,
+    'author': 'Alex Tremblay',
+    'author_email': 'alex.tremblay@utoronto.ca',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'extras_require': extras_require,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
