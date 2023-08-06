15fa726a18acf92186b3e8069e343a2795868ed0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:23 +0000

    Add package relative-to-now/0.0.2/relative_to_now-0.0.2.tar.gz

---
+++ packages/relative-to-now/relative_to_now-0.0.2.tar.gz/relative_to_now-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", mode="r", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="relative_to_now",
+    version="0.0.2",
+    description="Print a datetimes distance from now",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Riverside-Healthcare/RelativeToNow",
+    author="Christopher Pickering",
+    author_email="cpickering@rhc.net",
+    license="GPL-3.0-or-later",
+    packages=["RelativeToNow"],
+    classifiers=[
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.7',
+)
