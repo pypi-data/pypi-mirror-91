72465e52b3609d280c6ae226daf2646433792ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:10:51 +0000

    Add package beneath/1.2.11/beneath-1.2.11.tar.gz

---
+++ packages/beneath/beneath-1.2.11.tar.gz/beneath-1.2.11/beneath/cli/__init__.py
@@ -0,0 +1,30 @@
+import argparse
+
+from beneath import __version__
+from beneath.cli import auth
+from beneath.cli import organization
+from beneath.cli import project
+from beneath.cli import service
+from beneath.cli import stream
+
+
+def main():
+    parser = create_argument_parser()
+    args = parser.parse_args()
+    try:
+        func = args.func
+    except AttributeError:
+        parser.error("too few arguments")
+    func(args)
+
+
+def create_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+    root = parser.add_subparsers()
+    auth.add_subparser(root)
+    organization.add_subparser(root)
+    project.add_subparser(root)
+    service.add_subparser(root)
+    stream.add_subparser(root)
+    return parser
