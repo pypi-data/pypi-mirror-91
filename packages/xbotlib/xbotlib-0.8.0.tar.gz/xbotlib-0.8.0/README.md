9151db9e2483d9971ca5264b1a0d259ca9fc5cb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:24 +0000

    Add package xbotlib/0.8.0/xbotlib-0.8.0.tar.gz

---
+++ packages/xbotlib/xbotlib-0.8.0.tar.gz/xbotlib-0.8.0/README.md
@@ -0,0 +1,148 @@
+# xbotlib
+
+[![PyPI version](https://badge.fury.io/py/xbotlib.svg)](https://badge.fury.io/py/xbotlib)
+[![Build Status](https://drone.autonomic.zone/api/badges/decentral1se/xbotlib/status.svg?ref=refs/heads/main)](https://drone.autonomic.zone/decentral1se/xbotlib)
+
+## XMPP bots for humans
+
+> status: experimental
+
+A friendly lightweight wrapper around
+[slixmpp](https://slixmpp.readthedocs.io/) for writing XMPP bots in Python. The
+goal is to make writing and running XMPP bots easy and fun. `xbotlib` is a
+[single file implementation](./xbotlib.py) which can easily be understood and
+extended. It provides a small API surface which reflects the `slixmpp` way of
+doing things.
+
+## Install
+
+```sh
+$ pip install xbotlib
+```
+
+## Example
+
+Put the following in a `echo.py` file.
+
+`xbotlib` provides a number of example bots which you can use to get moving
+fast and try things out.
+
+```python
+from xbotlib import EchoBot
+
+EchotBot()
+```
+
+And then `python echo.py`. You will be asked a few questions in order to load
+the account details that your bot will be using. This will generate a
+`bot.conf` file in the same working directory for further use. See the
+[configuration](#configure-your-bot) section for more.
+
+Here's the code for the `EchoBot`.
+
+```python
+class EchoBot(Bot):
+    def direct(self, message):
+        self.reply(message.body, to=message.sender)
+
+    def group(self, message):
+        if "echobot" in message.body:
+            self.reply(message.body.split(":")[-1], room=message.room)
+```
+
+Read more in the [API reference](#api-reference) for how to write your own bots.
+
+## All examples
+
+- **EchoBot**: Sends back what you sent it
+- **WhisperBot**: Anonymous whispering in group chats
+
+See [xbotlib.py](./xbotlib.py) for all example bots.
+
+## API Reference
+
+When writing your own bot, you always sub-class the `Bot` class provided from
+`xbotlib`. Then if you want to respond to a direct message, you write a
+[direct](#botdirectmessage) function. If you want to respond to a group chat
+message, you write a [group](#botgroupmessage) function.
+
+### Bot.direct(message)
+
+Respond to direct messages.
+
+Arguments:
+
+- **message**: received message (see [SimpleMessage](#simplemessage) below for available attributes)
+
+### Bot.group(message)
+
+Respond to a message in a group chat.
+
+Arguments:
+
+- **message**: received message (see [SimpleMessage](#simplemessage) below for available attributes)
+
+### SimpleMessage
+
+A simple message interface.
+
+Attributes:
+
+- **body**: the body of the message
+- **sender**: the user the message came from
+- **room**: the room the message came from
+- **receiver**: the receiver of the message
+- **nickname**: the nickname of the sender
+- **type**: the type of message (`chat` or `groupchat`)
+
+## Documenting your bot
+
+Add a `help = "my help"` to your `Bot` class like so.
+
+```python
+class MyBot(Bot):
+    help = "My help"
+```
+
+The bot will then respond to:
+
+- `!uptime` commands in direct messages
+- `<nick>:!uptime` commands in group chats (use your own nick)
+
+## Configure your bot
+
+All the ways you can pass configuration details to your bot.
+
+### Using the bot.conf
+
+If you run simply run your Python script which contains the bot then `xbotlib`
+will generate a configuration for you by asking a few questions. This is the
+simplest way to run your bot locally.
+
+### Using the command-line interface
+
+Every bot accepts a number of comand-line arguments to load configuration. You
+can use the `--help` option to see what is available (e.g. `python bot.py --help`).
+
+### Using the environment
+
+`xbotlib` will try to read the following configuration values from the
+environment if it cannot read them from a configuration file or the
+command-line interface. This can be useful when doing remote server
+deployments.
+
+- **XBOT_ACCOUNT**: The bot account
+- **XBOT_PASSWORD**: The bot password
+- **XBOT_NICK**: The bot nickname
+
+## Roadmap
+
+See the [issue tracker](https://git.autonomic.zone/decentral1se/xbotlib/issues).
+
+## Changes
+
+See the [CHANGELOG.md](./CHANGELOG.md).
+
+## License
+
+See the [LICENSE](./LICENSE.md).
---
+++ packages/xbotlib/xbotlib-0.8.0.tar.gz/xbotlib-0.8.0/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['xbotlib']
+install_requires = \
+['humanize>=3.2.0,<4.0.0', 'slixmpp>=1.6.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'xbotlib',
+    'version': '0.8.0',
+    'description': 'XMPP bots for humans',
+    'long_description': '# xbotlib\n\n[![PyPI version](https://badge.fury.io/py/xbotlib.svg)](https://badge.fury.io/py/xbotlib)\n[![Build Status](https://drone.autonomic.zone/api/badges/decentral1se/xbotlib/status.svg?ref=refs/heads/main)](https://drone.autonomic.zone/decentral1se/xbotlib)\n\n## XMPP bots for humans\n\n> status: experimental\n\nA friendly lightweight wrapper around\n[slixmpp](https://slixmpp.readthedocs.io/) for writing XMPP bots in Python. The\ngoal is to make writing and running XMPP bots easy and fun. `xbotlib` is a\n[single file implementation](./xbotlib.py) which can easily be understood and\nextended. It provides a small API surface which reflects the `slixmpp` way of\ndoing things.\n\n## Install\n\n```sh\n$ pip install xbotlib\n```\n\n## Example\n\nPut the following in a `echo.py` file.\n\n`xbotlib` provides a number of example bots which you can use to get moving\nfast and try things out.\n\n```python\nfrom xbotlib import EchoBot\n\nEchotBot()\n```\n\nAnd then `python echo.py`. You will be asked a few questions in order to load\nthe account details that your bot will be using. This will generate a\n`bot.conf` file in the same working directory for further use. See the\n[configuration](#configure-your-bot) section for more.\n\nHere\'s the code for the `EchoBot`.\n\n```python\nclass EchoBot(Bot):\n    def direct(self, message):\n        self.reply(message.body, to=message.sender)\n\n    def group(self, message):\n        if "echobot" in message.body:\n            self.reply(message.body.split(":")[-1], room=message.room)\n```\n\nRead more in the [API reference](#api-reference) for how to write your own bots.\n\n## All examples\n\n- **EchoBot**: Sends back what you sent it\n- **WhisperBot**: Anonymous whispering in group chats\n\nSee [xbotlib.py](./xbotlib.py) for all example bots.\n\n## API Reference\n\nWhen writing your own bot, you always sub-class the `Bot` class provided from\n`xbotlib`. Then if you want to respond to a direct message, you write a\n[direct](#botdirectmessage) function. If you want to respond to a group chat\nmessage, you write a [group](#botgroupmessage) function.\n\n### Bot.direct(message)\n\nRespond to direct messages.\n\nArguments:\n\n- **message**: received message (see [SimpleMessage](#simplemessage) below for available attributes)\n\n### Bot.group(message)\n\nRespond to a message in a group chat.\n\nArguments:\n\n- **message**: received message (see [SimpleMessage](#simplemessage) below for available attributes)\n\n### SimpleMessage\n\nA simple message interface.\n\nAttributes:\n\n- **body**: the body of the message\n- **sender**: the user the message came from\n- **room**: the room the message came from\n- **receiver**: the receiver of the message\n- **nickname**: the nickname of the sender\n- **type**: the type of message (`chat` or `groupchat`)\n\n## Documenting your bot\n\nAdd a `help = "my help"` to your `Bot` class like so.\n\n```python\nclass MyBot(Bot):\n    help = "My help"\n```\n\nThe bot will then respond to:\n\n- `!uptime` commands in direct messages\n- `<nick>:!uptime` commands in group chats (use your own nick)\n\n## Configure your bot\n\nAll the ways you can pass configuration details to your bot.\n\n### Using the bot.conf\n\nIf you run simply run your Python script which contains the bot then `xbotlib`\nwill generate a configuration for you by asking a few questions. This is the\nsimplest way to run your bot locally.\n\n### Using the command-line interface\n\nEvery bot accepts a number of comand-line arguments to load configuration. You\ncan use the `--help` option to see what is available (e.g. `python bot.py --help`).\n\n### Using the environment\n\n`xbotlib` will try to read the following configuration values from the\nenvironment if it cannot read them from a configuration file or the\ncommand-line interface. This can be useful when doing remote server\ndeployments.\n\n- **XBOT_ACCOUNT**: The bot account\n- **XBOT_PASSWORD**: The bot password\n- **XBOT_NICK**: The bot nickname\n\n## Roadmap\n\nSee the [issue tracker](https://git.autonomic.zone/decentral1se/xbotlib/issues).\n\n## Changes\n\nSee the [CHANGELOG.md](./CHANGELOG.md).\n\n## License\n\nSee the [LICENSE](./LICENSE.md).\n',
+    'author': 'decentral1se',
+    'author_email': 'lukewm@riseup.net',
+    'maintainer': 'decentral1se',
+    'maintainer_email': 'lukewm@riseup.net',
+    'url': 'https://git.autonomic.zone/decentral1se/xbotlib',
+    'py_modules': modules,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
