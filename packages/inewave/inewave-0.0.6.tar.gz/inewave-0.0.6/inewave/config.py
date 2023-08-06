3663d01822475b775ce47a7354abcb8a81e5658d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:47:36 +0000

    Add package inewave/0.0.6/inewave-0.0.6.tar.gz

---
+++ packages/inewave/inewave-0.0.6.tar.gz/inewave-0.0.6/inewave/config.py
@@ -0,0 +1,17 @@
+SUBMERCADOS = ["SUDESTE", "SUL", "NORDESTE", "NORTE"]
+NUM_CENARIOS = 2000
+NUM_ANOS_ESTUDO = 5
+NUM_PATAMARES = 3
+NUM_VARIAVEIS_MEDIAS = 52
+MESES = ["JANEIRO",
+         "FEVEREIRO",
+         "MARCO",
+         "ABRIL",
+         "MAIO",
+         "JUNHO",
+         "JULHO",
+         "AGOSTO",
+         "SETEMBRO",
+         "OUTUBRO",
+         "NOVEMBRO",
+         "DEZEMBRO"]
