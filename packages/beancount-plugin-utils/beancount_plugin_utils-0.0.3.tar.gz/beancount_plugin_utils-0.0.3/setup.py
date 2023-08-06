41a318dde1f7c08ee67fcebfcc5ff608155c04c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:13 +0000

    Add package beancount-plugin-utils/0.0.3/beancount_plugin_utils-0.0.3.tar.gz

---
+++ packages/beancount-plugin-utils/beancount_plugin_utils-0.0.3.tar.gz/beancount_plugin_utils-0.0.3/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='beancount_plugin_utils',
+    version='0.0.3',
+    description='Utils for beancount plugin writers - BeancountError, mark, metaset, etc.',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    author='Akuukis',
+    author_email='akuukis@kalvis.lv',
+    download_url='https://pypi.python.org/pypi/beancount_plugin_utils',
+    license='GNU AGPLv3',
+    package_data={'beancount_plugin_utils': ['../README.md']},
+    package_dir={'beancount_plugin_utils': 'beancount_plugin_utils'},
+    packages=['beancount_plugin_utils'],
+    requires=['beancount (>2.0)'],
+    url='https://github.com/Akuukis/beancount_plugin_utils',
+)
