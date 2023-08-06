b6486f1d0b44702ac4bb9bc4aef7bfceed4826b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:53 +0000

    Add package eyecu-bumblebee/0.5.2/eyecu-bumblebee-0.5.2.tar.gz

---
+++ packages/eyecu-bumblebee/eyecu-bumblebee-0.5.2.tar.gz/eyecu-bumblebee-0.5.2/bumblebee/datasets/Single.py
@@ -0,0 +1,20 @@
+from ..bases.Dataset import Dataset
+
+
+class Single(Dataset):
+
+    def __init__(self, data_source ):
+
+        self.src = data_source
+
+    def __iter__(self):
+        return self
+
+    def get_props(self):
+        return self.src.get_props()
+
+    def __len__(self):
+        return 1
+
+
+
