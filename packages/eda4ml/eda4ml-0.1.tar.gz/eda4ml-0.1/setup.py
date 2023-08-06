265663e4820754c6009c2328153a9c00ef5b9aef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:17 +0000

    Add package eda4ml/0.1/eda4ml-0.1.tar.gz

---
+++ packages/eda4ml/eda4ml-0.1.tar.gz/eda4ml-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='eda4ml',
+      version='0.1',
+      description='Methods for EDA in the ML process',
+      packages=['eda4ml'],
+      author='Athanasios Dritsas',
+      author_email='thanasis.dritsas@gmail.com',
+      zip_safe=False)
