4606903caa9c4ff26dd85c40a780220c5c31a70e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:30 +0000

    Add package openhivenpy/0.1.1.2/openhivenpy-0.1.1.2.tar.gz

---
+++ packages/openhivenpy/openhivenpy-0.1.1.2.tar.gz/openhivenpy-0.1.1.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as file:
+    long_description = file.read()
+
+with open("requirements.txt") as file:
+    requirements = file.read()
+
+setuptools.setup(
+    name="openhivenpy", 
+    version="0.1.1.2",
+    author="FrostbyteSpace",
+    author_email="nicolas.klatzer@gmail.com",
+    description="An unofficial Opensource API wrapper for Hiven\n © FrostbyteSpace",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FrostbyteSpace/openhiven.py",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=requirements
+)
