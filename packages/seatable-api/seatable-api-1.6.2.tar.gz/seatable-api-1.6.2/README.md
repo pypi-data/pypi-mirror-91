a18ec57f8f105617bdd2a88df056470a8b2cf175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:33 +0000

    Add package seatable-api/1.6.2/seatable-api-1.6.2.tar.gz

---
+++ packages/seatable-api/seatable-api-1.6.2.tar.gz/seatable-api-1.6.2/README.md
@@ -0,0 +1,5 @@
+# seatable-api-python
+
+Python client for SeaTable web api
+
+Document: https://docs.seatable.io/published/seatable-api/python/seatable-api.md
---
+++ packages/seatable-api/seatable-api-1.6.2.tar.gz/seatable-api-1.6.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__version__ = '1.6.2'
+
+
+setup(name='seatable-api',
+      version=__version__,
+      license='Apache Licence',
+      description='Client interface for SeaTable Web API',
+      author='seatable',
+      author_email='support@seafile.com',
+      url='https://github.com/seatable/seatable-api-python',
+
+      platforms='any',
+      packages=find_packages(),  # folder with __init__.py
+      install_requires=['requests', 'socketIO-client-nexus', 'ply'],
+      classifiers=['Programming Language :: Python'],
+      )
