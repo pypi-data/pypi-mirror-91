9496cb0aa227eb128f13f8dfa63ef06f8808078e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:27 +0000

    Add package gnodatapackages/1.2.8/gnodatapackages-1.2.8.tar.gz

---
+++ packages/gnodatapackages/gnodatapackages-1.2.8.tar.gz/gnodatapackages-1.2.8/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+from os import path
+
+# def readme():
+with open('README.md') as f:
+    long_description = f.read()
+    
+
+# def readme():
+#     with open('README.rst') as f:
+#         return f.read()
+   
+# this_directory = path.abspath(path.dirname(__file__))
+# with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+#     long_description = f.read()
+
+    
+setuptools.setup(
+    name="gnodatapackages", # Replace with your own username
+    version="1.2.8",
+    author="charmandersgsg",
+    author_email="charmandersgsg@gmail.com",
+    description="data def packages for gno",
+#     long_description = long_description,
+    long_description="data def packages for gno",
+    long_description_content_type="text/markdown",
+    url="https://github.com/charmandersgsg/gnodatapackages",
+    packages=['gnodatapackages'],
+    include_package_data=True,
+    install_requires=[
+          'pandas',
+          'ftfy'
+      ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
