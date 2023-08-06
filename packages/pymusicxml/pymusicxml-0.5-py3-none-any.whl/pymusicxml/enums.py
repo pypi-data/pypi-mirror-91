c9c9edbf22b4a7f8d931a8f2444d7d60c5cc3d9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:19 +0000

    Add package pymusicxml/0.5/pymusicxml-0.5-py3-none-any.whl

---
+++ packages/pymusicxml/pymusicxml-0.5-py3-none-any.whl/pymusicxml/enums.py
@@ -0,0 +1,24 @@
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  #
+#  This file is part of SCAMP (Suite for Computer-Assisted Music in Python)                      #
+#  Copyright © 2020 Marc Evanstein <marc@marcevanstein.com>.                                     #
+#                                                                                                #
+#  This program is free software: you can redistribute it and/or modify it under the terms of    #
+#  the GNU General Public License as published by the Free Software Foundation, either version   #
+#  3 of the License, or (at your option) any later version.                                      #
+#                                                                                                #
+#  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;     #
+#  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.     #
+#  See the GNU General Public License for more details.                                          #
+#                                                                                                #
+#  You should have received a copy of the GNU General Public License along with this program.    #
+#  If not, see <http://www.gnu.org/licenses/>.                                                   #
+#  ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++  #
+
+from enum import Enum
+
+LineEnd = Enum("LineEnd", "up down both arrow none")
+LineType = Enum("LineType", "solid dashed dotted wavy")
+AccidentalType = Enum("AccidentalType", "flat-flat flat natural sharp double-sharp")
+HairpinType = Enum("HairpinType", "crescendo diminuendo")
+StaffPlacement = Enum("StaffPlacement", "above below")
+ArpeggiationDirection = Enum("ArpeggiationDirection", "up down")
