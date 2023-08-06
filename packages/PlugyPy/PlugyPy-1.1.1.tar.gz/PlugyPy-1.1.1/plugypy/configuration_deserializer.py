eae60a16b3c43ba79817688304298683e65bac2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:19 +0000

    Add package PlugyPy/1.1.1/PlugyPy-1.1.1.tar.gz

---
+++ packages/PlugyPy/PlugyPy-1.1.1.tar.gz/PlugyPy-1.1.1/plugypy/configuration_deserializer.py
@@ -0,0 +1,19 @@
+import json
+from .plugypy_errors import  InvalidConfigurationFile
+from .configuration import Configuration
+
+class ConfigurationDeserializer():
+    def __init__(self, config_file_location):
+        self.__config_file_location =  config_file_location
+    
+    def deserialize_config(self):
+        config = None
+
+        with open(self.__config_file_location, 'r') as config_file:
+            try:
+                config_json = json.loads(config_file.read())
+                config = Configuration(**config_json)
+            except: 
+                raise InvalidConfigurationFile() from None
+
+        return config
