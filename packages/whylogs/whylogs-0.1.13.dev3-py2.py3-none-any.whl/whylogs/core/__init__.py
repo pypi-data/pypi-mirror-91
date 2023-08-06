a909670d674a7a3cd31563da16f2a17996eb046a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:05 +0000

    Add package whylogs/0.1.13.dev3/whylogs-0.1.13.dev3-py2.py3-none-any.whl

---
+++ packages/whylogs/whylogs-0.1.13.dev3-py2.py3-none-any.whl/whylogs/core/__init__.py
@@ -0,0 +1,7 @@
+"""
+This subpackage defines the core components of the whylogs python library.
+
+In particular, look at the `DatasetProfile` class
+"""
+from .columnprofile import ColumnProfile
+from .datasetprofile import DatasetProfile
