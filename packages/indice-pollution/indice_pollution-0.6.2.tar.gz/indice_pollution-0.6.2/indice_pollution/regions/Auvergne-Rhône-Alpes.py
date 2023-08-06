8b51bf843fea8298e34ec68039e8dcf8f6d0407f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:37 +0000

    Add package indice-pollution/0.6.2/indice_pollution-0.6.2.tar.gz

---
+++ packages/indice-pollution/indice_pollution-0.6.2.tar.gz/indice_pollution-0.6.2/indice_pollution/regions/Auvergne-Rhône-Alpes.py
@@ -0,0 +1,39 @@
+from . import ForecastMixin, EpisodeMixin
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+class Service(object):
+    website = 'https://www.atmo-auvergnerhonealpes.fr/'
+    insee_list = [
+        '73065', '74081', '43157', '69123', '73248', '03190', '74012', '38544', '73011',
+        '63300', '01053', '74056', '03310', '03185', '42187', '15014', '38185', '74010',
+        '42218', '26362', '38053', '01328', '63113'
+    ]
+
+    def get_close_insee(self, insee):
+        return insee
+
+class Forecast(Service, ForecastMixin):
+    url = 'https://services3.arcgis.com/o7Q3o5SkiSeZD5LK/arcgis/rest/services/ind_atmo_aura/FeatureServer/0/query'
+    use_dateutil_parser = True
+
+    def get_from_scraping(self, previous_results, date_, insee):
+        r = requests.get(f'https://www.atmo-auvergnerhonealpes.fr/monair/commune/{insee}')
+        r.raise_for_status()
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        controls = soup.find_all('div', class_='day-controls')
+        days = controls[0].find_all('a', class_='raster-control-link')
+
+        return [
+            self.getter({
+                "date": str(datetime.fromtimestamp(int(day.attrs.get('data-rasterid'))).date()),
+                "indice": int(int(day.attrs.get('data-index'))/10),
+            })
+            for day in days
+        ]
+
+
+class Episode(Service, EpisodeMixin):
+    url = 'https://services3.arcgis.com/o7Q3o5SkiSeZD5LK/arcgis/rest/services/Episodes%20de%20pollution%20pr%C3%A9vus%20ou%20constat%C3%A9s/FeatureServer/0/query'
