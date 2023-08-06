d5ab6cdb55c949eb7230cc77e3e737db6259d5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:28 +0000

    Add package pgkit/1.4.4/pgkit-1.4.4.tar.gz

---
+++ packages/pgkit/pgkit-1.4.4.tar.gz/pgkit-1.4.4/pgkit/application/pitr.py
@@ -0,0 +1,16 @@
+from pgkit.application.models import Master, Replica
+
+
+def backup(name, host, port, version, dbname, username, password, slot, replica_port, replica_delay):
+    master = Master(name, host, port, version, dbname, username, password, slot)
+    replica = Replica(master, replica_port, replica_delay)
+
+    replica.start_backup()
+
+
+def print_status(name, host, port, version, dbname, username, password, slot, replica_port):
+    master = Master(name, host, port, version, dbname, username, password, slot)
+    replica = Replica(master, replica_port, None)
+
+    replica.print_status()
+
