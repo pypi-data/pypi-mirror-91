dee4f2aa3ed767234f2372f97dceeb6eed461d12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:55 +0000

    Add package xinyujaychou-nester/0.0.1/xinyujaychou-nester-0.0.1.tar.gz

---
+++ packages/xinyujaychou-nester/xinyujaychou-nester-0.0.1.tar.gz/xinyujaychou-nester-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'xinyujaychou-nester',
+    version = '0.0.1',
+    py_modules = ['xinyujaychou-nester'],
+    author = 'chao.wang',
+    author_email = '1002674163@qq.com',
+    url = '',
+    description = '尝试发布模块'
+)
