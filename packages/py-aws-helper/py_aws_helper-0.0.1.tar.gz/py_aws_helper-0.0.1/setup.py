84022a493d15fa01f39096abf0b4fa9206e7bebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:51 +0000

    Add package py-aws-helper/0.0.1/py_aws_helper-0.0.1.tar.gz

---
+++ packages/py-aws-helper/py_aws_helper-0.0.1.tar.gz/py_aws_helper-0.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+requires = [
+    'boto3'
+]
+
+
+setup(
+    name='py_aws_helper',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='Helper for AWS services like S3 written in python...',
+    long_description=open('README.md').read(),
+    install_requires=requires,
+    url='https://github.com/1CloudHub/py_aws_helper.git',
+    author='Sripranav P',
+    author_email='sripranav@1cloudhub.com'
+)
