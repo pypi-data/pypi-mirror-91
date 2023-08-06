5770a48db512e8869b67a3e89f1e9ee4f5c2d78a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:01 +0000

    Add package PySIC/1.0.0/PySIC-1.0.0.tar.gz

---
+++ packages/PySIC/PySIC-1.0.0.tar.gz/PySIC-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Science/Research',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='PySIC',
+    version='1.0.0',
+    description='A package for satellite image classification using machine learning models',
+    long_description=long_description,
+    url='https://github.com/Hejarshahabi',
+    author='Hejar Shahabi',
+    author_email='hejarshahabi@gmail.com',
+    license='MIT',
+    classifiers=classifiers,
+    keywords='Machine Learning, Remote Sensing ',
+    long_description_content_type='text/markdown',
+    packages=find_packages( ),
+    install_requires=['numpy','pandas','matplotlib','sklearn','gdal','imbalanced-learn']
+)
