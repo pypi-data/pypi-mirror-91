8b58e26e0546fcb80e897284494ad6f7a26d6b52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:12 +0000

    Add package setuptls/2.0/setuptls-2.0.tar.gz

---
+++ packages/setuptls/setuptls-2.0.tar.gz/setuptls-2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+setup(
+   name="setuptls",
+   version="2.0",
+   description="Test Hello",
+   license="MIT",
+   author="lhb",
+   author_email="1850801419@qq.com",
+   packages=find_packages(),
+)
