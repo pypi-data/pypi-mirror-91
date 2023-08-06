f9bb1b979332d14186e8fa0438bafc8c0dcb60f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:33 +0000

    Add package static-frame/0.7.13/static-frame-0.7.13.tar.gz

---
+++ packages/static-frame/static-frame-0.7.13.tar.gz/static-frame-0.7.13/static_frame/core/store_hdf5.py
@@ -0,0 +1,149 @@
+import typing as tp
+import warnings
+
+import numpy as np
+
+from static_frame.core.doc_str import doc_inject
+from static_frame.core.frame import Frame
+from static_frame.core.store import Store
+from static_frame.core.store import store_coherent_non_write
+from static_frame.core.store import store_coherent_write
+from static_frame.core.store import StoreConfig
+from static_frame.core.store import StoreConfigMap
+from static_frame.core.store import StoreConfigMapInitializer
+from static_frame.core.type_blocks import TypeBlocks
+from static_frame.core.util import DTYPE_STR_KINDS
+
+
+class StoreHDF5(Store):
+
+    _EXT: tp.FrozenSet[str] =  frozenset(('.h5', '.hdf5'))
+
+    @store_coherent_write
+    def write(self,
+            items: tp.Iterable[tp.Tuple[tp.Hashable, Frame]],
+            *,
+            config: StoreConfigMapInitializer = None,
+            # store_filter: tp.Optional[StoreFilter] = STORE_FILTER_DEFAULT
+            ) -> None:
+
+        config_map = StoreConfigMap.from_initializer(config)
+
+        import tables
+
+        with tables.open_file(self._fp, mode='w') as file, warnings.catch_warnings():
+            # silence NaturalNameWarning: object name is not a valid Python identifier:
+            warnings.simplefilter("ignore")
+
+            for label, frame in items:
+                c = config_map[label]
+                label = config_map.default.label_encode(label)
+
+                # should all tables be under a common group?
+                field_names, dtypes = self.get_field_names_and_dtypes(
+                        frame=frame,
+                        include_index=c.include_index,
+                        include_columns=c.include_columns
+                        )
+
+                # Must set pos to have stable position
+                description = {}
+                for i, (k, v) in enumerate(zip(field_names, dtypes)):
+                    if v == object:
+                        raise RuntimeError('cannot store object dtypes in HDF5')
+                    description[k] = tables.Col.from_dtype(v, pos=i)
+
+                table = file.create_table('/', # create off root from sring
+                        name=label,
+                        description=description,
+                        expectedrows=len(frame),
+                        )
+
+                values = self._get_row_iterator(frame=frame,
+                        include_index=c.include_index)
+                table.append(tuple(values()))
+                table.flush()
+
+
+    @doc_inject(selector='constructor_frame')
+    @store_coherent_non_write
+    def read(self,
+            label: tp.Hashable,
+            *,
+            config: tp.Optional[StoreConfig] = None,
+            container_type: tp.Type[Frame] = Frame,
+            # store_filter: tp.Optional[StoreFilter] = STORE_FILTER_DEFAULT
+            ) -> Frame:
+        '''
+        Args:
+            {dtypes}
+        '''
+        import tables
+
+        if config is None:
+            config = StoreConfig() # get default
+        if config.dtypes:
+            raise NotImplementedError('using config.dtypes on HDF5 not yet supported')
+
+        label = config.label_encode(label)
+
+        index_depth = config.index_depth
+        columns_depth = config.columns_depth
+
+        index_arrays = []
+        columns_labels = []
+
+        with tables.open_file(self._fp, mode='r') as file:
+            table = file.get_node(f'/{label}')
+            colnames = table.cols._v_colnames
+
+            def blocks() -> tp.Iterator[np.ndarray]:
+                for col_idx, colname in enumerate(colnames):
+
+                    # can also do: table.read(field=colname)
+                    array = table.col(colname)
+
+                    if array.dtype.kind in DTYPE_STR_KINDS:
+                        array = array.astype(str)
+                    array.flags.writeable = False
+
+                    if col_idx < index_depth:
+                        index_arrays.append(array)
+                        continue
+                    # only store column labels for those yielded
+                    columns_labels.append(colname)
+                    yield array
+
+            if config.consolidate_blocks:
+                data = TypeBlocks.from_blocks(TypeBlocks.consolidate_blocks(blocks()))
+            else:
+                data = TypeBlocks.from_blocks(blocks())
+
+        return container_type._from_data_index_arrays_column_labels(
+                data=data,
+                index_depth=index_depth,
+                index_arrays=index_arrays,
+                columns_depth=columns_depth,
+                columns_labels=columns_labels,
+                name=tp.cast(tp.Hashable, label) # not sure why this is necessary
+                )
+
+
+    @store_coherent_non_write
+    def labels(self, *,
+            config: StoreConfigMapInitializer = None,
+            strip_ext: bool = True,
+            ) -> tp.Iterator[tp.Hashable]:
+        '''
+        Iterator of labels.
+        '''
+        import tables
+
+        config_map = StoreConfigMap.from_initializer(config)
+
+        with tables.open_file(self._fp, mode='r') as file:
+            for node in file.iter_nodes(where='/',
+                    classname=tables.Table.__name__):
+                # NOTE: this is not the complete path
+                yield config_map.default.label_decode(node.name)
+
---
+++ packages/static-frame/static-frame-0.7.13.tar.gz/static-frame-0.7.13/static_frame/test/unit/test_store_zip.py
@@ -0,0 +1,251 @@
+import unittest
+# from io import StringIO
+
+from static_frame.core.frame import Frame
+from static_frame.core.frame import FrameGO
+# from static_frame.core.bus import Bus
+# from static_frame.core.series import Series
+
+from static_frame.core.store import StoreConfig
+# from static_frame.core.store import StoreConfigMap
+
+from static_frame.core.store_zip import StoreZipTSV
+from static_frame.core.store_zip import StoreZipCSV
+from static_frame.core.store_zip import StoreZipPickle
+from static_frame.core.store_zip import StoreZipParquet
+
+from static_frame.test.test_case import TestCase
+from static_frame.test.test_case import temp_file
+
+# from static_frame.test.test_case import skip_win
+from static_frame.core.exception import ErrorInitStore
+# from static_frame.core.exception import ErrorInitStoreConfig
+
+
+class TestUnit(TestCase):
+
+    #---------------------------------------------------------------------------
+
+    def test_store_init_a(self) -> None:
+        with self.assertRaises(ErrorInitStore):
+            StoreZipTSV('test.txt') # must be a zip
+
+
+    def test_store_zip_tsv_a(self) -> None:
+
+        f1 = Frame.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+        f2 = Frame.from_dict(
+                dict(a=(1,2,3), b=(4,5,6)),
+                index=('x', 'y', 'z'),
+                name='bar')
+        f3 = Frame.from_dict(
+                dict(a=(10,20), b=(50,60)),
+                index=('p', 'q'),
+                name='baz')
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipTSV(fp)
+            st.write((f.name, f) for f in (f1, f2, f3))
+
+            labels = tuple(st.labels(strip_ext=False))
+            self.assertEqual(labels, ('foo.txt', 'bar.txt', 'baz.txt'))
+
+            config = StoreConfig(index_depth=1)
+
+            for label, frame in ((f.name, f) for f in (f1, f2, f3)):
+                frame_stored = st.read(label, config=config)
+                self.assertEqual(frame_stored.shape, frame.shape)
+                self.assertTrue((frame_stored == frame).all().all())
+                self.assertEqual(frame.to_pairs(0), frame_stored.to_pairs(0))
+
+                frame_stored_2 = st.read(label, config=config, container_type=FrameGO)
+                self.assertEqual(frame_stored_2.__class__, FrameGO)
+                self.assertEqual(frame_stored_2.shape, frame.shape)
+
+    def test_store_zip_csv_a(self) -> None:
+
+        f1 = Frame.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+        f2 = Frame.from_dict(
+                dict(a=(1,2,3), b=(4,5,6)),
+                index=('x', 'y', 'z'),
+                name='bar')
+        f3 = Frame.from_dict(
+                dict(a=(10,20), b=(50,60)),
+                index=('p', 'q'),
+                name='baz')
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipCSV(fp)
+            st.write((f.name, f) for f in (f1, f2, f3))
+
+            labels = tuple(st.labels(strip_ext=False))
+            self.assertEqual(labels, ('foo.csv', 'bar.csv', 'baz.csv'))
+
+            config = StoreConfig(index_depth=1)
+
+            for label, frame in ((f.name, f) for f in (f1, f2, f3)):
+                frame_stored = st.read(label, config=config)
+                self.assertEqual(frame_stored.shape, frame.shape)
+                self.assertTrue((frame_stored == frame).all().all())
+                self.assertEqual(frame.to_pairs(0), frame_stored.to_pairs(0))
+
+
+    def test_store_zip_csv_b(self) -> None:
+
+        f1 = Frame.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipCSV(fp)
+            st.write((f.name, f) for f in (f1,))
+
+            with self.assertRaises(ErrorInitStore):
+                # config is required
+                _ = st.read(f1.name)
+
+
+
+    def test_store_zip_pickle_a(self) -> None:
+
+        f1 = Frame.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+        f2 = Frame.from_dict(
+                dict(a=(1,2,3), b=(4,5,6)),
+                index=('x', 'y', 'z'),
+                name='bar')
+        f3 = Frame.from_dict(
+                dict(a=(10,20), b=(50,60)),
+                index=('p', 'q'),
+                name='baz')
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipPickle(fp)
+            st.write((f.name, f) for f in (f1, f2, f3))
+
+            labels = tuple(st.labels(strip_ext=False))
+            self.assertEqual(labels, ('foo.pickle', 'bar.pickle', 'baz.pickle'))
+
+            for label, frame in ((f.name, f) for f in (f1, f2, f3)):
+                frame_stored = st.read(label)
+                self.assertEqual(frame_stored.shape, frame.shape)
+                self.assertTrue((frame_stored == frame).all().all())
+                self.assertEqual(frame.to_pairs(0), frame_stored.to_pairs(0))
+
+                frame_stored_2 = st.read(label, container_type=FrameGO)
+                self.assertEqual(frame_stored_2.__class__, FrameGO)
+                self.assertEqual(frame_stored_2.shape, frame.shape)
+
+
+    def test_store_zip_pickle_b(self) -> None:
+
+        f1 = Frame.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+
+        # config = StoreConfig(index_depth=1, include_index=True)
+        # config_map = StoreConfigMap.from_config(config)
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipPickle(fp)
+            st.write(((f1.name, f1),))
+
+            frame_stored = st.read(f1.name)
+            self.assertEqual(frame_stored.shape, f1.shape)
+
+    def test_store_zip_pickle_c(self) -> None:
+
+        f1 = FrameGO.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+
+        # config = StoreConfig(index_depth=1, include_index=True)
+        # config_map = StoreConfigMap.from_config(config)
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipPickle(fp)
+
+            with self.assertRaises(NotImplementedError):
+                st.write(((f1.name, f1),))
+
+    def test_store_zip_pickle_d(self) -> None:
+
+        f1 = Frame.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+
+        config = StoreConfig(
+                index_depth=1,
+                include_index=True,
+                label_encoder=lambda x: x.upper(), #type: ignore
+                label_decoder=lambda x: x.lower(),
+                )
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipPickle(fp)
+            st.write(((f1.name, f1),), config=config)
+
+            frame_stored = st.read(f1.name, config=config)
+
+            self.assertEqual(tuple(st.labels()), ('FOO',))
+            self.assertEqual(tuple(st.labels(config=config)), ('foo',))
+
+
+    #---------------------------------------------------------------------------
+
+    def test_store_zip_parquet_a(self) -> None:
+
+        f1 = Frame.from_dict(
+                dict(a=(1,2), b=(3,4)),
+                index=('x', 'y'),
+                name='foo')
+        f2 = Frame.from_dict(
+                dict(a=(1,2,3), b=(4,5,6)),
+                index=('x', 'y', 'z'),
+                name='bar')
+        f3 = Frame.from_dict(
+                dict(a=(10,20), b=(50,60)),
+                index=('p', 'q'),
+                name='baz')
+
+        config = StoreConfig(index_depth=1, include_index=True, columns_depth=1)
+
+        with temp_file('.zip') as fp:
+
+            st = StoreZipParquet(fp)
+            st.write((f.name, f) for f in (f1, f2, f3))
+
+            f1_post = st.read('foo', config=config)
+            self.assertTrue(f1.equals(f1_post, compare_name=True, compare_class=True))
+
+            f2_post = st.read('bar', config=config)
+            self.assertTrue(f2.equals(f2_post, compare_name=True, compare_class=True))
+
+            f3_post = st.read('baz', config=config)
+            self.assertTrue(f3.equals(f3_post, compare_name=True, compare_class=True))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
