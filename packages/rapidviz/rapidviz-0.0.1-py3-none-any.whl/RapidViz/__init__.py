346c76ea2efe6003a0f739dd3df150d4a2965224
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:48 +0000

    Add package rapidviz/0.0.1/rapidviz-0.0.1-py3-none-any.whl

---
+++ packages/rapidviz/rapidviz-0.0.1-py3-none-any.whl/RapidViz/__init__.py
@@ -0,0 +1,10 @@
+"""RapidPlot is a Package containing random classes for fast Exploratory Data Analysis"""
+
+from .rapidviz import(
+    Plotter,
+    CustomPalette,
+    Encoder,
+    PieCompositionPlots,
+    CatComposition,
+    OutlierAnalysis,
+)
