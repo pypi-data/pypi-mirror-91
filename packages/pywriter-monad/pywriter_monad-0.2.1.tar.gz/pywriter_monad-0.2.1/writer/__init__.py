1a8db3ab68cd5265ef409af5bce123d28a1e8822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:11 +0000

    Add package pywriter-monad/0.2.1/pywriter_monad-0.2.1.tar.gz

---
+++ packages/pywriter-monad/pywriter_monad-0.2.1.tar.gz/pywriter_monad-0.2.1/writer/__init__.py
@@ -0,0 +1,5 @@
+from .writer import Writer, bind, map, add, compose
+from .dictwriter import DictWriter
+from .VERSION import __version__
+
+__all__ = ["Writer", "bind", "map", "add", "compose", "DictWriter", "__version__"]
