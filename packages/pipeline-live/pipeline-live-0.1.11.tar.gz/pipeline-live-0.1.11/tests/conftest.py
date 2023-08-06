03f73a7b9abc782d277786cea0ac7d6a0a675d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:28 +0000

    Add package pipeline-live/0.1.11/pipeline-live-0.1.11.tar.gz

---
+++ packages/pipeline-live/pipeline-live-0.1.11.tar.gz/pipeline-live-0.1.11/tests/conftest.py
@@ -0,0 +1,39 @@
+import pytest
+import tempfile
+from unittest.mock import patch
+
+from pipeline_live.data.sources import polygon as sources_polygon
+from pipeline_live.data.sources import iex as sources_iex
+from pipeline_live.data.sources import alpaca as sources_alpaca
+
+
+@pytest.fixture
+def tradeapi():
+    with patch.object(sources_polygon, 'tradeapi') as tradeapi:
+        yield tradeapi
+
+
+@pytest.fixture
+def alpaca_tradeapi():
+    with patch.object(sources_alpaca, 'tradeapi') as tradeapi:
+        yield tradeapi
+
+
+@pytest.fixture
+def refdata():
+    with patch.object(sources_iex, 'refdata') as refdata:
+        yield refdata
+
+
+@pytest.fixture
+def stocks():
+    with patch.object(sources_iex, 'Stock') as stocks:
+        yield stocks
+
+
+@pytest.fixture
+def data_path():
+    with patch('zipline.utils.paths.data_path') as data_path:
+        with tempfile.TemporaryDirectory() as t:
+            data_path.return_value = t
+            yield data_path
