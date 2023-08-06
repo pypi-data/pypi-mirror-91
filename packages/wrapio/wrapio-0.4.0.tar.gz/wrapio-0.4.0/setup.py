c07dbd496d1b9e1552fefa6d34cf8476f16fbd00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:03 +0000

    Add package wrapio/0.4.0/wrapio-0.4.0.tar.gz

---
+++ packages/wrapio/wrapio-0.4.0.tar.gz/wrapio-0.4.0/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+import os
+
+with open('README.rst') as file:
+    readme = file.read()
+
+name = 'wrapio'
+
+version = '0.4.0'
+
+author = 'Exahilosys'
+
+url = f'https://github.com/{author}/{name}'
+
+setuptools.setup(
+    name = name,
+    python_requires = '>=3.5',
+    version = version,
+    author = author,
+    url = url,
+    packages = setuptools.find_packages(),
+    license = 'MIT',
+    description = 'Handling event-based streams.',
+    long_description = readme,
+    extras_require = {
+        'docs': [
+            'sphinx'
+        ]
+    }
+)
