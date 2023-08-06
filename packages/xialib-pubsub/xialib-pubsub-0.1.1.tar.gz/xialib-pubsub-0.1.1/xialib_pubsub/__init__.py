d11e98e585445a764d8a9003eceef1f7d04cf0d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:09 +0000

    Add package xialib-pubsub/0.1.1/xialib-pubsub-0.1.1.tar.gz

---
+++ packages/xialib-pubsub/xialib-pubsub-0.1.1.tar.gz/xialib-pubsub-0.1.1/xialib_pubsub/__init__.py
@@ -0,0 +1,6 @@
+from xialib_pubsub.pubsub_publisher import PubsubPublisher
+from xialib_pubsub.pubsub_subscriber import PubsubSubscriber
+
+__all__ = ['PubsubPublisher', 'PubsubSubscriber']
+
+__version__ = "0.1.1"
