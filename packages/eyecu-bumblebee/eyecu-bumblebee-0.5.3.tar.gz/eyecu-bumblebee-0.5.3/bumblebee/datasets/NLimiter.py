24c8aa2b64b7be2943309430ea26789bfc84529f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:28 +0000

    Add package eyecu-bumblebee/0.5.3/eyecu-bumblebee-0.5.3.tar.gz

---
+++ packages/eyecu-bumblebee/eyecu-bumblebee-0.5.3.tar.gz/eyecu-bumblebee-0.5.3/bumblebee/datasets/NLimiter.py
@@ -0,0 +1,34 @@
+from ..bases.Dataset import Dataset
+
+
+class NLimiter(Dataset):
+
+    def __init__(self, data_source, N: int):
+        self.src = data_source
+        self.N = N
+        self.rem = N
+
+    def __next__(self):
+
+        if self.rem == 0:
+            raise StopIteration
+        else:
+            data = self.src.read()
+            self.rem -= 1
+            return data
+
+    def __iter__(self):
+        self.rem = self.N
+        return self
+
+    def get_props(self):
+        return self.src.get_props()
+
+    def __len__(self):
+        return self.N
+
+    def __getitem__(self, item):
+        return self.__next__()
+
+    def read(self):
+        return self.__next__()
