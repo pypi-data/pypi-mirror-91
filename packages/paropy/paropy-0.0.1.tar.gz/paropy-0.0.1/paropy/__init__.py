93a119a9cbd16b9ddab587862a5915e6dce49c2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:21 +0000

    Add package paropy/0.0.1/paropy-0.0.1.tar.gz

---
+++ packages/paropy/paropy-0.0.1.tar.gz/paropy-0.0.1/paropy/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 14 14:45:52 2021
+
+@author: wongj
+
+The parody_py package contains the routines needed to postprocess data from
+PARODY-JA4.3 dynamo simulations.
+
+Modules
+_______
+data_utils.py
+    Functions for data input/output
+plot_utils.py
+    Functions for plotting results
+routines.py
+    Functions for postprocessing data
+"""
