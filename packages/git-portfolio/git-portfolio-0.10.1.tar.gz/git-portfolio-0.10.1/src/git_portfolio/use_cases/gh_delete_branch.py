4fcda43b31ce27fa5f0035a1a0a68e7c3719cae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:14 +0000

    Add package git-portfolio/0.10.1/git-portfolio-0.10.1.tar.gz

---
+++ packages/git-portfolio/git-portfolio-0.10.1.tar.gz/git-portfolio-0.10.1/src/git_portfolio/use_cases/gh_delete_branch.py
@@ -0,0 +1,31 @@
+"""Delete branch on Github use case."""
+from typing import Union
+
+import git_portfolio.config_manager as cm
+import git_portfolio.github_service as ghs
+import git_portfolio.responses as res
+
+
+class GhDeleteBranchUseCase:
+    """Github delete branch use case."""
+
+    def __init__(
+        self, config_manager: cm.ConfigManager, github_service: ghs.GithubService
+    ) -> None:
+        """Initializer."""
+        self.config_manager = config_manager
+        self.github_service = github_service
+
+    def execute(
+        self, branch: str, github_repo: str = ""
+    ) -> Union[res.ResponseFailure, res.ResponseSuccess]:
+        """Delete branches."""
+        if github_repo:
+            output = self.github_service.delete_branch_from_repo(github_repo, branch)
+        else:
+            output = ""
+            for github_repo in self.config_manager.config.github_selected_repos:
+                output += self.github_service.delete_branch_from_repo(
+                    github_repo, branch
+                )
+        return res.ResponseSuccess(output)
