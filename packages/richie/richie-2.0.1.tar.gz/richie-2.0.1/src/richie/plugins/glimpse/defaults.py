5de86667d4b463607e8cf3e1c7e312ce640a6ba6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:12:49 +0000

    Add package richie/2.0.1/richie-2.0.1.tar.gz

---
+++ packages/richie/richie-2.0.1.tar.gz/richie-2.0.1/src/richie/plugins/glimpse/defaults.py
@@ -0,0 +1,26 @@
+"""
+Glimpse plugin default settings
+"""
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
+
+CARD_SQUARE, ROW_HALF, ROW_FULL, QUOTE, BADGE = (
+    "card_square",
+    "row_half",
+    "row_full",
+    "quote",
+    "badge",
+)
+
+GLIMPSE_VARIANTS = getattr(
+    settings,
+    "RICHIE_GLIMPSE_VARIANTS",
+    [
+        (None, _("Inherit")),
+        (CARD_SQUARE, _("Square card")),
+        (ROW_HALF, _("Half row")),
+        (ROW_FULL, _("Full row")),
+        (QUOTE, _("Quote")),
+        (BADGE, _("Badge")),
+    ],
+)
