8b7ed1a5a77b63232455cf0c14e3f327f156c7a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:27 +0000

    Add package odoo12-addon-somconnexio/12.0.0.0.0rc62/odoo12_addon_somconnexio-12.0.0.0.0rc62-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-somconnexio/odoo12_addon_somconnexio-12.0.0.0.0rc62-py2.py3-none-any.whl/odoo/addons/somconnexio/models/res_company.py
@@ -0,0 +1,18 @@
+from odoo import fields, models
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    # TODO: Remove this code when a release of EasyMyCoop with:
+    # https://github.com/coopiteasy/vertical-cooperative/pull/146
+    send_certificate_email = fields.Boolean(
+        string="Send certificate email",
+        default=True
+    )
+    # TODO: Remove this code when a release of EasyMyCoop with:
+    # https://github.com/coopiteasy/vertical-cooperative/pull/146
+    send_confirmation_email = fields.Boolean(
+        string="Send confirmation email",
+        default=True
+    )
