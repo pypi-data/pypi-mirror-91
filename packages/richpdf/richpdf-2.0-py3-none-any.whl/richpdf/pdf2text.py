03b28033d57df9b3da1ec0570a8ca1eb255bebd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:30 +0000

    Add package richpdf/2.0/richpdf-2.0-py3-none-any.whl

---
+++ packages/richpdf/richpdf-2.0-py3-none-any.whl/richpdf/pdf2text.py
@@ -0,0 +1,15 @@
+""" This module provides function to convert PDF to text """
+
+
+def convert(path):
+    """
+    Convert the given PDF to Text
+
+    Parameters:
+    path (str): the path to the PDF file
+
+    Returns:
+    str:  the content of the PDF file
+
+    """
+    print("pdf2text.py")
