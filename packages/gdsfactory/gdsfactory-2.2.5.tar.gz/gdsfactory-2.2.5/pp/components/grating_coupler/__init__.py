1881cf836fe1bde3b916d9f8f26b42e7cdd8df5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:08 +0000

    Add package gdsfactory/2.2.5/gdsfactory-2.2.5.tar.gz

---
+++ packages/gdsfactory/gdsfactory-2.2.5.tar.gz/gdsfactory-2.2.5/pp/components/grating_coupler/__init__.py
@@ -0,0 +1,3 @@
+from pp.components.grating_coupler.grating_coupler_decorator import grating_coupler
+
+__all__ = ["grating_coupler"]
---
+++ packages/gdsfactory/gdsfactory-2.2.5.tar.gz/gdsfactory-2.2.5/pp/components/test_netlist.py
@@ -0,0 +1,6 @@
+import pp
+
+if __name__ == "__main__":
+    c = pp.c.mzi_lattice()
+    n = c.get_netlist()
+    print(n)
