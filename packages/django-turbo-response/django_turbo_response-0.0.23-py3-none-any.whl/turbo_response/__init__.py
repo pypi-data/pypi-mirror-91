d5a8943f63d464faa274d49e47829a6f8c2b5da9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:24 +0000

    Add package django-turbo-response/0.0.23/django_turbo_response-0.0.23-py3-none-any.whl

---
+++ packages/django-turbo-response/django_turbo_response-0.0.23-py3-none-any.whl/turbo_response/__init__.py
@@ -0,0 +1,34 @@
+# Local
+from .constants import Action
+from .redirects import HttpResponseSeeOther, redirect_303
+from .renderers import render_turbo_frame, render_turbo_stream
+from .response import (
+    TemplateFormResponse,
+    TurboFrameResponse,
+    TurboFrameTemplateResponse,
+    TurboStreamIterableResponse,
+    TurboStreamResponse,
+    TurboStreamStreamingResponse,
+    TurboStreamTemplateResponse,
+)
+from .shortcuts import TurboFrame, TurboStream
+from .template import render_turbo_frame_template, render_turbo_stream_template
+
+__all__ = [
+    "Action",
+    "HttpResponseSeeOther",
+    "TemplateFormResponse",
+    "TurboFrame",
+    "TurboFrameResponse",
+    "TurboFrameTemplateResponse",
+    "TurboStream",
+    "TurboStreamIterableResponse",
+    "TurboStreamResponse",
+    "TurboStreamStreamingResponse",
+    "TurboStreamTemplateResponse",
+    "redirect_303",
+    "render_turbo_frame",
+    "render_turbo_frame_template",
+    "render_turbo_stream",
+    "render_turbo_stream_template",
+]
