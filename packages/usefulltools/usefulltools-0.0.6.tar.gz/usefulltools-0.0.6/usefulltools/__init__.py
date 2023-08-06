05d81374fe1f69cd67e9978585c72e2b82fb3f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:34 +0000

    Add package usefulltools/0.0.6/usefulltools-0.0.6.tar.gz

---
+++ packages/usefulltools/usefulltools-0.0.6.tar.gz/usefulltools-0.0.6/usefulltools/__init__.py
@@ -0,0 +1,47 @@
+import os
+import requests
+import linecache
+
+class File:
+  def directory(directory):
+    os.chdir(directory)
+
+  def ls(directory=''):
+    os.system(f'ls {directory}')
+
+  def run(file,language='python'):
+    os.system(f'{language} {file}')
+
+
+  def file(filename,content='',mode='a'):
+    with open(filename,mode) as f:
+      f.write(content)
+
+  def remove(filename):
+    os.system(f'rm {filename}')
+
+  def download_file(file_url,file_name,file_exten):
+    types = ['.png','.jpg','.gif']
+    x = requests.get(file_url, allow_redirects=True)
+    if file_exten in types:
+      with open(file_name+file_exten,'wb') as f:
+        f.write(x.content)
+        return True
+    else:
+      try:
+        with open(file_name+file_exten,'w') as f:
+          f.write(x.text)
+          return True
+      except TypeError:
+        os.remove(file_name+file_exten)
+        return False
+  def get_line(self,file,line_num):
+    return linecache.getline(file,line_num)
+
+class Math:
+  def fib(n):
+    list = []
+    a, b = 0, 1
+    while a < n:
+      list.append(a)
+      a, b = b, a+b
