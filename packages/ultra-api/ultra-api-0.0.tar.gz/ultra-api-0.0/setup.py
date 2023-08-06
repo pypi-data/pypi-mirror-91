c4f284c95afca1565fac4cde833e00867e4fb675
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:30:04 +0000

    Add package ultra-api/0.0/ultra-api-0.0.tar.gz

---
+++ packages/ultra-api/ultra-api-0.0.tar.gz/ultra-api-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ultra-api',
+      version='0.0',
+      description='ultra-api',
+      packages=['ultra-api'],
+      author_email='riclud@mail.ru',
+      zip_safe=False)
