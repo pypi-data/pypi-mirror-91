cc797b394571b4e9f930fc5f67cf3a2c799bd776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:02 +0000

    Add package chosco/0.1.2/chosco-0.1.2-py3-none-any.whl

---
+++ packages/chosco/chosco-0.1.2-py3-none-any.whl/chosco/annotation/domain/annotation_task_status.py
@@ -0,0 +1,13 @@
+class AnnotationTaskStatus:
+    def __init__(
+        self,
+        number_of_annotations: int,
+        total_annotations: int,
+        current_task_id: str = None,
+    ):
+        self.number_of_annotations = number_of_annotations
+        self.total_annotations = total_annotations
+        self.current_task_id = current_task_id
+
+    def get_progress(self):
+        return f"{self.number_of_annotations+1} of {self.total_annotations}"
