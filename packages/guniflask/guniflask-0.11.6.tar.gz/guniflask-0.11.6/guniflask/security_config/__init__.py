3f9c3b2aa33c20a2ae577fb6fadea3fade5b09a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:30 +0000

    Add package guniflask/0.11.6/guniflask-0.11.6.tar.gz

---
+++ packages/guniflask/guniflask-0.11.6.tar.gz/guniflask-0.11.6/guniflask/security_config/__init__.py
@@ -0,0 +1,7 @@
+from .authentication_manager_builder import AuthenticationManagerBuilder
+from .dao_authentication_configurer import DaoAuthenticationConfigurer
+from .http_security import HttpSecurity
+from .http_security_builder import HttpSecurityBuilder
+from .security_configurer import SecurityConfigurer
+from .web_security import WebSecurity
+from .web_security_configurer import WebSecurityConfigurer
---
+++ packages/guniflask/guniflask-0.11.6.tar.gz/guniflask-0.11.6/guniflask/security_config/security_configurer.py
@@ -0,0 +1,20 @@
+from guniflask.security_config.security_builder import SecurityBuilder
+
+
+class SecurityConfigurer:
+    def __init__(self):
+        self._security_builder: SecurityBuilder = None
+
+    def init(self, builder: SecurityBuilder):
+        pass
+
+    def configure(self, builder: SecurityBuilder):
+        pass
+
+    @property
+    def builder(self):
+        return self._security_builder
+
+    @builder.setter
+    def builder(self, value: SecurityBuilder):
+        self._security_builder = value
