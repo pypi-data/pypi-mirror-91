3b30d70675b08a188786e6109081c88debfd1373
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:22 +0000

    Add package opennem/3.6.0a2/opennem-3.6.0a2.tar.gz

---
+++ packages/opennem/opennem-3.6.0a2.tar.gz/opennem-3.6.0a2/opennem/db/tasks.py
@@ -0,0 +1,15 @@
+from opennem.db import get_database_engine
+
+
+def refresh_views() -> None:
+    """Refresh material views"""
+    __query = "REFRESH MATERIALIZED VIEW mv_facility_all with data"
+
+    engine = get_database_engine()
+
+    with engine.connect() as c:
+        c.execute(__query)
+
+
+if __name__ == "__main__":
+    refresh_views()
