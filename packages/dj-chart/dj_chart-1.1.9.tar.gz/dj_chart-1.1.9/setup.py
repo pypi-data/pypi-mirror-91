54e29ad5417144edc788f906f0ac786349f2a77b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:45 +0000

    Add package dj-chart/1.1.9/dj_chart-1.1.9.tar.gz

---
+++ packages/dj-chart/dj_chart-1.1.9.tar.gz/dj_chart-1.1.9/setup.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = '1.1.9'
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel
+        print("Wheel version: ", wheel.__version__)
+    except ImportError:
+        print('Wheel library missing. Please run "pip install wheel"')
+        sys.exit()
+    os.system('python setup.py sdist upload')
+    os.system('python setup.py bdist_wheel upload')
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on git:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+setup(
+    name='dj_chart',
+    version=version,
+    description="""Create Charts with the awesome Chart.js Library""",
+    long_description=readme + '\n\n' + history,
+    author='Robert Dollinger',
+    author_email='robert.d@systent.it',
+    url='https://gitlab.com/systent/dj_chart',
+    packages=[
+        'dj_chart',
+    ],
+    include_package_data=True,
+    install_requires=['django>=1.10',
+                      'python-dateutil>=2.6'
+                      ],
+    license="BSD",
+    zip_safe=False,
+    keywords='dj_chart',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Framework :: Django :: 1.10',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
