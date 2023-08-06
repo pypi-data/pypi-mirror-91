aeb4693b079ece30a373c3e0f33f148bd67841a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:55 +0000

    Add package contract-deploy-tools/0.9.0/contract-deploy-tools-0.9.0.tar.gz

---
+++ packages/contract-deploy-tools/contract-deploy-tools-0.9.0.tar.gz/contract-deploy-tools-0.9.0/testcontracts/ManyArgumentsContract.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.8;
+
+contract ManyArgumentsContract {
+
+    uint public a;
+    int public b;
+    int32 public c;
+    bool public d;
+    address public e;
+    bytes public f;
+
+    constructor(uint _a, int _b, int32 _c, bool _d, address _e, bytes memory _f) public {
+        a = _a;
+        b = _b;
+        c = _c;
+        d = _d;
+        e = _e;
+        f = _f;
+    }
+}
---
+++ packages/contract-deploy-tools/contract-deploy-tools-0.9.0.tar.gz/contract-deploy-tools-0.9.0/testcontracts/TestContract.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.8;
+
+import './subfolder/OtherContract.sol';
+
+contract TestContract {
+
+    uint public state;
+
+    constructor(uint val) public {
+        state = val;
+    }
+
+    function set(uint a) public {
+        state = a;
+    }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function pay() payable public {}
+
+    function testFunction(uint a) public view returns (uint) {
+        return a + state;
+    }
+
+    function duplicatedDifferentArgumentLength(uint a) public {
+        state = a;
+    }
+
+    function duplicatedDifferentArgumentLength(uint a, uint b) public {
+        state = a + b;
+    }
+
+    function functionWithArrayArgument(address[] memory a) public {
+    }
+
+    function duplicatedSameArgumentLength(uint a) public pure returns (uint) {
+        return a;
+    }
+
+    function duplicatedSameArgumentLength(bytes memory a) public pure returns (bytes memory) {
+        return a;
+    }
+
+    function failingFunction() public pure returns (bytes memory) {
+        require(false, "This function can only fail.");
+    }
+}
