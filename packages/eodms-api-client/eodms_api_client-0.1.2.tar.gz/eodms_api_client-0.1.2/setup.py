73a2c6daa67816ce13e8efdbb8c813b52a2d4b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:18 +0000

    Add package eodms-api-client/0.1.2/eodms_api_client-0.1.2.tar.gz

---
+++ packages/eodms-api-client/eodms_api_client-0.1.2.tar.gz/eodms_api_client-0.1.2/setup.py
@@ -0,0 +1,41 @@
+import re
+from setuptools import find_packages, setup
+
+with open('eodms_api_client/__init__.py') as f:
+    init_txt = f.read()
+name = re.search(r"__name__ = \s*'([\S.*]+)'", init_txt).group(1)
+version = re.search(r"__version__ = \s*'([\d.*]+)'", init_txt).group(1)
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+setup(
+    name=name,
+    version=version,
+    author="Mike Brady",
+    author_email="mike.brady@canada.ca",
+    license="MIT",
+    description="Tool for querying and submitting image orders to Natural Resources Canada\'s Earth Observation Data Management System (EODMS)",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    packages=find_packages(exclude=['docs']),
+    install_requires=requirements,
+    url="https://eodms-api-client.readthedocs.io/en/latest",
+    project_urls={
+        "Source": "https://github.com/m9brady/eodms-api-client",
+        "Tracker": "https://github.com/m9brady/eodms-api-client/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='~=3.6',
+    entry_points='''
+    [console_scripts]
+    eodms=eodms_api_client.cli:cli
+    '''
+)
