6357a14eb56510d421d358233dde5d43673da114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:09 +0000

    Add package bmlx/2.5.6/bmlx-2.5.6.tar.gz

---
+++ packages/bmlx/bmlx-2.5.6.tar.gz/bmlx-2.5.6/bmlx/context_test.py
@@ -0,0 +1,33 @@
+import unittest
+from bmlx.context import BmlxContext
+from bmlx import config as bmlx_config
+
+
+class BmlxContextTest(unittest.TestCase):
+    def testRewriteCustomParameters(self):
+        ctx = BmlxContext()
+
+        ctx.custom_parameters = bmlx_config.Configuration()
+        ctx.custom_parameters.set_args(
+            {"model.learning_rate.decay_type": "sin", "model.keep_hour": 200}
+        )
+        params = {
+            "bmlx.parameters.model.learning_rate.decay_type": "cosine",
+            "bmlx.parameters.sample-selector.max-sample-hours": 100,
+            "model.keep_hour": 10,
+        }
+        ctx.rewrite_custom_parameters(params)
+        self.assertTrue(
+            ctx.custom_parameters["model"]["learning_rate"][
+                "decay_type"
+            ].exists()
+        )
+        self.assertEqual(
+            ctx.custom_parameters["model"]["learning_rate"][
+                "decay_type"
+            ].as_str(),
+            "cosine",
+        )
+        self.assertEqual(
+            ctx.custom_parameters["model"]["keep_hour"].as_number(), 10
+        )
