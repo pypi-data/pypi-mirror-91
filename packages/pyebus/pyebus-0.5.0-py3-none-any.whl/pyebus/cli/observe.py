68fe89254ac0d8302f956d5fb9a070b687de7e3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:55 +0000

    Add package pyebus/0.5.0/pyebus-0.5.0-py3-none-any.whl

---
+++ packages/pyebus/pyebus-0.5.0-py3-none-any.whl/pyebus/cli/observe.py
@@ -0,0 +1,35 @@
+"""Observe."""
+from .common import (
+    add_msgdef_args,
+    add_patterns_arg,
+    add_read_args,
+    create_ebus,
+    disable_stdout_buffering,
+    load_msgdefs,
+    print_msg,
+)
+
+
+def parse_args(subparsers):
+    """Parse Arguments."""
+    parser = subparsers.add_parser(
+        "observe",
+        help=(
+            "Read all known messages once and continue listening so that ALL EBUS values are available, "
+            "decode every message and print."
+        ),
+    )
+    add_msgdef_args(parser)
+    add_read_args(parser)
+    add_patterns_arg(parser, opt=True)
+    parser.set_defaults(main=_main)
+
+
+async def _main(args):
+    disable_stdout_buffering()
+    ebus = create_ebus(args)
+    await load_msgdefs(ebus, args)
+    msgdefs = ebus.msgdefs.resolve(args.patterns.split(";"))
+    print(f"Observing {msgdefs.summary()}")
+    async for msg in ebus.observe(msgdefs=msgdefs, defaultprio=9, setprio=args.prio, ttl=args.ttl):
+        print_msg(msg)
