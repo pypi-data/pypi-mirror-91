7befecae2c426f6dc9b4ac1c8ed81b9c4c9adbb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:27 +0000

    Add package fsds2021/0.1.3/fsds2021-0.1.3-py2.py3-none-any.whl

---
+++ packages/fsds2021/fsds2021-0.1.3-py2.py3-none-any.whl/fsds2021/__init__.py
@@ -0,0 +1,11 @@
+"""Top-level package for fsds2021."""
+
+__author__ = """James Irving"""
+__email__ = 'james.irving.phd@gmail.com'
+__version__ = '0.1.3'
+
+import fsds2021.reference
+import fsds2021.stats
+import fsds2021.utils 
+
+import IPython.display as display 
