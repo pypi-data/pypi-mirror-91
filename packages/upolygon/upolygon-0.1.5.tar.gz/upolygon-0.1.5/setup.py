d6c404acd069dbf0cd5db785d435c6010f2e86a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:45 +0000

    Add package upolygon/0.1.5/upolygon-0.1.5.tar.gz

---
+++ packages/upolygon/upolygon-0.1.5.tar.gz/upolygon-0.1.5/setup.py
@@ -0,0 +1,36 @@
+import setuptools
+import os
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+USE_CYTHON = os.getenv("USE_CYTHON") in ["TRUE", "true"]
+ext = ".pyx" if USE_CYTHON else ".c"
+
+extensions = [
+    setuptools.Extension("draw_polygon", ["upolygon/draw_polygon" + ext], extra_compile_args=["-O3", "-Wall"]),
+    setuptools.Extension("find_contours", ["upolygon/find_contours" + ext], extra_compile_args=["-O3", "-Wall"]),
+    setuptools.Extension("simplify_polygon", ["upolygon/simplify_polygon" + ext], extra_compile_args=["-O3", "-Wall"]),
+    setuptools.Extension("rle", ["upolygon/rle" + ext], extra_compile_args=["-O3", "-Wall"]),
+]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+
+    extensions = cythonize(extensions)
+
+setuptools.setup(
+    name="upolygon",
+    version="0.1.5",
+    author="V7",
+    author_email="simon@v7labs.com",
+    description="Collection of fast polygon operations for DL",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/v7labs/upolygon",
+    ext_modules=extensions,
+    install_requires=[],
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3", "License :: OSI Approved :: MIT License",],
+    python_requires=">=3.6",
+)
