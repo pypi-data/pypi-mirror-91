0dc5e50ca5e5c7a0cc9f5abe02af0a851c90a548
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:26 +0000

    Add package deepdrr/1.0.2/deepdrr-1.0.2.tar.gz

---
+++ packages/deepdrr/deepdrr-1.0.2.tar.gz/deepdrr-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='deepdrr',
+    version='1.0.2',
+    author='Benjamin D. Killeen',
+    author_email='killeen@jhu.edu',
+    description='A Catalyst for Machine Learning in Fluoroscopy-guided Procedures.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy',
+        'torch',
+        'torchvision',
+        'nibabel',
+        'scikit-image',
+        'pydicom',
+        'pycuda',
+        'scipy',
+    ],
+    include_package_data=True,
+    python_requires='>=3.9',
+)
