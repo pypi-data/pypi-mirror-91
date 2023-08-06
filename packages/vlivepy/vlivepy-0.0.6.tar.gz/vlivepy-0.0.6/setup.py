52917424064694bdb41bbb78606f89c1621db290
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:16 +0000

    Add package vlivepy/0.0.6/vlivepy-0.0.6.tar.gz

---
+++ packages/vlivepy/vlivepy-0.0.6.tar.gz/vlivepy-0.0.6/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='vlivepy',
+    version='0.0.6',
+    packages=['vlivepy'],
+    url='https://github.com/box-archived/vlive-py',
+    license='MIT License',
+    author='box-archived',
+    author_email='box.cassette@gmail.com',
+    description='vlivepy is reverse-engineered Python-based API of VLIVE(vlive.tv)',
+    install_requires=[
+        'requests>=2.*',
+        'reqWrapper>=0.2.*',
+        'beautifulsoup4>=4.*'
+    ]
+)
