6335b0463fcea116e0d5c350744f37c2ecc05f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:45 +0000

    Add package sels8s/0.0.3/sels8s-0.0.3.tar.gz

---
+++ packages/sels8s/sels8s-0.0.3.tar.gz/sels8s-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from os import path
+import sys
+
+if sys.version_info < (3, 6):
+    sys.exit('Sorry, Python < 3.6 is not supported')
+
+here = path.abspath(path.dirname(__file__))
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sels8s",
+    version="0.0.3",
+    author_email="sls@selectel.ru",
+    description="SDK for python serverless functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/selectel/serverless-python",
+    packages=["sels8s"]
+    )
