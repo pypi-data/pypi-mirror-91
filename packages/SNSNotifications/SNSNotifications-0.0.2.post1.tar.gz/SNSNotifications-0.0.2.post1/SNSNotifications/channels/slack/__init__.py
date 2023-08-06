47085e24344f15dfa7b575c1ae16552a38a6a416
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:13 +0000

    Add package SNSNotifications/0.0.2.post1/SNSNotifications-0.0.2.post1.tar.gz

---
+++ packages/SNSNotifications/SNSNotifications-0.0.2.post1.tar.gz/SNSNotifications-0.0.2.post1/SNSNotifications/channels/slack/__init__.py
@@ -0,0 +1,36 @@
+from slack_sdk import WebClient
+
+from SNSNotifications.types import NotificationChannel, Notification
+from .message import SlackMessage
+
+__all__ = ['SlackClient', 'SlackMessage']
+
+
+class SlackClient(NotificationChannel):
+
+    def __init__(self, token, channel="#random", **kwargs):
+        self.default_channel = channel
+        self.client = WebClient(token)
+
+    def send(self, notification: Notification):
+        if not self.can_send(notification):
+            return
+        msg: SlackMessage = getattr(notification, "to_{}".format(self.channel_identify()))()
+        channel = msg.channel or self.default_channel
+        payload = {}
+
+        if msg.has_blocks():
+            payload['blocks'] = msg.blocks
+            if msg.has_mentions:
+                payload['blocks'] = msg.with_mentions(msg.blocks)
+        payload['text'] = msg.text
+        if msg.has_mentions:
+            payload['text'] = msg.with_mentions(msg.text)
+
+        if msg.has_link:
+            payload['link_names'] = 1
+
+        self.client.chat_postMessage(channel=channel, **payload)
+
+    def channel_identify(self) -> str:
+        return "slack"
---
+++ packages/SNSNotifications/SNSNotifications-0.0.2.post1.tar.gz/SNSNotifications-0.0.2.post1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", "r") as fh:
+    requirements = fh.readlines()
+    requirements = map(lambda x: x.strip(), requirements)
+    requirements = filter(lambda x: len(x) > 0, requirements)
+    requirements = list(requirements)
+
+print(requirements)
+
+setuptools.setup(name="SNSNotifications",
+                 version="0.0.2-1",
+                 author="TrinhQuan",
+                 author_email="quantv@aimesoft.com",
+                 description="This module use for abstract work to send notification a cross sns provider",
+                 long_description=long_description,
+                 url='https://gitlab.com/aimesoft/libraries/snsnotifications.git',
+                 download_url='https://gitlab.com/aimesoft/libraries/snsnotifications/-/archive/master/snsnotifications-master.zip',
+                 install_requires=requirements,
+                 long_description_content_type="text/plain",
+                 packages=setuptools.find_packages(),
+                 data_files=['requirements.txt'],
+                 classifiers=["Programming Language :: Python :: 3.6", "Programming Language :: Python :: 3.7",
+                              "License :: OSI Approved :: MIT License"], )
