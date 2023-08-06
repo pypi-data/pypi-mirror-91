db73f27cc7316490297d56e6a2d6469a8b68019c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:09 +0000

    Add package villa-digiofunction-pinpinteamakorn/0.0.5/villa_digiofunction_pinpinteamakorn-0.0.5-py3-none-any.whl

---
+++ packages/villa-digiofunction-pinpinteamakorn/villa_digiofunction_pinpinteamakorn-0.0.5-py3-none-any.whl/digioFunction/__version__.py
@@ -0,0 +1,14 @@
+"""
+__version__.py
+~~~~~~~~~~~~~~
+
+Information about the current version of the py-package-template package.
+"""
+
+__title__ = 'villa-digiofunction-pinpinteamakorn'
+__description__ = 'interface for digio adapter'
+__version__ = '0.0.5'
+__author__ = 'Pin Pin Tea-makorn'
+__author_email__ = 'teamakorn@pinnaree.com'
+__license__ = 'Apache 2.0'
+__url__ = 'https://github.com/thanakijwanavit/villaWallet'
