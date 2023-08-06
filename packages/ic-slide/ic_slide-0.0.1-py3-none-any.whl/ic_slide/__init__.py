38f0e2e756dd51ba48e7cfdd2af7b77c8ddc79b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:25 +0000

    Add package ic-slide/0.0.1/ic_slide-0.0.1-py3-none-any.whl

---
+++ packages/ic-slide/ic_slide-0.0.1-py3-none-any.whl/ic_slide/__init__.py
@@ -0,0 +1,9 @@
+from .config import configure
+
+from .slide_source import open_slide, enumerate_tiles,\
+    enumerate_tiles_auto_scaling,\
+    enumrate_content_tiles,\
+    enumrate_content_tiles_auto_scaling
+
+from .slide_index import enumerate_slide_indices,\
+    enumrate_distinct_slide_ids
