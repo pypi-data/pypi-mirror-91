d6c07b4d7ab4881c6e9ab51a7639f32d3a7e3c04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:21 +0000

    Add package webstack-django-jwt-auth/1.4.0/webstack_django_jwt_auth-1.4.0-py2.py3-none-any.whl

---
+++ packages/webstack-django-jwt-auth/webstack_django_jwt_auth-1.4.0-py2.py3-none-any.whl/jwt_auth/forms.py
@@ -0,0 +1,90 @@
+from datetime import datetime, timedelta
+
+from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.signals import user_logged_in
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import ugettext as _
+from jwt_auth import settings as jwt_auth_settings
+from jwt_auth.core import User
+from jwt_auth.utils import jwt_get_user_id_from_payload_handler
+
+
+class JSONWebTokenForm(forms.Form):
+    password = forms.CharField()
+
+    def __init__(self, *args, **kwargs):
+        super(JSONWebTokenForm, self).__init__(*args, **kwargs)
+
+        # Dynamically add the USERNAME_FIELD to self.fields.
+        self.fields[self.username_field] = forms.CharField()
+
+    @property
+    def username_field(self):
+        try:
+            return User.USERNAME_FIELD
+        except AttributeError:
+            return "username"
+
+    def clean(self):
+        cleaned_data = super(JSONWebTokenForm, self).clean()
+        credentials = {
+            self.username_field: cleaned_data.get(self.username_field),
+            "password": cleaned_data.get("password"),
+        }
+
+        if not all(credentials.values()):
+            raise forms.ValidationError(_("Must include 'username' and 'password'."))
+
+        user = authenticate(**credentials)
+
+        if user:
+            if not user.is_active:
+                raise forms.ValidationError(_("User account is disabled."))
+        else:
+
+            raise forms.ValidationError(_("Unable to login with provided credentials."))
+
+        cleaned_data["user"] = user
+        user_logged_in.send(sender=user.__class__, request=None, user=user)
+
+
+class JSONWebTokenRefreshForm(forms.Form):
+    token = forms.CharField()
+
+    def clean(self):
+        cleaned_data = super(JSONWebTokenRefreshForm, self).clean()
+
+        old_payload = jwt_auth_settings.JWT_DECODE_HANDLER(cleaned_data.get("token"))
+        user_id = jwt_get_user_id_from_payload_handler(old_payload)
+
+        # Verify user
+        try:
+            user = User.objects.get(id=user_id)
+        except ObjectDoesNotExist:
+            raise forms.ValidationError(_("Unable to login with provided credentials."))
+
+        if not user.is_active:
+            raise forms.ValidationError(_("User account is disabled."))
+
+        # Verify orig_iat
+        orig_iat = old_payload.get("orig_iat")
+        if not orig_iat:
+            raise forms.ValidationError(_("orig_iat was missing from payload."))
+
+        # Verify expiration
+        refresh_limit = jwt_auth_settings.JWT_REFRESH_EXPIRATION_DELTA
+
+        if isinstance(refresh_limit, timedelta):
+            refresh_limit = refresh_limit.days * 24 * 3600 + refresh_limit.seconds
+
+        expiration_timestamp = orig_iat + int(refresh_limit)
+        now_timestamp = datetime.utcnow().timestamp()
+
+        if now_timestamp > expiration_timestamp:
+            raise forms.ValidationError(_("Refresh has expired."))
+
+        # Data to re-issue new token. Include original issued at time for a
+        # brand new token, to allow token refresh.
+        cleaned_data["user"] = user
+        cleaned_data["orig_iat"] = orig_iat
---
+++ packages/webstack-django-jwt-auth/webstack_django_jwt_auth-1.4.0-py2.py3-none-any.whl/jwt_auth/mixins.py
@@ -0,0 +1,100 @@
+import jwt
+from django.http import JsonResponse
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext as _
+from django.views.decorators.csrf import csrf_exempt
+from jwt_auth import exceptions, settings
+from jwt_auth.core import User
+from jwt_auth.utils import get_authorization_header
+
+jwt_decode_handler = settings.JWT_DECODE_HANDLER
+jwt_get_user_id_from_payload = settings.JWT_PAYLOAD_GET_USER_ID_HANDLER
+
+
+def get_token_from_request(request):
+    auth = get_authorization_header(request).split()
+    auth_header_prefix = settings.JWT_AUTH_HEADER_PREFIX.lower()
+
+    if not auth or auth[0].lower().decode("utf-8") != auth_header_prefix:
+        raise exceptions.AuthenticationFailed()
+
+    if len(auth) == 1:
+        raise exceptions.AuthenticationFailed(
+            _("Invalid Authorization header. No credentials provided.")
+        )
+    elif len(auth) > 2:
+        raise exceptions.AuthenticationFailed(
+            _(
+                "Invalid Authorization header. Credentials string "
+                "should not contain spaces."
+            )
+        )
+
+    return auth[1]
+
+
+def get_payload_from_token(token):
+    try:
+        payload = jwt_decode_handler(token)
+    except jwt.ExpiredSignatureError:
+        raise exceptions.AuthenticationFailed(_("Signature has expired."))
+    except jwt.DecodeError:
+        raise exceptions.AuthenticationFailed(_("Error decoding signature."))
+
+    return payload
+
+
+def get_user_id_from_payload(payload):
+    user_id = jwt_get_user_id_from_payload(payload)
+    if not user_id:
+        raise exceptions.AuthenticationFailed(_("Invalid payload"))
+
+    return user_id
+
+
+def get_user(user_id):
+    try:
+        return User.objects.get(pk=user_id, is_active=True)
+    except User.DoesNotExist:
+        return None
+
+
+class JSONWebTokenAuthMixin:
+    """
+    Token based authentication using the JSON Web Token standard.
+
+    Clients should authenticate by passing the token key in the "Authorization"
+    HTTP header, prepended with the string specified in the setting
+    `JWT_AUTH_HEADER_PREFIX`. For example:
+
+        Authorization: JWT eyJhbGciOiAiSFMyNTYiLCAidHlwIj
+    """
+
+    www_authenticate_realm = "api"
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            request.user, request.token = self.authenticate(request)
+        except exceptions.AuthenticationFailed as error:
+            response = JsonResponse({"errors": [str(error)]}, status=401)
+            response["WWW-Authenticate"] = self.authenticate_header(request)
+
+            return response
+
+        return super(JSONWebTokenAuthMixin, self).dispatch(request, *args, **kwargs)
+
+    def authenticate(self, request):  # pylint: disable=no-self-use
+        """Method required."""
+        token = get_token_from_request(request)
+        payload = get_payload_from_token(token)
+        user_id = get_user_id_from_payload(payload)
+        return get_user(user_id), token
+
+    def authenticate_header(self, request):
+        """
+        Return a string to be used as the value of the `WWW-Authenticate`
+        header in a `401 Unauthenticated` response, or `None` if the
+        authentication scheme should return `403 Permission Denied` responses.
+        """
+        return 'JWT realm="{0}"'.format(self.www_authenticate_realm)
