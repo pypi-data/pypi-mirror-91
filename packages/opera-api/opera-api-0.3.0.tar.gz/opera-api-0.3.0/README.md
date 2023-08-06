abbc43dd22fc9e245e9a5326c5387dc5a2f06173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:39 +0000

    Add package opera-api/0.3.0/opera-api-0.3.0.tar.gz

---
+++ packages/opera-api/opera-api-0.3.0.tar.gz/opera-api-0.3.0/README.md
@@ -0,0 +1,103 @@
+# xOpera API
+
+An HTTP API interface to the `opera` orchestrator.
+Mimics CLI commands.
+
+WIP.
+
+## Development
+
+Requires Python >= 3.7.
+
+To begin:
+
+```shell script
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+OpenAPI base code is generated with `generate.sh` and is _not_ checked in to the repository.
+
+Other useful commands:
+
+```shell script
+# openapi-generator basics
+java -jar openapi-generator-cli-4.3.0.jar
+java -jar openapi-generator-cli-4.3.0.jar help generate
+java -jar openapi-generator-cli-4.3.0.jar validate --input-spec openapi-spec.yml --recommend
+java -jar openapi-generator-cli-4.3.0.jar config-help --generator-name python-flask --full-details
+```
+
+## Usage
+
+With Docker:
+
+```shell script
+docker-compose up --build -d
+docker cp test.csar xopera-api_api_1:/app/
+docker exec xopera-api_api_1 unzip test.csar
+docker logs -f xopera-api_api_1
+# prepare request inputs: service_template, inputs (in JSON object form, not a string)
+curl -XPOST localhost:8080/validate -H "Content-Type: application/json" -d @inputs-request.json
+curl -XPOST localhost:8080/deploy -H "Content-Type: application/json" -d @inputs-request.json
+curl localhost:8080/status
+curl localhost:8080/outputs
+curl localhost:8080/info
+curl -XPOST localhost:8080/undeploy
+```
+
+With a local development installation:
+
+```shell script
+./generate.sh
+python3 -m venv .venv
+source .venv/bin/activate
+pip install wheel
+pip install -r requirements.txt
+cd src/
+python3 -m opera.api.cli
+curl localhost:8080
+```
+
+## Building for PyPI, releases
+
+* If the `opera` dependency version has changed, you **must** reflect that change in `setup.cfg`.
+* Generate sources with `./generate.sh`.
+* Test built packages in a Docker container.
+* Only release tags without any local changes present.
+* Manually create and upload releases onto GitHub, no automation for that.
+* Build and test before pushing tags to reduce rollbacks.
+
+```shell script
+pip3 install -r requirements.txt
+./generate.sh
+git tag -a 1.2.3 -m 1.2.3
+
+rm -rfv dist/
+python setup.py sdist bdist_wheel
+
+docker run -it --rm \
+    -v $(realpath ./dist/):/dist/:ro \
+    -v $(realpath ./test.csar):/test.csar:ro \
+    -v $(realpath ./test-inputs-request.json):/test/test-inputs-request.json:ro \
+    python:3.8-buster bash
+    
+    unzip -d /test/ /test.csar && cd /test/
+    pip3 install /dist/*.whl
+    opera-api
+    pip3 uninstall -y opera-api
+    pip3 install /dist/*.tar.gz
+    opera-api &
+
+    curl localhost:8080/info
+    curl -XPOST localhost:8080/deploy -H "Content-Type: application/json" -d @/test/test-inputs-request.json
+    curl localhost:8080/status
+    curl localhost:8080/outputs
+    curl -XPOST localhost:8080/undeploy
+
+twine upload --repository <pypi|testpypi> dist/*
+# upload to github manually
+
+git push --tags
+```
---
+++ packages/opera-api/opera-api-0.3.0.tar.gz/opera-api-0.3.0/src/opera/api/controllers/default.py
@@ -0,0 +1,136 @@
+import traceback
+from pathlib import PurePath
+
+from opera.commands.info import info as opera_info
+from opera.commands.init import init_compressed_csar as opera_init_compressed_csar
+from opera.commands.outputs import outputs as opera_outputs
+from opera.commands.package import package as opera_package
+from opera.commands.unpackage import unpackage as opera_unpackage
+from opera.commands.validate import validate_service_template as opera_validate
+from opera.storage import Storage
+
+from opera.api.controllers.background_invocation import InvocationService
+from opera.api.log import get_logger
+from opera.api.openapi.models import ValidationResult, OperationType, CsarInitializationInput, PackagingInput, \
+    UnpackagingInput, PackagingResult, Info
+from opera.api.openapi.models.deployment_input import DeploymentInput
+
+logger = get_logger(__name__)
+
+invocation_service = InvocationService()
+
+
+def deploy(body: DeploymentInput = None):
+    logger.debug("Entry: deploy")
+    logger.debug(body)
+
+    deployment_input = DeploymentInput.from_dict(body)
+    result = invocation_service.invoke(OperationType.DEPLOY, deployment_input.service_template, deployment_input.inputs)
+    return result, 200
+
+
+def undeploy():
+    logger.debug("Entry: undeploy")
+
+    result = invocation_service.invoke(OperationType.UNDEPLOY, None, None)
+    return result, 200
+
+
+def outputs():
+    logger.debug("Entry: outputs")
+
+    try:
+        opera_storage = Storage.create()
+        result = opera_outputs(opera_storage)
+    except Exception as e:
+        logger.error("Error getting outputs.", e)
+        return {"message": str(e)}, 500
+
+    if not result:
+        return {"message": "No outputs exist for this deployment."}, 404
+    return result, 200
+
+
+def status():
+    logger.debug("Entry: status")
+    history = invocation_service.invocation_history()
+    return history, 200
+
+
+def invocation_status(invocation_id):
+    logger.debug("Entry: invocation_status")
+    history = invocation_service.invocation_history()
+    try:
+        return next(inv for inv in history if inv.id == invocation_id), 200
+    except StopIteration:
+        return {"message": "No invocation with id {}".format(invocation_id)}, 404
+
+
+def validate(body: DeploymentInput = None):
+    logger.debug("Entry: validate")
+    logger.debug(body)
+
+    deployment_input = DeploymentInput.from_dict(body)
+
+    result = ValidationResult()
+    try:
+        opera_validate(deployment_input.service_template, deployment_input.inputs)
+        result.success = True
+    except Exception as e:
+        result.success = False
+        result.message = "{}: {}\n\n{}".format(e.__class__.__name__, str(e), traceback.format_exc())
+
+    return result, 200
+
+
+def info():
+    logger.debug("Entry: info")
+
+    try:
+        opera_storage = Storage.create()
+        result = opera_info(PurePath("."), opera_storage)
+    except Exception as e:
+        return {"message": "General error: {}".format(str(e))}, 500
+
+    serialized = Info(
+        service_template=str(result["service_template"]),
+        content_root=str(result["content_root"]),
+        inputs=result["inputs"],
+        status=result["status"]
+    )
+
+    return serialized, 200
+
+
+def init(csar_initialization_input: CsarInitializationInput):
+    logger.debug("Entry: init")
+
+    try:
+        opera_storage = Storage.create()
+        opera_init_compressed_csar(".", csar_initialization_input.inputs,
+                                   opera_storage, csar_initialization_input.clean)
+        return {"success": True, "message": ""}, 200
+    except Exception as e:
+        return {"success": False, "message": "General error: {}".format(str(e))}, 500
+
+
+def package(packaging_input: PackagingInput):
+    logger.debug("Entry: package")
+
+    try:
+        path = opera_package(packaging_input.service_template_folder, packaging_input.output,
+                             packaging_input.service_template, str(packaging_input.format))
+        result = PackagingResult(path)
+        return result, 200
+    except Exception as e:
+        return {"success": False, "message": "General error: {}".format(str(e))}, 500
+
+
+def unpackage(unpackaging_input: UnpackagingInput):
+    logger.debug("Entry: package")
+
+    try:
+        opera_unpackage(unpackaging_input.csar, unpackaging_input.destination, "")
+        return {"success": True, "message": ""}, 200
+    except Exception as e:
+        return {"success": False, "message": "General error: {}".format(str(e))}, 500
