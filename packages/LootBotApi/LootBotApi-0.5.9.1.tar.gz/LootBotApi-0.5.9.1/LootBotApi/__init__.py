e266c9a1aa4665f0619ec309e707ef04bf5a0137
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:14 +0000

    Add package LootBotApi/0.5.9.1/LootBotApi-0.5.9.1.tar.gz

---
+++ packages/LootBotApi/LootBotApi-0.5.9.1.tar.gz/LootBotApi-0.5.9.1/LootBotApi/__init__.py
@@ -0,0 +1,9 @@
+__title__ = 'LootBotApi'
+__version__ = '0.1'
+__author__ = 'Anatras02'
+__license__ = 'MIT License'
+__copyright__ = 'Copyright 2020 Anatras02'
+
+from .LootBotApi import LootBotApi
+from .LootBotApi import Error500
+from .LootBotApi import LootBotApiError
