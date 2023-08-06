7bc851ad46d4b5f11ac02ff37c264b5a8f08df83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:38 +0000

    Add package pyFFM/0.0.6/pyFFM-0.0.6.tar.gz

---
+++ packages/pyFFM/pyFFM-0.0.6.tar.gz/pyFFM-0.0.6/setup.py
@@ -0,0 +1,19 @@
+import pathlib
+import setuptools
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setuptools.setup(name="pyFFM",
+                 version="0.0.6",
+                 author="Aaron Mascaro",
+                 author_email="mascaroa1@gmail.com",
+                 description="Python implementation of Factorization Machines (+ Field Aware)",
+                 long_description=README,
+                 long_description_content_type="text/markdown",
+                 license="MIT",
+                 url="https://github.com/mascaroa/pyffm",
+                 packages=setuptools.find_packages(),
+                 install_requires=['numpy>=1.16',
+                                   'pandas>=0.24',
+                                   'numba>=0.49.1']
+                 )
