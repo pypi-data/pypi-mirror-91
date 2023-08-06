7a6e1e8fae036d4a7c71a35cd8406c7b05b17f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:37 +0000

    Add package pySmartKD/0.1.3/pySmartKD-0.1.3.tar.gz

---
+++ packages/pySmartKD/pySmartKD-0.1.3.tar.gz/pySmartKD-0.1.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+      name='pySmartKD',
+      version = '0.1.3',
+      description = 'Functions to analyze and plot geochemical data and to predict distribution coefficients between chemical species and minerals',
+      py_modules=["pySmartKD"],
+      author = 'Jadallah Zouabe',
+      author_email ='jzouabe@berkeley.edu',
+      license ='MIT',
+      packages=['pySmartKD'],
+      install_requires=[
+        'csv',
+        'pandas',
+        'numpy',
+        'seaborn',
+        'matplotlib',
+        'sklearn',
+        'scipy',
+        'pprint'],
+      include_package_Data=True
+      )
+
+         
+
