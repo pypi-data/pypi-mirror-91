7703b83fe9d28e84199798a787742da5c3a6055e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:53:50 +0000

    Add package ctranslate2/1.17.1/ctranslate2-1.17.1-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/ctranslate2/ctranslate2-1.17.1-cp38-cp38-macosx_10_9_x86_64.whl/ctranslate2/converters/converter.py
@@ -0,0 +1,129 @@
+import abc
+import filecmp
+import inspect
+import os
+import shutil
+
+from ctranslate2.specs import catalog
+from ctranslate2.specs.model_spec import ModelSpec
+
+
+def _list_specs():
+    return {
+        symbol: getattr(catalog, symbol)
+        for symbol in dir(catalog)
+        if inspect.isclass(getattr(catalog, symbol)) and not symbol.startswith("_")
+    }
+
+
+class Converter(abc.ABC):
+    @staticmethod
+    def declare_arguments(parser):
+        parser.add_argument(
+            "--output_dir", required=True, help="Output model directory."
+        )
+        parser.add_argument(
+            "--model_spec",
+            required=True,
+            choices=list(_list_specs().keys()),
+            help="Type of model to convert.",
+        )
+        parser.add_argument(
+            "--vocab_mapping", default=None, help="Vocabulary mapping file (optional)."
+        )
+        parser.add_argument(
+            "--quantization",
+            default=None,
+            choices=["int8", "int16", "float16"],
+            help="Weight quantization type.",
+        )
+        parser.add_argument(
+            "--force",
+            action="store_true",
+            help="Force conversion even if the output directory already exists.",
+        )
+        return parser
+
+    def convert_from_args(self, args):
+        return self.convert(
+            args.output_dir,
+            args.model_spec,
+            vmap=args.vocab_mapping,
+            quantization=args.quantization,
+            force=args.force,
+        )
+
+    def convert(
+        self, output_dir, model_spec, vmap=None, quantization=None, force=False
+    ):
+        if os.path.exists(output_dir) and not force:
+            raise RuntimeError(
+                "output directory %s already exists, use --force to override"
+                % output_dir
+            )
+        if isinstance(model_spec, str):
+            spec_class = _list_specs()[model_spec]
+            model_spec = spec_class()
+        if not isinstance(model_spec, ModelSpec):
+            raise TypeError("model_spec should extend ctranslate2.specs.ModelSpec")
+        try:
+            src_vocab, tgt_vocab = self._load(model_spec)
+        except NotImplementedError:
+            raise NotImplementedError(
+                "This converter does not support the model %s" % model_spec
+            )
+        model_spec.validate()
+        self._check_vocabulary_size(
+            "source", src_vocab, model_spec.source_vocabulary_size
+        )
+        self._check_vocabulary_size(
+            "target", tgt_vocab, model_spec.target_vocabulary_size
+        )
+        model_spec.optimize(quantization=quantization)
+
+        # Create model directory.
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+        os.makedirs(output_dir)
+        model_spec.serialize(os.path.join(output_dir, "model.bin"))
+        if vmap is not None:
+            shutil.copy(vmap, os.path.join(output_dir, "vmap.txt"))
+        src_vocab_path = os.path.join(output_dir, "source_vocabulary.txt")
+        tgt_vocab_path = os.path.join(output_dir, "target_vocabulary.txt")
+        self._save_vocabulary(src_vocab, src_vocab_path)
+        self._save_vocabulary(tgt_vocab, tgt_vocab_path)
+        # For shared vocabularies, keep a single file in the model directory.
+        if filecmp.cmp(src_vocab_path, tgt_vocab_path, shallow=False):
+            os.remove(tgt_vocab_path)
+            os.rename(src_vocab_path, os.path.join(output_dir, "shared_vocabulary.txt"))
+        return output_dir
+
+    @abc.abstractmethod
+    def _load(self, model_spec):
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def _save_vocabulary(self, vocab, destination):
+        raise NotImplementedError()
+
+    def _vocabulary_size(self, vocab):
+        """Returns the vocabulary size.
+
+        When defined, this enables additional error checking when converting models.
+        """
+        return None
+
+    def _check_vocabulary_size(self, name, vocab, expected_size):
+        """Raises an exception if expected and actual vocabulary sizes are known but
+        do not match.
+        """
+        if expected_size is None:
+            return
+        vocab_size = self._vocabulary_size(vocab)
+        if vocab_size is None:
+            return
+        if vocab_size != expected_size:
+            raise ValueError(
+                "%s vocabulary has size %d but the model expected a vocabulary "
+                "of size %d" % (name.capitalize(), vocab_size, expected_size)
+            )
---
+++ packages/ctranslate2/ctranslate2-1.17.1-cp38-cp38-macosx_10_9_x86_64.whl/ctranslate2/specs/model_spec.py
@@ -0,0 +1,226 @@
+"""Specifications declare the expected variables layout of CTranslate2 models
+that do not load a computation graph. The model converter should make sure that
+each required variable of the specification is set.
+"""
+
+import struct
+import numpy as np
+
+OPTIONAL = "optional"
+
+
+def _join_scope(scope, name):
+    if not scope:
+        return name
+    return "%s/%s" % (scope, name)
+
+
+def _split_scope(scope):
+    return scope.split("/")
+
+
+def _parent_scope(scope):
+    keys = _split_scope(scope)
+    scope, attr = keys[:-1], keys[-1]
+    return "/".join(scope), attr
+
+
+def visit_spec(spec, fn, scope=""):
+    """Recursively visits a layer spec."""
+    for name, value in list(spec.__dict__.items()):
+        if isinstance(value, list):
+            for i, elem in enumerate(value):
+                visit_spec(elem, fn, scope=_join_scope(scope, "%s_%d" % (name, i)))
+        elif isinstance(value, LayerSpec):
+            visit_spec(value, fn, scope=_join_scope(scope, name))
+        else:
+            fn(spec, _join_scope(scope, name), value)
+
+
+def index_spec(spec, index):
+    if not index:
+        return spec
+    keys = _split_scope(index)
+    for key in keys:
+        try:
+            spec = getattr(spec, key)
+        except AttributeError:
+            attr, index = key.rsplit("_", 1)
+            spec = getattr(spec, attr)[int(index)]
+    return spec
+
+
+class LayerSpec(object):
+    """Layer specification."""
+
+    def validate(self):
+        """Checks that required variables are set to a valid value."""
+
+        def _check(spec, name, value):
+            if value is None:
+                raise ValueError("Missing value for attribute %s" % name)
+            attr_name = _split_scope(name)[-1]
+            if isinstance(value, np.ndarray):
+                # Use float32 as the working floating point type.
+                if value.dtype in (np.float16, np.float64):
+                    setattr(spec, attr_name, value.astype(np.float32))
+            elif isinstance(value, bool):
+                # Convert bool to an integer type.
+                setattr(spec, attr_name, np.dtype("int8").type(value))
+
+        self.visit(_check)
+
+    def variables(self, prefix="", ordered=False):
+        """Returns a dict mapping variables name to value. If ordered is True,
+        returns an ordered list of (name, value) pairs instead.
+        """
+        var = {}
+
+        def _register_var(spec, name, value):
+            if isinstance(value, str) and value == OPTIONAL:
+                return
+            var[_join_scope(prefix, name)] = value
+
+        self.visit(_register_var)
+        if ordered:
+            return list(sorted(var.items(), key=lambda x: x[0]))
+        return var
+
+    def _alias_variables(self):
+        """Find duplicate variables in spec and create aliases."""
+        # When a variable is duplicated, keep the version that comes first in
+        # the alphabetical order and alias the others.
+        variables = self.variables(ordered=True)
+        for name, value in reversed(variables):
+            for other_name, other_value in variables:
+                if name == other_name:
+                    break
+                # Because variables can be transformed on load (e.g. transposed),
+                # we use an element-wise equality check.
+                if value.dtype == other_value.dtype and np.array_equal(
+                    value, other_value
+                ):
+                    # Replace variable value by the alias name.
+                    scope, attr_name = _parent_scope(name)
+                    spec = index_spec(self, scope)
+                    setattr(spec, attr_name, other_name)
+                    break
+
+    def _quantize(self, quantization):
+        """Possibly quantizes the variable of the layer."""
+
+        def _quantize(spec, name, value):
+            if not isinstance(value, np.ndarray):
+                return
+            if quantization == "float16":
+                if value.dtype == np.float32:
+                    key = _split_scope(name)[-1]
+                    setattr(spec, key, value.astype(np.float16))
+            elif "weight" in name:
+                if quantization == "int16":
+                    # Represent the value with 10 bits so the multiplication is 20 bits
+                    # and 12 bits are left for accumulation.
+                    scale = np.dtype(value.dtype).type(
+                        2 ** 10 / np.amax(np.absolute(value))
+                    )
+                    value *= scale
+                    value = np.clip(
+                        value, np.iinfo(np.int16).min, np.iinfo(np.int16).max
+                    )
+                    value = value.astype(np.int16)
+                elif quantization == "int8":
+                    scale = 127.0 / np.amax(np.absolute(value), axis=1)
+                    value *= np.expand_dims(scale, 1)
+                    value = value.astype(np.int8)
+                setattr(spec, "weight_scale", scale)
+                setattr(spec, "weight", value)
+
+        self.visit(_quantize)
+
+    def optimize(self, quantization=None):
+        """Applies some optimizations on this layer."""
+        self._alias_variables()
+        if quantization is not None:
+            self._quantize(quantization)
+
+    def visit(self, fn):
+        """Recursively visits this layer and its children."""
+        visit_spec(self, fn)
+
+
+def _dtype_to_type_id(object_dtype):
+    # Order should match the DataType enum in include/ctranslate2/types.h
+    dtypes = (np.float32, np.int8, np.int16, np.int32, np.float16)
+    try:
+        return dtypes.index(object_dtype)
+    except ValueError:
+        raise ValueError(
+            "%s is not in list of supported dtypes: %s"
+            % (str(object_dtype), ", ".join(map(str, dtypes)))
+        )
+
+
+class ModelSpec(LayerSpec):
+    """The top level layer specification."""
+
+    def __init__(self):
+        self.with_source_bos = False
+        self.with_source_eos = False
+
+    @property
+    def name(self):
+        """The name of the model specification."""
+        raise NotImplementedError()
+
+    @property
+    def revision(self):
+        """The model specification revision.
+
+        This value is incremented each time the weights layout of the model is
+        changed (e.g. a weight is renamed).
+        """
+        return 1
+
+    @property
+    def source_vocabulary_size(self):
+        """Source vocabulary size based on the model weights."""
+        return None
+
+    @property
+    def target_vocabulary_size(self):
+        """Target vocabulary size based on the model weights."""
+        return None
+
+    def serialize(self, path):
+        """Serializes this specification."""
+        variables = []
+        aliases = []
+        for variable in self.variables(ordered=True):
+            if isinstance(variable[1], str):
+                aliases.append(variable)
+            else:
+                variables.append(variable)
+
+        with open(path, "wb") as model:
+
+            def _write_string(string):
+                model.write(struct.pack("H", len(string) + 1))
+                model.write(string.encode("utf-8"))
+                model.write(struct.pack("B", 0))
+
+            model.write(struct.pack("I", 4))  # Binary version.
+            _write_string(self.name)
+            model.write(struct.pack("I", self.revision))
+            model.write(struct.pack("I", len(variables)))
+            for name, value in variables:
+                _write_string(name)
+                model.write(struct.pack("B", len(value.shape)))
+                for dim in value.shape:
+                    model.write(struct.pack("I", dim))
+                model.write(struct.pack("B", _dtype_to_type_id(value.dtype)))
+                model.write(struct.pack("I", value.nbytes))
+                model.write(value.tobytes())
+            model.write(struct.pack("I", len(aliases)))
+            for alias, variable_name in aliases:
+                _write_string(alias)
+                _write_string(variable_name)
