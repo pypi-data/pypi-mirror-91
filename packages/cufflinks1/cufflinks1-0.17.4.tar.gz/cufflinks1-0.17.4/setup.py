3bd42b739f520850ff825a74e85b20f2d3293b84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:05 +0000

    Add package cufflinks1/0.17.4/cufflinks1-0.17.4.tar.gz

---
+++ packages/cufflinks1/cufflinks1-0.17.4.tar.gz/cufflinks1-0.17.4/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from os import path
+import io
+
+here = path.abspath(path.dirname(__file__))
+with io.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requires = f.read().split()
+
+setup(
+    name='cufflinks1',
+    version='0.17.4',
+    description='Productivity Tools for Plotly + Pandas',
+    author='Jorge Santos',
+    author_email='santos.jorge@gmail.com',
+    license='MIT',
+    keywords=['pandas', 'plotly', 'plotting'],
+    url='https://github.com/santosjorge/cufflinks',
+    packages=['cufflinks'],
+    package_data={'cufflinks': ['../helper/*.json']},
+    include_package_data=True,
+    install_requires=requires,
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    zip_safe=False
+)
