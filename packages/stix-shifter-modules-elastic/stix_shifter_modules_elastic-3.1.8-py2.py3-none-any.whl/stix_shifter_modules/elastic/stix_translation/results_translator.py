e4a1df418f5d3a3dcd3196cd0e9f33a320c7f806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:12 +0000

    Add package stix-shifter-modules-elastic/3.1.8/stix_shifter_modules_elastic-3.1.8-py2.py3-none-any.whl

---
+++ packages/stix-shifter-modules-elastic/stix_shifter_modules_elastic-3.1.8-py2.py3-none-any.whl/stix_shifter_modules/elastic/stix_translation/results_translator.py
@@ -0,0 +1,6 @@
+from stix_shifter_utils.stix_translation.src.json_to_stix.json_to_stix import JSONToStix
+
+class ResultsTranslator(JSONToStix):
+
+  def read_mapping_file(self, path):
+        return '{}'
