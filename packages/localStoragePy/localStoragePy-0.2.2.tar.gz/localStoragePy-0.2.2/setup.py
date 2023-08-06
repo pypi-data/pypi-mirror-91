dd87c12c83234ad404c5752195f18478401a5dbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:55 +0000

    Add package localStoragePy/0.2.2/localStoragePy-0.2.2.tar.gz

---
+++ packages/localStoragePy/localStoragePy-0.2.2.tar.gz/localStoragePy-0.2.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='localStoragePy',
+      version='0.2.2',
+      description='A familiar API from the Web, adapted to storing data locally with Python.',
+      url='http://github.com/jkelol111/localStoragePy',
+      author='Nguyen Thanh Nam (jkelol111)',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author_email='jkelol111@gmail.com',
+      license='MIT',
+      packages=['localStoragePy'],
+      classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+      ],
+      zip_safe=False
+)
+
