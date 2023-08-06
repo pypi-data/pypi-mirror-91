9b20759b0d99e6a8a582dbbad2f3009d837c0865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:32:49 +0000

    Add package jeiutils/0.3/jeiutils-0.3.tar.gz

---
+++ packages/jeiutils/jeiutils-0.3.tar.gz/jeiutils-0.3/setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+setup(name="jeiutils",
+version="0.3",
+description="This is code with harry package",
+long_description = "This is a very very long description",
+author="sergio",
+packages=['jeiutils'],
+install_requires=[])
