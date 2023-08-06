60145fc93963c5220e1904404554b810c40054df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:53 +0000

    Add package GlassConversionPy/0.1.1/GlassConversionPy-0.1.1.tar.gz

---
+++ packages/GlassConversionPy/GlassConversionPy-0.1.1.tar.gz/GlassConversionPy-0.1.1/GlassConversionPy/__init__.py
@@ -0,0 +1,4 @@
+from GlassConversionPy.convert import mol2weight, weight2mol, get_molar_mass, molar_mass, formula2components
+
+if __name__ == "__main__":
+    print("Library to do simple conversions in glass science.")
