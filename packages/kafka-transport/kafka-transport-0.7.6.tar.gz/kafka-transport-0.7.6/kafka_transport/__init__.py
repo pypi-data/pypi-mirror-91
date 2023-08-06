2bb9817f52236e5098abae068fac6ffca95b8196
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:31 +0000

    Add package kafka-transport/0.7.6/kafka-transport-0.7.6.tar.gz

---
+++ packages/kafka-transport/kafka-transport-0.7.6.tar.gz/kafka-transport-0.7.6/kafka_transport/__init__.py
@@ -0,0 +1,3 @@
+from .__main__ import init, finalize, subscribe, push, fetch
+from .listener import Listener, Response
+from .requester import Requester
---
+++ packages/kafka-transport/kafka-transport-0.7.6.tar.gz/kafka-transport-0.7.6/kafka_transport/requester.py
@@ -0,0 +1,27 @@
+from kafka_transport import Listener
+
+
+class Requester(object):
+    def __init__(self, to_topic, from_topic, consumer_options=None):
+        self.to = to_topic
+        self._from = from_topic
+
+        self.listener = Listener(from_topic, to_topic, consumer_options)
+        
+    async def start(self) -> 'Requester':
+        await self.listener.start()
+        return self
+    
+    async def stop(self):
+        await self.listener.stop()
+
+    def push(self, data):
+        return self.listener.fetch(data)
+
+    def __getattr__(self, name):
+        def method(data=None):
+            return self.listener.fetch(
+                {'action': name, 'data': data}
+            )
+
+        return method
