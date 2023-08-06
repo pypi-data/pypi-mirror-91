24e1bb1a9b5a4047986fc606a280ab5b1f9c9e94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:25:10 +0000

    Add package sktmls/2021.1.13rc3/sktmls-2021.1.13rc3.tar.gz

---
+++ packages/sktmls/sktmls-2021.1.13rc3.tar.gz/sktmls-2021.1.13rc3/sktmls/__init__.py
@@ -0,0 +1,18 @@
+from .mls_env import MLSENV, MLSRuntimeENV
+from .mls_client import MLSClient, MLSResponse, MLSClientError, DoesNotExist
+from .model_registry import ModelRegistry, ModelRegistryError
+
+__all__ = [
+    "MLSENV",
+    "MLSRuntimeENV",
+    "ModelRegistry",
+    "ModelRegistryError",
+    "MLSClient",
+    "MLSResponse",
+    "MLSClientError",
+    "DoesNotExist",
+]
+
+__pdoc__ = {"autogluon": False}
+
+__version__ = "2021.1.13rc3"
