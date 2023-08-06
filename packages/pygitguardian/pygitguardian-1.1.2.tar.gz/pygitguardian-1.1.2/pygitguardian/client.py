69de7eefcc019e3e606a29f55bec2b257e66208c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:16 +0000

    Add package pygitguardian/1.1.2/pygitguardian-1.1.2.tar.gz

---
+++ packages/pygitguardian/pygitguardian-1.1.2.tar.gz/pygitguardian-1.1.2/pygitguardian/client.py
@@ -0,0 +1,206 @@
+import platform
+import urllib.parse
+from typing import Dict, List, Optional, Union
+
+from requests import Response, Session, codes
+
+from .config import (
+    DEFAULT_API_VERSION,
+    DEFAULT_BASE_URI,
+    DEFAULT_TIMEOUT,
+    MULTI_DOCUMENT_LIMIT,
+)
+from .models import Detail, Document, MultiScanResult, ScanResult
+
+
+def load_detail(resp: Response) -> Detail:
+    """
+    load_detail loads a Detail from a response
+    be it JSON or html.
+
+    :param resp: API response
+    :type resp: Response
+    :return: detail object of response
+    :rtype: Detail
+    """
+    if resp.headers["content-type"] == "application/json":
+        data = resp.json()
+    else:
+        data = {"detail": resp.text}
+
+    return Detail.SCHEMA.load(data)
+
+
+def is_ok(resp: Response) -> bool:
+    """
+    is_ok returns True is the API responded with 200
+    and the content type is JSON.
+    """
+    return (
+        resp.headers["content-type"] == "application/json"
+        and resp.status_code == codes.ok
+    )
+
+
+class GGClient:
+    _version = "undefined"
+    session: Session
+    api_key: str
+    base_uri: str
+    timeout: Optional[float]
+    user_agent: str
+
+    def __init__(
+        self,
+        api_key: str,
+        base_uri: Optional[str] = None,
+        session: Optional[Session] = None,
+        user_agent: Optional[str] = None,
+        timeout: Optional[float] = DEFAULT_TIMEOUT,
+    ):
+        """
+        :param api_key: API Key to be added to requests
+        :param base_uri: Base URI for the API, defaults to "https://api.gitguardian.com"
+        :param session: custom requests session, defaults to requests.Session()
+        :param user_agent: user agent to identify requests, defaults to ""
+        :param timeout: request timeout, defaults to 20s
+
+        :raises ValueError: if the protocol is invalid
+        """
+
+        if isinstance(base_uri, str):
+            if not base_uri.startswith(("http://", "https://")):
+                raise ValueError("Invalid protocol, prepend with http:// or https://")
+        else:
+            base_uri = DEFAULT_BASE_URI
+
+        if not isinstance(api_key, str):
+            raise TypeError("api_key is not a string")
+
+        self.base_uri = base_uri
+        self.api_key = api_key
+        self.session = session if isinstance(session, Session) else Session()
+        self.timeout = timeout
+        self.user_agent = "pygitguardian/{0} ({1};py{2})".format(
+            self._version, platform.system(), platform.python_version()
+        )
+
+        if isinstance(user_agent, str):
+            self.user_agent = " ".join([self.user_agent, user_agent])
+
+        self.session.headers.update(
+            {
+                "User-Agent": self.user_agent,
+                "Authorization": "Token {0}".format(api_key),
+            }
+        )
+
+    def request(
+        self,
+        method: str,
+        endpoint: str,
+        version: Optional[str] = DEFAULT_API_VERSION,
+        **kwargs
+    ) -> Response:
+        if version:
+            endpoint = urllib.parse.urljoin(version + "/", endpoint)
+
+        url = urllib.parse.urljoin(self.base_uri, endpoint)
+
+        return self.session.request(
+            method=method, url=url, timeout=self.timeout, **kwargs
+        )
+
+    def post(
+        self,
+        endpoint: str,
+        data: str = None,
+        version: str = DEFAULT_API_VERSION,
+        **kwargs
+    ) -> Response:
+        return self.request(
+            "post", endpoint=endpoint, json=data, version=version, **kwargs
+        )
+
+    def get(
+        self, endpoint: str, version: Optional[str] = DEFAULT_API_VERSION, **kwargs
+    ) -> Response:
+        return self.request(method="get", endpoint=endpoint, version=version, **kwargs)
+
+    def health_check(self) -> Detail:
+        """
+        health_check handles the /health endpoint of the API
+
+        use Detail.status_code to check the response status code of the API
+
+        200 if server is online and api_key is valid
+        :return: Detail response and status code
+        """
+        resp = self.get(endpoint="health")
+
+        obj = load_detail(resp)
+        obj.status_code = resp.status_code
+
+        return obj
+
+    def content_scan(
+        self, document: str, filename: Optional[str] = None
+    ) -> Union[Detail, ScanResult]:
+        """
+        content_scan handles the /scan endpoint of the API
+
+        :param filename: name of file, example: "intro.py"
+        :param document: content of file
+        :return: Detail or ScanResult response and status code
+        """
+
+        doc_dict = {"document": document}
+        if filename:
+            doc_dict["filename"] = filename
+
+        request_obj = Document.SCHEMA.load(doc_dict)
+
+        resp = self.post(endpoint="scan", data=request_obj)
+        if is_ok(resp):
+            obj = ScanResult.SCHEMA.load(resp.json())
+        else:
+            obj = load_detail(resp)
+
+        obj.status_code = resp.status_code
+
+        return obj
+
+    def multi_content_scan(
+        self,
+        documents: List[Dict[str, str]],
+    ) -> Union[Detail, MultiScanResult]:
+        """
+        multi_content_scan handles the /multiscan endpoint of the API
+
+        :param documents: List of dictionaries containing the keys document
+        and, optionally, filename.
+            example: [{"document":"example content","filename":"intro.py"}]
+        :return: Detail or ScanResult response and status code
+        """
+        if len(documents) > MULTI_DOCUMENT_LIMIT:
+            raise ValueError(
+                "too many documents submitted for scan (max={0})".format(
+                    MULTI_DOCUMENT_LIMIT
+                )
+            )
+
+        if all(isinstance(doc, dict) for doc in documents):
+            request_obj = Document.SCHEMA.load(documents, many=True)
+        else:
+            raise TypeError("each document must be a dict")
+
+        resp = self.post(endpoint="multiscan", data=request_obj)
+
+        if is_ok(resp):
+            obj = MultiScanResult.SCHEMA.load(dict(scan_results=resp.json()))
+        else:
+            obj = load_detail(resp)
+
+        obj.status_code = resp.status_code
+
+        return obj
---
+++ packages/pygitguardian/pygitguardian-1.1.2.tar.gz/pygitguardian-1.1.2/setup.py
@@ -0,0 +1,53 @@
+import io
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+VERSION_RE = re.compile(r"__version__\s*=\s*\"(.*?)\"")
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*args):
+    """Reads complete file contents."""
+    return io.open(os.path.join(HERE, *args), encoding="utf-8").read()
+
+
+def get_version():
+    """Reads the version from this module."""
+    init = read("pygitguardian", "__init__.py")
+    return VERSION_RE.search(init).group(1)
+
+
+setup(
+    name="pygitguardian",
+    version=get_version(),
+    packages=find_packages(exclude=["tests"]),
+    description="Python Wrapper for GitGuardian's API -- Scan security policy breaks everywhere",
+    long_description=read("README.md"),
+    long_description_content_type="text/markdown",
+    url="https://github.com/GitGuardian/py-gitguardian",
+    author="GitGuardian",
+    author_email="support@gitguardian.com",
+    maintainer="GitGuardian",
+    install_requires=["marshmallow>=3.5", "requests>=2"],
+    include_package_data=True,
+    zip_safe=True,
+    license="MIT",
+    keywords="api-client devsecops secrets-detection security-tools library gitguardian",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+        "Topic :: Security",
+    ],
+)
