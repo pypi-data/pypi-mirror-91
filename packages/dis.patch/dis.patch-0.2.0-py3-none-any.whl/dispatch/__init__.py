ed74355c245e9789de25b40b983b4c65df37aa03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:53 +0000

    Add package dis.patch/0.2.0/dis.patch-0.2.0-py3-none-any.whl

---
+++ packages/dis.patch/dis.patch-0.2.0-py3-none-any.whl/dispatch/__init__.py
@@ -0,0 +1,20 @@
+import logging
+
+from discord.ext import commands
+
+from utils.context import Context
+from utils.bot import Bot, AutoShardedBot
+from utils.settings import Settings
+from utils.paginator import EmbedPaginator
+
+
+logging.getLogger("utils").addHandler(logging.NullHandler())
+
+
+def monkey_patch():
+    logging.info("MonkeyPatch: Context")
+    commands.Context = Context
+    logging.info("MonkeyPatch: Bot")
+    commands.Bot = Bot
+    logging.info("MonkeyPatch: AutoShardedBot")
+    commands.AutoShardedBot = AutoShardedBot
