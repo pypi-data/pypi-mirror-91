4ce4801caa33d1689375412ee51470b8299c1a36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:25 +0000

    Add package weightGIS/0.8.0/weightGIS-0.8.0-py3-none-any.whl

---
+++ packages/weightGIS/weightGIS-0.8.0-py3-none-any.whl/weightGIS/__init__.py
@@ -0,0 +1,11 @@
+
+from weightGIS.weighting.Access.access_weighted import access_weighted
+from weightGIS.weighting.Access.SQLParser import SQLParser
+
+from weightGIS.weighting.ConstructWeights import ConstructWeights
+from weightGIS.weighting.AdjustWeights import AdjustWeights
+from weightGIS.weighting.AssignWeights import AssignWeights
+from weightGIS.weighting.WeightExternal import WeightExternal
+
+from weightGIS.AdjacentRelations import AdjacentRelations
+from weightGIS.GeoLookup import GeoLookup
