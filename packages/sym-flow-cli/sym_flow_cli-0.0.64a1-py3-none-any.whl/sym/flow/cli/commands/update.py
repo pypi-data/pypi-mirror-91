54ffd8beedb4c0c8335bdc2ea5a2131e5dd4e3a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:54 +0000

    Add package sym-flow-cli/0.0.64a1/sym_flow_cli-0.0.64a1-py3-none-any.whl

---
+++ packages/sym-flow-cli/sym_flow_cli-0.0.64a1-py3-none-any.whl/sym/flow/cli/commands/update.py
@@ -0,0 +1,7 @@
+from ..helpers.updater import SymUpdater
+from .symflow import symflow
+
+
+@symflow.command(short_help="Update the Sym Flow CLI")
+def update() -> None:
+    SymUpdater().manual_update()
