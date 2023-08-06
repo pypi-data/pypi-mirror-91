37027f9997f299c1af485f675bd5226fca8745a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:41 +0000

    Add package python-project-skeleton/1.5/python_project_skeleton-1.5.tar.gz

---
+++ packages/python-project-skeleton/python_project_skeleton-1.5.tar.gz/python_project_skeleton-1.5/src/python_project_skeleton/main.py
@@ -0,0 +1,10 @@
+import os
+
+from python_project_skeleton.tools import config
+
+
+pwd = os.path.dirname(os.path.abspath(__file__))
+config.add_config_ini('%s/main.ini' % pwd)
+
+if __name__ == '__main__':
+    print('Hello World!\nValue for NAME is %s.' % config.NAME)
