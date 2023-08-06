b739ab4890850254389dff89890ad3a1c8c2a1ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:19 +0000

    Add package freedomrobotics-api/1.0.11/freedomrobotics-api-1.0.11.tar.gz

---
+++ packages/freedomrobotics-api/freedomrobotics-api-1.0.11.tar.gz/freedomrobotics-api-1.0.11/freedomrobotics_api/helpers.py
@@ -0,0 +1,39 @@
+import time
+
+def to_struct_time(t):
+    """Converts a UNIX time, a time string, or a struct_time to a struct_time.
+    """
+
+    if t is None:
+        return t
+
+    if type(t) == float:
+        return time.gmtime(t)
+    elif type(t) == int:
+        return time.gmtime(t)
+    elif type(t) == str:
+        return time.strptime(t, "%Y-%m-%d %H:%M:%S")
+    elif type(t) == time.struct_time:
+        return t
+
+    print("parse_time: could not parse time: ", t)
+    return None
+
+def to_unix_time(t):
+    """Converts a UNIX time, a time string, or a struct_time to a UNIX time.
+    """
+
+    if t is None:
+        return t
+
+    if type(t) == float:
+        return t
+    elif type(t) == int:
+        return float(t)
+    elif type(t) == str:
+        return time.mktime(time.strptime(t, "%Y-%m-%d %H:%M:%S"))
+    elif type(t) == time.struct_time:
+        return time.mktime(t)
+
+    print("parse_time: could not parse time: ", t)
+    return None
