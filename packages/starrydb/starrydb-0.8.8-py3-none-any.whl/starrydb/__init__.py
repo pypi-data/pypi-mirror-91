3a90ce72098545f3c74525d423a6bf51d8b27304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:09 +0000

    Add package starrydb/0.8.8/starrydb-0.8.8-py3-none-any.whl

---
+++ packages/starrydb/starrydb-0.8.8-py3-none-any.whl/starrydb/__init__.py
@@ -0,0 +1,6 @@
+from starrydb.starrydb import StarryServer, StarryClient, StarryError
+__author__ = "Luo Xi <rossi913@163.com>"
+__license__ = "MIT License"
+
+__all__ = ['StarryServer', 'StarryClient', 'StarryError']
+
