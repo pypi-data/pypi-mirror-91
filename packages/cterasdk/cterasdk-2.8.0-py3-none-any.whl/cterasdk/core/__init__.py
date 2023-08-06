2291f3b55e27e65a81f9ae4e0293e8d15139fe15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:54 +0000

    Add package cterasdk/2.8.0/cterasdk-2.8.0-py3-none-any.whl

---
+++ packages/cterasdk/cterasdk-2.8.0-py3-none-any.whl/cterasdk/core/__init__.py
@@ -0,0 +1,25 @@
+__all__ = list(
+    map(
+        str,
+        [
+            'connection',
+            'activation',
+            'directoryservice',
+            'enum',
+            'login',
+            'query',
+            'logs',
+            'portals',
+            'reports',
+            'remote',
+            'servers',
+            'devices',
+            'session',
+            'users',
+            'cloudfs',
+            'zones',
+            'files',
+            'decorator'
+            ]
+    )
+)
