237ba2d318716541aaa8bd35c99590c4d8a79af6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:33 +0000

    Add package k-ege/0.1.1/k_ege-0.1.1.tar.gz

---
+++ packages/k-ege/k_ege-0.1.1.tar.gz/k_ege-0.1.1/k_ege/test.py
@@ -0,0 +1,30 @@
+# <!DOCTYPE html>
+# <html>
+# <head>
+# 	<title>GTSK - company future</title>
+# 	<meta charset="UTF-8">
+# 	<link rel="stylesheet" type="text/css" href="../CSS/gtsk.css">
+# 	<meta name="viewport" content="width=device-width, initial=scale=1.0,
+#     maximum-scale=1.0, user-scalable=0">
+# 	<meta name="format-detection" content="telephone=no">
+# 	<link rel="shortcut icon" href=../img/ty.png>
+# </head>
+# <body>
+# 	<div class="wrapper">
+# 		<div class="container">
+# 			<header class="menu">
+# 			<a href="../index.html">home</a>
+# 			<a href="gtsk.html" id="active_link">gtsk</a>
+# 		</header>
+# 		<div class="preview">
+# 			<div class="obo"></div>
+# 		</div>
+# 		<div class="container">
+# 			<h2>GTSK - Global Total Science Knowledge</h2>
+# 			<p></p>
+# 			<div class="f_img"></div>
+# 		</div>
+# 		</div>
+# 	</div>
+# </body>
+# </html>
