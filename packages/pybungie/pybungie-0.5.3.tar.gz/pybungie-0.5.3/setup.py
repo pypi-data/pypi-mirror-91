2e1fcc1fc878be34f7c76b2875e245b04d5ba0f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:28 +0000

    Add package pybungie/0.5.3/pybungie-0.5.3.tar.gz

---
+++ packages/pybungie/pybungie-0.5.3.tar.gz/pybungie-0.5.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="pybungie",
+    version="0.5.3",
+    author="Hayden Cole",
+    author_email="cole.haydenj@gmail.com",
+    description="For interacting with the Bungie.net API",
+    url="https://github.com/Hayden-J-C/pybungie.git",
+    packages=['pybungie'],
+    package_data={'pybungie': ['config/*', ]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
