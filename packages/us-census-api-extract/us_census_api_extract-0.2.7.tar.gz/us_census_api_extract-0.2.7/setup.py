c4537c171723634b1221eca65c6fd8c6ffd334af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:09 +0000

    Add package us-census-api-extract/0.2.7/us_census_api_extract-0.2.7.tar.gz

---
+++ packages/us-census-api-extract/us_census_api_extract-0.2.7.tar.gz/us_census_api_extract-0.2.7/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="us_census_api_extract",
+    version="0.2.7",
+    author="Anders Bergman",
+    author_email="",
+    description="Extract US census data (as a DataFrame) by inputting a list of demographic variables and US state codes.",
+    long_description=long_description, 
+    long_description_content_type="text/markdown",
+    url="https://github.com/AndoKalrisian/us_census_api_extract",
+    packages=setuptools.find_packages(where="src"),
+    package_dir={"": "src"},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['progress-keeper','pandas','numpy'],
+    license="MIT License"
+)
