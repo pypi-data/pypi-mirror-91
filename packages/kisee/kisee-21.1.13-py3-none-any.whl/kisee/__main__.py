e2e1cb8eb58968ff3dc437a98db511776c000611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:19 +0000

    Add package kisee/21.1.13/kisee-21.1.13-py3-none-any.whl

---
+++ packages/kisee/kisee-21.1.13-py3-none-any.whl/kisee/__main__.py
@@ -0,0 +1,6 @@
+"""Command line entry point (for python -m kisee)
+"""
+from kisee import kisee
+
+if __name__ == "__main__":
+    kisee.main()
---
+++ packages/kisee/kisee-21.1.13-py3-none-any.whl/kisee/middlewares.py
@@ -0,0 +1,48 @@
+"""Middlewares for Kisee service
+"""
+
+import json
+from typing import Awaitable, Callable
+
+from aiohttp import web
+
+Handler = Callable[[web.Request], Awaitable[web.StreamResponse]]
+
+
+@web.middleware
+async def vary_origin(request: web.Request, handler: Handler) -> web.StreamResponse:
+    """Add a Vary: Origin to the responses.
+
+    See: https://github.com/aio-libs/aiohttp-cors/issues/351
+    """
+    response = await handler(request)
+    response.headers["Vary"] = "Origin"
+    return response
+
+
+@web.middleware
+async def enforce_json(request: web.Request, handler: Handler) -> web.StreamResponse:
+    """Middleware enforcing a JSON response."""
+    try:
+        return await handler(request)
+    except json.JSONDecodeError as err:
+        document = {
+            "_type": "error",
+            "_meta": {"title": "Malformed JSON"},
+            "title": "Malformed JSON",
+            "detail": str(err),
+            "status": 400,
+        }
+    except web.HTTPException as err:
+        document = {
+            "_type": "error",
+            "_meta": {"title": err.reason},
+            "title": err.reason,
+            "status": err.status,
+        }
+    return web.Response(
+        text=json.dumps(document, indent=4),
+        status=document["status"],  # type: ignore
+        reason=document["title"],  # type: ignore
+        content_type="application/problem+json",
+    )
