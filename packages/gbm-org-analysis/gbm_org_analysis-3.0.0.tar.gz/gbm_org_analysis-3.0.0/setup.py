3c5bafe19907e881bfc2f80e7044d6ebb937a435
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:24 +0000

    Add package gbm-org-analysis/3.0.0/gbm_org_analysis-3.0.0.tar.gz

---
+++ packages/gbm-org-analysis/gbm_org_analysis-3.0.0.tar.gz/gbm_org_analysis-3.0.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gbm_org_analysis",
+    version="3.0.0",
+    author="Matt Rybin",
+    author_email="mxr2011@miami.edu",
+    description="Extracts and analyzes GBM-organoid data exported from Imaris",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rybinmj/gbm_org_analysis",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'pandas',
+        'statistics',
+        'matplotlib',
+        'seaborn',
+        'scipy',
+        'statsmodels',
+        'openpyxl',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
