540488c66d27e9018667f9a6fdda6f605e192356
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:47 +0000

    Add package gstools/1.3.0rc1/gstools-1.3.0rc1-cp35-cp35m-win_amd64.whl

---
+++ packages/gstools/gstools-1.3.0rc1-cp35-cp35m-win_amd64.whl/gstools/field/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+GStools subpackage providing tools for spatial random fields.
+
+.. currentmodule:: gstools.field
+
+Subpackages
+^^^^^^^^^^^
+
+.. autosummary::
+    generator
+    upscaling
+    base
+
+Spatial Random Field
+^^^^^^^^^^^^^^^^^^^^
+
+.. autosummary::
+   SRF
+
+----
+"""
+
+from gstools.field.srf import SRF
+
+__all__ = ["SRF"]
