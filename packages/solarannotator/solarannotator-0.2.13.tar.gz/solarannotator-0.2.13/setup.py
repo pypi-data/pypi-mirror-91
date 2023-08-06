9dd91d57c95cf5920e33bc65a589e32b4190e16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:16 +0000

    Add package solarannotator/0.2.13/solarannotator-0.2.13.tar.gz

---
+++ packages/solarannotator/solarannotator-0.2.13.tar.gz/solarannotator-0.2.13/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='solarannotator',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='0.2.13',
+    packages=['solarannotator'],
+    url='',
+    license='',
+    author='J. Marcus Hughes',
+    author_email='j-marcus.hughes@noaa.gov',
+    description='A tool to annotate images of the Sun',
+    install_requires=["PyQt5",
+                      "matplotlib",
+                      "astropy",
+                      "numpy",
+                      "goes-solar-retriever",
+                      "scipy",
+                      "scikit-image",
+                      "pillow"],
+    data_files=[('solarannotator', ['cfg/default.json'])],
+    entry_points={"console_scripts": ["SolarAnnotator = solarannotator.main:main"]}
+
+)
