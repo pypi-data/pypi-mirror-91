dee6f6e403c7e8f0845824d9432fb1a823db50db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:08 +0000

    Add package gnodatapackages/1.1.7/gnodatapackages-1.1.7.tar.gz

---
+++ packages/gnodatapackages/gnodatapackages-1.1.7.tar.gz/gnodatapackages-1.1.7/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setuptools.setup(
+    name="gnodatapackages", # Replace with your own username
+    version="1.1.7",
+    author="charmandersgsg",
+    author_email="charmandersgsg@gmail.com",
+    description="data def packages for gno",
+    long_description="data def packages for gno",
+    long_description_content_type="text/markdown",
+    url="https://github.com/charmandersgsg/gnodatapackages",
+    packages=['gnodatapackages'],
+    include_package_data=True,
+    install_requires=[
+          'pandas',
+          'string', 
+          'ftfy'
+      ],
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
