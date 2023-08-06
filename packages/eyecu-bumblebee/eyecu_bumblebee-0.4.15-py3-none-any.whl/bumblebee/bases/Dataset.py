934c50a37cc6c575625d0592bd269f10226f235c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:50:09 +0000

    Add package eyecu-bumblebee/0.4.15/eyecu_bumblebee-0.4.15-py3-none-any.whl

---
+++ packages/eyecu-bumblebee/eyecu_bumblebee-0.4.15-py3-none-any.whl/bumblebee/bases/Dataset.py
@@ -0,0 +1,27 @@
+
+
+class Dataset:
+
+    def __iter__(self):
+        abstract
+
+    def __len__(self):
+        abstract
+
+    def get_props(self):
+        abstract
+
+    def __next__(self):
+
+        try:
+            data = self.src.read()
+            return data
+        except Exception:
+            raise StopIteration()
+
+    def __getitem__(self, item):
+        return self.__next__()
+
+    def read(self):
+        return self.__next__()
+
