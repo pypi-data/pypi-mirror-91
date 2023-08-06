7ac1f2abd3e86d182b0eabff1dd18205661fd383
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:04 +0000

    Add package all-distributions/1.0/all_distributions-1.0.tar.gz

---
+++ packages/all-distributions/all_distributions-1.0.tar.gz/all_distributions-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='all_distributions',
+      version='1.0',
+      description='Gaussian and binomial distributions',
+      packages=['all_distributions'],
+      author = 'Laila Shahreen',
+      author_email = 'lailashahreen17@gmail.com',
+      zip_safe=False)
