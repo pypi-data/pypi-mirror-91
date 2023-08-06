d87e245d26f1b9a401b72482653798b9f73ca7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:59 +0000

    Add package firefly-framework/1.1.29/firefly_framework-1.1.29-py3-none-any.whl

---
+++ packages/firefly-framework/firefly_framework-1.1.29-py3-none-any.whl/firefly/infrastructure/repository/rdb_storage_interfaces/templates/sql/update.sql
@@ -0,0 +1,25 @@
+{%- block update -%}
+    update {% block fqtn %}{{ fqtn | sqlsafe }} {% endblock %} set
+
+    {% block columns %}
+        {%- for k, v in data.items() -%}
+            {% if k not in ids %}
+                {{ _q | sqlsafe }}{{ k | sqlsafe }}{{ _q | sqlsafe }}=
+                    {%- block update_value scoped %}
+                        {% if k == 'version' %}
+                            {{ v + 1 }}
+                        {% else %}
+                            {{ v }}
+                        {% endif %}
+                    {% endblock %}
+                {% if not loop.last %},{% endif %}
+            {% endif %}
+        {%- endfor -%}
+    {% endblock %}
+
+    {% if criteria %}
+        where
+        {% import "sql/macros.sql" as macros %}
+        {% block where_clause scoped %}{{ macros.where_clause(criteria, macros.default_attribute_macro, macros.default_value_macro, ids, field_types) }}{% endblock %}
+    {% endif %}
+{%- endblock -%}
