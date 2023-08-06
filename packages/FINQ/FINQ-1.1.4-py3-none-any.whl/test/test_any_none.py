6ae3472a3cb0dbc55fc62355170fe4d0986bcd8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:08 +0000

    Add package FINQ/1.1.4/FINQ-1.1.4-py3-none-any.whl

---
+++ packages/FINQ/FINQ-1.1.4-py3-none-any.whl/test/test_any_none.py
@@ -0,0 +1,44 @@
+from finq import FINQ
+
+
+def test_any():
+    a = [1, 2, 5, 7]
+    expected = [5, 7]
+
+    a_f = FINQ(a)
+    func = lambda i: i > 4
+
+    assert a_f.any(func)
+    assert a_f.filter(func).to_list() == expected
+
+
+def test_any_false():
+    a = [1, 2, 5, 7]
+    expected = []
+
+    a_f = FINQ(a)
+    func = lambda i: i > 40
+
+    assert not a_f.any(func)
+    assert a_f.filter(func).to_list() == expected
+
+def test_none_false():
+    a = [1, 2, 5, 7]
+    expected = [5, 7]
+
+    a_f = FINQ(a)
+    func = lambda i: i > 4
+
+    assert not a_f.none(func)
+    assert a_f.filter(func).to_list() == expected
+
+
+def test_none():
+    a = [1, 2, 5, 7]
+    expected = []
+
+    a_f = FINQ(a)
+    func = lambda i: i > 40
+
+    assert a_f.none(func)
+    assert a_f.filter(func).to_list() == expected
---
+++ packages/FINQ/FINQ-1.1.4-py3-none-any.whl/test/test_join.py
@@ -0,0 +1,19 @@
+from finq import FINQ
+
+
+def test_join():
+    a = [1, 2, 5, 7]
+    expected = '1257'
+
+    a_f = FINQ(a)
+
+    assert a_f.map(str).join() == expected
+
+
+def test_join_with_delim():
+    a = [1, 2, 5, 7]
+    expected = '1, 2, 5, 7'
+
+    a_f = FINQ(a)
+
+    assert a_f.map(str).join(', ') == expected
