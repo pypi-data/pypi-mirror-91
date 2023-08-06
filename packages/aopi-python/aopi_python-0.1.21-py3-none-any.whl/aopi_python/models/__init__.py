568048b3103ef2dadfba9de528d19037e09aff56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:01 +0000

    Add package aopi-python/0.1.21/aopi_python-0.1.21-py3-none-any.whl

---
+++ packages/aopi-python/aopi_python-0.1.21-py3-none-any.whl/aopi_python/models/__init__.py
@@ -0,0 +1,4 @@
+from aopi_python.models.package_version import PythonPackageVersion
+from aopi_python.models.pythonpackage import PythonPackage
+
+models_list = [PythonPackageVersion, PythonPackage]
