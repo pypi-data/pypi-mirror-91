82abcb97a3e0316f901d68a67a122f67c1e2e37c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:19:21 +0000

    Add package django-password-policies-iplweb/0.8.2/django-password-policies-iplweb-0.8.2.tar.gz

---
+++ packages/django-password-policies-iplweb/django-password-policies-iplweb-0.8.2.tar.gz/django-password-policies-iplweb-0.8.2/password_policies/urls.py
@@ -0,0 +1,41 @@
+from django.conf.urls import url
+
+try:
+    # patterns was deprecated in Django 1.8
+    from django.conf.urls import patterns
+except ImportError:
+    # patterns is unavailable in Django 1.10+
+    patterns = False
+
+from password_policies.views import PasswordChangeFormView
+from password_policies.views import PasswordChangeDoneView
+from password_policies.views import PasswordResetCompleteView
+from password_policies.views import PasswordResetConfirmView
+from password_policies.views import PasswordResetFormView
+from password_policies.views import PasswordResetDoneView
+
+
+urlpatterns = [
+    url(r'^change/done/$',
+        PasswordChangeDoneView.as_view(),
+        name="password_change_done"),
+    url(r'^change/$',
+        PasswordChangeFormView.as_view(),
+        name="password_change"),
+    url(r'^reset/$',
+        PasswordResetFormView.as_view(),
+        name="password_reset"),
+    url(r'^reset/complete/$',
+        PasswordResetCompleteView.as_view(),
+        name="password_reset_complete"),
+    url(r'^reset/confirm/([0-9A-Za-z_\-]+)/([0-9A-Za-z]{1,13})/([0-9A-Za-z-=_]{1,32})/$',
+        PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm"),
+    url(r'^reset/done/$',
+        PasswordResetDoneView.as_view(),
+        name="password_reset_done"),
+]
+
+if patterns:
+    # Django 1.7
+    urlpatterns = patterns('', *urlpatterns)
