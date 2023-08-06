13ac1e0aa65240a959506f8013cdc66d77d63391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:35 +0000

    Add package injecta/0.9.0a2/injecta-0.9.0a2-py3-none-any.whl

---
+++ packages/injecta/injecta-0.9.0a2-py3-none-any.whl/injecta/service/argument/ServiceArgument.py
@@ -0,0 +1,54 @@
+from injecta.generator.ServiceMethodNameTranslator import ServiceMethodNameTranslator
+from injecta.service.argument.ArgumentInterface import ArgumentInterface
+from injecta.service.argument.validator.ArgumentsValidatorException import ArgumentsValidatorException
+from injecta.service.class_.InspectedArgument import InspectedArgument
+from injecta.dtype.DType import DType
+from injecta.dtype.classLoader import loadClass
+
+class ServiceArgument(ArgumentInterface):
+
+    __serviceMethodTranslator = ServiceMethodNameTranslator()
+
+    def __init__(self, serviceName: str, name: str = None):
+        self.__serviceName = serviceName
+        self.__name = name
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def serviceName(self):
+        return self.__serviceName
+
+    def getStringValue(self):
+        return 'self.' + self.__serviceMethodTranslator.translate(self.__serviceName) + '()'
+
+    def checkTypeMatchesDefinition(self, inspectedArgument: InspectedArgument, services2Classes: dict, aliases2Services: dict):
+        serviceClassType = self.__resolveServiceClassType(services2Classes, aliases2Services)
+
+        if serviceClassType == inspectedArgument.dtype:
+            return
+
+        serviceClass = loadClass(serviceClassType.moduleName, serviceClassType.className)
+        inspectedArgumentClass = loadClass(inspectedArgument.dtype.moduleName, inspectedArgument.dtype.className)
+
+        if not issubclass(serviceClass, inspectedArgumentClass):
+            raise ArgumentsValidatorException(inspectedArgument.name, str(inspectedArgument.dtype), str(serviceClassType))
+
+    def __resolveServiceClassType(self, services2Classes: dict, aliases2Services: dict) -> DType:
+        if self.__serviceName in services2Classes:
+            return services2Classes[self.__serviceName]
+
+        if self.__serviceName in aliases2Services:
+            aliasedServiceName = aliases2Services[self.__serviceName]
+
+            if aliasedServiceName not in services2Classes:
+                raise Exception(f'Aliased service "{aliasedServiceName}" does not exist')
+
+            return services2Classes[aliasedServiceName]
+
+        raise Exception(f'Undefined service {self.__serviceName}')
+
+    def __eq__(self, other: 'ServiceArgument'):
+        return self.name == other.name and self.getStringValue() == other.getStringValue()
