3bf3ed2f0ec7ec0155d7d68f7341fd5e53881b65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:54 +0000

    Add package meerk40t/0.7.0.post1/meerk40t-0.7.0.post1.tar.gz

---
+++ packages/meerk40t/meerk40t-0.7.0.post1.tar.gz/meerk40t-0.7.0.post1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    install_requires=[
+        "pyusb",
+        "Pillow",
+        "wxPython",
+        "opencv-python-headless",
+        "ezdxf",
+    ],
+)
