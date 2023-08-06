a4348e466cfe7a94b86cf81b12415d22b1a89f85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:05 +0000

    Add package pgkit/1.5.5/pgkit-1.5.5-py3-none-any.whl

---
+++ packages/pgkit/pgkit-1.5.5-py3-none-any.whl/pgkit/cli/cli.py
@@ -0,0 +1,20 @@
+import click
+from pgkit.cli.commands.config import config
+from pgkit.cli.commands.pitr import pitr
+from pgkit.cli.commands.status import status
+from pgkit.cli.commands.list import list
+from pgkit.cli.commands.shellx import shell
+
+
+@click.group()
+def cli():
+    pass
+
+
+def main():
+    cli.add_command(config)
+    cli.add_command(pitr)
+    cli.add_command(status)
+    cli.add_command(list)
+    cli.add_command(shell)
+    cli()
