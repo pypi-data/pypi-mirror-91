f265948613d2ec665232d0be192d9e97d7b7b238
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:28:01 +0000

    Add package AaditsHangman/0.0.1/AaditsHangman-0.0.1.tar.gz

---
+++ packages/AaditsHangman/AaditsHangman-0.0.1.tar.gz/AaditsHangman-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="AaditsHangman", # Replace with your own username
+    version="0.0.1",
+    author="Aadit Bansal",
+    author_email="aaditisawesome@gmail.com",
+    description="A hangman game created on tkinter python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aaditisawesome/hangman",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
