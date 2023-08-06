3f00d411f1889295a238153477bb5c7966ac3174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:38 +0000

    Add package pipelinewise-tap-snowflake/2.0.3/pipelinewise-tap-snowflake-2.0.3.tar.gz

---
+++ packages/pipelinewise-tap-snowflake/pipelinewise-tap-snowflake-2.0.3.tar.gz/pipelinewise-tap-snowflake-2.0.3/setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open('README.md') as f:
+      long_description = f.read()
+
+setup(name='pipelinewise-tap-snowflake',
+      version='2.0.3',
+      description='Singer.io tap for extracting data from Snowflake - PipelineWise compatible',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author="TransferWise",
+      url='https://github.com/transferwise/pipelinewise-tap-snowflake',
+      classifiers=[
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=['tap_snowflake'],
+      install_requires=[
+            'pipelinewise-singer-python==1.*',
+            'snowflake-connector-python[pandas]==2.3.7',
+            'pendulum==1.2.0',
+            'python-dateutil>=2.1,<2.8.2'
+      ],
+      extras_require={
+          'test': [
+            'nose==1.3.7',
+            'pylint==2.6.0',
+            'unify==0.5'
+          ]
+      },
+      entry_points='''
+          [console_scripts]
+          tap-snowflake=tap_snowflake:main
+      ''',
+      packages=['tap_snowflake', 'tap_snowflake.sync_strategies'],
+)
---
+++ packages/pipelinewise-tap-snowflake/pipelinewise-tap-snowflake-2.0.3.tar.gz/pipelinewise-tap-snowflake-2.0.3/tap_snowflake/connection.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+from typing import Union, List, Dict
+
+import backoff
+import singer
+import sys
+import snowflake.connector
+
+LOGGER = singer.get_logger('tap_snowflake')
+
+
+class TooManyRecordsException(Exception):
+    """Exception to raise when query returns more records than max_records"""
+
+
+def retry_pattern():
+    """Retry pattern decorator used when connecting to snowflake
+    """
+    return backoff.on_exception(backoff.expo,
+                                snowflake.connector.errors.OperationalError,
+                                max_tries=5,
+                                on_backoff=log_backoff_attempt,
+                                factor=2)
+
+
+def log_backoff_attempt(details):
+    """Log backoff attempts used by retry_pattern
+    """
+    LOGGER.info('Error detected communicating with Snowflake, triggering backoff: %d try', details.get('tries'))
+
+
+def validate_config(config):
+    """Validate configuration dictionary"""
+    errors = []
+    required_config_keys = [
+        'account',
+        'dbname',
+        'user',
+        'password',
+        'warehouse',
+        'tables'
+    ]
+
+    # Check if mandatory keys exist
+    for k in required_config_keys:
+        if not config.get(k, None):
+            errors.append(f'Required key is missing from config: [{k}]')
+
+    return errors
+
+
+class SnowflakeConnection:
+    """Class to manage connection to snowflake data warehouse"""
+
+    def __init__(self, connection_config):
+        """
+        connection_config:      Snowflake connection details
+        """
+        self.connection_config = connection_config
+        config_errors = validate_config(connection_config)
+        if len(config_errors) == 0:
+            self.connection_config = connection_config
+        else:
+            LOGGER.error('Invalid configuration:\n   * %s', '\n   * '.join(config_errors))
+            sys.exit(1)
+
+    def open_connection(self):
+        """Connect to snowflake database"""
+        return snowflake.connector.connect(
+            user=self.connection_config['user'],
+            password=self.connection_config['password'],
+            account=self.connection_config['account'],
+            database=self.connection_config['dbname'],
+            warehouse=self.connection_config['warehouse'],
+            insecure_mode=self.connection_config.get('insecure_mode', False)
+            # Use insecure mode to avoid "Failed to get OCSP response" warnings
+            # insecure_mode=True
+        )
+
+    @retry_pattern()
+    def connect_with_backoff(self):
+        """Connect to snowflake database and retry automatically a few times if fails"""
+        return self.open_connection()
+
+    def query(self, query: Union[List[str], str], params: Dict = None, max_records=0):
+        """Run a query in snowflake"""
+        result = []
+
+        if params is None:
+            params = {}
+        else:
+            if 'LAST_QID' in params:
+                LOGGER.warning('LAST_QID is a reserved prepared statement parameter name, '
+                               'it will be overridden with each executed query!')
+
+        with self.connect_with_backoff() as connection:
+            with connection.cursor(snowflake.connector.DictCursor) as cur:
+
+                # Run every query in one transaction if query is a list of SQL
+                if isinstance(query, list):
+                    cur.execute('START TRANSACTION')
+                    queries = query
+                else:
+                    queries = [query]
+
+                qid = None
+
+                for sql in queries:
+                    LOGGER.debug('Running query: %s', sql)
+
+                    # update the LAST_QID
+                    params['LAST_QID'] = qid
+
+                    cur.execute(sql, params)
+                    qid = cur.sfqid
+
+                    # Raise exception if returned rows greater than max allowed records
+                    if 0 < max_records < cur.rowcount:
+                        raise TooManyRecordsException(
+                            f'Query returned too many records. This query can return max {max_records} records')
+
+                    if cur.rowcount > 0:
+                        result = cur.fetchall()
+
+        return result
