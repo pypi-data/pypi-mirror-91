69630a951fdc25f58da712fd573ccba7adb7f808
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:44 +0000

    Add package flowers/0.1.3/flowers-0.1.3.tar.gz

---
+++ packages/flowers/flowers-0.1.3.tar.gz/flowers-0.1.3/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='flowers',
+    version='0.1.3',
+    description=(
+        'utils'
+    ),
+    author='liyuhao',
+    author_email='1241225413@qq.com',
+    license='Apache License 2.0',
+    packages=find_packages(),
+    url='https://github.com/hgliyuhao/flower',
+    install_requires=[
+        'pdfminer3k',
+        'requests'
+    ],
+)
