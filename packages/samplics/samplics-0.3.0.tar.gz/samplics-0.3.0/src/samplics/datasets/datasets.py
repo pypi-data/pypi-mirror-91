bab371b0b401c7badf408c627f113a0788afb2ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:23:17 +0000

    Add package samplics/0.3.0/samplics-0.3.0.tar.gz

---
+++ packages/samplics/samplics-0.3.0.tar.gz/samplics-0.3.0/src/samplics/datasets/datasets.py
@@ -0,0 +1,12 @@
+from typing import Any, Dict, Tuple, Union, Optional
+
+import numpy as np
+import pandas as pd
+
+from samplics.utils import checks, formats
+from samplics.utils.types import Array, Number
+
+
+class SampleData:
+
+    pass
