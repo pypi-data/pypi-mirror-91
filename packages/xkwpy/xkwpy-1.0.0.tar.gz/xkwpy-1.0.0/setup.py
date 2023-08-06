563d0ab6eb6a38619852ff7871507f6156777455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:45 +0000

    Add package xkwpy/1.0.0/xkwpy-1.0.0.tar.gz

---
+++ packages/xkwpy/xkwpy-1.0.0.tar.gz/xkwpy-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(
+    name='xkwpy',
+    version='1.0.0',
+    description='just for test',
+    author='royalxkw',
+    url='https://github.com',
+    author_email='284440954@qq.com',
+)
