f662d463c34e9e56eb0b6a3c2d8509cffacc9b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:02 +0000

    Add package f4ratk/2021.1/F4RATK-2021.1.tar.gz

---
+++ packages/f4ratk/F4RATK-2021.1.tar.gz/F4RATK-2021.1/f4ratk/file/analyze.py
@@ -0,0 +1,43 @@
+##############################################################################
+# Copyright (C) 2020 - 2021 Tobias Röttger <dev@roettger-it.de>
+#
+# This file is part of F4RATK.
+#
+# F4RATK is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License version 3
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+##############################################################################
+
+from f4ratk.analyze import Analyzer
+from f4ratk.domain import AnalysisConfig
+from f4ratk.fama import FamaReader
+from f4ratk.file.reader import FileConfig, read_file
+from f4ratk.shared import Normalizer
+
+
+def analyze_file(
+    file_config: FileConfig,
+    analysis_config: AnalysisConfig,
+) -> None:
+    fama_reader = FamaReader(Normalizer())
+    analyzer = Analyzer()
+
+    data = read_file(
+        file_config=file_config,
+        frame=analysis_config.frame,
+    )
+
+    analyzer.analyze(
+        data,
+        fama_reader.fama_data(
+            region=analysis_config.region, frequency=analysis_config.frame.frequency
+        ),
+    )
