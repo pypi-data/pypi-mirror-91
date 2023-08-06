3353f58c59f2ff558b18e27c2c98a18bfec963c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:52:55 +0000

    Add package gstools/1.3.0rc1/gstools-1.3.0rc1-cp39-cp39-win_amd64.whl

---
+++ packages/gstools/gstools-1.3.0rc1-cp39-cp39-win_amd64.whl/gstools/krige/__init__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+GStools subpackage providing kriging.
+
+.. currentmodule:: gstools.krige
+
+Kriging Classes
+^^^^^^^^^^^^^^^
+
+.. autosummary::
+   :toctree: generated
+
+   Krige
+   Simple
+   Ordinary
+   Universal
+   ExtDrift
+   Detrended
+
+----
+"""
+from gstools.krige.base import Krige
+from gstools.krige.methods import (
+    Simple,
+    Ordinary,
+    Universal,
+    ExtDrift,
+    Detrended,
+)
+
+__all__ = ["Krige", "Simple", "Ordinary", "Universal", "ExtDrift", "Detrended"]
