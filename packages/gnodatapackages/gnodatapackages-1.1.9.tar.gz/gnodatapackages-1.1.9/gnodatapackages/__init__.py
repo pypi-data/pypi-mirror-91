a7c6035c0e0db3fd02b0504b260c6ac5eeeb5dea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:12 +0000

    Add package gnodatapackages/1.1.9/gnodatapackages-1.1.9.tar.gz

---
+++ packages/gnodatapackages/gnodatapackages-1.1.9.tar.gz/gnodatapackages-1.1.9/gnodatapackages/__init__.py
@@ -0,0 +1,9 @@
+from .gno_gen_stopwords import gno_generalstopwords
+from .gno_gen_stopwords import gno_generalstopwords_cn
+from .gno_name_stopwords import gno_inpersonname_remove
+from .gno_name_stopwords import gno_honorifics_remove
+from .gno_coname_stopwords import gno_inconame_stopwords
+from .gno_uploadfile_clean.py import load_files
+
+def joke():
+    return (u'simi lan test test.')
