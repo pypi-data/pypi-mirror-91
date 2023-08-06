0577ead49b1b2fc6d65404a98a117d37dcdd8419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package statuscheck/3.9.0/statuscheck-3.9.0-py2.py3-none-any.whl

---
+++ packages/statuscheck/statuscheck-3.9.0-py2.py3-none-any.whl/statuscheck/services/bases/_base.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+import click
+
+from statuscheck.services.models.generic import Summary
+
+
+class BaseServiceAPI:
+    name: str = ""
+    summary: Optional[Summary] = None
+    service_url: str = ""
+    status_url: str = ""
+
+    @property
+    def _module_name(self):
+        module_relpath = self.__class__.__module__
+        return module_relpath.rsplit(".", 1)[1]
+
+    def __init__(self):
+        self.summary = self.get_summary()
+
+    def get_summary(self):
+        raise NotImplementedError
+
+    def _print_summary(self, verbose=False):
+        click.echo(f"Current {self.name} status: {self.summary.status.name}")
+
+        incidents = self.summary.incidents
+        if incidents:
+            click.echo("Registered events:")
+            for incident in incidents:
+                if incident.status:
+                    click.echo(f"- [{incident.status}] {incident.name}")
+                else:
+                    click.echo(f"- {incident.name}")
+                if hasattr(incident, "scheduled_for") and incident.scheduled_for:
+                    click.echo(f"  {incident.scheduled_for}")
+
+                if verbose and incident.components:
+                    click.echo("  Affected components:")
+                    for component in incident.components:
+                        if component.status:
+                            click.echo(f"    - {component.name} [{component.status}]")
+                        else:
+                            click.echo(f"    - {component.name}")
+
+        if incidents or not self.summary.status.is_ok:
+            click.echo()
+            click.echo(f"More: {self.status_url}")
