bf9b791a6062cde6eae0a6ce32fe0f6679b207a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:49:57 +0000

    Add package LJHpdf/4.0/LJHpdf-4.0.tar.gz

---
+++ packages/LJHpdf/LJHpdf-4.0.tar.gz/LJHpdf-4.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+# from pathlib import Path
+
+setuptools.setup(
+    name="LJHpdf",
+    version=4.0,
+    long_description=open("README.md").read(),
+    packages=setuptools.find_packages()
+)
