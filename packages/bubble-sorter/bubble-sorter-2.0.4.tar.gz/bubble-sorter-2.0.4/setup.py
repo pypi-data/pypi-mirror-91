796538199c7f2a2e1afd5c5153fb105b979e51de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:25 +0000

    Add package bubble-sorter/2.0.4/bubble-sorter-2.0.4.tar.gz

---
+++ packages/bubble-sorter/bubble-sorter-2.0.4.tar.gz/bubble-sorter-2.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bubble-sorter", 
+    version="2.0.4",
+    author="Joshua Bode",
+    author_email="joshuabode43@gmail.com",
+    description="An educational module to demonstrate the efficiency of the bubble sort algorithm. Ideal for small sets of data. Also contains a 'speed_test' function to see how quickly your computer can sort!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MasterJindu/Bubble-Sort-Python",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    
+)
