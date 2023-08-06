15564358a59a177d1c9fee86e0d5bceb0797df77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:50 +0000

    Add package doubleagent/1.1/doubleagent-1.1.tar.gz

---
+++ packages/doubleagent/doubleagent-1.1.tar.gz/doubleagent-1.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="doubleagent",
+    version="1.1",
+    license='MIT',
+    author="complexplayer",
+    author_email="didutc@gmail.com",
+    description="Double list makes a list",
+    long_description=open('README.md').read(),
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # 패키지에 대한 태그
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+)
