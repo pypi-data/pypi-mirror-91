21c641feb14c1077fa691ef03bf2732492db1e52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:27 +0000

    Add package simber/0.2.1/simber-0.2.1.tar.gz

---
+++ packages/simber/simber-0.2.1.tar.gz/simber-0.2.1/simber/__init__.py
@@ -0,0 +1,12 @@
+"""Initiate the logger modules that
+are to be exposed to the user
+"""
+
+from simber.logger import Logger
+
+
+__all__ = [
+            'configurations',
+            'logger',
+            'formatter'
+]
