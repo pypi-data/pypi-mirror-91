b416d8b2ab9cc90b1c2553e07fa3e2ecc1e50ee8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:22 +0000

    Add package pypresident/0.1.0/pypresident-0.1.0.tar.gz

---
+++ packages/pypresident/pypresident-0.1.0.tar.gz/pypresident-0.1.0/president/alignment.py
@@ -0,0 +1,32 @@
+"""Alignment Modules used in president."""
+import tempfile
+import subprocess
+
+
+def pblat(threads, reference, query, verbose=0):
+    """Perform blat alignment.
+
+    Parameters
+    ----------
+    threads : int
+        Number of threads to use.
+    reference : str
+        file location of reference FASTA.
+    query : str
+        file location of query FASTA.
+    verbose : bool
+        If True, print pblat command.
+
+    Returns
+    -------
+    alignments.
+
+    """
+    print('Running pblat ...')
+    _, alignments = tempfile.mkstemp()
+    cmd = f'pblat -threads={threads} {reference} {query} {alignments}'
+    if verbose:
+        print(cmd)
+    _ = subprocess.check_output(cmd, shell=True)
+    print('Finished pblat.')
+    return alignments
