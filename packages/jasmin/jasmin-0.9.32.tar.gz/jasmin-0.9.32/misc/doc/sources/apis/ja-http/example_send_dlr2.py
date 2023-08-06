0b1d8cd8d3b3d78c6a212ddc0b4255f3464f882e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:47 +0000

    Add package jasmin/0.9.32/jasmin-0.9.32.tar.gz

---
+++ packages/jasmin/jasmin-0.9.32.tar.gz/jasmin-0.9.32/misc/doc/sources/apis/ja-http/example_send_dlr2.py
@@ -0,0 +1,9 @@
+# Python example
+# http://jasminsms.com
+import urllib2
+import urllib
+
+# Send an SMS-MT and request terminal level acknowledgement callback to http://myserver/acknowledgement
+params = {'username':'foo', 'password':'bar', 'to':'+336222172', 'content':'Hello',
+          'dlr-url':'http://myserver/acknowledgement', 'dlr-level':2}
+urllib2.urlopen("http://127.0.0.1:1401/send?%s" % urllib.urlencode(params)).read()
