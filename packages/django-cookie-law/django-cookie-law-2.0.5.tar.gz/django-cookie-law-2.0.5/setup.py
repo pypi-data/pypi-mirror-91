b40c73936daa166ba55196d6648ef34dee10782a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:10 +0000

    Add package django-cookie-law/2.0.5/django-cookie-law-2.0.5.tar.gz

---
+++ packages/django-cookie-law/django-cookie-law-2.0.5.tar.gz/django-cookie-law-2.0.5/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+from itertools import chain
+from glob import glob
+
+import cookielaw
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: JavaScript',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Session',
+]
+
+package_data_globs = (
+    'cookielaw/templates/cookielaw/*.html',
+    'cookielaw/static/cookielaw/*/*',
+    'cookielaw/locale/*/*/*'
+)
+
+package_data = []
+for f in chain(*map(glob, package_data_globs)):
+    package_data.append(f.split('/', 1)[1])
+
+setup(
+    author='Piotr Kilczuk',
+    author_email='piotr@tymaszweb.pl',
+    name='django-cookie-law',
+    version='.'.join(str(v) for v in cookielaw.VERSION),
+    description='Helps your Django project comply with EU cookie law regulations',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+    url='https://github.com/TyMaszWeb/django-cookie-law',
+    license='BSD License',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django>=1.8',
+        'django-classy-tags>=0.3.0',
+        'pytest-runner>=2.11.1'
+    ],
+    tests_require=[
+        'six',
+        'pytest>=3.0.0',
+        'pytest-django>=3.1.2',
+        'pytest-selenium>=1.9.1',
+        'selenium>=3.3.1',
+    ],
+    packages=find_packages(),
+    package_data={'cookielaw': package_data},
+    include_package_data=False,
+    zip_safe=False,
+    test_suite='tests',
+)
