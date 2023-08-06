1520144fd2d914f8a7e6b96f66386eb55ee12c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:01 +0000

    Add package esbmtk/0.3.2.2/esbmtk-0.3.2.2.tar.gz

---
+++ packages/esbmtk/esbmtk-0.3.2.2.tar.gz/esbmtk-0.3.2.2/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="esbmtk",  # Replace with your own username
+    version="0.3.2.2",
+    author="Ulrich G. Wortmann",
+    author_email="uli.wortmann@utoronto.ca",
+    description="An Earth Sciences Box Modeling Toolkit",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/uliw/esbmtk",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Chemistry",
+    ],
+    python_requires=">=3.6",
+    install_requires=[
+        "nptyping",
+        "typing",
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "pint",
+    ],
+)
