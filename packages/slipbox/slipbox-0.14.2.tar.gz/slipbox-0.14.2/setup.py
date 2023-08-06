326c0d5609aaa49ce1c6a331e19297799b2103d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:33:14 +0000

    Add package slipbox/0.14.2/slipbox-0.14.2.tar.gz

---
+++ packages/slipbox/slipbox-0.14.2.tar.gz/slipbox-0.14.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("../README.md") as fp:
+    long_description = fp.read()
+
+setuptools.setup(
+    name="slipbox",
+    version="0.14.2",
+    author="Levi Gruspe",
+    author_email="mail.levig@gmail.com",
+    description="Static-site generator for Zettelkasten notes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lggruspe/slipbox",
+    packages=setuptools.find_packages(),
+    package_data={
+        "slipbox": ["data/frontend.js", "filters/*.lua", "*.sql", "data/*.html"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+    ],
+    python_requires=">=3.7",
+)
