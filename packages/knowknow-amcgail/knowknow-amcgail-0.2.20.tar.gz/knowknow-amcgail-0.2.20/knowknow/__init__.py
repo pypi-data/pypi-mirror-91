550a781b1cbccb5c1d5049817934eccbe96bb03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:52 +0000

    Add package knowknow-amcgail/0.2.20/knowknow-amcgail-0.2.20.tar.gz

---
+++ packages/knowknow-amcgail/knowknow-amcgail-0.2.20.tar.gz/knowknow-amcgail-0.2.20/knowknow/__init__.py
@@ -0,0 +1,46 @@
+if __name__ == '__main__':
+    import sys
+    print(sys.argv)
+else:
+    from IPython.core.display import display, HTML, Markdown, Image
+    from collections import Counter, defaultdict
+    from random import sample, shuffle
+    from itertools import chain
+
+    from tabulate import tabulate
+    import re
+
+    from pathlib import Path
+    from os.path import dirname, join
+    BASEDIR = dirname(__file__)
+
+    from csv import reader as creader
+
+    import networkx as nx
+    import pandas as pd
+    import seaborn as sns
+
+
+    import matplotlib.pyplot as plt
+    plt.style.use('seaborn-whitegrid')
+    import numpy as np
+
+    from logzero import logger
+
+
+    from .exceptions import *
+
+
+
+    from . import env
+    from .utility import *
+
+    # this uses some imports from here...
+    from .datastore_cnts import *
+    #from .datastore_sql import *
+    from .code_sharing import *
+    from . import viz
+    #from .meta_notebook import *
+
+    #from .knowknow_base import  *
+    #from .archive import *
