d67abd3ff0b41d606adcb610ac3c183e48be38f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:38:08 +0000

    Add package quarry/1.6.3/quarry-1.6.3.tar.gz

---
+++ packages/quarry/quarry-1.6.3.tar.gz/quarry-1.6.3/quarry/types/buffer/__init__.py
@@ -0,0 +1,24 @@
+class BufferUnderrun(Exception):
+    pass
+
+
+from quarry.types.buffer.v1_7 import Buffer1_7
+from quarry.types.buffer.v1_9 import Buffer1_9
+from quarry.types.buffer.v1_13 import Buffer1_13
+from quarry.types.buffer.v1_13_2 import Buffer1_13_2
+from quarry.types.buffer.v1_14 import Buffer1_14
+
+
+# Versioned buffers used after handshaking
+buff_types = [
+    (0,   Buffer1_7),
+    (107, Buffer1_9),
+    (393, Buffer1_13),
+    (404, Buffer1_13_2),
+    (477, Buffer1_14),
+]
+
+
+# Used by NBT and during handshaking
+class Buffer(buff_types[-1][1]):
+    pass
---
+++ packages/quarry/quarry-1.6.3.tar.gz/quarry-1.6.3/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+
+setup(
+    name='quarry',
+    version='1.6.3',
+    author='Barney Gale',
+    author_email='barney@barneygale.co.uk',
+    url='https://github.com/barneygale/quarry',
+    license='MIT',
+    description='Minecraft protocol library',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'bitstring >= 3.1.0',
+        'cached_property >= 1.2.0',
+        'twisted >= 13.0.0',
+        'cryptography >= 0.9',
+        'pyOpenSSL >= 0.15.1',
+        'service_identity >= 14.0.0',
+    ],
+    test_requires=[
+        'pytest'
+    ],
+    packages=[
+        "quarry",
+        "quarry.data",
+        "quarry.net",
+        "quarry.types",
+        "quarry.types.buffer",
+    ],
+    package_data={'quarry': [
+        'data/packets/*.csv',
+        'data/data_packs/*.nbt']},
+)
