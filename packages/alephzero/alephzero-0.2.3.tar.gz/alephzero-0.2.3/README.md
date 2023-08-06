bed3eafdfa22d38f61dc184efae680bf8b8f29c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:36 +0000

    Add package alephzero/0.2.3/alephzero-0.2.3.tar.gz

---
+++ packages/alephzero/alephzero-0.2.3.tar.gz/alephzero-0.2.3/README.md
@@ -0,0 +1,12 @@
+# Aleph Zero: Python
+[![CI](https://github.com/alephzero/py/workflows/CI/badge.svg)](https://github.com/alephzero/py/actions?query=workflow%3ACI)
+
+TODO: What is it? Why is it? Who is it????
+
+See https://github.com/alephzero/playground for examples.
+
+## Install Instructions
+
+```sh
+pip install git+https://github.com/alephzero/py.git
+```
---
+++ packages/alephzero/alephzero-0.2.3.tar.gz/alephzero-0.2.3/setup.py
@@ -0,0 +1,19 @@
+import glob
+from setuptools import setup
+from pybind11.setup_helpers import Pybind11Extension
+
+sources = ['module.cc'] + glob.glob('./alephzero/src/*.c*')
+module = Pybind11Extension('alephzero_bindings',
+                           sources=sources,
+                           include_dirs=['./alephzero/include/'],
+                           extra_compile_args=['-std=c++17', '-O3'])
+
+setup(name='alephzero',
+      version='0.2.3',
+      description='TODO: description',
+      author='Leonid Shamis',
+      author_email='leonid.shamis@gmail.com',
+      url='https://github.com/alephzero/py',
+      long_description='''TODO: long description''',
+      ext_modules=[module],
+      py_modules=['a0'])
