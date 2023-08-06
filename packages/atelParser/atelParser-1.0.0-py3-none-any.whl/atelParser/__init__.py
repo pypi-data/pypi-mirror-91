c77251be66fbd19b8c0e1b0fd8ecc7b56f8afb86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:17 +0000

    Add package atelParser/1.0.0/atelParser-1.0.0-py3-none-any.whl

---
+++ packages/atelParser/atelParser-1.0.0-py3-none-any.whl/atelParser/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+from . import cl_utils
+from .__version__ import __version__
+from . import utKit
+from . import cl_utils
+from .download import download
+from .mysql import mysql
