db8e1f6033b25c3f46ee2894c13fcda55084070e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:24 +0000

    Add package python-networkmanager/2.2/python-networkmanager-2.2.tar.gz

---
+++ packages/python-networkmanager/python-networkmanager-2.2.tar.gz/python-networkmanager-2.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(name = "python-networkmanager",
+      version = "2.2",
+      author = "Dennis Kaarsemaker",
+      author_email = "dennis@kaarsemaker.net",
+      url = "http://github.com/seveas/python-networkmanager",
+      description = "Easy communication with NetworkManager",
+      py_modules = ["NetworkManager"],
+      install_requires = ["dbus-python", "six"],
+      classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: zlib/libpng License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: System :: Networking',
+      ]
+)
