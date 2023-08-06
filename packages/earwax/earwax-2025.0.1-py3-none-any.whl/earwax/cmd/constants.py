ea716f3eccbad2ba5a0efe4015ec770d94190637
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:07:58 +0000

    Add package earwax/2025.0.1/earwax-2025.0.1-py3-none-any.whl

---
+++ packages/earwax/earwax-2025.0.1-py3-none-any.whl/earwax/cmd/constants.py
@@ -0,0 +1,34 @@
+"""Provides various constants used by the script."""
+
+from pathlib import Path
+
+# Base directory.
+
+cd: Path = Path.cwd()
+
+# Where to store the earwax options.
+options_path: Path = cd / 'options.yaml'
+
+# Where to store the main project file.
+project_filename: Path = cd / 'project.yaml'
+
+# Directory where maps are stored.
+maps_directory: Path = cd / 'maps'
+
+# Directory where scripts are stored.
+scripts_directory: Path = cd / 'scripts'
+
+# The main sounds directory.
+sounds_directory: Path = cd / 'sounds'
+
+# The directory where surface directories are stored.
+surfaces_directory: Path = sounds_directory / 'surfaces'
+
+# The directory where ambiances are stored.
+ambiances_directory: Path = sounds_directory / 'ambiances'
+
+# The directory where music files are store.
+music_directory: Path = sounds_directory / 'music'
+
+# Where to store the surfaces.py file.
+surfaces_filename: Path = cd / 'surfaces.py'
