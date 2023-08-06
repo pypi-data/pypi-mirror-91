86f45dec009030d0c935f020e9af037c7e5fdc2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:55 +0000

    Add package flights-time-series-dataset/1.1.7/flights_time_series_dataset-1.1.7-py3-none-any.whl

---
+++ packages/flights-time-series-dataset/flights_time_series_dataset-1.1.7-py3-none-any.whl/flights_time_series_dataset/convert_year_month_array_to_datetime.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+def convert_year_month_array_to_datetime(year_month_array):
+    def convert_singe_year_month_array_to_datetime(single_year_month_array):
+        return datetime(year=single_year_month_array[0], month=single_year_month_array[1], day=15)
+    year_month_array_size = year_month_array.size
+    if year_month_array_size == 2:
+        return convert_singe_year_month_array_to_datetime(year_month_array)
+    return [convert_singe_year_month_array_to_datetime(year_month_array[idx, ...]) for idx in range(len(year_month_array))]
